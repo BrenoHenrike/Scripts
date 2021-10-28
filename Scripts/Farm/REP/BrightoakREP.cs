e22f2e83827e804221ccfa5970a85db7673143c2
BrenoHenrike <70817267+BrenoHenrike@users.noreply.github.com>
2021-10-28 17:43:34 -0300

    Brightoak and Spellcrafting added

---
+++ Scripts/Farm/REP/BrightoakREP.cs
@@ -0,0 +1,20 @@
+﻿//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using RBot;
+
+public class BrightoakREP
+{
+	public CoreBots Core = new CoreBots();
+	public CoreFarms Farm = new CoreFarms();
+
+	public void ScriptMain(ScriptInterface bot)
+	{
+		Core.SetOptions();
+
+		//Farm.UseREPBoost(REPBoost.REP20);
+
+		Farm.BrightoakREP();
+
+		Core.SetOptions(false);
+	}
+}
---
+++ Scripts/Farm/REP/SpellCraftingREP.cs
@@ -0,0 +1,18 @@
+﻿//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using RBot;
+public class SpellCraftingREP
+{
+	public CoreBots Core = new CoreBots();
+	public CoreFarms Farm = new CoreFarms();
+	public void ScriptMain(ScriptInterface bot)
+	{
+		Core.SetOptions();
+
+		//Farm.UseREPBoost(REPBoost.REP20);
+
+		Farm.SpellCraftingREP();
+
+		Core.SetOptions(false);
+	}
+}
