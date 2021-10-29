1d7b2c64ea1c1f9780898ba3772e0c079beaed84
BrenoHenrike <70817267+BrenoHenrike@users.noreply.github.com>
2021-10-29 14:44:05 -0300

    Blood Moon Token added

---
+++ Scripts/Farm/BloodMoonToken.cs
@@ -0,0 +1,26 @@
+﻿using RBot;
+
+public class BloodMoonToken
+{
+	public CoreBots Core = new CoreBots();
+
+	public void ScriptMain(ScriptInterface bot)
+	{
+		Core.SetOptions();
+
+		Core.AddDrop("Blood Moon Token", "Black Blood Vial", "Moon Stone");
+
+		while (!bot.ShouldExit() || !Core.CheckInventory("Blood Moon Token", 300))
+		{
+			Core.EnsureAccept(6059);
+
+			Core.HuntMonster("bloodmoon", "Lycan Guard", "Black Blood Vial", 1, false);
+			Core.HuntMonster("bloodmoon", "Black Unicorn", "Moon Stone", 1, false);
+
+			Core.EnsureComplete(6059);
+			bot.Wait.ForPickup("Blood Moon Token");
+		}
+
+		Core.SetOptions(false);
+	}
+}
