3ada44d2ce1fbffd3c984c86850d915fce4c22db
BrenoHenrike <70817267+BrenoHenrike@users.noreply.github.com>
2021-11-08 19:55:07 -0300

    Voucher Item: Totem of Nulgath added

---
+++ Scripts/Nulgath/VoucherItemTotemofNulgath.cs
@@ -0,0 +1,24 @@
+﻿//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/Nulgath/CoreNulgath.cs
+using RBot;
+
+public class VoucherItemTotemofNulgath
+{
+	public CoreBots Core = new CoreBots();
+	public CoreNulgath Nulgath = new CoreNulgath();
+
+	public void ScriptMain(ScriptInterface bot)
+	{
+		Core.SetOptions();
+
+		while (!bot.ShouldExit())
+		{
+			Nulgath.VoucherItemTotemofNulgath(ChooseReward.TotemofNulgath);
+			// Comment the line above and uncomment the line bellow to farm Gem of Nulgath
+			//Nulgath.VoucherItemTotemofNulgath(ChooseReward.GemofNulgath);
+		}
+
+		Core.SetOptions(false);
+	}
+}
