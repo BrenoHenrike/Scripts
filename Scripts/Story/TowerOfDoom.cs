f60b84cc6770e5ec783042e5a11e610332c16f0a
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-07 23:39:12 +0100

    Fixed numerous bugs
    Removed delay between checks from high to low
    Now properly sends the packets, they had to be in json

--- Scripts/Story/TowerOfDoom.cs
+++ Scripts/Story/TowerOfDoom.cs
@@ -18,32 +18,30 @@ public class TowerOfDoom
 
     public void TowerProgress(int Floor = 10)
     {
-        if (Floor == 1)
-        {
-            Core.Logger("Tower of Doom 1");
-            Core.SendPackets("{\"t\":\"xt\",\"b\":{\"r\":-1,\"o\":{\"cmd\":\"updateQuest\",\"iValue\":1,\"iIndex\":159}}}", 1, true);
-            Core.EquipClass(ClassType.Solo);
-            Core.SmartKillMonster(3475, "towerofdoom", "Dread Klunk", 1, true);
-            return;
-        }
-        Bot.Player.Join($"towerofdoom{Floor}");
+        string Map = Floor == 1 ? "towerofdoom" : "towerofdoom"+Floor.ToString();
+        Bot.SendPacket($"%xt%zm%cmd%{Bot.Map.RoomID}%tfer%{Bot.Player.Username}%{Map}%");
+        Bot.Wait.ForMapLoad($"towerofdoom{Floor}");
         if (Bot.Player.Cell == "Prison")
         {
             Core.Logger($"Tower of Doom {Floor-1} not completed yet, doing that first");
             TowerProgress(Floor-1);
         }
-        Bot.Player.Join($"towerofdoom{Floor}");
-        Core.Jump("r10", "Left");
+        if (Bot.Map.Name != Map)
+        {
+            Bot.SendPacket($"%xt%zm%cmd%{Bot.Map.RoomID}%tfer%{Bot.Player.Username}%{Map}%");
+            Bot.Wait.ForMapLoad($"towerofdoom{Floor}");
+        }
+        Bot.Player.Jump("r10", "Left");
         if (Bot.Player.Cell == "r10") 
         {
             Core.Logger($"Tower of Doom {Floor} already complete");
             return;
         }
         Core.Logger($"Tower of Doom {Floor}");
-        Core.SendPackets($"{{\"t\":\"xt\",\"b\":{{\"r\":-1,\"o\":{{\"cmd\":\"updateQuest\",\"iValue\":{Floor},\"iIndex\":159}}}}}}", 1, true);
+        Bot.SendClientPacket($"{{\"t\":\"xt\",\"b\":{{\"r\":-1,\"o\":{{\"cmd\":\"updateQuest\",\"iValue\":{Floor},\"iIndex\":159}}}}}}", "json");
         Core.EquipClass(ClassType.Solo);
         Core.EnsureAccept(3474+Floor);
-        Core.KillMonster($"towerofdoom{Floor}", "r10", "Left", "*");
-        Core.EnsureAccept(3474+Floor);
+        Core.KillMonster(Map, "r10", "Left", "*");
+        Core.EnsureComplete(3474+Floor);
     }
 }
