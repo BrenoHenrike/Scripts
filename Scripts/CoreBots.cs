ae36b9ca258d914fc3bad7705a8b528a8a8c1801
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-13 20:17:27 +0100

    Fixed JoinTercessuinotlim

--- Scripts/CoreBots.cs
+++ Scripts/CoreBots.cs
@@ -1079,7 +1079,7 @@ public class CoreBots
         Join("citadel", "m22", "Left");
         if (Bot.Player.Cell != "m22")
             Bot.Player.Jump("m22", "Left");
-        Join("tercessuinotlim");
+        Bot.Player.Join("tercessuinotlim");
         Bot.Wait.ForMapLoad("tercessuinotlim");
     }
 
