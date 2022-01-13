ae900772dfa31bc13203e086cbd698154b4d181c
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-13 12:08:33 +0100

    Fixed typo

--- Scripts/Chaos/AscendedDrakathGear.cs
+++ Scripts/Chaos/AscendedDrakathGear.cs
@@ -2,7 +2,7 @@
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreDailys.cs
 //cs_include Scripts/Good/BLoD/CoreBLOD.cs
-//cs_include Script/Nulgath/CoreNulgath.cs
+//cs_include Scripts/Nulgath/CoreNulgath.cs
 //cs_include Scripts/Chaos/DrakathArmorBot.cs
 //cs_include Scripts/Story/TowerOfDoom.cs
 using RBot;
