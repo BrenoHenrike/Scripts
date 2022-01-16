26f4fb435becf6393663c7b45ed7e45ad5d088a9
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-16 23:14:43 +0100

    Fixed recipy of contract

--- Scripts/Hollowborn/CoreHollowborn.cs
+++ Scripts/Hollowborn/CoreHollowborn.cs
@@ -15,15 +15,15 @@ public class CoreHollowborn
             return;
         Farm.IcestormArena(65);
 
-        Core.AddDrop("Soul Potion", "Necrot", "Arashtite Ore", "Human Soul", "Fallen Soul", "Lae\'s Hardcore Contract");
+        Core.AddDrop("Soul Potion", "Dried Slime", "Arashtite Ore", "Human Soul", "Fallen Soul", "Lae\'s Hardcore Contract");
         Core.Logger("Getting Lae's Hardcore Contract");
         if (!Core.CheckInventory("Soul Potion"))
         {
             Core.Logger("Getting Soul Potion");
             Core.EquipClass(ClassType.Farm);
-            Core.HuntMonster("deathsrealm", "Skeleton Fighter", "Necrot", 1, false);
+            Core.HuntMonster("orecavern", "Crashroom", "Dried Slime", 1, false);
             Core.HuntMonster("orecavern", "Deathmole", "Arashtite Ore", 1, false);
-            Farm.AlchemyPacket("Necrot", "Arashtite Ore", AlchemyRunes.Gebo, rank: 8, loop: false, modifier: "Man");
+            Farm.AlchemyPacket("Dried Slime", "Arashtite Ore", AlchemyRunes.Gebo, rank: 8, loop: false, modifier: "Man");
         }
         HumanSoul(50);
         Core.EquipClass(ClassType.Solo);
