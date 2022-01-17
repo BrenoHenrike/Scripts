39837940aafc3b077a7bc406dd8e79144005efbb
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-17 21:04:03 +0100

    Uploaded Necromancer class bot for NSOD dependency

---
+++ Scripts/Other/Necromancer.cs
@@ -0,0 +1,37 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using RBot;
+
+public class Necromancer
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetNecromancer();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetNecromancer(bool rankUp = true)
+    {
+        if (Core.CheckInventory("Necromancer"))
+            return;
+        
+        Farm.DoomwoodREP();
+        Core.BuyItem("lightguard", 277, "NUE Necronomicon");
+        Core.EquipClass(ClassType.Solo);
+        Core.KillMonster("maul", "r3", "Down", "Creature Creation", "Creature Shard", isTemp: false, publicRoom: true);
+        Core.BuyItem("necrotower", 285, "Necromancer");
+        if (rankUp)
+        {
+            Bot.Player.EquipItem("Necromancer");
+            Farm.IcestormArena(rankUpClass: true);
+        }
+    }
+}
