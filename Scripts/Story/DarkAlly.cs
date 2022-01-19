49e85ea577a1caa84e5961abdce73ba3fbafa153
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-19 17:12:22 +0100

    Added DarkAlly by Violeta for YnR

---
+++ Scripts/Story/DarkAlly.cs
@@ -0,0 +1,47 @@
+//cs_include Scripts/CoreBots.cs
+using RBot;
+
+public class DarkAlly_Story
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        DarkAlly_Questline();
+
+        Core.SetOptions(false);
+    }
+
+    public void DarkAlly_Questline()
+    {
+        if(Bot.Quests.IsUnlocked(7443))
+            return;
+        
+        Core.MapItemQuest(7419, "darkally", 7179, 6);
+        Core.KillQuest(7419, "darkally", "Shadow");
+
+        Core.KillQuest(7420, "darkally", new[] { "Underworld Golem", "Underworld Golem" });
+        
+        Core.MapItemQuest(7421, "darkally", 7180, 1);
+        Core.MapItemQuest(7421, "darkally", 7181, 8);
+
+        Core.KillQuest(7422, "darkally", "Dark Makai");
+
+        Core.KillQuest(7423, "darkally", "Shadow|Creeping Shadow");
+
+        Core.KillQuest(7424, "darkally", new[] { "Legion Defector", "Legion Defector" });
+
+        Core.KillQuest(7425, "darkally", "Frozen Pyromancer");
+
+        Core.KillQuest(7426, "darkally", "Underworld Golem");
+
+        Core.MapItemQuest(7427, "darkally", 7182, 1);
+
+        Core.KillQuest(7428, "darkally", "Underfiend", hasFollowup : false);
+        
+    }
+
+}
