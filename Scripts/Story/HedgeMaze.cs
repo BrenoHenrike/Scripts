d3680c55555ab5804a32a71d00352f03030d9fa8
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-19 15:22:00 +0100

    Converted lines to MapItemQuest

--- Scripts/Story/HedgeMaze.cs
+++ Scripts/Story/HedgeMaze.cs
@@ -25,20 +25,10 @@ public class HedgeMaze
 
     public void HedgeMaze_Questline()
     {
-        //Quest1:
             Core.KillQuest(QuestID: 5298, MapName: "hedgemaze", MonsterName: "Knight's Reflection");
-        //Quest2:
-            Core.EnsureAccept(5299);
-            Core.Jump("r11", "Bottom"); 
-            Bot.Sleep(1500);          
-            Core.GetMapItem(itemID: 4679, quant: 1, map: "hedgemaze");
+            Core.MapItemQuest(QuestID: 5299, MapName: "hedgemaze", MapItemID: 4679);
             Core.KillQuest(QuestID: 5299, MapName: "hedgemaze", MonsterNames: new[] { "Mirrored Shard", "Hedge Goblin", "Minotaur" } );
-        //Quest3:
             Core.KillQuest(QuestID: 5300, MapName: "hedgemaze", MonsterName: "Knight's Reflection");
-        //Quest4:
-            Core.EnsureAccept(5301);
-            Core.Jump("r11", "Bottom");       
-            Core.GetMapItem(itemID: 4680, quant: 1, map: "hedgemaze");
-            Core.EnsureComplete(5301);
+            Core.MapItemQuest(QuestID: 5301, MapName: "hedgemaze", MapItemID: 4680, hasFollowup: false);
     }
 }
