c2c3e70336eec62beb5b20a4c223b350f51dd725
Tato <47118168+wtffidy@users.noreply.github.com>
2022-01-24 08:19:27 -0500

    +NecroDungeon +fixes/Changes
    +necrodungeon storyline
    ~VHL_Combo - Eldres' blood daily check fixed(deleted as the challenge part below already checks it)
    ~CoreNulgath updated SwindleReturn(); with Hadmos's changes.

--- Scripts/Story/Necrodungeon.cs
+++ Scripts/Story/Necrodungeon.cs
@@ -18,70 +18,70 @@ public class Necrodungeon
     {
         Core.SetOptions();
 
-        StoryLine1();
+        NecrodungeonStoryLine();
 
         Core.SetOptions(false);
     }
 
-    public void StoryLine1()
+    public void NecrodungeonStoryLine()
     {
         //descent into darkness - 2044
         Core.KillQuest(QuestID: 2044, MapName: "necrodungeon", MonsterName: "Bellhop");
         //retrieve the past, room 1 - 2045
         Core.KillQuest(QuestID: 2045, MapName: "necrodungeon", MonsterName: "Necropolis Soldier");
-        Core.MapItemQuest(QuestID: 2045, MapName: "necrodungeon", MapItemID: 1000);
+        Core.MapItemQuest(QuestID: 2045, MapName: "necrodungeon", MapItemID: 1001);
         //retrieve the past, room 2 - 2046
-        Core.MapItemQuest(QuestID: 2046, MapName: "necrodungeon", MapItemID: 1001, Amount: 5);
+        Core.MapItemQuest(QuestID: 2046, MapName: "necrodungeon", MapItemID: 1015, Amount: 5);
         Core.MapItemQuest(QuestID: 2046, MapName: "necrodungeon", MapItemID: 1002);
         //retrieve the past, room 3 - 2047
         Core.KillQuest(QuestID: 2047, MapName: "necrodungeon", MonsterName: "SlimeSkull");
         Core.MapItemQuest(QuestID: 2047, MapName: "necrodungeon", MapItemID: 1003);
         //retrieve the past, room 4 - 2048
-        Core.KillQuest(QuestID: 2048, MapName: "necrodungeon", MonsterName: "Necropolis Soldier");
-        Core.KillQuest(QuestID: 2048, MapName: "necrodungeon", MonsterName: "Ghoul");
+        Core.KillQuest(QuestID: 2048, MapName: "necrodungeon", MonsterNames:  new[] { "Necropolis Soldier", "Ghoul"});
         Core.MapItemQuest(QuestID: 2048, MapName: "necrodungeon", MapItemID: 1004);
         //deeper into darkness - 2049
         Core.KillQuest(QuestID: 2049, MapName: "necrodungeon", MonsterName: "Bellhop");
         //retrieve the past, room 5 - 2050
-        Core.KillQuest(QuestID: 2050, MapName: "necrodungeon", MonsterName: "SlimeSkull");
-        Core.KillQuest(QuestID: 2050, MapName: "necrodungeon", MonsterName: "Necropolis Soldier");
-        Core.MapItemQuest(QuestID: 2050, MapName: "necrodungeon", MapItemID: 1005, Amount: 3);
-        Core.MapItemQuest(QuestID: 2050, MapName: "necrodungeon", MapItemID: 1006);
+        Core.KillQuest(QuestID: 2050, MapName: "necrodungeon", MonsterNames: new[] {"SlimeSkull", "Necropolis Soldier"});
+        Core.MapItemQuest(QuestID: 2050, MapName: "necrodungeon", MapItemID: 1016, Amount: 3);
+        Core.MapItemQuest(QuestID: 2050, MapName: "necrodungeon", MapItemID: 1005);
         //retrieve the past, room 6 - 2051
-        Core.MapItemQuest(QuestID: 2051, MapName: "necrodungeon", MapItemID: 1007, Amount: 5);
-        Core.MapItemQuest(QuestID: 2051, MapName: "necrodungeon", MapItemID: 1008);
+        Core.MapItemQuest(QuestID: 2051, MapName: "necrodungeon", MapItemID: 1017, Amount: 5);
+        Core.MapItemQuest(QuestID: 2051, MapName: "necrodungeon", MapItemID: 1006);
         //retrieve the past, room 7 - 2052
         Core.KillQuest(QuestID: 2052, MapName: "necrodungeon", MonsterName: "SlimeSkull");
-        Core.MapItemQuest(QuestID: 2052, MapName: "necrodungeon", MapItemID: 1009);
+        Core.MapItemQuest(QuestID: 2052, MapName: "necrodungeon", MapItemID: 1007);
         //monter subway ahead - 2053
         Core.KillQuest(QuestID: 2053, MapName: "necrodungeon", MonsterName: "Doom Crawler");
-        Core.MapItemQuest(QuestID: 2053, MapName: "necrodungeon", MapItemID: 1010);
+        Core.MapItemQuest(QuestID: 2053, MapName: "necrodungeon", MapItemID: 1008);
         //underground railroad to... DOOM! - 2054
-        Core.KillQuest(QuestID: 2053, MapName: "necrodungeon", MonsterName: "Ghoul");
-        Core.MapItemQuest(QuestID: 2053, MapName: "necrodungeon", MapItemID: 1011, Amount: 8);
-        Core.MapItemQuest(QuestID: 2053, MapName: "necrodungeon", MapItemID: 1012);
+        Core.KillQuest(QuestID: 2054, MapName: "necrodungeon", MonsterName: "Ghoul");
+        Core.MapItemQuest(QuestID: 2054, MapName: "necrodungeon", MapItemID: 1018, Amount: 8);
+        Core.MapItemQuest(QuestID: 2054, MapName: "necrodungeon", MapItemID: 1009);
         //retrieve the past, room 10 - 2055
         Core.KillQuest(QuestID: 2055, MapName: "necrodungeon", MonsterName: "Necropolis Soldier");
-        Core.MapItemQuest(QuestID: 2055, MapName: "necrodungeon", MapItemID: 1013);
+        Core.MapItemQuest(QuestID: 2055, MapName: "necrodungeon", MapItemID: 1010);
         //the deepest descent - 2056
         Core.KillQuest(QuestID: 2056, MapName: "necrodungeon", MonsterName: "Bellhop");
         //retrieve the past, room 11 - 2057
-        Core.MapItemQuest(QuestID: 2057, MapName: "necrodungeon", MapItemID: 1014, Amount: 5);
-        Core.MapItemQuest(QuestID: 2057, MapName: "necrodungeon", MapItemID: 1015);
+        Core.MapItemQuest(QuestID: 2057, MapName: "necrodungeon", MapItemID: 1016, Amount: 5);
+        Core.MapItemQuest(QuestID: 2057, MapName: "necrodungeon", MapItemID: 1011);
         //retrieve the past, room 12 - 2058
         Core.KillQuest(QuestID: 2058, MapName: "necrodungeon", MonsterName: "Necropolis Soldier");
-        Core.MapItemQuest(QuestID: 2058, MapName: "necrodungeon", MapItemID: 1016);
+        Core.MapItemQuest(QuestID: 2058, MapName: "necrodungeon", MapItemID: 1012);
         //retrieve the past, room 13 - 2059
-        Core.MapItemQuest(QuestID: 2059, MapName: "necrodungeon", MapItemID: 1017, Amount: 5);
-        Core.MapItemQuest(QuestID: 2059, MapName: "necrodungeon", MapItemID: 1018);
+        Core.MapItemQuest(QuestID: 2059, MapName: "necrodungeon", MapItemID: 1019, Amount: 5);
+        Core.MapItemQuest(QuestID: 2059, MapName: "necrodungeon", MapItemID: 1013);
         //five times the fury - 2060
-        Core.KillQuest(QuestID: 2060, MapName: "necrodungeon", MonsterName: "Dracolich Head 1");
-        Core.KillQuest(QuestID: 2060, MapName: "necrodungeon", MonsterName: "Dracolich Head 2");
-        Core.KillQuest(QuestID: 2060, MapName: "necrodungeon", MonsterName: "Dracolich Head 3");
-        Core.KillQuest(QuestID: 2060, MapName: "necrodungeon", MonsterName: "Dracolich Head 4");
-        Core.KillQuest(QuestID: 2060, MapName: "necrodungeon", MonsterName: "Dracolich Head 5");
+        Core.EnsureAccept(2060);
+        Core.KillMonster(map: "necrodungeon", cell: "r22", pad: "Down", monsterID: 889, item: "Dracolich Head 1 Defeated!");
+        Core.KillMonster(map: "necrodungeon", cell: "r22", pad: "Down", monsterID: 890, item: "Dracolich Head 2 Defeated!");
+        Core.KillMonster(map: "necrodungeon", cell: "r22", pad: "Down", monsterID: 891, item: "Dracolich Head 3 Defeated!");
+        Core.KillMonster(map: "necrodungeon", cell: "r22", pad: "Down", monsterID: 892, item: "Dracolich Head 4 Defeated!");
+        Core.KillMonster(map: "necrodungeon", cell: "r22", pad: "Down", monsterID: 893, item: "Dracolich Head 5 Defeated!");
+        Core.EnsureComplete(2060);
         //the past will haunt you - 2061
-        Core.MapItemQuest(QuestID: 2061, MapName: "necrodungeon", MapItemID: 1019, hasFollowup: false);
+        Core.MapItemQuest(QuestID: 2061, MapName: "necrodungeon", MapItemID: 1020, hasFollowup: false);
     }
 
 }
