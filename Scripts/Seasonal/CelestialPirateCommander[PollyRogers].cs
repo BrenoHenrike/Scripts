a7e6b99e55de304a482d05a61167ba71271eb6f8
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-24 13:45:55 +0100

    Release of CPC

---
+++ Scripts/Seasonal/CelestialPirateCommander[PollyRogers].cs
@@ -0,0 +1,57 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using RBot;
+
+public class CelestialPirateCommander
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+
+    public CoreBots Core => CoreBots.Instance;
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetCPC();
+
+        Core.SetOptions(false);
+    }
+
+    private string[] Rewards = {
+        "Celestial Pirate Commander",
+        "Celestial Commander's Hat",
+        "Celestial Commander's Locks",
+        "Celestial Commander's Locks + Hat",
+        "Celestial Commander's Wings",
+        "Celestial Commander's Back Blade",
+        "Celestial Commander's Wings+ Blade",
+        "Celestial Commander's Sword",
+        "Celestial Commander's Hat + Morph",
+        "Celestial Commander's Morph + Locks",
+        "Celestial Commander's Plank",
+        "Polly Roger"
+    };
+
+    public void GetCPC(bool OnlyPolly = false)
+    {
+        if (OnlyPolly && Core.CheckInventory("Polly Roger"))
+            return;
+        if (!OnlyPolly && Core.CheckInventory(Rewards))
+            return;
+        
+        Core.AddDrop(Rewards);
+        while (!Core.CheckInventory(Rewards))
+        {
+            Core.EnsureAccept(7713);
+            Core.HuntMonster("lostruinswar", "Diabolical Warlord", "Rumors of the Celestial Commander", 5, false, publicRoom: true);
+            Core.HuntMonster("iceplane", "Animus of Ice", "Starlit Journal Page 1 Scraps", 10, false);
+            Core.HuntMonster("ivoliss", "Ivoliss", "Starlit Journal Page 2 Scraps", 10, false, publicRoom: true);
+            Core.HuntMonster("thevoid", "Nightbane", "Starlit Journal Page 3 Scraps", 10, false, publicRoom: true);
+            Core.HuntMonster("extinction", "Ultra SN.O.W.", "Starlit Journal Page 4 Scraps", 10, false, publicRoom: true);
+            Core.HuntMonster("starsinc", "Empowered Prime", "Map of the Celestial Seas", 1, false, publicRoom: true);
+            Core.HuntMonster("underlair", "ArchFiend DragonLord", "Coffer of the Stars", 1, false, publicRoom: true);
+            Core.EnsureCompleteChoose(7713, Rewards);
+            Bot.Sleep(Core.ActionDelay);
+        }
+    }
+}
