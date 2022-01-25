c9373b62b0aca8358a79ca4bb571f47c771c3934
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-25 21:28:51 +0100

    Renamed XanCave to XansLair and removed redunancies

--- Scripts/Story/XansLair.cs
+++ Scripts/Story/XansLair.cs
@@ -1,18 +1,13 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
-//cs_include Scripts/CoreDailys.cs
-
-//cs_include Scripts/CoreFile(Or folder)/Filename.cs
-
 using RBot;
 
-public class BotName
+public class XansLair
 {
     public ScriptInterface Bot => ScriptInterface.Instance;
 
     public CoreBots Core => CoreBots.Instance;
     public CoreFarms Farm = new CoreFarms();
-    public CoreDailys Dailys = new CoreDailys();
 
     public void ScriptMain(ScriptInterface bot)
     {
