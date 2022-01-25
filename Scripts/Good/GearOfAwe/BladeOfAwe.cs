39940aa00c37c88d594614d719d335557ea06b59
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-25 22:22:35 +0100

    Quick BoA script for ease of acces

---
+++ Scripts/Good/GearOfAwe/BladeOfAwe.cs
@@ -0,0 +1,31 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using RBot;
+
+public class BladeOfAwe
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetBoA();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetBoA()
+    {
+        if (Core.CheckInventory("Blade of Awe"))
+            return;
+        else Core.BuyItem("museum", 631, "Blade of Awe");
+        if (Core.CheckInventory("Blade of Awe"))
+            return;
+            
+        Farm.BladeofAweREP(1, true);
+    }
+}
