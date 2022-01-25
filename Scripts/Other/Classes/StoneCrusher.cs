f80838568ea50df7f5ec646ca5deeb70f19b4cf6
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-01-25 16:59:16 +0100

    Release of StoneCrusher

---
+++ Scripts/Other/Classes/StoneCrusher.cs
@@ -0,0 +1,37 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using RBot;
+
+public class StoneCrusher
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetSC();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetSC(bool rankUp = true)
+    {
+        if (Core.CheckInventory("StoneCrusher"))
+            return;
+
+        Farm.MythsongREP();
+        Farm.ArcangroveREP();
+        Farm.BrightoakREP();
+
+        Core.BuyItem("Gaiazor", 1210, "Earth's Song Token");
+        Core.BuyItem("Gaiazor", 1210, "Shaman Armor");
+        Core.BuyItem("Gaiazor", 1210, "StoneCrusher");
+
+        if (rankUp)
+            Farm.rankUpClass("StoneCrusher");
+    }
+}
