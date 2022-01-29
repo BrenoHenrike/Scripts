338accde3851a32def2134a01e3ced8d6d8cdd92
Tato <47118168+wtffidy@users.noreply.github.com>
2022-01-29 18:35:15 -0500

    :thumbsup:

--- Scripts/Farm/GoldFarm.cs
+++ Scripts/Farm/GoldFarm.cs
@@ -6,9 +6,12 @@ public class GoldFarm
 {
 	public CoreBots Core => CoreBots.Instance;
 	public CoreFarms Farm = new CoreFarms();
+
 	public void ScriptMain(ScriptInterface bot)
 	{
 		Core.SetOptions();
+
+		//Core.ActionDelay = 1000; //if script is having issues turning in or is slow; un // this line ^_^
 
 		//Farm.UseBoost(BoostIDs.Gold20, RBot.Items.BoostType.Gold);
 
