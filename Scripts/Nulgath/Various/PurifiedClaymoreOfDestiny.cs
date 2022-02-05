c622924e24321c9a8273afc71b59976928e65d17
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-02-05 16:11:34 +0100

    Released as dependency for Taro's Prismatic Manslayer

---
+++ Scripts/Nulgath/Various/PurifiedClaymoreOfDestiny.cs
@@ -0,0 +1,33 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using RBot;
+public class PurifiedClaymoreOfDestiny
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetPCoD();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetPCoD()
+    {
+        if (Core.CheckInventory("Purified Claymore of Destiny"))
+            return;
+
+        Core.AddDrop("Purified Claymore of Destiny");
+
+        Farm.Experience(15);
+        Farm.GoodREP(8);
+
+        Core.EnsureAccept(548);
+        Core.HuntMonster("battleunder", "Undead Berserker", "Warrior Claymore Blade", isTemp: false);
+        Core.EnsureComplete(548);
+    }
+}
