beac339efe0a581460691e11c2d3c6f821d0ffba
Hao <pphenrique9@hotmail.com>
2022-02-06 11:43:15 -0300

    ADD Tower of Mirros

---
+++ Story/ThroneofDarkness/TowerofMirrors.cs
@@ -0,0 +1,69 @@
+//cs_include Scripts/CoreBots.cs
+using RBot;
+public class TowerofMirros
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+
+    public CoreBots Core => CoreBots.Instance;
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        TowerofMirrosSaga();
+
+        Core.SetOptions(false);
+    }
+
+    public void TowerofMirrosSaga()
+    {
+        // Drink Me
+        Core.KillQuest(5314, "towerofmirrors",new[] { "Glassgoyle|Glass Serpent", "Glass Serpent"});
+        // The Key To Success
+        Core.KillQuest(5315, "towerofmirrors", "Silver Elemental");
+        Core.MapItemQuest(5315, "towerofmirrors", 4691, 1);
+        Core.MapItemQuest(5315, "towerofmirrors", 4692, 1);
+        // Phanatics
+        Core.KillQuest(5316, "towerofmirrors",new[] { "Phans", "Phans" });
+        // But I Have A Backstage Pass!
+        Core.KillQuest(5317, "towerofmirrors", "Lothahnos the Reversed");
+        // True Love
+        Core.KillQuest(5318, "towerofmirrors", "Silver Elemental");
+        Core.MapItemQuest(5318, "towerofmirrors", 4687, 1);
+        Core.MapItemQuest(5318, "towerofmirrors", 4693, 1);  
+        // Turn to the Left
+        Core.KillQuest(5319, "towerofmirrors",new[] { "Runway Wraith", "Runway Wraith", "Runway Wraith", "Runway Wraith"});
+        // Now Turn to the Right
+        Core.KillQuest(5320, "towerofmirrors", "Lukcrisio the Buffed");
+        // Or Maybe THIS Is True Love
+        Core.KillQuest(5321, "towerofmirrors", "Silver Elemental");
+        Core.MapItemQuest(5321, "towerofmirrors", 4688, 1);
+        Core.MapItemQuest(5321, "towerofmirrors", 4694, 1);          
+        // Those Harpies!
+        Core.KillQuest(5322, "towerofmirrors",new[] { "Pageant Mom", "Pageant Mom"});
+        // Drink your Go-Go Juice
+        Core.KillQuest(5323, "towerofmirrors", "Medeskar the Smudged");
+        // Oh Sure, Why Not
+        Core.KillQuest(5324, "towerofmirrors", "Silver Elemental");
+        Core.MapItemQuest(5324, "towerofmirrors", 4689, 1);
+        Core.MapItemQuest(5324, "towerofmirrors", 4695, 1);          
+        // Behind the Scenes
+        Core.KillQuest(5325, "towerofmirrors",new[] { "Stage Tech", "Stage Tech"});
+        // In the Spotlight
+        Core.KillQuest(5326, "towerofmirrors", "Atticus the Warped");
+        // Oh, I Give Up
+        Core.KillQuest(5327, "towerofmirrors", "Silver Elemental");
+        Core.MapItemQuest(5327, "towerofmirrors", 4690, 1);
+        Core.MapItemQuest(5327, "towerofmirrors", 4696, 1);         
+        // We Gotta Wendi-GO
+        Core.KillQuest(5328, "towerofmirrors",new[] { "Sasquatch", "Sasquatch"});
+        // Kick His- Wait, Did We Already Use That Pun?
+        Core.KillQuest(5329, "towerofmirrors", "Leofire the Shattered");
+        // Fiend Zoned
+        Core.KillQuest(5330, "towerofmirrors", "Fervent Suitor");
+        // Find Scarletta
+        Core.MapItemQuest(5331, "towerofmirrors", 4697, 1);
+        // Defeat ... Wait. What?
+        Core.KillQuest(5332, "towerofmirrors", "Scarletta", hasFollowup: false);
+    }
+}
