1766fe9c41903cdee8785c9da1c84cd9f6758622
Tato <47118168+wtffidy@users.noreply.github.com>
2022-02-07 09:56:36 -0500

    Update SagaTemplate.cs

--- Story/LordsofChaos/SagaTemplate.cs
+++ Story/LordsofChaos/SagaTemplate.cs
@@ -1,49 +1,28 @@
-//cs_include Scripts/CoreBots.cs
-using RBot;
-using RBot.Options;
-using System.Collections.Generic;
+
+using Rbot;
 
 public class SagaTemplate
 {
+    public ScriptInterface Bot => ScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
-
-    public int questStart = 0;
-
-    public string OptionsStorage = "Template";
-    public bool DontPreconfigure = true;
-
-    public List<IOption> Options = new List<IOption>()
-    {
-        new Option<int>("startQuest", "Quest start index", "This will save the progress through the script.")
-    };
-
-    public static readonly int[] qIDs =
-    {
-        0
-    };
 
     public void ScriptMain(ScriptInterface bot)
     {
         Core.SetOptions();
 
-        questStart = bot.Config.Get<int>("startQuest");
-
-        for (int i = questStart; i < qIDs.Length; i++)
-        {
-            bot.Config.Set("startQuest", i);
-            Core.Logger($"Starting {i}");
-            Core.EnsureAccept(qIDs[i]);
-            switch (i)
-            {
-                case 0: //
-                    
-                    break;
-            }
-            Core.EnsureComplete(qIDs[i]);
-            Core.Logger($"Finished {i}");
-            Core.Rest();
-        }
+        Core.AcceptandCompleteTries = 5;
+        StoryLine();
 
         Core.SetOptions(false);
+    }
+    public void StoryLine()
+    {
+        if (Core.QuestProgression(QuestID))
+            return;
+         
+        Core.KillQuest(QuestID: , MapName: "", MonsterName: "");
+        Core.KillQuest(QuestID: , MapName: "", MonsterNames: new[] { "Monstername", "Monstername" });
+        Core.MapItemQuest(QuestID: , MapName: "", MapItemID: , Amount: ); 
+        Core.ChainQuest(QuestID: );
     }
 }
