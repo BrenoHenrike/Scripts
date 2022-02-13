885ff3821d66f85eedffdd4106355c2916c09e59
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-02-13 15:25:14 +0100

    Removed CoreDaily dependency

--- Legion/Revenant/0LegionRevenant.cs
+++ Legion/Revenant/0LegionRevenant.cs
@@ -1,6 +1,5 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
-//cs_include Scripts/CoreDailys.cs
 //cs_include Scripts/Legion/CoreLegion.cs
 //cs_include Scripts/Legion/Revenant/CoreLR.cs
 //cs_include Scripts/Legion/JoinLegion[UndeadWarrior].cs
