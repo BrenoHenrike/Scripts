6128dc508df87f7bf3d1853c60c6b2124f436557
Tato <47118168+wtffidy@users.noreply.github.com>
2022-02-15 18:16:06 -0500

    +Collection storyline
    +template tomfolery

--- DefaultTemplate.cs
+++ DefaultTemplate.cs
@@ -3,7 +3,7 @@
 
 //cs_include Scripts/CoreFile(Or folder)/Filename.cs
 
-using RBot; //LEAVE THIS ALONE
+using RBot;
 
 public class Example 
 {
@@ -14,12 +14,12 @@ public class Example
 
     public void ScriptMain(ScriptInterface bot)
     {
-        Core.SetOptions(); //LEAVE THIS ALONE
+        Core.SetOptions();
 
         Core.AddDrop("ExampleItem1", "ExampleItem2", "ExampleItem3", "ExampleItem4");
 
         //some code goes here probably
 
-        Core.SetOptions(false); //LEAVE THIS ALONE
+        Core.SetOptions(false);
     }
 }
---
+++ Story/Collection.cs
@@ -0,0 +1,104 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+
+using RBot;
+
+public class Collections
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Terrarium();
+        Prehistoric();
+        TheFuture();
+
+        Core.SetOptions(false);
+    }
+
+    public void Terrarium()
+    {
+        if (Core.isCompletedBefore(1309))
+            return;
+
+
+        // This Town in a Desktop Globe
+        Core.MapItemQuest(1293, "Terrarium", 586, AutoCompleteQuest: false);
+        // Dust Bunnies Can't Hide
+        Core.KillQuest(1294, "Terrarium", "Dustbunny of Doom");
+        // Au Contr-air holes
+        Core.MapItemQuest(1295, "Terrarium", 587, 10);
+        // Hero Eats Everything. Even Pellets
+        Core.MapItemQuest(1296, "Terrarium", 588, 12);
+        // The Moth Defeats the Man?
+        Core.KillQuest(1297, "Terrarium", "Death on Wings");
+        // Can you find them?
+        Core.MapItemQuest(1298, "Terrarium", 589);
+        Core.MapItemQuest(1298, "Terrarium", 590);
+        Core.MapItemQuest(1298, "Terrarium", 591);
+        Core.MapItemQuest(1298, "Terrarium", 592);
+        Core.MapItemQuest(1298, "Terrarium", 593);
+        // You've Been Looking Everywhere!
+
+        Core.MapItemQuest(1299, "Terrarium", 593);
+        Core.MapItemQuest(1299, "Terrarium", 594);
+        Core.MapItemQuest(1299, "Terrarium", 595);
+        Core.MapItemQuest(1299, "Terrarium", 596);
+        Core.MapItemQuest(1299, "Terrarium", 604, AutoCompleteQuest: false);
+
+        // Go for Grease!
+        Core.KillQuest(1300, "Terrarium", "Death on Wings");
+        // Doppelganger Wants to Hit You
+        Core.KillQuest(1308, "Terrarium", new[] { "Doppleganger of Will", "Doppleganger of Fred" });
+        // Catapult Climb
+        Core.MapItemQuest(1309, "Terrarium", 598);
+    }
+
+    public void Prehistoric()
+    {
+        if (Core.isCompletedBefore(1343))
+            return;
+
+        // The Treasure that You Seek - 976
+        Core.KillQuest(1339, "prehistoric", "Gigantosaurus");
+        // These Trees - 1340
+        Core.MapItemQuest(1340, "prehistoric", 630, 11);
+        // Ewwww... Totally Gross - 1341
+        Core.KillQuest(1341, "prehistoric", "Gigantosaurus");
+        // The Eggs, Exciting and New - 1342
+        Core.MapItemQuest(1342, "prehistoric", 631, 4);
+        // Someday (Today) Their Mother Will Die - 1343
+        Core.KillQuest(1343, "prehistoric", "Mother TerrorDOOMKill");
+
+    }
+
+    public void TheFuture()
+    {
+        if (Core.isCompletedBefore(1348))
+            return;
+
+        // More Aliens Than You Can Handle?
+        Core.KillQuest(1344, "Future", "Alien Dino|Alien Juggernaut|Furious 4-Eyed Guard|Red-Eyed Alien");
+        // Spy Eyes
+        Core.MapItemQuest(1345, "Future", 632, 2);
+        // Smashed to Pieces
+        Core.MapItemQuest(1346, "Future", 633, 7);
+        // Tired of My Vigilette Dreams
+        if(!Core.QuestProgression(1347))
+        {
+        Core.EnsureAccept(1347);
+        Core.HuntMonster("Future", "Red-Eyed Alien", "Small Fragment Acquired", 5);
+        Core.HuntMonster("Future", "Red-Eyed Alien", "Medium Piece Acquired", 5);
+        Core.EnsureComplete(1347);
+        }
+        // You're Not the Boss of Me Now
+        Core.KillQuest(1348, "Future", "The Collector");
+    }
+
+
+}
