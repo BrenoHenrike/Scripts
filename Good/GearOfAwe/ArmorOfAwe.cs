5158abdf7909aa078b3c7c8cc5ec975be18f2cd7
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-02-16 16:20:59 +0100

    Added CoreAdvanced to dependency

--- Good/GearOfAwe/ArmorOfAwe.cs
+++ Good/GearOfAwe/ArmorOfAwe.cs
@@ -1,17 +1,14 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/Good/GearOfAwe/CoreAwe.cs
-
 using RBot;
 
 public class ArmorOfAwe
 {
     public ScriptInterface Bot => ScriptInterface.Instance;
-
     public CoreBots Core => CoreBots.Instance;
-
     public CoreFarms Farm = new CoreFarms();
-
     public CoreAwe Awe = new CoreAwe();
 
     public void ScriptMain(ScriptInterface bot)
