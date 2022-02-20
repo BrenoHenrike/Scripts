0eccb0c4bc275fc4d48211450f90d748f6700bbb
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-02-20 16:32:33 +0100

    Removed extra s from fortress

--- Farm/REP/1GetAllRanks.cs
+++ Farm/REP/1GetAllRanks.cs
@@ -1,7 +1,7 @@
 ﻿//cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/Story/ThroneofDarkness/05bSekt(FourthDimensionalPyramid).cs
-//cs_include Scripts/Story/ThroneofDarkness/03aZiri(BaconCatFortresss).cs
+//cs_include Scripts/Story/ThroneofDarkness/03aZiri(BaconCatFortress).cs
 //cs_include Scripts/Story/LordsofChaos/Core13LoC.cs
 using RBot;
 public class GetAllRanks
