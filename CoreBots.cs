5b3bd7868530a5920b67d45178499ba80b65c8ac
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-02-21 16:04:50 +0100

    tiny change to restoreguild

--- CoreBots.cs
+++ CoreBots.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
+using System.Web;
 
 public class CoreBots
 {
@@ -149,7 +150,7 @@ public class CoreBots
                     Bot.CallGameFunction("world.toggleMonsters");
             }
 
-            GuildRestore = Bot.GetGameObject<string>("world.myAvatar.pMC.pname.tg.text").Replace("&lt; ", "< ").Replace(" &gt;", " >");
+            GuildRestore = Bot.GetGameObject<string>("world.myAvatar.pMC.pname.tg.text");
             Bot.Options.CustomName = "AUQW RBOT MASTER";
             Bot.Options.CustomGuild = "HTTPS://AUQW.TK/";
 
@@ -1031,7 +1032,7 @@ public class CoreBots
         Bot.Options.LagKiller = true;
         Bot.Options.LagKiller = false;
         Bot.Options.CustomName = Bot.Player.Username.ToUpper();
-        Bot.Options.CustomGuild = GuildRestore;
+        Bot.Options.CustomGuild = HttpUtility.HtmlDecode(GuildRestore); ;
         Logger("Bot Stopped Successfully", messageBox: true);
         ScriptManager.StopScript();
     }
