afc301b6f1f5a60de75ce5b61b3c3f19357fd141
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-02-22 02:52:07 +0100

    Fixed cs_includes

--- Other/Classes/REP-based/Bard.cs
+++ Other/Classes/REP-based/Bard.cs
@@ -2,7 +2,7 @@
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/CoreStory.cs
-//cs_include Scripts/Story/LordofChaos/Core13LoC.cs
+//cs_include Scripts/Story/LordsofChaos/Core13LoC.cs
 using RBot;
 
 public class Bard
