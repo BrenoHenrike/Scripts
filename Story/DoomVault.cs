7ee0a1dfcc8e23b34a91e5f9f00b7fe71bac9fde
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-02-23 22:03:27 +0100

    Changed bypass packet stuff to updateQuests

--- Story/DoomVault.cs
+++ Story/DoomVault.cs
@@ -24,77 +24,74 @@ public class DoomVaultA
 
         // the challenge begins
         Story.KillQuest(QuestID: 2952, MapName: "doomvault", MonsterName: "Grim Soldier");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //fight to survive
         Story.KillQuest(QuestID: 2953, MapName: "doomvault", MonsterName: "Grim Fighter");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         // the battle's heating up
         Story.KillQuest(QuestID: 2954, MapName: "doomvault", MonsterName: "Grim Fire Mage");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         // a close shave
         Story.KillQuest(QuestID: 2955, MapName: "doomvault", MonsterName: "Grim Shelleton");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         // eye spy a victim
         Story.KillQuest(QuestID: 2965, MapName: "doomvault", MonsterName: "Flying Spyball");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         // help me!
         Story.KillQuest(QuestID: 2966, MapName: "doomvault", MonsterName: "Princess Angler");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         // get your hands dirty
         Story.KillQuest(QuestID: 2967, MapName: "doomvault", MonsterName: "Grim Ectomancer");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         // a rocky battle
         Story.KillQuest(QuestID: 2968, MapName: "doomvault", MonsterName: "Fallen Light Statue");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //soul-d of defeat
         Story.KillQuest(QuestID: 2969, MapName: "doomvault", MonsterName: "Grim Soldier");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //the key to help me
         Story.KillQuest(QuestID: 2970, MapName: "doomvault", MonsterName: "Grim Shelleton");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //help me again!
         Story.KillQuest(QuestID: 2971, MapName: "doomvault", MonsterName: "Princess Angler");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //overheated hero
         Story.KillQuest(QuestID: 2974, MapName: "doomvault", MonsterName: "Grim Fire Mage");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //the blade-breaker
         Story.KillQuest(QuestID: 2981, MapName: "doomvault", MonsterName: "Grim Lich");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //anti-magic warrior
         Story.KillQuest(QuestID: 2982, MapName: "doomvault", MonsterName: "Grim Fighter");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //elemental destroyer
         Story.KillQuest(QuestID: 2983, MapName: "doomvault", MonsterName: "Grim Ectomancer");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //the unkillable
         Story.KillQuest(QuestID: 3006, MapName: "doomvault", MonsterName: "Grim Shelleton");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //key to victory
         Story.KillQuest(QuestID: 3007, MapName: "doomvault", MonsterName: "Fallen Light Statue");
-        BypassPacket();
+        Story.UpdateQuest(3008);
 
         //i command you, help me!
         Story.KillQuest(QuestID: 3008, MapName: "doomvault", MonsterName: "Ghost King Angler");
-        BypassPacket();
+        Story.UpdateQuest(3008);
     }
-
-    private void BypassPacket()
-        => Bot.SendClientPacket("{\"t\":\"xt\",\"b\":{\"r\":-1,\"o\":{\"cmd\":\"updateQuest\",\"iValue\":18,\"iIndex\":126}}}");
 }
