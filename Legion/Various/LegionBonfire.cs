6bc710b036b42511f7f063fd706081a07f3e3842
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-02-28 14:56:01 +0100

    Released SoulEssence + dependencies

---
+++ Legion/Various/LegionBonfire.cs
@@ -0,0 +1,31 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/Legion/CoreLegion.cs
+using RBot;
+
+public class LegionBonfire
+{
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreLegion Legion = new CoreLegion();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetLegionBonfire();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetLegionBonfire()
+    {
+        if (Core.CheckInventory("Legion Bonfire"))
+            return;
+
+        Legion.FarmLegionToken(10000);
+        Farm.Gold(5000000);
+
+        Core.BuyItem("underworld", 1985, "Legion Bonfire");
+    }
+}
---
+++ Legion/Various/SoulEssence.cs
@@ -0,0 +1,51 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/Legion/CoreLegion.cs
+//cs_include Scripts/Legion/Various/SoulSand.cs
+//cs_include Scripts/Legion/Various/LegionBonfire.cs
+using RBot;
+
+public class LetItBurn
+{
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreLegion Legion = new CoreLegion();
+    public AnotherOneBitesTheDust SSand = new AnotherOneBitesTheDust();
+    public LegionBonfire Bon = new LegionBonfire();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        SoulEssence();
+
+        Core.SetOptions(false);
+    }
+
+    private string[] rewards = { "Legion Undead Spawn", "Legion Undead Visor", "Legion Forge Banisher", "Legion Spawn Bonker" };
+
+    public void SoulEssence(int quant = 50)
+    {
+        if (Core.CheckInventory("Soul Essence", quant))
+            return;
+
+        Core.AddDrop("Soul Essence");
+        Core.AddDrop(rewards);
+        int i = 1;
+
+        Farm.Experience(65);
+        Bon.GetLegionBonfire();
+
+        Core.Logger($"Farming {quant} Soul Essence");
+        while (!Core.CheckInventory("Soul Essence", quant))
+        {
+            Core.EnsureAccept(7992);
+            Core.HuntMonster("dagefortress", "Grrrberus", "Grrberus' Flame");
+            SSand.SoulSand(3);
+            if (!Core.CheckInventory(rewards))
+                Core.EnsureCompleteChoose(7992, rewards);
+            else Core.EnsureComplete(7992);
+            Core.Logger($"Completed x{i++}");
+        }
+    }
+}
