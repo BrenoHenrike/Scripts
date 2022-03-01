b44991a73cd87369a67ccf576f0ad31050425d49
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-03-01 02:06:17 +0100

    Added Comments and more formatting

--- Story/J6Saga.cs
+++ Story/J6Saga.cs
@@ -1,17 +1,14 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreStory.cs
-
 using RBot;
 
 public class J6Saga
 {
     public ScriptInterface Bot => ScriptInterface.Instance;
-
     public CoreBots Core => CoreBots.Instance;
     public CoreFarms Farm = new CoreFarms();
     public CoreStory Story = new CoreStory();
-
 
     public void ScriptMain(ScriptInterface bot)
     {
@@ -27,111 +24,96 @@ public class J6Saga
         if (Core.isCompletedBefore(2858))
             return;
 
-        Core.AddDrop("J6's Secret Hideout Map", "Dwakel Decoder", "Mission 1 Item", "Datadisk 5",
-        "Datadisk 4", "Fanciful Feather", "Datadisk 3", "Absorbent Mop", "Hyperium Spaceship Key");
+        Core.AddDrop("Mission 1 Item", "Datadisk 5", "Datadisk 4", "Fanciful Feather", "Datadisk 3", "Absorbent Mop", "Hyperium Spaceship Key");
 
         Core.EquipClass(ClassType.Farm);
 
-        if (!Core.CheckInventory("J6's Secret Hideout Map"))
-            Core.KillMonster("j6", "R4", "Left", "*", "J6's Secret Hideout Map", isTemp: false);
+        //J6 Quiz Game
+        Story.ChainQuest(674);
 
-        if (!Core.CheckInventory("Dwakel Decoder"))
-            Core.GetMapItem(106, map: "crashsite");
+        //Access Zephyrus
+        Story.ChainQuest(693);
 
-        if (!Story.QuestProgression(674))
-        {
-            Core.SendPackets("%xt%zm%serverUseItem%327106%+%5041%345,200%saloon%");
-            Story.MapItemQuest(674, "saloon", 109);
-        }
-
+        //Zephyrus End
         if (!Story.QuestProgression(694))
         {
-            Core.SendPackets("%xt%zm%serverUseItem%327705%+%5041%525,275%hyperium%");
-            while (!Bot.Player.Cell.Equals("R10"))
-                Core.Jump("R10", "Up");
-            Core.SendPackets($"%xt%zm%cmd%328430%tfer%{Bot.Player.Username}%zephyrus%Enter%Spawn%");
-            Core.EnsureAccept(694);
-            Core.GetMapItem(116, map: "zephyrus");
-            Core.GetMapItem(117, map: "zephyrus");
-            Core.EnsureComplete(694);
+            JoinHyperium();
+            Story.MapItemQuest(694, "zephyrus", new[] { 116, 117 });
         }
 
+        //Mission1
         Story.KillQuest(698, "forest", "Zardman Grunt");
 
+        //Mission2
         Story.KillQuest(699, "boxes", "Sneeviltron");
 
-        if (!Core.CheckInventory("Datadisk 5"))
-            Core.KillMonster("frozenfotia", "r5", "Left", "*", "Datadisk 5", isTemp: false);
+        Core.KillMonster("frozenfotia", "r5", "Left", "*", "Datadisk 5", isTemp: false);
 
-        if (!Story.QuestProgression(1171))
-        {
-            Core.SendPackets("%xt%zm%serverUseItem%327705%+%5041%525,275%hyperium%");
-            while (!Bot.Player.Cell.Equals("R10"))
-                Core.Jump("R10", "Up");
-            Core.SendPackets($"%xt%zm%cmd%507613%tfer%{Bot.Player.Username}%moonyard%Enter%Spawn%");
-            Story.KillQuest(1171, "moonyard", "Junkyard Wall");
-        }
+        JoinHyperium();
+        //A Gate and Terrible Monster
+        Story.KillQuest(1171, "moonyard", "Junkyard Wall");
 
+        //I'm on the Hunt, I'm after 58-Sr3
         Story.MapItemQuest(1172, "moonyard", 495);
 
+        //Intruder Alert! Intruder Alert!
         if (!Story.QuestProgression(1173))
         {
             if (!Bot.Map.Name.Equals("moonyard"))
             {
-                Core.SendPackets("%xt%zm%serverUseItem%327705%+%5041%525,275%hyperium%");
-                while (!Bot.Player.Cell.Equals("R10"))
-                    Core.Jump("R10", "Up");
-                Core.SendPackets($"%xt%zm%cmd%507613%tfer%{Bot.Player.Username}%moonyard%Enter%Spawn%");
+                JoinHyperium();
+                Core.Join("moonyard");
             }
             Core.Jump("MoonCut", "Left");
-            Core.SendPackets($"%xt%zm%cmd%510002%tfer%{Bot.Player.Username}%moonyardb%r24%Right%");
             Story.KillQuest(1173, "moonyardb", "Robo Guard", GetReward: false);
         }
 
+        //Mission 3
         Story.KillQuest(1177, "marsh2", "Lesser Shadow Serpent");
 
+        //Mission 4
         Story.KillQuest(1178, "marsh2", "Lesser Groglurk");
 
         if (!Core.CheckInventory("Datadisk 4"))
             Core.GetMapItem(1258, map: "sewer");
 
+        //Planet Banzai!
         if (!Story.QuestProgression(2168))
         {
-            Core.SendPackets("%xt%zm%serverUseItem%327705%+%5041%525,275%hyperium%");
-            while (!Bot.Player.Cell.Equals("R10"))
-                Core.Jump("R10", "Up");
-            Core.SendPackets($"%xt%zm%cmd%512688%tfer%{Bot.Player.Username}%banzai%Enter%Spawn%");
+            JoinHyperium();
             Story.MapItemQuest(2168, "banzai", 1259);
         }
 
+        //L.O.S.E.R. Battle!
         Story.ChainQuest(2169);
 
+        //Down to the Wire
         Story.ChainQuest(2170);
 
+        //J6 in SPAAAAACE!
         Story.ChainQuest(2171);
 
+        //Mission 5
         Story.KillQuest(2173, "djinn", "Harpy");
 
         if (!Core.CheckInventory("Datadisk 3"))
             Story.KillQuest(2830, "xantown", "*", GetReward: false);
 
+        //Fuel For Flight
         if (!Story.QuestProgression(2831))
         {
             Core.GetMapItem(1741, map: "timevoid");
             Core.EnsureAccept(2831);
             Core.KillMonster("sandsea", "r8", "Left", "*", "Cactus Creeper Oil", 3);
             Core.KillMonster("cloister", "r7", "Left", "Acornent", "Acornent Oil", 3);
-            Core.SendPackets("%xt%zm%serverUseItem%327705%+%5041%525,275%hyperium%");
-            while (!Bot.Player.Cell.Equals("R10"))
-                Core.Jump("R10", "Up");
-            Core.SendPackets($"%xt%zm%cmd%507613%tfer%{Bot.Player.Username}%moonyard%Enter%Spawn%");
-            Core.Jump("MoonCut", "Left");
-            Core.SendPackets($"%xt%zm%cmd%510002%tfer%{Bot.Player.Username}%moonyardb%r24%Right%");
+            JoinHyperium();
+            Core.Join("moonyard", "MoonCut", "Left");
             Core.KillMonster("moonyardb", "r4", "Left", "*", "Robo Dog Oil", 3);
             Core.KillMonster("farm", "Crop1", "Left", "*", "Scarecrow Canola Oil", 3);
             Core.EnsureComplete(2831);
         }
 
+        //Food For Flight
         if (!Story.QuestProgression(2832))
         {
             Core.EnsureAccept(2832);
@@ -142,6 +124,7 @@ public class J6Saga
             Core.EnsureComplete(2832);
         }
 
+        //Vomit Comet
         if (!Story.QuestProgression(2833))
         {
             Core.EnsureAccept(2833);
@@ -152,49 +135,74 @@ public class J6Saga
             Core.EnsureComplete(2833);
         }
 
-        if (!Story.QuestProgression(2834))
-        {
-            Farm.ChronoSpanREP(2);
-            Story.BuyQuest(2834, "thespan", 439, "Comfy Pillow");
-        }
+        Farm.ChronoSpanREP(2);
+        //Sweet Dreams for a Safe Flight
+        Story.BuyQuest(2834, "thespan", 439, "Comfy Pillow");
 
+        //Reach VR Room
         Story.MapItemQuest(2837, "hyperspace", 1742);
 
+        //Speak with H.A.L.
         Story.MapItemQuest(2838, "hyperspace", 1743);
 
+        //Defeat H.A.L.
         Story.MapItemQuest(2839, "hyperspace", 1744);
 
+        //Spare J6
         Story.ChainQuest(2856);
 
+        //Choose J6 cutscene
         Story.MapItemQuest(2840, "hyperspace", 1745);
 
-        if (!Story.QuestProgression(2841))
-        {
-            if (!Bot.Map.Name.Equals("hyperspace"))
-                Core.Join("hyperspace");
-            Core.Jump("R10", "Up");
-            Core.SendPackets($"%xt%zm%cmd%136830%tfer%{Bot.Player.Username}%Alley%Enter%Spawn%");
-            Story.KillQuest(2841, "alley", "Trash Can");
-        }
+        JoinAlley();
+        //Reduce, Respawn, Recycle
+        Story.KillQuest(2841, "alley", "Trash Can");
 
+        //Mind your Manners
         Story.KillQuest(2842, "alley", "Thug Bully");
 
+        //FUNdraisers!
         Story.KillQuest(2843, "alley", "Thug Bully");
 
+        //Bully the Thug
         Story.KillQuest(2849, "alley", "Thug Boss");
 
+        //Security Breach
         Story.KillQuest(2844, "alley", new[] { "Guard Robot", "Security Cam" });
 
+        //No Sniffin' Around
         Story.KillQuest(2845, "alley", "Guard Dog Robot");
 
+        //Key to Winning
         Story.KillQuest(2846, "alley", "Guard Dog Robot|Security Cam|Guard Robot");
 
+        //Watch Chapter 1
         Story.MapItemQuest(2850, "hyperspace", 1747);
 
+        JoinAlley();
+        //Watch Chapter 1
         Story.KillQuest(2851, "alley", "Thug Minion");
 
+        //Watch Chapter 1
         Story.MapItemQuest(2852, "hyperspace", 1749);
 
+        //Watch Chapter 1
         Story.ChainQuest(2858);
+
+        void JoinHyperium()
+        {
+            Core.SendPackets("%xt%zm%serverUseItem%327705%+%5041%525,275%hyperium%");
+            while (Bot.Player.Cell != "R10")
+                Core.Jump("R10", "Up");
+        }
+
+        void JoinAlley()
+        {
+            if (Bot.Map.Name == "alley")
+                return;
+            Core.Join("hyperspace", "R10", "Up");
+            Core.Join("alley");
+        }
     }
+
 }
