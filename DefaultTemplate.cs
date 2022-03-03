3b3dedfb2d2c5a329cd4cea53347a075463bf844
Tato <47118168+wtffidy@users.noreply.github.com>
2022-03-03 14:26:30 -0500

    +story & adv to Default Template, NSOD OptimizeInv = false > true; , +BrightOak story

--- DefaultTemplate.cs
+++ DefaultTemplate.cs
@@ -1,5 +1,7 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
 using RBot;
 
 public class ScriptTemplate
@@ -7,6 +9,8 @@ public class ScriptTemplate
     public ScriptInterface Bot => ScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreFarms Farm = new CoreFarms();
+    public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreStory Story = new CoreStory();
 
     public void ScriptMain(ScriptInterface bot)
     {
