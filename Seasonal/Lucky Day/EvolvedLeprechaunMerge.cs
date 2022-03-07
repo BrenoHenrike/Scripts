844edba30492b1ddea1557f6aa5141ee095b87ba
Vladimir <67055046+VladimirBienvenue@users.noreply.github.com>
2022-03-07 08:30:28 -0800

    Added Lucky Day folder into Seasonal and Added EvolvedLeperchaunMerge.cs

---
+++ Seasonal/Lucky Day/EvolvedLeprechaunMerge.cs
@@ -0,0 +1,59 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using RBot;
+
+public class EvolvedLeprechaun
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new CoreStory();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetItems();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetItems()
+    {
+
+        if (Core.CheckInventory("Evolved Leprechaun"))
+            return;
+
+        Core.AddDrop("Lucky Clover", "Rainbow Shard", "Golden Ticket");
+
+        //Rainbow Shard
+        while (!Core.CheckInventory("Rainbow Shard", 20))
+        {
+            Core.EnsureAccept(1758);
+            Core.HuntMonster("rainbow", "Rainbow Rat", "Prismatic Rainbow Fur", 5);
+            Core.EnsureComplete(1758);
+        }
+
+        //Golden Ticket
+        while (!Core.CheckInventory("Golden Ticket", 2000))
+        {
+            Core.GetMapItem(101, 1, "luck");
+        }
+
+        //Lucky Clover
+        if (!Core.CheckInventory("Lucky Clover", 12))
+            if (!Bot.Quests.IsDailyComplete(1759))
+            {
+                Core.SmartKillMonster(1759, "rainbow", "Lucky Harms ", 20, true);
+            }
+            if (!Core.CheckInventory("Lucky Clover", 12))
+            {
+                Core.Logger($"Not enough \"Lucky Clovers\", please do the daily until you have 12. You can run it once a day (Server resets at 12am EST)");
+            }
+
+        //Buy Evolved Leprechaun
+        if (Core.CheckInventory("Lucky Clover", 12) && (Core.CheckInventory("Rainbow Shard", 20) && (Core.CheckInventory("Golden Ticket", 2000))))
+        {
+            Core.BuyItem("luck", 256, "Evolved Leprechaun");
+        }
+    }
+}
