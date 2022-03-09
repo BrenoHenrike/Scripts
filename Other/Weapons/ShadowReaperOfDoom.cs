55f40095d5dbe0ce273dae4f29a29918426c8a03
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-03-09 17:33:44 +0100

    Added ;

--- Other/Weapons/ShadowReaperOfDoom.cs
+++ Other/Weapons/ShadowReaperOfDoom.cs
@@ -1,5 +1,4 @@
 //cs_include Scripts/CoreBots.cs
-
 using RBot;
 
 public class SRoD
@@ -20,7 +19,7 @@ public class SRoD
     {
         if (Core.CheckInventory("ShadowReaper Of Doom"))
             return;
-        Core.Logger("Farming for ShadowReaper Of Doom")
+        Core.Logger("Farming for ShadowReaper Of Doom");
         Core.KillMonster("overworld", "r2", "Up", "*", "Mirror Realm Token", 300, false);
         Core.KillMonster("overworld", "boss1", "Left", "Undead Artix", "Undead Paladin Token", isTemp: false);
         Core.BuyItem("overworld", 618, 17488);
