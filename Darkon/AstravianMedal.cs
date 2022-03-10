48a1bec45780dbcb6ce61e4f79f125f08465e834
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-03-10 01:27:33 +0100

    Made public file

---
+++ Darkon/AstravianMedal.cs
@@ -0,0 +1,22 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Darkon/CoreDarkon.cs
+using RBot;
+
+public class AstravianMedal
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreDarkon Darkon = new CoreDarkon();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Darkon.AstravianMedal();
+
+        Core.SetOptions(false);
+    }
+}
