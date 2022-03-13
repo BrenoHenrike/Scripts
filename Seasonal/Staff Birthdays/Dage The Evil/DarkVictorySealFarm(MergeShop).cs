cde8a9fba2cc1a0d6953b289d537c3f9d1668606
Vladimir <67055046+VladimirBienvenue@users.noreply.github.com>
2022-03-13 10:46:34 -0700

    Release DageRecruit, Release DarkVictorySealFarm(MergeShop), Added Staff Birthday Folder into Seasonal

---
+++ Seasonal/Staff Birthdays/Dage The Evil/DarkVictorySealFarm(MergeShop).cs
@@ -0,0 +1,39 @@
+//cs_include Scripts/CoreBots.cs
+using RBot;
+
+public class DarkVictorySeal
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        DarkVictorySealFarm();
+
+        Core.SetOptions(false);
+    }
+
+    public void DarkVictorySealFarm()
+    {
+        Core.AddDrop("Dark Victory Seal");
+
+        //Progress Check
+        if (!Core.isCompletedBefore(8575))
+            {
+                Core.Logger("Please run the DageRecruit Story bot before this one as you have not unlocked the required quests.");
+                return;
+            }
+
+        //Dark Victory Seal
+        while (!Core.CheckInventory("Dark Victory Seal", 1000))
+        {
+            Core.EnsureAccept(8576);
+            Core.HuntMonster("dagerecruit", "Dark Makai", "Dark Makai Defeated", 6);
+            Core.HuntMonster("dagerecruit", "Dreadfiend", "Dreadfiend Defeated", 6);
+            Core.HuntMonster("dagerecruit", "Bloodfiend", "Bloodfiend Defeated", 6);
+            Core.HuntMonster("dagerecruit", "Infernal Fiend", "Infernal Fiend Defeated", 6);
+            Core.EnsureComplete(8576);
+        }
+    }
+}
