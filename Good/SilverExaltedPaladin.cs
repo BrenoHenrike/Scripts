42a23c6a7da2cac1d6d7bf7217011d4d07ca0920
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-03-15 15:23:28 +0100

    Added PreLoad and fixed some things

--- Good/SilverExaltedPaladin.cs
+++ Good/SilverExaltedPaladin.cs
@@ -20,30 +20,39 @@ public class SEP
 
     public void SilverExaltedPaladin()
     {
-        Core.AddDrop("Silver Exalted Paladin");
         if (Core.CheckInventory("Silver Exalted Paladin"))
             return;
-        if (!Story.QuestProgression(7580))
+
+        Story.PreLoad();
+
+        Core.AddDrop("Silver Exalted Paladin");
+
+        if (!Core.isCompletedBefore(7586))
         {
-            Core.EnsureAccept(7580);
-            Core.KillMonster("dragonheart", "r3", "Left", "*", "Ancient Paladin Chest 1");
-            Core.KillMonster("dragonheart", "r5", "Left", "*", "Ancient Paladin Chest 2");
-            Core.KillMonster("dragonheart", "r7", "Left", "*", "Ancient Paladin Chest 3");
-            Core.KillMonster("dragonheart", "r9", "Left", "*", "Ancient Paladin Chest 4");
-            Core.KillMonster("dragonheart", "r10", "Left", "Tempest Dracolich", "Ancient Paladin Chest 5");
-            Core.KillMonster("dragonheart", "r11", "Left", "Granite Dracolich", "Ancient Paladin Chest 6");
-            Core.KillMonster("dragonheart", "r12", "Left", "Avatar of Desolich", "Ancient Paladin Chest 7");
-            Core.EnsureComplete(7580);
+            if (!Story.QuestProgression(7580))
+            {
+                Core.EnsureAccept(7580);
+                Core.KillMonster("dragonheart", "r3", "Left", "*", "Ancient Paladin Chest 1");
+                Core.KillMonster("dragonheart", "r5", "Left", "*", "Ancient Paladin Chest 2");
+                Core.KillMonster("dragonheart", "r7", "Left", "*", "Ancient Paladin Chest 3");
+                Core.KillMonster("dragonheart", "r9", "Left", "*", "Ancient Paladin Chest 4");
+                Core.KillMonster("dragonheart", "r10", "Left", "Tempest Dracolich", "Ancient Paladin Chest 5");
+                Core.KillMonster("dragonheart", "r11", "Left", "Granite Dracolich", "Ancient Paladin Chest 6");
+                Core.KillMonster("dragonheart", "r12", "Left", "Avatar of Desolich", "Ancient Paladin Chest 7");
+                Core.EnsureComplete(7580);
+            }
+            Story.KillQuest(7581, "ectocave", "Ichor Dracolich", AutoCompleteQuest: false);
+            Core.EnsureCompleteChoose(7581);
+            Story.KillQuest(7582, "frozenruins", "Frostdeep Dweller", AutoCompleteQuest: false);
+            Core.EnsureCompleteChoose(7582);
+            Story.KillQuest(7583, "thirdspell", "Great Solar Elemental", GetReward: false);
+            Story.KillQuest(7584, "table", "Roach", AutoCompleteQuest: false);
+            Core.EnsureCompleteChoose(7584);
+            Story.KillQuest(7585, "dracocon", "Singer", AutoCompleteQuest: false);
+            Core.EnsureCompleteChoose(7585);
         }
-        Story.KillQuest(7581, "ectocave", "Ichor Dracolich", AutoCompleteQuest: false);
-        Core.EnsureCompleteChoose(7581);
-        Story.KillQuest(7582, "frozenruins", "Frostdeep Dweller", AutoCompleteQuest: false);
-        Core.EnsureCompleteChoose(7582);
-        Story.KillQuest(7583, "thirdspell", "Great Solar Elemental", GetReward: false);
-        Story.KillQuest(7584, "table", "Roach", AutoCompleteQuest: false);
-        Core.EnsureCompleteChoose(7584);
-        Story.KillQuest(7585, "dracocon", "Singer", AutoCompleteQuest: false);
-        Core.EnsureCompleteChoose(7585);
-        Story.KillQuest(7586, "warhorc", "General Drox");
+        Core.EnsureAccept(7586);
+        Core.HuntMonster("warhorc", "General Drox", "Paladin Armor Found");
+        Core.EnsureComplete(7586);
     }
 }
