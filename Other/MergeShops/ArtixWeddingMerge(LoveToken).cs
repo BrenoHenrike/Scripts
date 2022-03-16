ba1ae4700daf6ed17b7f0a0008f6af2981d6c177
Vladimir <67055046+VladimirBienvenue@users.noreply.github.com>
2022-03-16 15:20:19 -0700

    Create ArtixWeddingMerge(LoveToken).cs

---
+++ Other/MergeShops/ArtixWeddingMerge(LoveToken).cs
@@ -0,0 +1,25 @@
+//cs_include Scripts/CoreBots.cs
+using RBot;
+
+public class ArtixWeddinMerge
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetItems();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetItems()
+    {
+        //Needed AddDrop
+        Core.AddDrop("Love Token");
+
+        Core.HuntMonster("grimskullannex", "Grim Mage|Grim Soldier|Grim Fighter", "Love Token", 1000, false);
+    }
+}
