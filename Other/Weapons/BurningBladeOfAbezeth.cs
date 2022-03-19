1a5c301ab555d5877b40048a3e3a090d0214d4ee
Tato <47118168+wtffidy@users.noreply.github.com>
2022-03-19 12:10:06 -0400

    Update BurningBladeOfAbezeth.cs

--- Other/Weapons/BurningBladeOfAbezeth.cs
+++ Other/Weapons/BurningBladeOfAbezeth.cs
@@ -1,9 +1,13 @@
 //cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreFarms.cs
 using RBot;
 public class BurningBladeOfAbezeth
 {
     public ScriptInterface Bot => ScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
+    public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreFarms Farm = new CoreFarms();
 
     public void ScriptMain(ScriptInterface bot)
     {
@@ -23,5 +27,6 @@ public class BurningBladeOfAbezeth
         Bot.Quests.UpdateQuest(6042);
         Core.HuntMonster("celestialarenad", "Aranx", "Burning Blade Of Abezeth", isTemp: false);
         Bot.Wait.ForPickup("Burning Blade Of Abezeth");
+        Adv.EnhanceItem("Burning Blade Of Abezeth", EnhancementType.Lucky);
     }
 }
