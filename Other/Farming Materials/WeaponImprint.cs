c4cd3d4189442d5afc65231a7bbcb44c7c512529
Tato <47118168+wtffidy@users.noreply.github.com>
2022-03-19 13:30:32 -0400

    Create WeaponImprint.cs

---
+++ Other/Farming Materials/WeaponImprint.cs
@@ -0,0 +1,37 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreDailys.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+
+using RBot;
+
+public class WeaponImprint
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreStory Story = new CoreStory();
+    public CoreDailys Dailys = new CoreDailys();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetWeaponImprint();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetWeaponImprint(int Imprintquant = 15)
+    {
+        if (Core.CheckInventory("weapon imprint", Imprintquant))
+            return;
+
+        Core.AddDrop("weapon imprint");
+
+        if (!Core.CheckInventory("weapon imprint", Imprintquant))
+            Adv.KillUltra("doomvaultb", "r26", "Left", "Undead Raxgore", "Weapon Imprint", Imprintquant, false);
+    }
+}
