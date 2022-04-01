d9449001aa9a42bd5fc2046684d7d6b325da8412
Tato <47118168+wtffidy@users.noreply.github.com>
2022-04-01 12:56:34 -0400

    magic (idk if the drops will work but i took it from an old dm with exe)

--- Seasonal/AutoAttackGiantChickencow.cs
+++ Seasonal/AutoAttackGiantChickencow.cs
@@ -1,6 +1,7 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
 using RBot;
 
 public class AAGiantChickenCow
@@ -8,6 +9,7 @@ public class AAGiantChickenCow
     public CoreBots Core => CoreBots.Instance;
     public ScriptInterface Bot => ScriptInterface.Instance;
     public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreStory Story = new CoreStory();
 
     public readonly int[] SkillOrder = { 2, 4, 3, 1 };
 
@@ -22,14 +24,24 @@ public class AAGiantChickenCow
 
     private void KFC()
     {
-        Core.Join("battleontown", "r9", "Right");
-        Bot.Player.SetSpawnPoint();
-        while (!Bot.ShouldExit())
+        Core.AddDrop(Core.EnsureLoad(8605).Rewards.Select(x => x.Name).ToArray());
+
+
+        if (!Story.QuestProgression(8603))
         {
-            if (Bot.Map.Name == "battleontown" && Bot.Player.Cell == "r9" && Bot.Player.Alive)
-                foreach (var Skill in SkillOrder)
-                    if (Bot.Player.CanUseSkill(Skill))
-                        Bot.Player.UseSkill(Skill);
+            Core.EnsureAccept(8603);
+            Core.GetMapItem(10031, 1, "battleontown");
+            Core.EnsureComplete(8603);
         }
+        if (!Core.CheckInventory(Core.EnsureLoad(8605).Rewards.Select(x => x.Name).ToArray()))
+        {
+            Core.EnsureAccept(8605);
+            Core.Join("battleontown", "r9", "Right");
+            Bot.Player.SetSpawnPoint();
+
+            Adv.BoostKillMonster("battleontown", "r9", "Right", "Giant ChickenCow", "Flaming Feather", 25);
+            Core.EnsureComplete(8605);
+        }
+
     }
 }
