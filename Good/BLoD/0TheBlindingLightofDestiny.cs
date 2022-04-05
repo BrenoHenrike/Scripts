e85508265c744cf7994e827a06d87934739e4e42
Tato <47118168+wtffidy@users.noreply.github.com>
2022-04-05 09:38:29 -0400

    not sure which quest id to use from wolfwing so when one ya'all get around to it please plug in an update quest

--- Good/BLoD/0TheBlindingLightofDestiny.cs
+++ Good/BLoD/0TheBlindingLightofDestiny.cs
@@ -3,6 +3,7 @@
 //cs_include Scripts/CoreDailies.cs
 //cs_include Scripts/CoreStory.cs
 //cs_include Scripts/Good/BLoD/CoreBLOD.cs
+//cs_include Scripts/Story/LordsofChaos/Core13LoC.cs
 using RBot;
 
 public class TheBlindingLightofDestiny
@@ -12,11 +13,13 @@ public class TheBlindingLightofDestiny
     public CoreBots Core => CoreBots.Instance;
     public CoreBLOD BLOD = new CoreBLOD();
     public CoreStory Story = new CoreStory();
+    public Core13LoC LOC => new Core13LoC();
 
     public void ScriptMain(ScriptInterface bot)
     {
         Core.SetOptions();
 
+        LOC.Wolfwing();
         BLOD.DoAll();
 
         Core.SetOptions(false);
