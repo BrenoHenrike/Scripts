79801f3be11d5ede6a82786de6cc1b82e4b6a200
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-04-05 12:59:29 +0200

    Added UnlockMinecrafting

--- Dailies/MineCrafting.cs
+++ Dailies/MineCrafting.cs
@@ -1,5 +1,6 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/Good/BLoD/CoreBLOD.cs
 using RBot;
 
 public class MineCrafting
@@ -7,11 +8,13 @@ public class MineCrafting
     public ScriptInterface Bot => ScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreDailies Daily = new();
+    public CoreBLOD BLOD = new();
 
     public void ScriptMain(ScriptInterface bot)
     {
         Core.SetOptions();
 
+        BLOD.UnlockMineCrafting();
         Daily.MineCrafting();
 
         Core.SetOptions(false);
