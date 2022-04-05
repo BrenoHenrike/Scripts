b2da1395d68d2391cf15a9350d35ca2ac6050e58
Vladimir <67055046+VladimirBienvenue@users.noreply.github.com>
2022-04-05 09:19:22 -0700

    Update 0TheBlindingLightofDestiny.cs

--- Good/BLoD/0TheBlindingLightofDestiny.cs
+++ Good/BLoD/0TheBlindingLightofDestiny.cs
@@ -2,6 +2,7 @@
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreDailies.cs
 //cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/Good/BLoD/CoreBLOD.cs
 //cs_include Scripts/Story/LordsofChaos/Core13LoC.cs
 using RBot;
@@ -13,6 +14,7 @@ public class TheBlindingLightofDestiny
     public CoreBots Core => CoreBots.Instance;
     public CoreBLOD BLOD = new CoreBLOD();
     public CoreStory Story = new CoreStory();
+    public CoreAdvanced Adv = new();
     public Core13LoC LOC => new Core13LoC();
 
     public void ScriptMain(ScriptInterface bot)
