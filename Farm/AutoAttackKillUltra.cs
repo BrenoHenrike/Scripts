092f74282ac37488fbd8e0d6ee7128c8b8551efb
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-04-06 10:23:46 +0200

    Nullhandling + changed HP prop sort to MaxHP

--- Farm/AutoAttackKillUltra.cs
+++ Farm/AutoAttackKillUltra.cs
@@ -2,15 +2,23 @@
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 using RBot;
+using RBot.Monsters;
 
 public class AAKillUltra
 {
     public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
     public CoreAdvanced Adv = new();
 
     public void ScriptMain(ScriptInterface bot)
     {
+        Monster? Target = Bot.Monsters.CurrentMonsters.MaxBy(x => x.MaxHP);
+        if (Target == null)
+        {
+            Core.Logger("No monsters found", messageBox: true);
+            return;
+        }
         while (!Bot.ShouldExit())
-            Adv.KillUltra(Bot.Map.Name, Bot.Player.Cell, Bot.Player.Pad, Bot.Monsters.CurrentMonsters.MaxBy(x => x.HP).Name);
+            Adv.KillUltra(Bot.Map.Name, Bot.Player.Cell, Bot.Player.Pad, Target.Name);
     }
 }
