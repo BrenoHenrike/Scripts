d92b9ec3af0f299145076f8cb643255745524554
Tato <47118168+wtffidy@users.noreply.github.com>
2022-04-07 10:47:45 -0400

    100k > 500k 12.5m :sleepy:

--- Nulgath/AFDL/EnoughDOOMforanArchfiend.cs
+++ Nulgath/AFDL/EnoughDOOMforanArchfiend.cs
@@ -57,11 +57,19 @@ public class EnoughDOOMforanArchfiend
 
         Core.BuyItem("digitalyulgar", 16, "Aelita's Emerald");
 
+        // while (!Core.CheckInventory("Essence Potion", 5))
+        // {
+        // Core.HuntMonster("orecavern", "Deathmole", "Arashtite Ore", 2, false);
+        // Core.HuntMonster("deathsrealm", "Skeleton Fighter", "Necrot", 2, false);
+        // Farm.AlchemyPacket("Necrot", "Arashtite Ore", AlchemyRunes.Uruz); //alchemy packets need fixed
+        // Bot.Player.Pickup("Essence Potion");
+        // }
+
+
         if (!Core.CheckInventory("Essence Potion", 5))
         {
-            // Farm.AlchemyPacket("Necrot", "Arashtite Ore", AlchemyRunes.Uruz); //alchemy packets need fixed
-            Farm.Gold(2500000);
-            Core.BuyItem("alchemyacademy", 395, "Gold Voucher 100k", 25);
+            Farm.Gold(12500000);
+            Core.BuyItem("alchemyacademy", 2115, "Gold Voucher 500k", 25);
             Core.BuyItem("alchemyacademy", 2115, "Essence Potion", 5, 1, 9770);
         }
         Bot.Wait.ForDrop("Essence Potion");
