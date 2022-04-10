9b2f9325ae06c7d9df978b64587cfe253b7a9852
Vladimir <67055046+VladimirBienvenue@users.noreply.github.com>
2022-04-10 11:02:35 -0700

    LVLQuickto100> Added Core.ActionDelay to it to not spam server with packets.

--- Farm/LVLQuickto100.cs
+++ Farm/LVLQuickto100.cs
@@ -35,6 +35,7 @@ public class LVLQuick
             bot.Player.UseSkill(4);
             bot.Player.UseSkill(3);
             bot.Player.UseSkill(2);
+            bot.Sleep(Core.ActionDelay);
         }
 
         Core.SetOptions(false);
