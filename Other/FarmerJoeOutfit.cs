ebfc6c5c8a6270b265d5185c3c14412bd345c0ea
Tato <47118168+wtffidy@users.noreply.github.com>
2022-04-10 14:11:58 -0400

    FarmerJoe: Re-enabled Scythe, the "servers are down" is a cape... good cape to

--- Other/FarmerJoeOutfit.cs
+++ Other/FarmerJoeOutfit.cs
@@ -14,14 +14,14 @@ public class FarmerJoeOutfit
     {
         Core.SetOptions();
 
-        // scythe.GetHBReapersScythe();
+        scythe.GetHBReapersScythe();
         RagsandHat();
         ServersAreDown();
         // DeathsScythe();
         // BackItem();
         // Pet();
 
-        Core.Equip(new[] { "Peasant Rags", "Scarecrow Hat", "The Server is Down"});
+        Core.Equip(new[] { "Peasant Rags", "Scarecrow Hat", "The Server is Down", "Hollowborn Reaper's Scythe"});
 
         Core.Logger("We are farmers, bum ba dum bum bum bum bum", messageBox: true);
 
@@ -32,9 +32,13 @@ public class FarmerJoeOutfit
     {
         if (Core.CheckInventory("Peasant Rags") | Core.CheckInventory("Scarecrow Hat"))
             return;
-
-        Core.BuyItem("battlleontown", 41, "Peasant Rags");
-        Core.BuyItem("battlleontown", 16, "Scarecrow Hat");
+        Core.Join("digitalyulgar");
+        Bot.Shops.BuyItem(41, "Peasant Rags");
+        Bot.Wait.ForPickup("Peasant Rags");
+        Bot.Shops.BuyItem(16, "Scarecrow Hat");
+        Bot.Wait.ForPickup("Scarecrow Hat");
+        // Core.BuyItem("digitalyulgar", 41, "Peasant Rags");
+        // Core.BuyItem("digitalyulgar", 16, "Scarecrow Hat");
     }
 
     public void ServersAreDown()
