967c4881fa91aee5a1fa6b4346497007299a751f
Riicii <103598329+Riicii@users.noreply.github.com>
2022-04-14 01:58:12 +0800

    Release Archfiend Dragon Egg

---
+++ Nulgath/Various/ArchfiendDragonEgg.cs
@@ -0,0 +1,46 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Hollowborn/CoreHollowborn.cs
+using RBot;
+
+public class ArchfiendDragonEgg
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreStory Story = new CoreStory();
+    public CoreDailies Daily = new();
+    public CoreHollowborn HB = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetAFDE();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetAFDE()
+    {
+        if (Core.CheckInventory("ArchFiend Baby Dragon Pet"))
+            return;
+
+        Core.AddDrop("ArchFiend Baby Dragon Pet");
+
+        //Your code here
+        Core.EnsureAccept(7296);
+        Core.BuyItem("Airstorm", 357, "Breath of life");
+        Core.HuntMonster("queenspire", "Fire Guardian Dragon", "Fire Guardian Dragon Soul", isTemp: false);
+        if (!Core.CheckInventory("Fresh Soul", 10) & !Core.CheckInventory("Unidentified 36"))
+            HB.FreshSouls(10);
+        Core.HuntMonster("Underlair", "ArchFiend DragonLord", "Fiendish Brimstone");
+        Core.BuyItem("Ariapet", 12, "ArchFiend Dragon Egg");
+        Core.EnsureComplete(7296);
+        Bot.Wait.ForPickup("ArchFiend Baby Dragon Pet");
+    }
+}
