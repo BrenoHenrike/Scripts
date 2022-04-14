8f25167e23ed8653816363b108a33b713a516c43
Tato <47118168+wtffidy@users.noreply.github.com>
2022-04-14 13:43:22 -0400

    Release: Nulgath\Various\GoldenHanzoVoid.cs

---
+++ Nulgath/Various/GoldenHanzoVoid.cs
@@ -0,0 +1,42 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Nulgath/CoreNulgath.cs
+
+using RBot;
+public class GoldenHanzoVoid
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreStory Story = new CoreStory();
+    public CoreDailies Daily = new();
+    public CoreNulgath Nulgath = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetGHV();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetGHV()
+    {
+        if (Core.CheckInventory("Golden Hanzo Void"))
+            return;
+
+        Nulgath.ApprovalAndFavor(50, 200);
+        Farm.BattleGroundE(100000);
+        Nulgath.SwindleBulk(30);
+        Nulgath.TheAssistant("Dark Crystal Shard", 15);
+        Nulgath.FarmDiamondofNulgath(50);
+        Core.BuyItem("evilwarnul", 456, "Golden Hanzo Void");
+        Bot.Wait.ForPickup("Golden Hanzo Void");
+    }
+}
+
