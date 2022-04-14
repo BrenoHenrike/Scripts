49434f82dd3c433d8fefb7896f9464a54519f8d1
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-04-14 14:51:13 +0200

    TowerOfDoom: Optimized Code with new findings

--- Story/TowerOfDoom.cs
+++ Story/TowerOfDoom.cs
@@ -18,30 +18,20 @@ public class TowerOfDoom
 
     public void TowerProgress(int Floor = 10)
     {
-        string Map = Floor == 1 ? "towerofdoom" : "towerofdoom" + Floor.ToString();
-        Bot.SendPacket($"%xt%zm%cmd%{Bot.Map.RoomID}%tfer%{Bot.Player.Username}%{Map}%");
-        Bot.Wait.ForMapLoad($"towerofdoom{Floor}");
-        if (Bot.Player.Cell == "Prison")
-        {
-            Core.Logger($"Tower of Doom {Floor - 1} not completed yet, doing that first");
-            TowerProgress(Floor - 1);
-        }
-        if (Bot.Map.Name.ToLower() != Map)
-        {
-            Bot.SendPacket($"%xt%zm%cmd%{Bot.Map.RoomID}%tfer%{Bot.Player.Username}%{Map}%");
-            Bot.Wait.ForMapLoad($"towerofdoom{Floor}");
-        }
-        Bot.Player.Jump("r10", "Left");
-        if (Bot.Player.Cell == "r10")
-        {
-            Core.Logger($"Tower of Doom {Floor} already complete");
+        if (!Bot.Quests.QuestTree.Any(x => x.ID == 3474 + Floor))
+            Bot.Quests.Load(3475, 3476, 3477, 3478, 3479, 3480, 3481, 3482, 3483, 3484);
+        if (Core.isCompletedBefore(3474 + Floor))
             return;
-        }
+
+        if (Floor != 1 && !Core.isCompletedBefore(3474 + (Floor - 1)))
+            TowerProgress(Floor - 1);
+
+        Bot.Quests.UpdateQuest(3474 + Floor);
         Core.Logger($"Tower of Doom {Floor}");
-        Bot.SendClientPacket($"{{\"t\":\"xt\",\"b\":{{\"r\":-1,\"o\":{{\"cmd\":\"updateQuest\",\"iValue\":{Floor},\"iIndex\":159}}}}}}", "json");
+
         Core.EquipClass(ClassType.Solo);
         Core.EnsureAccept(3474 + Floor);
-        Core.KillMonster(Map, "r10", "Left", "*", publicRoom: true);
+        Core.KillMonster(Floor == 1 ? "towerofdoom" : "towerofdoom" + Floor.ToString(), "r10", "Left", "*", publicRoom: true);
         Core.EnsureComplete(3474 + Floor);
     }
 }
