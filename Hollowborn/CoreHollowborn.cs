f3186906575787d4f4bc20392d26d02167365319
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-04-14 10:05:08 +0200

    CoreHB: Formatting, -Redundancies, Fixed Fresh Soul

--- Hollowborn/CoreHollowborn.cs
+++ Hollowborn/CoreHollowborn.cs
@@ -13,11 +13,13 @@ public class CoreHollowborn
     {
         if (Core.CheckInventory("Lae\'s Hardcore Contract"))
             return;
+
+        Core.AddDrop("Human Soul", "Fallen Soul", "Lae\'s Hardcore Contract");
         Farm.IcestormArena(65);
 
-        Core.AddDrop("Soul Potion", "Dried Slime", "Arashtite Ore", "Human Soul", "Fallen Soul", "Lae\'s Hardcore Contract");
         Core.Logger("Getting Lae's Hardcore Contract");
         Core.EnsureAccept(7556);
+
         if (!Core.CheckInventory("Soul Potion"))
         {
             Farm.Gold(2500000);
@@ -25,9 +27,12 @@ public class CoreHollowborn
             Core.BuyItem("alchemyacademy", 2036, "Soul Potion");
             Bot.Wait.ForItemBuy();
         }
+
         HumanSoul(50);
+
         Core.EquipClass(ClassType.Solo);
         Core.HuntMonster("doomwood", "Undead Paladin", "Fallen Soul", 13, false);
+
         Core.EnsureComplete(7556);
     }
 
@@ -35,7 +40,9 @@ public class CoreHollowborn
     {
         if (Core.CheckInventory("Human Soul", quant))
             return;
+
         Core.AddDrop("Human Soul");
+
         Core.EquipClass(ClassType.Farm);
         Core.KillMonster("noxustower", "r14", "Left", "*", "Human Soul", quant, false);
     }
@@ -46,13 +53,14 @@ public class CoreHollowborn
             return;
 
         Core.AddDrop("Fresh Soul", "Unidentified 36");
-
         Farm.Experience(50);
+
         Core.Logger($"Farming x{quant} Fresh Soul");
+
         while (!Core.CheckInventory("Fresh Soul", quant))
         {
             Core.EnsureAccept(7293);
-            Core.HuntMonster("citadel", "Inquisitor Guard", "Fresh Soul?", 10, log: false);
+            Core.HuntMonster("citadel", "Inquisitor Guard", "Fresh Soul", 10, log: false);
             Core.EnsureComplete(7293);
             Bot.Wait.ForPickup("Fresh Soul");
         }
