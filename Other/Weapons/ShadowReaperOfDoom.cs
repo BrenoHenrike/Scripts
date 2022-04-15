1b6698b58746683c17875a1bc8a6b132bd95bfee
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-04-15 14:14:54 +0200

    ShadowReaperOfDoom: Now equips Farm and SOlo

--- Other/Weapons/ShadowReaperOfDoom.cs
+++ Other/Weapons/ShadowReaperOfDoom.cs
@@ -19,9 +19,15 @@ public class SRoD
     {
         if (Core.CheckInventory("ShadowReaper Of Doom"))
             return;
+
         Core.Logger("Farming for ShadowReaper Of Doom");
+
+        Core.EquipClass(ClassType.Farm);
         Core.KillMonster("overworld", "r2", "Up", "*", "Mirror Realm Token", 300, false);
+
+        Core.EquipClass(ClassType.Solo);
         Core.KillMonster("overworld", "boss1", "Left", "Undead Artix", "Undead Paladin Token", isTemp: false);
+
         Core.BuyItem("overworld", 618, 17488);
     }
 }
