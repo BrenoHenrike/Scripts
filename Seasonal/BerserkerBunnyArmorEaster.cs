98274af36e57ef7e798750b12768cd0808e8a637
Tato <47118168+wtffidy@users.noreply.github.com>
2022-04-16 13:36:25 -0400

    Seasonal\BerserkerBunnyArmorEaster released fore Easter? idk what its used for yet will probably edit when it is.

---
+++ Seasonal/BerserkerBunnyArmorEaster.cs
@@ -0,0 +1,43 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+using RBot;
+
+public class BerserkerBunnyArmorEaster
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreStory Story = new CoreStory();
+    public CoreDailies Daily = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetBBA();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetBBA()
+    {
+        if (Core.CheckInventory("Berserker Bunny Armor"))
+            return;
+
+        if (!Core.CheckInventory("Berserker Bunny Armor"))
+        {
+            Core.EnsureAccept(236);
+            Core.HuntMonster("greenguardwest", "Big Bad Boar", "Were Egg", log: false);
+            Core.EnsureComplete(236);
+            Bot.Wait.ForPickup("Berserker Bunny Armor")
+            return;
+        }
+    }
+}
+
+
+
