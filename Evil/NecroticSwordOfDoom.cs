fd9cdddf19f776cd9318f0822a09296febdb6c58
Vladimir <vladimirbienvenue@gmail.com>
2022-04-18 11:07:04 -0700

    NSoD> Reverted accidental default value swap.

--- Evil/NecroticSwordOfDoom.cs
+++ Evil/NecroticSwordOfDoom.cs
@@ -15,12 +15,12 @@ public class NecroticSwordOfDoom
     // True = Farms each boss for 100 essence in "Retreive Void Auras"
     // False = Farms each boss for 20 essence in "Retreive Void Auras"
     // Recommended: true
-    private bool MaxStack = false;
+    private bool MaxStack = true;
     // [Can Change]
     // True = Bot will try to keep your inventory as empty as possible by farming each individual piece one by one when they are needed in the merge shop.
     // False = Bot will farm all items first and then merge them all. Best used if you want overview of how far you are. And will also be faster because less /join's
     // Recommended: false
-    private bool OptimizeInv = false;
+    private bool OptimizeInv = true;
     private int EssenceQuantity;
 
     public ScriptInterface Bot => ScriptInterface.Instance;
