300e70daf56da1247d77094487f79074cb84dd93
DQ4443 <93507257+DQ4443@users.noreply.github.com>
2022-04-18 11:32:13 -0700

    Add files via upload

---
+++ DarkonGarden.cs
@@ -0,0 +1,58 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using RBot;
+public class DarkonGarden
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new CoreStory();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        StoryLine();
+
+        Core.SetOptions(false);
+    }
+
+    public void StoryLine()
+    {
+        if (Core.isCompletedBefore(7494))
+            return;
+
+        Story.PreLoad();
+
+        //Picking Flowers 7485
+        Story.MapItemQuest(7485, "garden", 7324, 10);
+
+        //Bunny Food 7486
+        Story.KillQuest(7486, "garden", "Creature 343");
+
+        //Feed the Bunnies 7487
+        Story.MapItemQuest(7487, "garden", 7325, 10);
+
+        //Control the Bunnies 7488
+        Story.KillQuest(7488, "garden", "Creature 83");
+
+        //Time for Tea 7489
+        Story.KillQuest(7489, "garden", "Creature 343");
+
+        //Needs Somethinig Else... 7490
+        Story.KillQuest(7490, "garden", "Creature 72");
+
+        //Snacktime 7491
+        Story.KillQuest(7491, "garden", "Creature 83");
+
+        //Big, MEAN Bunnies 7492
+        Story.KillQuest(7492, "garden", "Creature 35");
+        Story.MapItemQuest(7492, "garden", 7326);
+
+        //What Crack? 7493
+        Story.MapItemQuest(7493, "garden", 7327);
+
+        //OMG What is THAT? 7494
+        Story.KillQuest(7494, "garden", "Creature 12");
+    }
+}
---
+++ TimeinnStory.cs
@@ -0,0 +1,47 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using RBot;
+public class TimeinnSTory
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new CoreStory();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        StoryLine();
+
+        Core.SetOptions(false);
+
+    }
+
+    public void StoryLine()
+    {
+        if (Core.isCompletedBefore(8151))
+            return;
+
+        Story.PreLoad();
+
+        //Unlocking the Antechamber 8146
+        Story.KillQuest(8146, "timeinn", new[] { "Fire Elemental", "Ice Elemental" });
+
+        //Ezrajal 8147
+        Story.KillQuest(8147, "timeinn", "Ezrajal");
+
+        //Unlocking the Reliquary 8148
+        Story.KillQuest(8147, "timeinn", new[] { "Nature Elemental", "Wind Elemental" });
+        
+        //The Warden 8149
+        Story.KillQuest(8147, "timeinn", "The Warden");
+        
+        //Unlocking the Apex 8150
+        Story.KillQuest(8150, "timeinn", new[] { "Energy Elemental", "Water Elemental" });
+        
+        //The Engineer 8151
+        Story.KillQuest(8151, "timeinn", "The Engineer");
+        
+    }
+}
