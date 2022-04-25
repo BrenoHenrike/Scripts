2ff121fb86b3222f4cb34d04082802e3342eabbb
NotNovar <104199716+NotNovar@users.noreply.github.com>
2022-04-25 17:10:53 +0200

    ChronoAssassin[Mem]

--- Other/Classes/Members-CLasses/ChronoAssassin[Mem].cs
+++ Other/Classes/Members-CLasses/ChronoAssassin[Mem].cs
@@ -1,4 +1,7 @@
 //cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreStory.cs
 //cs_include Scripts/CoreAdvanced.cs
 using RBot;
 
@@ -6,7 +9,10 @@ public class ChronoAssassin
 {
     public ScriptInterface Bot => ScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
     public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreStory Story = new CoreStory();
+    public CoreDailies Daily = new();
 
     public void ScriptMain(ScriptInterface bot)
     {
