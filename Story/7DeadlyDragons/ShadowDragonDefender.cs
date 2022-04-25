69a1386f85c78669a7f5561b218d7df4a6989f21
Tato <47118168+wtffidy@users.noreply.github.com>
2022-04-25 10:42:45 -0400

    7DD - SDD - made 6908 a quest prog. as there are multiple "void energy"

--- Story/7DeadlyDragons/ShadowDragonDefender.cs
+++ Story/7DeadlyDragons/ShadowDragonDefender.cs
@@ -38,7 +38,12 @@ public class GetSDD
             Story.KillQuest(6905, "crossroads", "Koalion");
             Story.KillQuest(6906, "mountain", "Giant Scorpion");
             Story.MapItemQuest(6907, "void", 6453);
-            Story.KillQuest(6908, "void", "Void Elemental");
+            if (!Story.QuestProgression(6908))
+            {
+                Core.EnsureAccept(6908);
+                Core.HuntMonster("void", "Void Elemental", "Void Energy", 8);
+                Core.EnsureAccept(6908);
+            }
             Story.MapItemQuest(6909, "void", 6454);
             Story.KillQuest(6910, "void", "Void Bear");
             Story.KillQuest(6911, "void", "Void Elemental");
