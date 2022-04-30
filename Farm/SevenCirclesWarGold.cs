c97dca5c0b95f822883671e5954032854957ecf8
SElune7 <elune769@gmail.com>
2022-04-30 15:12:08 +1000

    Create SevenCirclesWarGold.cs
    Added an alternative SCW Gold for people who don't open the script and read. >.<

---
+++ Farm/SevenCirclesWarGold.cs
@@ -0,0 +1,31 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/Legion/SevenCircles(War).cs
+using RBot;
+
+public class SevenCirclesWarXP
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    public CoreAdvanced Adv = new();
+    public SevenCircles SC = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        SC.Circles();
+
+        Adv.BestGear(GearBoost.exp);
+        Adv.BestGear(GearBoost.gold);
+        //Farm.UseBoost(ChangeToBoostID, RBot.Items.BoostType.Experience, true);
+        // Change the level to 101 so it will run until you stop it. - (101, 100000000)
+        
+        Farm.SevenCirclesWar(101, 100000000);
+
+        Core.SetOptions(false);
+    }
+}
