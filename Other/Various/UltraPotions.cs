cd6ebdf8aaaf9fee0d19999d8ce7f122db2c28ee
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-05-09 10:53:51 +0200

    UltraPotions: Nullability things

--- Other/Various/UltraPotions.cs
+++ Other/Various/UltraPotions.cs
@@ -24,7 +24,7 @@ public class PotionBuyer
     }
 
 
-    public void INeedYourStrongestPotions(int potionQuant = 50, List<string> potions = null)
+    public void INeedYourStrongestPotions(int potionQuant = 50, List<string>? potions = null)
     {
         Farm.AlchemyREP();
         Core.Logger($"{Bot.Player.Username}: Hello potion seller, I’m going into battle and I want your strongest potions");
@@ -34,7 +34,7 @@ public class PotionBuyer
         Core.Logger($"Potion Seller: You can’t handle my potions, they are too strong for you");
         if (ShopCache.Loaded.Any(s => s.ID == 2036))
         {
-            shopItems = ShopCache.Loaded.Find(s => s.ID == 2036).Items;
+            shopItems = ShopCache.Loaded.First(s => s.ID == 2036).Items;
         }
         else
         {
@@ -44,20 +44,20 @@ public class PotionBuyer
         }
         Core.Logger($"Potion Seller: My potions are too strong for you, traveller");
         Bot.Sleep(2500);
-        Core.Logger($"{Bot.Player.Username}: Potion seller I tell you I’m going into battle and I want only your strongest potions"); 
+        Core.Logger($"{Bot.Player.Username}: Potion seller I tell you I’m going into battle and I want only your strongest potions");
         Bot.Sleep(2500);
         Core.Logger($"Potion Seller: You can’t handle my potions, they are too strong for you");
         Bot.Sleep(2500);
-        Core.Logger($"{Bot.Player.Username}: Potion seller, listen to me, I want only your strongest potions"); 
+        Core.Logger($"{Bot.Player.Username}: Potion seller, listen to me, I want only your strongest potions");
         Bot.Sleep(2500);
         Core.Logger($"Potion Seller: My potions would kill you traveller, you cannot handle my potions");
         Bot.Sleep(2500);
-        Core.Logger($"{Bot.Player.Username}: POTION SELLER i require your strongest potions"); 
+        Core.Logger($"{Bot.Player.Username}: POTION SELLER i require your strongest potions");
         Bot.Sleep(2500);
         Core.Logger($"Potion Seller: My strongest potions would kill you traveller, you can’t handle my strongest potions.You better go to a seller that sells weaker potions");
         foreach (string potion in potions)
         {
-            int voucherQuant = shopItems.Find(p => p.Name.ToLower() == potion.ToLower()).Requirements[0].Quantity * potionQuant;
+            int voucherQuant = shopItems.Where(p => p.Name.ToLower() == potion.ToLower()).First().Requirements[0].Quantity * potionQuant;
             while (!Core.CheckInventory(potion, potionQuant))
             {
                 if (!Core.CheckInventory("Gold Voucher 500k", voucherQuant))
