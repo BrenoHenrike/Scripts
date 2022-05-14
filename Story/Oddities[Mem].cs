c5ee53ab9a71709eb4435cae10667eb4ea85cb56
Tato <47118168+wtffidy@users.noreply.github.com>
2022-05-14 10:14:18 -0400

    oddities: remove farm include

--- Story/Oddities[Mem].cs
+++ Story/Oddities[Mem].cs
@@ -1,5 +1,4 @@
 //cs_include Scripts/CoreBots.cs
-//cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreStory.cs
 using RBot;
 
@@ -7,7 +6,6 @@ public class Oddities
 {
     public ScriptInterface Bot => ScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
-    public CoreFarms Farm = new();
     public CoreStory Story = new();
 
     public void ScriptMain(ScriptInterface bot)
