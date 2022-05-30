b65ba460a30d527a581e25ac81781e8da95fa1f3
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-05-30 18:32:49 +0200

    Converted 3 little wolves merge to new template

---
+++ Other/MergeShops/3LittleWolvesHousesMerge.cs
@@ -0,0 +1,104 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+using RBot;
+using RBot.Items;
+using RBot.Options;
+
+public class newMergeTemplate
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    public CoreStory Story = new();
+    public CoreAdvanced Adv = new();
+    public static CoreAdvanced sAdv = new();
+
+    public List<IOption> Options = sAdv.MergeOptions;
+    // [Can Change] This should only be changed by the author.
+    //              Just name this the same as the script (without the .cs)
+    public string OptionsStorage = "3LittleWolvesHousesMerge";
+    // [Can Change] This should only be changed by the author.
+    //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
+    private bool dontStopMissingIng = true;
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        BuyAllMerge();
+
+        Core.SetOptions(false);
+    }
+
+    public void BuyAllMerge()
+    {
+        //Only edit the map and shopID here
+        Adv.StartBuyAllMerge("buyHouse", 1729, findIngredients);
+
+        #region Dont edit this part
+        void findIngredients()
+        {
+            ItemBase req = Adv.externalItem;
+            int quant = Adv.externalQuant;
+            int currentQuant = req.Temp ? Bot.Inventory.GetTempQuantity(req.Name) : Bot.Inventory.GetQuantity(req.Name);
+            if (req == null)
+            {
+                Core.Logger("req is NULL");
+                return;
+            }
+
+            switch (req.Name)
+            {
+                default:
+                    bool shouldStop = Adv.matsOnly ? !dontStopMissingIng : true;
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping."), messageBox: shouldStop, stopBot: shouldStop);
+                    break;
+                #endregion
+
+                // Add how to get items here
+                case "Building Material":
+                    Core.RegisterQuests(6915);
+                    Core.Logger($"Farming {req.Name} ({currentQuant}/{quant})");
+                    while (!Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonster("farm", "Treeant", "Wooden Planks", 5);
+                        Core.HuntMonster("bloodtusk", "Rhison", "Glue");
+                        Core.HuntMonster("crashsite", "ProtoSartorium", "Nails", 10);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Foundation Material":
+                    Core.RegisterQuests(6916);
+                    Core.Logger($"Farming {req.Name} ({currentQuant}/{quant})");
+                    while (!Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonster("river", "Zardman Fisher", "River Stones", 5);
+                        Core.HuntMonster("dwarfprison", "Balboa", "Boulder", 3);
+                        Core.HuntMonster("dragonplane", "Earth Elemental", "Marble");
+                        Core.HuntMonster("gilead", "Fire Elemental", "Flames", 3);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Decor Material":
+                    Core.RegisterQuests(6917);
+                    Core.Logger($"Farming {req.Name} ({currentQuant}/{quant})");
+                    while (!Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonster("farm", "Scarecrow", "Fabric", 5);
+                        Core.HuntMonster("goose", "Can of Paint", "Paint", 5);
+                        Core.HuntMonster("undergroundlabb", "Window", "Glass", 5);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+            }
+        }
+    }
+}
+
