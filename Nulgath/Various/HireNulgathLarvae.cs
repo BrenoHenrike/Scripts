3ba85cad4eda06303a4f660f5ea2359081b02874
Tato <47118168+wtffidy@users.noreply.github.com>
2022-06-01 07:39:13 -0400

    Update HireNulgathLarvae.cs

--- Nulgath/Various/HireNulgathLarvae.cs
+++ Nulgath/Various/HireNulgathLarvae.cs
@@ -24,7 +24,6 @@ public class HireNulgathLarvae
         if (Core.CheckInventory("Nulgath Larvae"))
             return;
 
-        Nulgath.FarmVoucher(true);
         Nulgath.HireNulgathLarvae();
     }
 }
