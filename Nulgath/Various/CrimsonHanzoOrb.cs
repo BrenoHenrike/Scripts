1cc0fa8bbc70f642406723dcfaa0d8c1a2f19a28
Tato <47118168+wtffidy@users.noreply.github.com>
2022-06-02 13:01:12 -0400

    was told to just make it its own thing not inside nulgath.

--- Nulgath/Various/CrimsonHanzoOrb.cs
+++ Nulgath/Various/CrimsonHanzoOrb.cs
@@ -14,8 +14,32 @@ public class CrimsonHanzoOrbQuest
     {
         Core.SetOptions();
 
-        Nulgath.CrimsonHanzoOrb();
+        CrimsonHanzoOrb();
 
         Core.SetOptions(false);
+    }
+    
+    
+    public void CrimsonHanzoOrb()
+    {
+        if (Core.CheckInventory(Nulgath.bagDrops) && Core.CheckInventory("Blood Star Blade"))
+            return;
+
+        Core.AddDrop(Nulgath.bagDrops);
+        Core.AddDrop("Blood Star Blade");
+
+
+        Core.RegisterQuests(4019);
+        while (!Core.CheckInventory(Nulgath.bagDrops) && !Core.CheckInventory("Blood Star Blade"))
+        {
+            Core.HuntMonster("graveyard", "Big Jack Sprat", "Jacked Eye", 5);
+            Core.HuntMonster("marsh", "Dreadspider", "Dreadspider Silk");
+            Core.HuntMonster("nulgath", "Dreadspider", "Dreadspider Silk");
+            if (Core.IsMember)
+                Core.HuntMonster("nulgath", "Dark Makai", "Makai Fang", 5);
+            else Core.HuntMonster("tercessuinotlim", "Dark Makai", "Makai Fang", 5);
+            Core.HuntMonster("bludrut", "Rattlebones", "Rattle Bones", 3);
+        }
+        Core.CancelRegisteredQuests();
     }
 }
