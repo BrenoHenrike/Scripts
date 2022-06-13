54b396eef46235f30d70a56280649eab2939b392
Tato <47118168+wtffidy@users.noreply.github.com>
2022-06-13 14:48:04 -0400

    +Release CtrlAltDelMemberBadge

---
+++ Other/Badges/CtrlAltDelMemberBadge.cs
@@ -0,0 +1,78 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+using RBot;
+
+public class CtrlAltDelMemberBadge
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    public CoreStory Story = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Badge();
+
+        Core.SetOptions(false);
+    }
+
+    public void Badge()
+    {
+        if (!Core.IsMember)
+            return;
+
+        if (Core.isCompletedBefore(953))
+            return;
+
+        // Map - GameHavaen:
+        Core.EquipClass(ClassType.Farm);
+        
+        // Agree to help Ethan
+        Story.ChainQuest(940);
+        
+        // Clues for the Clueless
+        Story.MapItemQuest(941, "gamehaven", 267, 10);
+
+        // Laundry Day
+        Story.KillQuest(942, "gamehaven", "Evil Arcade Machine");
+
+        // Strategy Guides are Key
+        Story.KillQuest(943, "gamehaven", "Evil Console Machine|Evil Arcade Machine");
+
+        // Investigate the Storage Room
+        Story.MapItemQuest(944, "gamehaven", 269);
+
+        // Map - WareHouse: 
+
+        // Inventory In Your Inventory
+        Story.MapItemQuest(945, "WareHouse", 270, 10);
+
+        // Sneevil Sabotage
+        Story.KillQuest(946, "WareHouse", "Delivery Sneevil");
+
+        // A Bribe for the Bride
+        Story.KillQuest(947, "WareHouse", "Snapper Shrub");
+
+        // Hey There Lie-lah
+        Story.MapItemQuest(948, "WareHouse", 272);
+
+
+        // Map - Arcadion:
+        Core.EquipClass(ClassType.Solo);
+
+        // Fuel for Fought
+        Story.MapItemQuest(950, "arcadion", 271, 8);
+
+        // In Ctrl of Controllers
+        Story.KillQuest(951, "arcadion", "Megadude");
+
+        // To Form a Platform
+        Story.KillQuest(952, "arcadion", "Blue Hedgehog");
+
+        // The Infamous Plumber
+        Story.KillQuest(953, "arcadion", "Orc Plumber");
+    }
+}
