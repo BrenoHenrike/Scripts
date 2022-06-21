205b38bc27aa6c4f7eb8e9776b6031171f2ff3e4
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-06-21 21:50:25 +0200

    Released HighSeasCommander

---
+++ Other/Classes/HighSeasCommander.cs
@@ -0,0 +1,71 @@
+using System.Data;
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using RBot;
+
+public class HighSeasCommander
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetHSC();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetHSC()
+    {
+        if (Core.CheckInventory("HighSeas Commander"))
+            return;
+
+
+        if (!Core.HasAchievement(27, "ip14"))
+            Core.Logger("This bot requiers you to have an account of 10 years or older.", messageBox: true, stopBot: true);
+
+        UnlockFarm();
+
+        Core.Logger("Farming for HighSeas Commander");
+        Core.BuyItem(Bot.Map.Name, 1647, "Enchanted Pirate");
+        Core.AddDrop("HighSeas Commander");
+        Core.EnsureAccept(6921);
+        if (!Core.CheckInventory("Enchanted Token", 50))
+        {
+            Core.RegisterQuests(6922);
+            Core.Logger($"Farming Enchanted Tokens ({Bot.Inventory.GetQuantity("Enchanted Token")}/50)");
+
+            while (!Core.CheckInventory("Enchanted Token", 50))
+            {
+                Core.HuntMonster("natatorium", "Merdraconian", "Merdraconian Defeated", 5);
+                Core.HuntMonster("natatorium", "Anglerfish", "Anglerfish Defeated", 5);
+                Core.HuntMonster("natatorium", "Nessie", "Nessie Defeated");
+                Bot.Wait.ForPickup("Enchanted Token");
+            }
+            Core.CancelRegisteredQuests();
+        }
+
+        Core.EnsureAccept(6921);
+    }
+
+    public void UnlockFarm()
+    {
+        if (Core.isCompletedBefore(6920))
+            return;
+
+        Story.PreLoad();
+
+        //6918 | Sweep the Deck
+        Story.KillQuest(6918, "highseas", "Skeleton Crew");
+
+        //6919 | Troubles In Deep Waters
+        Story.KillQuest(6919, "highseas", "Pirate Crew");
+
+        //6920 | The Captain Wants a Word
+        Story.KillQuest(6918, "highseas", "Capt. Beard");
+
+    }
+}
