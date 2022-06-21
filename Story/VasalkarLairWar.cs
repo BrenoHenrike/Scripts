070a7798acec92a03240cca0a33bf6372b166ed5
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-06-21 18:13:07 +0200

    Released VasalkarLairWar

---
+++ Story/VasalkarLairWar.cs
@@ -0,0 +1,88 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using RBot;
+
+public class LairWar
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        doAll();
+
+        Core.SetOptions(false);
+    }
+
+    public void doAll()
+    {
+        if (Core.isCompletedBefore(6689) && Core.isCompletedBefore(6694))
+            return;
+
+        Defend();
+        Attack();
+    }
+
+    public void Defend()
+    {
+        if (Core.isCompletedBefore(6689))
+            return;
+
+        Story.PreLoad();
+
+        //6685 | Flamefang Medals
+        Core.RegisterQuests(6685, 6686);
+        if (!Story.QuestProgression(6685, GetReward: false))
+        {
+            Core.KillMonster("lairdefend", "Bridge", "LeftUp", "*", "Flamefang Medal", 3);
+            Bot.Wait.ForQuestComplete(6685);
+        }
+
+        //6686 | Mega Flamefang Medals
+        //Can be skipped, has the same quest value
+
+        //6687 | SoulSucking Enemy
+        Story.KillQuest(6687, "lairdefend", "Soul Wyvern", GetReward: false);
+
+        //6688 | The Dragon Summoner
+        Story.KillQuest(6688, "lairdefend", "Dragon Summoner", GetReward: false);
+
+        //6689 | The Flame Dragon
+        Story.KillQuest(6689, "lairdefend", "Flame Dragon General", GetReward: false);
+
+        Core.CancelRegisteredQuests();
+    }
+
+    public void Attack()
+    {
+        if (Core.isCompletedBefore(6694))
+            return;
+
+        Story.PreLoad();
+
+        //6690 | Defender Medals
+        Core.RegisterQuests(6690, 6691);
+        if (!Story.QuestProgression(6690, GetReward: false))
+        {
+            Core.KillMonster("lairattack", "Bridge", "LeftUp", "*", "Defender Medal", 3);
+            Bot.Wait.ForQuestComplete(6690);
+        }
+
+        //6691 | Mega Defender Medals
+        //Can be skipped, has the same quest value
+
+        //6692 | Magic and Mayhem
+        Story.KillQuest(6692, "lairattack", "Dracomancer", GetReward: false);
+
+        //6693 | The Dragon Defender
+        Story.KillQuest(6693, "lairattack", "Dragon Defender", GetReward: false);
+
+        //6694 | The Flame Dragon
+        Story.KillQuest(6694, "lairattack", "Flame Dragon General", GetReward: false);
+
+        Core.CancelRegisteredQuests();
+    }
+}
