c7e25a7f9a981c23b69376de08785c5afe63cf05
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-06-21 18:12:25 +0200

    Released VoidBattleMageSet

---
+++ Nulgath/Various/VoidBattleMageSet.cs
@@ -0,0 +1,71 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/VasalkarLairWar.cs
+using RBot;
+using RBot.Items;
+
+public class VoidBattleMageSet
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public LairWar War = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetSet();
+
+        Core.SetOptions(false);
+    }
+
+    private string[] rewards =
+    {
+        "Void BattleMage",
+        "Void BattleMage Stare",
+        "Void BattleMage Male Morph",
+        "Void BattleMage Male Hood",
+        "Void BattleMage Locks",
+        "Void BattleMage Female Morph",
+        "Void BattleMage Female Hood",
+        "Void BattleMage Male Crown",
+        "Void BattleMage Female Crown",
+        "Void BattleMage Crown",
+        "Void BattleMage Runes",
+        "Void BattleMage Wrap",
+        "Void BattleMage Wrap Runes",
+        "Void BattleMage Spear",
+        "Void BattleMage Nation Staff"
+    };
+
+    public void GetSet()
+    {
+        if (Core.CheckInventory(rewards))
+            return;
+
+        int count = 0;
+        Core.CheckSpaces(ref count, rewards);
+        Core.AddDrop(rewards);
+        War.Attack();
+        Core.RegisterQuests(6694);
+        Bot.Events.ItemDropped += ItemDropped;
+        Core.Logger($"Farm for Void BattleMage set started. Farming to get {rewards.Count() - count} more item" + ((rewards.Count() - count) > 1 ? "s" : ""));
+
+        while (!Core.CheckInventory(rewards))
+        {
+            Core.KillMonster("lairattack", "Eggs", "Left", "Flame Dragon General", log: false);
+            Bot.Wait.ForPickup("*");
+        }
+
+        Bot.Events.ItemDropped -= ItemDropped;
+
+        void ItemDropped(ScriptInterface bot, ItemBase item, bool addedToInv, int quantityNow)
+        {
+            if (rewards.Contains(item.Name))
+            {
+                count++;
+                Core.Logger($"Got {item.Name}, {rewards.Length - count} items to go");
+            }
+        }
+    }
+}
