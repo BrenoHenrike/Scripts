a1e6384b92c90bcc9617083eeebe5f8c48be5dc8
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-06-25 15:16:03 +0200

    Ancient Remnant, Mourning Flower and Unfinished Musical Score full release

--- Darkon/CoreDarkon.cs
+++ Darkon/CoreDarkon.cs
@@ -2,18 +2,18 @@
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/CoreStory.cs
-//cs_include Scripts/Darkon/CoreDarkon.cs
 //cs_include Scripts/Story/ElegyofMadness(Darkon)/CoreAstravia.cs
 using RBot;
+using RBot.Items;
 
 public class CoreDarkon
 {
     public ScriptInterface Bot => ScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
-    public CoreAdvanced Adv = new CoreAdvanced();
-    public CoreStory Story = new CoreStory();
-    public CoreFarms Farm = new CoreFarms();
-    public CoreAstravia Astravia => new CoreAstravia();
+    public CoreAdvanced Adv = new();
+    public CoreStory Story = new();
+    public CoreFarms Farm = new();
+    public CoreAstravia Astravia => new();
 
     public void ScriptMain(ScriptInterface bot)
     {
@@ -30,22 +30,22 @@ public class CoreDarkon
     }
 
     public void FirstErrand(int Quantity = 222)
+
     {
         if (Core.CheckInventory("Darkon's Receipt", Quantity))
             return;
 
         Core.AddDrop("Darkon's Receipt");
+        Core.FarmingLogger("Darkon's Receipt", Quantity);
         Core.EquipClass(ClassType.Farm);
 
+        Core.RegisterQuests(7324);
         while (!Bot.ShouldExit() && !Core.CheckInventory("Darkon's Receipt", Quantity))
         {
-            Core.EnsureAccept(7324);
-
             Core.KillMonster("portalmaze", "r8", "Left", "*", "Banana", 22, false, log: false);
-
-            Core.EnsureComplete(7324);
             Bot.Wait.ForPickup("Darkon's Receipt");
         }
+        Core.CancelRegisteredQuests();
     }
 
     public void SecondErrand(int Quantity = 222, bool escapeWhile = false)
@@ -55,11 +55,12 @@ public class CoreDarkon
 
         bool EnoughPeople = false;
         Core.AddDrop("Darkon's Receipt");
+        Core.FarmingLogger("Darkon's Receipt", Quantity);
         Core.EquipClass(ClassType.Solo);
-
         Bot.Quests.UpdateQuest(2954);
         Core.Join("doomvault", "r5", "Left", true);
 
+        Core.RegisterQuests(7325);
         while (!Bot.ShouldExit() && !Core.CheckInventory("Darkon's Receipt", Quantity))
         {
             if (Bot.Map.Name.ToLower() == "doomvault")
@@ -75,17 +76,18 @@ public class CoreDarkon
             }
 
             if (!EnoughPeople && !Core.IsMember && escapeWhile)
+            {
+                Core.CancelRegisteredQuests();
                 return;
-
-            Core.EnsureAccept(7325);
+            }
 
             if (!EnoughPeople && Core.IsMember)
                 Core.HuntMonster("ultravoid", "Ultra Kathool", "Ingredients?", 22, false, publicRoom: true, log: false);
             else Adv.KillUltra("doomvault", "r5", "Left", "Binky", "Ingredients?", 22, false, publicRoom: true, log: false);
 
-            Core.EnsureComplete(7325);
             Bot.Wait.ForPickup("Darkon's Receipt");
         }
+        Core.CancelRegisteredQuests();
     }
 
     public void ThirdErrand(int Quantity = 222)
@@ -94,19 +96,17 @@ public class CoreDarkon
             return;
 
         Core.AddDrop("Darkon's Receipt");
+        Core.FarmingLogger("Darkon's Receipt", Quantity);
         Core.EquipClass(ClassType.Solo);
 
+        Core.RegisterQuests(7326);
         while (!Bot.ShouldExit() && !Core.CheckInventory("Darkon's Receipt", Quantity))
         {
-            Core.EnsureAccept(7326);
-
             Core.HuntMonster("tercessuinotlim", "Nulgath", "Nulgath's mask", 1, false, publicRoom: true);
-
-            Core.EnsureComplete(7326);
             Bot.Wait.ForPickup("Darkon's Receipt");
         }
+        Core.CancelRegisteredQuests();
     }
-
 
     public void Teeth(int Quantity = 300)
     {
@@ -114,21 +114,20 @@ public class CoreDarkon
             return;
 
         Core.AddDrop("Teeth");
-
         Astravia.Eridani();
-
+        Core.FarmingLogger("Teeth", Quantity);
         Core.EquipClass(ClassType.Solo);
 
-        Core.Logger($"Farming {Quantity} Teeth");
+        Core.RegisterQuests(7780);
         while (!Bot.ShouldExit() && (!Core.CheckInventory("Teeth", Quantity)))
         {
-            Core.EnsureAccept(7780);
             Core.HuntMonster("eridani", "Wolf-Like Creature", "Tooth", 28, false, log: false);
             Bot.Options.AttackWithoutTarget = true;
             Core.KillMonster("eridani", "r4", "Left", "Creature 15", "Wisdom Tooth", 4, false, log: false);
             Bot.Options.AttackWithoutTarget = false;
-            Core.EnsureComplete(7780);
+            Bot.Wait.ForPickup("Teeth");
         }
+        Core.CancelRegisteredQuests();
     }
 
     public void LasGratitude(int Quantity = 300)
@@ -137,19 +136,18 @@ public class CoreDarkon
             return;
 
         Core.AddDrop("La's Gratitude");
-
         Astravia.Astravia();
-
+        Core.FarmingLogger("La's Gratitude", Quantity);
         Core.EquipClass(ClassType.Solo);
 
-        Core.Logger($"Farming {Quantity} La's Gratitude");
+        Core.RegisterQuests(8001);
         while (!Bot.ShouldExit() && (!Core.CheckInventory("La's Gratitude", Quantity)))
         {
-            Core.EnsureAccept(8001);
             Core.HuntMonster("astravia", "Creature 27", "Broken Dog Tag", 20, log: false);
             Core.HuntMonster("astravia", "Creature 27", "Intact Dog Tag", 5, log: false);
-            Core.EnsureComplete(8001);
+            Bot.Wait.ForPickup("La's Gratitude");
         }
+        Core.CancelRegisteredQuests();
     }
 
     public void AstravianMedal(int Quantity = 300)
@@ -158,14 +156,12 @@ public class CoreDarkon
             return;
 
         Core.AddDrop("Astravian Medal");
-
         Astravia.AstraviaCastle();
-        Core.Join("astraviacastle");
+        Core.FarmingLogger("Astravian Medal", Quantity);
 
-        Core.Logger($"Farming {Quantity} Astravian Medal");
+        Core.RegisterQuests(8257);
         while (!Bot.ShouldExit() && (!Core.CheckInventory("Astravian Medal", Quantity)))
         {
-            Core.EnsureAccept(8257);
             Core.EquipClass(ClassType.Farm);
             Bot.Options.AttackWithoutTarget = true;
             Core.HuntMonster("astraviacastle", "Creature 27", "Defaced Portrait", 10, log: false);
@@ -173,8 +169,9 @@ public class CoreDarkon
             Bot.Options.AttackWithoutTarget = false;
             Core.EquipClass(ClassType.Solo);
             Core.HuntMonster("astraviacastle", "The Sun", "Burned Banana", log: false);
-            Core.EnsureComplete(8257);
+            Bot.Wait.ForPickup("Astravian Medal");
         }
+        Core.CancelRegisteredQuests();
     }
 
     public void AMelody(int Quantity = 300)
@@ -183,18 +180,18 @@ public class CoreDarkon
             return;
 
         Core.AddDrop("A Melody");
-
         Astravia.AstraviaJudgement();
+        Core.FarmingLogger("A Melody", Quantity);
 
-        Core.Logger($"Farming {Quantity} A Melody");
+        Core.RegisterQuests(8369);
         while (!Bot.ShouldExit() && (!Core.CheckInventory("A Melody", Quantity)))
         {
-            Core.EnsureAccept(8396);
             Core.HuntMonster("astraviajudge", "Trumpeter", "Brass", 10, log: false);
             Core.HuntMonster("astraviajudge", "Hand", "Sinew", 10, log: false);
             Core.HuntMonster("astraviajudge", "La", "Knight's Favor", log: false);
-            Core.EnsureComplete(8396);
+            Bot.Wait.ForPickup("A Melody");
         }
+        Core.CancelRegisteredQuests();
     }
 
     public void BanditsCorrespondence(int Quantity = 3000)
@@ -203,22 +200,22 @@ public class CoreDarkon
             return;
 
         Core.AddDrop("Bandit's Correspondence");
-
         Astravia.EridaniPast();
+        Core.FarmingLogger("Bandit's Correspondence", Quantity);
+        Bot.Quests.UpdateQuest(8531); //attempted fix
+        Core.EquipClass(ClassType.Solo);
 
-        Core.Logger($"Farming {Quantity} Bandit's Correspondence");
-        while (!Bot.ShouldExit() && (!Core.CheckInventory("Bandit's Correspondence", Quantity)))
+        Core.RegisterQuests(8531);
+        while (!Bot.ShouldExit() && !Core.CheckInventory("Bandit's Correspondence", Quantity))
         {
-            Bot.Quests.UpdateQuest(8531); //attempted fix
-            Core.EnsureAccept(8531);
-            Core.EquipClass(ClassType.Solo);
             Core.HuntMonster("eridanipast", "Bandit", "Bandit Contraband", 12, log: false);
             Bot.Options.AttackWithoutTarget = true;
             Core.KillMonster("eridanipast", "r10", "Left", "Suki", "Seraphic Sparred", log: false);
             Bot.Options.AttackWithoutTarget = false;
             Core.HuntMonster("eridanipast", "Dog", "Dogs Confiscated", 12, log: false);
-            Core.EnsureComplete(8531);
+            Bot.Wait.ForPickup("Bandit's Correspondence");
         }
+        Core.CancelRegisteredQuests();
     }
 
     public void SukisPrestiege(int Quantity = 300)
@@ -228,18 +225,75 @@ public class CoreDarkon
 
         Core.AddDrop("Suki's Prestige");
         Astravia.CompleteCoreAstravia();
+        Core.FarmingLogger("Suki's Prestige", Quantity);
 
-        Core.Logger($"Farming {Quantity} Suki's Prestige");
+        Core.RegisterQuests(8602);
         while (!Bot.ShouldExit() && (!Core.CheckInventory("Suki's Prestige", Quantity)))
         {
-            Core.EnsureAccept(8602);
             Core.EquipClass(ClassType.Solo);
             Core.HuntMonster("astraviapast", "Regulus", "Regulus' Rematch Won", log: false);
             Core.HuntMonster("astraviapast", "Titania", "Titania's Rematch Won", log: false);
             Core.HuntMonster("astraviapast", "Aurola", "Aurola's Rematch Won", log: false);
             Core.EquipClass(ClassType.Farm);
             Core.HuntMonster("astraviapast", "Astravian Soldier", "Soldiers Trained", 8, log: false);
-            Core.EnsureComplete(8602);
+            Bot.Wait.ForPickup("Suki's Prestige");
         }
+        Core.CancelRegisteredQuests();
+    }
+
+    public void AncientRemnant(int Quantity = 300)
+    {
+        if (Core.CheckInventory("Ancient Remnant", Quantity))
+            return;
+
+        Core.AddDrop("Ancient Remnant");
+        Astravia.FirstObservatory();
+        Core.FarmingLogger("Ancient Remnant", Quantity);
+
+        Core.RegisterQuests(8641);
+        while (!Bot.ShouldExit() && !Core.CheckInventory("Ancient Remnant", Quantity))
+        {
+            Core.EquipClass(ClassType.Farm);
+            Core.HuntMonster("firstobservatory", "Ancient Creature", "Creature Samples", 6);
+            Core.HuntMonster("firstobservatory", "Ancient Turret", "Turret Pieces", 12);
+            Core.EquipClass(ClassType.Solo);
+            Core.HuntMonster("firstobservatory", "Empress' Finger", "Alprecha Observed");
+            Bot.Wait.ForPickup("Ancient Remnant");
+        }
+        Core.CancelRegisteredQuests();
+    }
+
+    public void MourningFlower(int Quantity = 1000)
+    {
+        if (Core.CheckInventory("Mourning Flower", Quantity))
+            return;
+
+        Core.AddDrop("Mourning Flower");
+
+        Astravia.GenesisGarden();
+
+        Core.FarmingLogger("Mourning Flower", Quantity);
+        Core.EquipClass(ClassType.Solo);
+
+        Core.RegisterQuests(8688);
+        while (!Bot.ShouldExit() && !Core.CheckInventory("Mourning Flower", Quantity))
+        {
+            Core.KillMonster("genesisgarden", "r6", "Left", "*", "Beast Subject", 7);
+            Core.KillMonster("genesisgarden", "r9", "Left", "*", "Humanoid Subject", 7);
+            Core.KillMonster("genesisgarden", "r11", "Left", "Ancient Mecha", "Replacement Parts", 7);
+            Bot.Wait.ForPickup("Mourning Flower");
+        }
+        Core.CancelRegisteredQuests();
+    }
+
+    public void UnfinishedMusicalScore(int Quantity = 300)
+    {
+        if (Core.CheckInventory("Unfinished Musical Score", Quantity))
+            return;
+
+        Bot.Quests.UpdateQuest(8733);
+        Core.EquipClass(ClassType.Solo);
+
+        Core.HuntMonster("theworld", "Encore Darkon", "Unfinished Musical Score", Quantity, false);
     }
 }
---
+++ Darkon/UnfinishedMusicalScore.cs
@@ -0,0 +1,23 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Darkon/CoreDarkon.cs
+//cs_include Scripts/Story/ElegyofMadness(Darkon)/CoreAstravia.cs
+using RBot;
+
+public class UnfinishedMusicalScore
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreDarkon Darkon => new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Darkon.UnfinishedMusicalScore();
+
+        Core.SetOptions(false);
+    }
+}
