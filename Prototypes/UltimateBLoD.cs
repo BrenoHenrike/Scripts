e46e815f2908de5ea5f26b3e35560ed2b04a097b
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-06-27 15:16:57 +0200

    UltimateBLOD: Got rid of unknown character

--- Prototypes/UltimateBLoD.cs
+++ Prototypes/UltimateBLoD.cs
@@ -52,7 +52,7 @@ public class UltimateBLoD
         {
             Core.EnsureAccept(7654);
 
-            Core.KillMonster($"dflesson", "r12", "Right", "Fluffy the Dracolich", "Fluffy�s Bones", 10, isTemp: false);
+            Core.KillMonster($"dflesson", "r12", "Right", "Fluffy the Dracolich", "Fluffy’s Bones", 10, isTemp: false);
             Core.KillMonster("dflesson", "r3", "Right", "Fire Elemental", "Fire Elemental's Bracer", 5, isTemp: false);
             Core.KillMonster("dflesson", "r6", "Right", "Tog", "Tog Claw", 5, isTemp: false);
 
