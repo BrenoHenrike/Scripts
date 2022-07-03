39d771252f7608dffc442baa6a69aabbcd18d352
Vld Mrk <vladamark00@gmail.com>
2022-07-03 17:58:36 +0200

    Extriki: Quest Turn-in Bug fix

---
+++ Story/QueenofMonsters/BookofMonstersTheDestroyer(Extriki)/4Extriki.cs
@@ -0,0 +1,20 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/QueenofMonsters/BookofMonstersTheDestroyer(Extriki)/CoreExtriki.cs
+using RBot;
+
+public class CompleteExtrikiMap
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new CoreStory();
+    public CoreExtriki Extriki => new CoreExtriki();
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Extriki.Extriki();
+
+        Core.SetOptions(false);
+    }
+}
