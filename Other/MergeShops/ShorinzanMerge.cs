2fd57d1020cc44c9647aa2758f0cab1b8ebd65f1
Tato <47118168+wtffidy@users.noreply.github.com>
2022-07-15 10:18:02 -0400

    Release: ShorinzanMerge
    Requested by; @Malik#9353 & @Desolate#5043

---
+++ Other/MergeShops/ShorinzanMerge.cs
@@ -0,0 +1,118 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Story/StarFestival.cs
+using RBot;
+using RBot.Items;
+using RBot.Options;
+
+public class ShorinzanMerge
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    public CoreStory Story = new();
+    public CoreAdvanced Adv = new();
+    public static CoreAdvanced sAdv = new();
+    public StarFestival SF = new();
+
+    public List<IOption> Options = sAdv.MergeOptions;
+    public string OptionsStorage = sAdv.OptionsStorage;
+    // [Can Change] This should only be changed by the author.
+    //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
+    private bool dontStopMissingIng = false;
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+        SF.StoryLine();
+        BuyAllMerge();
+
+        Core.SetOptions(false);
+    }
+
+    public void BuyAllMerge()
+    {
+        //Only edit the map and shopID here
+        Adv.StartBuyAllMerge("starfest", 2148, findIngredients);
+
+        #region Dont edit this part
+        void findIngredients()
+        {
+            ItemBase req = Adv.externalItem;
+            int quant = Adv.externalQuant;
+            int currentQuant = req.Temp ? Bot.Inventory.GetTempQuantity(req.Name) : Bot.Inventory.GetQuantity(req.Name);
+            if (req == null)
+            {
+                Core.Logger("req is NULL");
+                return;
+            }
+
+            switch (req.Name)
+            {
+                default:
+                    bool shouldStop = Adv.matsOnly ? !dontStopMissingIng : true;
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping"), messageBox: shouldStop, stopBot: shouldStop);
+                    break;
+                #endregion
+
+                case "Deepest Desire":
+                    Core.FarmingLogger($"{req.Name}", quant);
+                    Core.EquipClass(ClassType.Farm);
+                    Core.RegisterQuests(8753);
+                    while (!Bot.ShouldExit() && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonster("Tercessuinotlim", "Tainted Elemental", "Tainted Essence Collected", 10);
+                        Core.HuntMonster("Tercessuinotlim", "Dark Makai", "Makai Essence Collected", 20);
+                        Core.HuntMonster("necrodungeon", "SlimeSkull", "Necropolis Soul Collected", 15);
+                        Core.EquipClass(ClassType.Solo);
+                        Core.HuntMonster("necrodungeon", "5 Headed Dracolich", "Dracolich Soul Collected", 15);
+                        Core.HuntMonster("necrodungeon", "Doom Overlord", "Doom Power Catalyst", 2);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Hidden Hope":
+                    Core.FarmingLogger($"{req.Name}", quant);
+                    Core.EquipClass(ClassType.Solo);
+                    Core.RegisterQuests(8751);
+                    while (!Bot.ShouldExit() && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.KillMonster($"battleunderb", "Enter", "Spawn", "*", "Bundle O’ Bones", 30);
+                        Core.EquipClass(ClassType.Solo);
+                        Core.HuntMonsterMapID($"Odokuro", 1, "Odokuro’s Occipital");
+                        Core.HuntMonster($"bonecastle", "Vaden", "Vaden’s Other Arm");
+                        Core.HuntMonster($"vordredboss", "Vordred", "Vordred’s Skull(s)", 3);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Simple Wish":
+                    Core.FarmingLogger($"{req.Name}", quant);
+                    Core.EquipClass(ClassType.Farm);
+                    Core.RegisterQuests(8748);
+                    while (!Bot.ShouldExit() && !Core.CheckInventory(req.Name, quant))
+                    {
+                        if (!Core.CheckInventory("Twilly Twig"))
+                        {
+                            Core.AddDrop("Twilly Twig");
+                            Core.EnsureAccept(11);
+                            Core.HuntMonster("farm", "Treeant", "Treeant Branch");
+                            Core.EnsureComplete(11);
+                            Bot.Wait.ForPickup("Twilly Twig");
+                        }
+                        Core.HuntMonster("brightoak", "Bright Treeant", "Brightest Branch", 6);
+                        Core.HuntMonster("farm", "Treeant", "Treant Leaf");
+                        Core.HuntMonster("guardiantree", "Blossoming Treeant", "Beautiful Blossom", 6);
+                        Core.HuntMonster("NibbleOn", "Mean Old Treeant", "Bitter Bark", 8);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+            }
+        }
+    }
+}
