fc2059358a0fe9d1f62f7bb1d127daf0bccd49c9
Tato <47118168+wtffidy@users.noreply.github.com>
2022-07-17 12:47:07 -0400

    did me a dumbdumb

--- Enhancement/ForgeEnhancments[Quests].cs
+++ Enhancement/ForgeEnhancments[Quests].cs
@@ -160,10 +160,13 @@ public class ForgeEnhancments
                 break;
 
             case "Vainglory":
+                Absolution();
                 Vainglory();
                 break;
 
             case "Avarice":
+                Absolution();
+                Vainglory();
                 Avarice();
                 break;
 
@@ -185,76 +188,76 @@ public class ForgeEnhancments
 
     public void ForgeCapeEnhancement()
     {
-        if (!Story.QuestProgression(8758))
-        {
-            Core.EquipClass(ClassType.Solo);
-            Core.EnsureAccept(8758);
-            Core.KillEscherion("1st Lord Of Chaos Staff");
-            Core.KillVath("Chaos Dragonlord Axe");
-            Core.HuntMonster("kitsune", "Kitsune", "Hanzamune Dragon Koi Blade", isTemp: false);
-            Core.HuntMonster("wolfwing", "Wolfwing", "Wrath of the Werepyre", isTemp: false);
-            Core.EnsureComplete(8758);
-        }
+        Core.EquipClass(ClassType.Solo);
+        Core.EnsureAccept(8758);
+        Core.KillEscherion("1st Lord Of Chaos Staff");
+        Core.KillVath("Chaos Dragonlord Axe");
+        Core.HuntMonster("kitsune", "Kitsune", "Hanzamune Dragon Koi Blade", isTemp: false);
+        Core.HuntMonster("wolfwing", "Wolfwing", "Wrath of the Werepyre", isTemp: false);
+        Core.EnsureComplete(8758);
+
+        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestWeapon>("ForgeQuestWeapon").ToString()} Unlocked");
     }
 
     public void Absolution()
     {
-        if (!Bot.Quests.IsUnlocked(8743))
+        Core.EnsureAccept(8743);
+        Farm.GoodREP(9);
+        if (!Core.CheckInventory("Ascended Paladin"))
         {
-            Core.EnsureAccept(8743);
-            Farm.GoodREP();
-            if (!Core.CheckInventory("Ascended Paladin"))
-            {
-                Core.HuntMonster("therift", "Plague Spreader", "Slimed Sigil", 200, isTemp: false);
-                Core.BuyItem("therift", 1399, 39091);
-                Core.BuyItem("therift", 1399, 39093);
-                Core.BuyItem("therift", 1399, 39094);
-            }
-            Core.EnsureComplete(8743);
+            Core.HuntMonster("therift", "Plague Spreader", "Slimed Sigil", 200, isTemp: false);
+            Core.BuyItem("therift", 1399, 39091);
+            Core.BuyItem("therift", 1399, 39093);
+            Core.BuyItem("therift", 1399, 39094);
         }
+        Core.EnsureComplete(8743);
 
+        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestCape>("ForgeQuestCape").ToString()} Unlocked");
     }
 
     public void Vainglory()
     {
+
         if (!Bot.Quests.IsUnlocked(8744))
         {
-            Core.EnsureAccept(8744);
-            Awe.GetAweRelic("Pauldron", 4160, 15, 15, "gravestrike", "Ultra Akriloth");
-            Awe.GetAweRelic("Breastplate", 4163, 10, 10, "aqlesson", "Carnax");
-            Awe.GetAweRelic("Vambrace", 4166, 15, 15, "bloodtitan", "Ultra Blood Titan");
-            Awe.GetAweRelic("Gauntlet", 4169, 25, 5, "alteonbattle", "Ultra Alteon");
-            Awe.GetAweRelic("Greaves", 4172, 10, 15, "bosschallenge", "Mutated Void Dragon");
-            Core.EnsureComplete(8744);
+            Absolution();
         }
 
+        Core.EnsureAccept(8744);
+        Awe.GetAweRelic("Pauldron", 4160, 15, 15, "gravestrike", "Ultra Akriloth");
+        Awe.GetAweRelic("Breastplate", 4163, 10, 10, "aqlesson", "Carnax");
+        Awe.GetAweRelic("Vambrace", 4166, 15, 15, "bloodtitan", "Ultra Blood Titan");
+        Awe.GetAweRelic("Gauntlet", 4169, 25, 5, "alteonbattle", "Ultra Alteon");
+        Awe.GetAweRelic("Greaves", 4172, 10, 15, "bosschallenge", "Mutated Void Dragon");
+        Core.EnsureComplete(8744);
+        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestCape>("ForgeQuestCape").ToString()} Unlocked");
     }
 
     public void Avarice()
     {
-        if (Bot.Quests.IsUnlocked(8745))
-        {
-            Core.EnsureAccept(8745);
-            HOTLB.Indulgence(75);
-            HOTLB.Penance(75);
-            Core.EnsureComplete(8745);
-        }
-        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestWeapon>("ForgeQuestWeapon").ToString()} Unlocked");
+        Absolution();
+        Vainglory();
+
+        Core.EnsureAccept(8745);
+        HOTLB.Indulgence(75);
+        HOTLB.Penance(75);
+        Core.EnsureComplete(8745);
+
+        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestCape>("ForgeQuestCape").ToString()} Unlocked");
 
     }
 
     public void ForgeWeaponEnhancement()
     {
-        if (!Story.QuestProgression(8738))
-        {
-            Core.EquipClass(ClassType.Solo);
-            Core.EnsureAccept(8738);
-            Core.KillEscherion("1st Lord Of Chaos Helm");
-            Core.KillVath("Chaos Dragonlord Helm");
-            Core.HuntMonster("kitsune", "Kitsune", "Chaos Shogun Helmet", isTemp: false);
-            Core.HuntMonster("wolfwing", "Wolfwing", "Wolfwing Mask", isTemp: false);
-            Core.EnsureComplete(8738);
-        }
+        Core.EquipClass(ClassType.Solo);
+        Core.EnsureAccept(8738);
+        Core.KillEscherion("1st Lord Of Chaos Helm");
+        Core.KillVath("Chaos Dragonlord Helm");
+        Core.HuntMonster("kitsune", "Kitsune", "Chaos Shogun Helmet", isTemp: false);
+        Core.HuntMonster("wolfwing", "Wolfwing", "Wolfwing Mask", isTemp: false);
+        Core.EnsureComplete(8738);
+
+        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestWeapon>("ForgeQuestWeapon").ToString()} Unlocked");
     }
 
     public void Lacerate()
@@ -281,84 +284,82 @@ public class ForgeEnhancments
                 Core.EnsureComplete(91);
             }
             Story.KillQuest(92, "greenguardwest", new[] { "Breken the Vile", "Ogug Stoneaxe" });
-
-            Core.HuntMonster("graveyard", "Big Jack Sprat", "Undead Plague Spear", isTemp: false);
-            Core.HuntMonster("river", "Kuro", "Kuro's Wrath", isTemp: false);
-
-            if (!Core.CheckInventory("Massive Horc Cleaver"))
-            {
-                Core.EnsureAccept(279);
-                Core.HuntMonster("warhorc", "General Drox", "Boss Prize");
-                Core.EnsureComplete(279);
-                Bot.Wait.ForPickup("Massive Horc Cleaver");
-            }
-            if (!Core.CheckInventory("Sword in the Stone"))
-            {
-                Core.EnsureAccept(316);
-                Core.GetMapItem(54, 7, "greenguardeast");
-                Core.HuntMonster("greenguardeast", "Spider", "Tiny Sword");
-                Core.EnsureComplete(316);
-                Bot.Wait.ForPickup("Sword in the Stone");
-            }
-            if (!Core.CheckInventory("Forest Axe"))
-            {
-                Core.EnsureAccept(301);
-                Core.GetMapItem(55, 4, "farm");
-                Core.HuntMonster("farm", "Mosquito", "Mosquito Juice");
-                Core.EnsureComplete(301);
-                Bot.Wait.ForPickup("Forest Axe");
-            }
-            Farm.BlackKnightOrb();
-            Core.EnsureComplete(8739);
         }
+        Core.HuntMonster("graveyard", "Big Jack Sprat", "Undead Plague Spear", isTemp: false);
+        Core.HuntMonster("river", "Kuro", "Kuro's Wrath", isTemp: false);
+
+        if (!Core.CheckInventory("Massive Horc Cleaver"))
+        {
+            Core.EnsureAccept(279);
+            Core.HuntMonster("warhorc", "General Drox", "Boss Prize");
+            Core.EnsureComplete(279);
+            Bot.Wait.ForPickup("Massive Horc Cleaver");
+        }
+        if (!Core.CheckInventory("Sword in the Stone"))
+        {
+            Core.EnsureAccept(316);
+            Core.GetMapItem(54, 7, "greenguardeast");
+            Core.HuntMonster("greenguardeast", "Spider", "Tiny Sword");
+            Core.EnsureComplete(316);
+            Bot.Wait.ForPickup("Sword in the Stone");
+        }
+        if (!Core.CheckInventory("Forest Axe"))
+        {
+            Core.EnsureAccept(301);
+            Core.GetMapItem(55, 4, "farm");
+            Core.HuntMonster("farm", "Mosquito", "Mosquito Juice");
+            Core.EnsureComplete(301);
+            Bot.Wait.ForPickup("Forest Axe");
+        }
+        Farm.BlackKnightOrb();
+        Core.EnsureComplete(8739);
+
+        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestWeapon>("ForgeQuestWeapon").ToString()} Unlocked");
     }
 
     public void Smite()
     {
-        if (!Bot.Quests.IsUnlocked(8740))
-        {
-            Core.EnsureAccept(8740);
-            SFR.StoryLine();
-            Farm.Experience(60);
-            Core.HuntMonster("shadowattack", "Death", "Death's Power", 3, isTemp: false);
-            Core.KillEscherion("Chaotic Power", 7);
-            Core.HuntMonster("shadowrealmpast", "*", "Empowered Essence", 50, isTemp: false);
-            Core.HuntMonster("undergroundlabb", "Ultra Battle Gem", "Gem Power", 25, false);
-            Adv.BuyItem("alchemyacademy", 2036, "Power Tonic", 10);
-            Core.EnsureComplete(8740);
-        }
+        SFR.StoryLine();
+
+        Core.EnsureAccept(8740);
+        Core.HuntMonster("shadowattack", "Death", "Death's Power", 3, isTemp: false);
+        Core.KillEscherion("Chaotic Power", 7);
+        Core.HuntMonster("shadowrealmpast", "*", "Empowered Essence", 50, isTemp: false);
+        Core.HuntMonster("undergroundlabb", "Ultra Battle Gem", "Gem Power", 25, false);
+        Adv.BuyItem("alchemyacademy", 2036, "Power Tonic", 10);
+        Core.EnsureComplete(8740);
+
+        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestWeapon>("ForgeQuestWeapon").ToString()} Unlocked");
     }
 
     public void HerosValiance()
     {
-        if (!Bot.Quests.IsUnlocked(8741))
+        LOC.Complete13LOC();
+        FCA.GetFireChampsArmor();
+        DOT.GetDoT();
+        ED.getSet();
+        if (!Core.CheckInventory("Eternity Blade"))
         {
-            LOC.Complete13LOC();
-            FCA.GetFireChampsArmor();
-            DOT.GetDoT();
-            ED.getSet();
-            if (!Core.CheckInventory("Eternity Blade"))
-            {
-                Core.EnsureAccept(3485);
-                Bot.Quests.UpdateQuest(3484);
-                Core.HuntMonster("towerofdoom10", "Slugbutter", "Eternity Blade");
-                Core.EnsureComplete(3485);
-            }
-            Core.EnsureAccept(8741);
-            Seppy.GravelynsDoomFireToken();
-            AP.GetAP(false); //purely for the last quest "Sacred Magic: Eden"
-            Adv.BuyItem("darkthronehub", 1303, "ArchPaladin");
-            Core.EnsureComplete(8741);
+            Core.EnsureAccept(3485);
+            Bot.Quests.UpdateQuest(3484);
+            Core.HuntMonster("towerofdoom10", "Slugbutter", "Eternity Blade");
+            Core.EnsureComplete(3485);
         }
+
+        Core.EnsureAccept(8741);
+        Seppy.GravelynsDoomFireToken();
+        AP.GetAP(false); //purely for the last quest "Sacred Magic: Eden"
+        Adv.BuyItem("darkthronehub", 1303, "ArchPaladin Armor");
+        Core.EnsureComplete(8741);
+
+        Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestWeapon>("ForgeQuestWeapon").ToString()} Unlocked");
     }
 
     public void ArcanasConcertoWIP()
     {
-
         // Arcana's Concerto   
         if (Bot.Quests.IsUnlocked(8742))
         {
-            Core.EnsureAccept(8742);
             // Must have completed the 'Darkon, the Conductor' quest.
             //8746|Darkon, the Conductor
             if (!Core.isCompletedBefore(8746))
@@ -381,12 +382,14 @@ public class ForgeEnhancments
                 if (!Core.CheckInventory("Darkon Insignia Insignia", 20))
                     Core.Logger(" x5 \"Darkon Insignia Insignia\" is Required to continue quest, our Bots cannot *currently* kill this mob Untill CoreArmy is Released and a script is made.", messageBox: true, stopBot: true);
             }
-            if (!Core.CheckInventory("King Drago Insignia", 5))
-                Core.Logger(" x5 \"King Drago Insignia\" is Required to continue quest, our Bots cannot *currently* kill this mob Untill CoreArmy is Released and a script is made.", messageBox: true, stopBot: true);
-            if (!Core.CheckInventory("Darkon Insignia Insignia", 5))
-                Core.Logger(" x5 \"Darkon Insignia Insignia\" is Required to continue quest, our Bots cannot *currently* kill this mob Untill CoreArmy is Released and a script is made.", messageBox: true, stopBot: true);
-            Core.EnsureComplete(8742);
         }
+        Core.EnsureAccept(8742);
+        if (!Core.CheckInventory("King Drago Insignia", 5))
+            Core.Logger(" x5 \"King Drago Insignia\" is Required to continue quest, our Bots cannot *currently* kill this mob Untill CoreArmy is Released and a script is made.", messageBox: true, stopBot: true);
+        if (!Core.CheckInventory("Darkon Insignia Insignia", 5))
+            Core.Logger(" x5 \"Darkon Insignia Insignia\" is Required to continue quest, our Bots cannot *currently* kill this mob Untill CoreArmy is Released and a script is made.", messageBox: true, stopBot: true);
+        Core.EnsureComplete(8742);
+
         Core.Logger($"Enh: {Bot.Config.Get<ForgeQuestWeapon>("ForgeQuestWeapon").ToString()} Unlocked");
     }
 
