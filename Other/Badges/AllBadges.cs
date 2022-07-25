9ed201aff2f5b471c593c420f70fe86308e6ce7f
Tato <47118168+wtffidy@users.noreply.github.com>
2022-07-25 12:40:27 -0400

    Create AllBadges.cs

---
+++ Other/Badges/AllBadges.cs
@@ -0,0 +1,29 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Other\Badges\CtrlAltDelMemberBadge.cs
+//cs_include Scripts/Other\Other\Badges\SkyPirateSlayerBadge.cs
+//cs_include Scripts/Other\Other\Badges\YouMadBroBadge.cs
+using RBot;
+
+public class AllBadges
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    public CtrlAltDelMemberBadge CADB = new();
+    public SkyPirateBadge SPB = new();
+    public YouMadBroBadge YMBB = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        CADB.Badge();
+        SPB.Badge();
+        YMBB.Badge();
+        //add more as they are made.
+
+        Core.SetOptions(false);
+    }
+}
