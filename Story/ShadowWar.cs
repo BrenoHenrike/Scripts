6324cc0a1f4e2673864e44e8ca500aea91fe0754
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-08-03 19:01:53 +0200

    ShadowWar: Removed redundant bit

--- Story/ShadowWar.cs
+++ Story/ShadowWar.cs
@@ -26,18 +26,23 @@ public class ShadowWar
 
         //Shadow Medals: Defend the Village! 6846
         Story.KillQuest(6846, "shadowwar", "Shadowflame Slasher");
+
         //Seed Spitter Oil 6847
         Story.KillQuest(6847, "shadowwar", "Seed Spitter");
+
         //Mega Shadow Medals 6848
         Story.KillQuest(6848, "shadowwar", "Shadowflame Slasher");
+
         //Shadow Samples 6849
         Story.KillQuest(6849, "shadowwar", "Umbral Goo");
+
         //Shadow Medals: Fight them Back! 6850
         Story.KillQuest(6850, "shadowwar", "Shadowflame Slasher");
+
         //Interrogation 6851
         Story.KillQuest(6851, "shadowwar", "Shadowflame Scout");
+
         //Defeat Malgor! 6852
-        Core.EquipClass(ClassType.Solo);
         Story.KillQuest(6852, "malgor", "Malgor");
     }
 }
