9966d83cf070428f96b052ee7147c5d5c50ef81d
Vld Mrk <vladamark00@gmail.com>
2022-08-15 15:40:13 +0200

    Shadows of War: Full Storyline added

---
+++ Story/ShadowsOfWar/9AozoraHills.cs
@@ -0,0 +1,27 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/ShadowsOfWar/CoreSoW.cs
+//cs_include Scripts/Story/ShadowWar.cs
+//cs_include Scripts/Story/Legion/DarkAlly.cs
+//cs_include Scripts/Story/Legion/DarkAlliance.cs
+using RBot;
+
+public class AozoraHills
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+    public SOW SoW = new();
+    public ShadowWar SW = new();
+    public DarkAlly_Story DAlly = new();
+    public DarkAlliance_Story DAlliance = new();
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        SoW.AozoraHills();
+
+        Core.SetOptions(false);
+    }
+}
