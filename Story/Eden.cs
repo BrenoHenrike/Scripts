1f7db17c75fadd0b8ebd4638d6b0ee2ee2068312
tanigorlx <105113702+tanigorlx@users.noreply.github.com>
2022-08-16 14:16:25 +0800

    Add files via upload

--- Story/Eden.cs
+++ Story/Eden.cs
@@ -31,21 +31,24 @@ public class Eden
 
         //Welcome to Eden! 8795
         Story.KillQuest(8795, "eden", "Harmless Choir");
+        
         //The Fashion and Arcade District 8796
-        Story.KillQuest(8796, "eden", "Cosplayer");
-        Story.KillQuest(8796, "eden", "Klawaii Machine");
+        Story.KillQuest(8796, "eden", new[] {"Cosplayer", "Klawaii Machine"});
+
         //Eden City KotaMart 8797
         Story.KillQuest(8797, "eden", "SalaryMan");
+
         //Save the Citizens! 8798
         Story.MapItemQuest(8798, "eden", 10448, 5);
-        Story.KillQuest(8798, "eden", "SalaryMan Anomaly");
-        Story.KillQuest(8798, "eden", "Cosplayer Anomaly");
-        Story.KillQuest(8798, "eden", "Police Anomaly");
+        Story.KillQuest(8798, "eden", new[] {"SalaryMan Anomaly", "Cosplayer Anomaly", "Police Anomaly"});
+        
         //Armorchy 8799
         Story.KillQuest(8799, "eden", "CRC Power Armor");
+
         //Clear the Way! 8800
         Story.MapItemQuest(8800, "eden", 10449, 3);
         Story.KillQuest(8800, "eden", "Yokaified Experiment 1");
+
         //Protect the Reactor! 8801
         Story.KillQuest(8801, "eden", "Major Anomaly");
     }
