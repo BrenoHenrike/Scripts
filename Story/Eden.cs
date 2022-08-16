8760ce7bdde0019ece22f7f42b8752ed19189177
tanigorlx <105113702+tanigorlx@users.noreply.github.com>
2022-08-16 21:35:32 +0800

    Eden Storyline && Gachapon Machine Quests (#150)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update Eden.cs
    
    * Update Eden.cs
    
    * Update Eden.cs
    
    * Add files via upload
    
    * Update Eden.cs
    
    * Update GachaponMachineQuests.cs

---
+++ Story/Eden.cs
@@ -0,0 +1,67 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/Story/Shinkansen.cs
+using RBot;
+
+public class Eden
+{
+    public ScriptInterface Bot => ScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+    public Shinkansen Shin = new();
+
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        EdenStoryline();
+
+        Core.SetOptions(false);
+    }
+
+    public void EdenStoryline()
+    {
+        if (Core.isCompletedBefore(8801))
+            return;
+
+        Story.PreLoad();
+        Shin.Storyline();
+
+        //Welcome to Eden! 8795
+        Story.KillQuest(8795, "eden", "Harmless Choir");
+        
+        //The Fashion and Arcade District 8796
+        if (!Story.QuestProgression(8796))
+        {           
+            Core.EnsureAccept(8796);
+            Core.HuntMonster("eden", "Cosplayer", "Salarymen Pictures Taken", 6);
+            Core.HuntMonster("eden", "Klawaii Machine", "Prize Won");
+            Core.EnsureComplete(8496);           
+        }
+
+        //Eden City KotaMart 8797
+        Story.KillQuest(8797, "eden", "SalaryMan");
+
+        //Save the Citizens! 8798
+        if (!Story.QuestProgression(8798))
+        {           
+            Core.EnsureAccept(8798);
+            Story.MapItemQuest(8798, "eden", 10448, 5);
+            Core.HuntMonster("eden", "SalaryMan Anomaly", "Salarymen Anomalies Slain", 7);
+            Core.HuntMonster("eden", "Cosplayer Anomaly", "Cosplayer Anomalies Slain", 7);
+            Core.HuntMonster("eden", "Police Anomaly", "Police Anomalies Slain", 7);
+            Core.EnsureComplete(8493);           
+        }
+        //Armorchy 8799
+        Story.KillQuest(8799, "eden", "CRC Power Armor");
+
+        //Clear the Way! 8800
+        Story.MapItemQuest(8800, "eden", 10449, 3);
+        Story.KillQuest(8800, "eden", "Yokaified Experiment 1");
+
+        //Protect the Reactor! 8801
+        Story.KillQuest(8801, "eden", "Major Anomaly");
+    }
+}
