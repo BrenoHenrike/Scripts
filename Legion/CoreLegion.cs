7aad52a6798f059bc857f3ca13baa89041d64f96
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-08-21 00:01:09 +0200

    DarkToken: Now wont stop

--- Legion/CoreLegion.cs
+++ Legion/CoreLegion.cs
@@ -64,8 +64,7 @@ public class CoreLegion
         Core.RegisterQuests(6248, 6249, 6251);
         while (!Bot.ShouldExit && !Core.CheckInventory("Dark Token", quant))
         {
-            Core.KillMonster("seraphicwardage", "r3", "Right", "*", "Seraphic Commanders Slain", 6, log: false);
-            Bot.Drops.Pickup("Dark Token");
+            Core.KillMonster("seraphicwardage", "r3", "Right", "*", log: false);
         }
         Core.CancelRegisteredQuests();
     }
