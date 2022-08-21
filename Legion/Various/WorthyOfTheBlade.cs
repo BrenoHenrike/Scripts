83d2fd52953a4eb239d51e60faa0d25af95f1473
Smooth Brain Leaf Eater <43081184+ShaunH321@users.noreply.github.com>
2022-08-21 13:44:30 +0100

    Add files via upload

---
+++ Legion/Various/WorthyOfTheBlade.cs
@@ -0,0 +1,49 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Nation/CoreNation.cs
+using Skua.Core.Interfaces;
+
+public class WorthyBlade
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    public CoreNation Nation = new();
+    public CoreAdvanced Adv = new CoreAdvanced();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        WorthyOfTheBlade();
+
+        Core.SetOptions(false);
+    }
+
+    public void WorthyOfTheBlade()
+    {
+        if (Core.CheckInventory("SoulKeeper Blade"))
+            return;
+
+        Core.AddDrop("SoulKeeper Blade", "SoulKeeper On your Back", "SoulKeeper Sword Pet");
+        Core.EnsureAccept(6738);
+        Nation.EssenceofNulgath(100);
+        Core.AddDrop("Undead Essence", "Undead Energy");
+        Farm.BattleUnderB("Undead Essence", 500);
+        Farm.BattleUnderB("Undead Energy", 500);
+        while (!Bot.ShouldExit && (!Core.CheckInventory("Essence of Blade Master")))
+        {
+            Core.EquipClass(ClassType.Farm);
+            Core.AddDrop("Essence of Blade Master");
+            Core.HuntMonster("underworld", "Blade Master", "Essence of Blade Master", 1, false);
+        }
+        while (!Bot.ShouldExit && (!Core.CheckInventory("Binky's Uni-horn")))
+        {
+            Core.EquipClass(ClassType.Solo);
+            Adv.KillUltra("doomvault", "r5", "Left", "Binky", "Binky's Uni-horn", isTemp: false, publicRoom: true);
+        }
+        Bot.Sleep(1500);
+        Core.EnsureComplete(6738);
+    }
+}
