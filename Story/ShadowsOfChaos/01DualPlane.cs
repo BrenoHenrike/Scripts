885be33b2e167374fb97903fd7149c31af5a0c7d
PapaShokry <islamshokry7@gmail.com>
2022-08-26 14:00:25 +0200

    moving files to its correct path and adding Shadows of chaos saga

---
+++ Story/ShadowsOfChaos/01DualPlane.cs
@@ -0,0 +1,24 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/ShadowsOfChaos/CoreSoC.cs
+
+using Skua.Core.Interfaces;
+
+public class DualPlane
+{
+   
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public SoC SoC = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        SoC.DualPlane();
+
+        Core.SetOptions(false);
+    }
+}
+
+ 
---
+++ Story/ShadowsOfChaos/10BrightForestExtra.cs
@@ -0,0 +1,20 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/ShadowsOfChaos/CoreSoC.cs
+
+using Skua.Core.Interfaces;
+
+public class BrightForestExtra
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+    public SoC SoC = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+        SoC.BrightForestExtra();
+        Core.SetOptions(false);
+    }
+}
