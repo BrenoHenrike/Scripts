05842c3a9f460447a694c030b059777645c4ab15
Smooth Brain Leaf Eater <shaunharries64@gmail.com>
2022-08-27 21:03:47 +0100

    Army script for leveling Ice Wing

---
+++ Prototypes/ArmyIceWing.cs
@@ -0,0 +1,57 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Options;
+
+public class IceWingLevelingArmy
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+
+    public int level = 75;
+    public bool DontPreconfigure = true;
+    public List<IOption> Options = new List<IOption>()
+    {
+        new Option<int>("armysize","Players", "Input the minimum of players to wait for", 1),
+        new Option<bool>("skipSetup", "Skip this window next time?", "You will be able to return to this screen via [Scripts] -> [Edit Script Options] if you wish to change anything.", false),
+    };
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        if (!Bot.Config.Get<bool>("skipSetup"))
+            Bot.Config.Configure();
+
+        ArmyIceWing();
+
+        Core.SetOptions(false);
+    }
+
+    public void ArmyIceWing()
+    {
+        Core.RegisterQuests(Core.IsMember ? 6635 : 6632);
+        while (!Bot.ShouldExit)
+            KillIceWing("icestormarena", "r23", "Left", "*");
+        Core.CancelRegisteredQuests();
+    }
+
+    public void KillIceWing(string map, string cell, string pad, string monster)
+    {
+        Core.Join(map);
+        if (Bot.Player.Cell != cell)
+        {
+            if (Bot.Player.Level < level)
+                Bot.Send.ClientPacket("{\"t\":\"xt\",\"b\":{\"r\":-1,\"o\":{\"cmd\":\"levelUp\",\"intExpToLevel\":\"0\",\"intLevel\":100}}}", "json");
+            Bot.Sleep(500);
+            Core.Jump(cell, pad);
+        }
+        //while (!Bot.Map.PlayerExists((Bot.Config.Get<string>("playerName"))) || Bot.Map.GetPlayer((Bot.Config.Get<string>("playerName"))).Cell != Bot.Player.Cell)
+        while ((cell != null && Bot.Map.CellPlayers.Count() > 0 ? Bot.Map.CellPlayers.Count() : Bot.Map.PlayerCount) < Bot.Config.Get<int>("armysize"))
+        {
+            Core.Logger($"Waiting for the squad. [{Bot.Map.PlayerNames.Count}/{Bot.Config.Get<int>("armysize")}]");
+            Bot.Sleep(2000);
+        }
+        Bot.Kill.Monster(monster);
+    }
+}
