a0a55466b5fc443d71b671ba16fb822c374bf40a
Smooth Brain Leaf Eater <43081184+ShaunH321@users.noreply.github.com>
2022-08-27 14:48:13 +0100

    Lair questline

---
+++ Nation/DragonSlayerReward.cs
@@ -0,0 +1,19 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/Nation/CoreNation.cs
+using Skua.Core.Interfaces;
+
+public class DragonSlayerReward
+{
+    public CoreBots Core => CoreBots.Instance;
+    public CoreNation Nation = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Nation.DragonSlayerReward();
+
+        Core.SetOptions(false);
+    }
+}
