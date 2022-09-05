9fb67e62990f7349b1eedacb095676b8f7f0c6bf
Tato <47118168+wtffidy@users.noreply.github.com>
2022-09-05 07:37:51 -0400

    updated to new standard.

--- Seasonal/LuckyDay/EvolvedLeprechaunMerge.cs
+++ Seasonal/LuckyDay/EvolvedLeprechaunMerge.cs
@@ -33,25 +33,23 @@ public class EvolvedLeprechaun
 
         //Golden Ticket
         while (!Bot.ShouldExit && !Core.CheckInventory("Golden Ticket", 2000))
-        {
             Core.GetMapItem(101, 1, "luck");
-        }
 
         //Lucky Clover
         if (!Core.CheckInventory("Lucky Clover", 12))
             if (!Bot.Quests.IsDailyComplete(1759))
             {
-                Core.SmartKillMonster(1759, "rainbow", "Lucky Harms ", 20, true);
+                Core.EnsureAccept(1759);
+                Core.HuntMonster("rainbow", "Lucky Harms", "Clover Leaves", 20);
+                Core.EnsureComplete(1759);
             }
+
         if (!Core.CheckInventory("Lucky Clover", 12))
-        {
             Core.Logger($"Not enough \"Lucky Clovers\", please do the daily until you have 12. You can run it once a day (Server resets at 12am EST)");
-        }
 
         //Buy Evolved Leprechaun
         if (Core.CheckInventory("Lucky Clover", 12) && (Core.CheckInventory("Rainbow Shard", 20) && (Core.CheckInventory("Golden Ticket", 2000))))
-        {
             Core.BuyItem("luck", 256, "Evolved Leprechaun");
-        }
+
     }
 }
