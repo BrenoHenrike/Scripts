f1615020bf6a3cbc1d804429d0d3ea396b33d781
Vld Mrk <vladamark00@gmail.com>
2022-09-15 18:44:44 +0200

    MemetsRealm: full saga added and some seasonal scripts modified

---
+++ Story/MemetsRealm/3MoonLab.cs
@@ -0,0 +1,20 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/MemetsRealm/CoreMemet.cs
+using Skua.Core.Interfaces;
+
+public class MoonLab
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public MemetsRealm Memet = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Memet.MoonLab();
+
+        Core.SetOptions(false);
+    }
+}
