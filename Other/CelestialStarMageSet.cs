eeb8c37347f824895a37f869ae984342c62ba5b1
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-09-16 22:40:11 +0200

    Released CelestialStarMageSet

--- Other/CelestialStarMageSet.cs
+++ Other/CelestialStarMageSet.cs
@@ -1,14 +1,14 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreStory.cs
-//cs_include Scripts/Story/VasalkarLairWar.cs
+//cs_include Scripts/Story/SepulchureSaga/04ShadowfallRise.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 
-public class VoidBattleMageSet
+public class CelestialStarMageSet
 {
-    public IScriptInterface Bot => IScriptInterface.Instance;
-    public CoreBots Core => CoreBots.Instance;
-    public LairWar War = new();
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private ShadowfallRise SFR = new();
 
     public void ScriptMain(IScriptInterface bot)
     {
@@ -21,22 +21,22 @@ public class VoidBattleMageSet
 
     public void GetSet()
     {
-        string[] rewards = Core.EnsureLoad(6694).Rewards.Select(i => i.Name).ToArray();
+        string[] rewards = Core.EnsureLoad(6592).Rewards.Select(i => i.Name).ToArray();
         if (Core.CheckInventory(rewards))
             return;
 
         int count = 0;
         Core.CheckSpaces(ref count, rewards);
         Core.AddDrop(rewards);
-        War.Attack();
+        SFR.StoryLine();
 
-        Core.RegisterQuests(6694);
+        Core.RegisterQuests(6592);
         Bot.Events.ItemDropped += ItemDropped;
-        Core.Logger($"Farm for Void BattleMage set started. Farming to get {rewards.Count() - count} more item" + ((rewards.Count() - count) > 1 ? "s" : ""));
+        Core.Logger($"Farm for the Celestial StarMage set started. Farming to get {rewards.Count() - count} more item" + ((rewards.Count() - count) > 1 ? "s" : ""));
 
         while (!Core.CheckInventory(rewards))
         {
-            Core.KillMonster("lairattack", "Eggs", "Left", "Flame Dragon General", log: false);
+            Core.KillMonster("lairdefend", "Eggs", "Left", "Flame Dragon General", log: false);
             Bot.Wait.ForPickup("*");
         }
 
