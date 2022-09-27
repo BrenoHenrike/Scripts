f462f9725f7f7531309dd4028d42065534f08c19
Smooth Brain Leaf Eater <shaunharries64@gmail.com>
2022-09-27 13:20:13 +0100

    Create ArmyPennyForYourThoughts.cs

---
+++ Army/ArmyPennyForYourThoughts.cs
@@ -0,0 +1,70 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Army/CoreArmyLite.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Options;
+
+public class ArmyPennyForYourThoughts
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+    private CoreAdvanced Adv => new();
+    private CoreArmyLite Army = new();
+
+    private static CoreBots sCore = new();
+    private static CoreArmyLite sArmy = new();
+
+    public string OptionsStorage = "ArmyPenny";
+    public bool DontPreconfigure = true;
+    public List<IOption> Options = new List<IOption>()
+    {
+        sArmy.player1,
+        sArmy.player2,
+        sArmy.player3,
+        sArmy.player4,
+        sArmy.player5,
+        sArmy.player6,
+        sArmy.packetDelay,
+        sCore.SkipOptions
+    };
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        if (!Bot.Config.Get<bool>("SkipOption"))
+        {
+            Core.Logger("Fill in all names at once or don't complain when it doesn't work", messageBox: true);
+            Bot.Config.Configure();
+        }
+            
+
+        Core.BankingBlackList.AddRange(Loot);
+
+        Core.SetOptions(disableClassSwap: true);
+        bot.Options.RestPackets = false;
+
+        Setup();
+
+        Core.SetOptions(false);
+    }
+    
+    public void Setup()
+    {
+        Core.PrivateRooms = true;
+        Core.PrivateRoomNumber = Army.getRoomNr();
+
+        Core.AddDrop(Loot);
+        Core.EquipClass(ClassType.Farm);
+        if (Core.IsMember)
+            Core.RegisterQuests(2089);
+
+        Army.SmartAggroMonStart("maul", "Slimeskull", "Shelleton", "Personal Chopper");
+
+        while (!Bot.ShouldExit)
+            Bot.Combat.Attack("*");
+        Army.AggroMonStop(true);
+    }
+
+    private string[] Loot = { "DoomCoin", "Dark Spirit Orb" };
+}
