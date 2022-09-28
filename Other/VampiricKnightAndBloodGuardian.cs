9a0e39b49dded709cd8788670ac73736940a4880
PapaShokry <islamshokry7@gmail.com>
2022-09-28 04:09:59 +0200

    deleting jump to bank as it was spamming

--- Other/VampiricKnightAndBloodGuardian.cs
+++ Other/VampiricKnightAndBloodGuardian.cs
@@ -37,33 +37,24 @@ public class VampiricKnightAndBloodGuardian
 
         Core.RegisterQuests(6068);
         while (!Bot.ShouldExit && !Core.CheckInventory(Quest1Rewards, toInv: false))
-        {
             //Lycan Medals 6068
             Core.HuntMonster("BloodWarVamp", "Lunar Blazebinder", "Lycan Medal", 5);
-            Core.Jump("Wait", "Spawn");
-            Core.ToBank(Quest1Rewards);
-        }
         Core.CancelRegisteredQuests();
+        Core.ToBank(Quest1Rewards);
 
         Core.RegisterQuests(6069);
         while (!Bot.ShouldExit && !Core.CheckInventory(Quest2Rewards, toInv: false))
-        {
             //Mega Lycan Medals 6069
             Core.HuntMonster("BloodWarVamp", "Lunar Blazebinder", "Mega Lycan Medal", 3);
-            Core.Jump("Wait", "Spawn");
-            Core.ToBank(Quest2Rewards);
-        }
         Core.CancelRegisteredQuests();
+        Core.ToBank(Quest2Rewards);
 
         Core.RegisterQuests(6070);
         while (!Bot.ShouldExit && !Core.CheckInventory(Quest3Rewards, toInv: false))
-        {
             //Blackened Incense 6072
             Core.HuntMonster("BloodWarVamp", "Lunar Blazebinder", "Blackened Incense", 5);
-            Core.Jump("Wait", "Spawn");
-            Core.ToBank(Quest3Rewards);
-        }
         Core.CancelRegisteredQuests();
+        Core.ToBank(Quest3Rewards);
 
 
     }
