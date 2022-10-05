f86288039e0ea316b525e7b1acca03d32842e076
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-10-05 22:17:45 +0200

    VoidKnightSwordQuests: Integrated into CoreNation and all Nation Farms

--- Nation/VoidKnightSwordQuest.cs
+++ Nation/VoidKnightSwordQuest.cs
@@ -6,9 +6,7 @@ using Skua.Core.Interfaces;
 public class VoidKnightSword
 {
     public IScriptInterface Bot => IScriptInterface.Instance;
-
     public CoreBots Core => CoreBots.Instance;
-    public CoreFarms Farm = new CoreFarms();
     public CoreNation Nation = new();
 
     public void ScriptMain(IScriptInterface bot)
@@ -16,35 +14,8 @@ public class VoidKnightSword
         Core.BankingBlackList.AddRange(Nation.bagDrops);
         Core.SetOptions();
 
-        VoidKightSwordQuest();
+        Nation.VoidKightSwordQuest();
 
         Core.SetOptions(false);
-    }
-
-    public void VoidKightSwordQuest(string item = "Any", int quant = 1)
-    {
-        if (Core.CheckInventory(item, quant) || (!Core.CheckInventory(38275) && !Core.CheckInventory(38254)))
-            return;
-
-        Core.Logger($"Farming for {item}({quant})");
-        int i = 1;
-        while (!Bot.ShouldExit && !Core.CheckInventory(item, quant))
-        {
-            if (Core.CheckInventory(38275))
-                Core.EnsureAccept(5662);
-            else
-                Core.EnsureAccept(5659);
-            Core.HuntMonster("mobius", "Slugfit", "Slugfit Horn", 5);
-            Core.HuntMonster("faerie", "Aracara", "Aracara Silk");
-            Core.KillMonster("tercessuinotlim", "m2", "Bottom", "Dark Makai", "Makai Fang", 5);
-            Core.HuntMonster("hydra", "Fire Imp", "Imp Flame", 3);
-            Core.HuntMonster("battleunderc", "Crystalized Jellyfish", "Aquamarine of Nulgath", 3, false);
-            if (Core.CheckInventory(38275))
-                Core.EnsureComplete(5662);
-            else
-                Core.EnsureComplete(5659);
-            Bot.Drops.Pickup(Nation.bagDrops);
-            Core.Logger($"Completed x{i++}");
-        }
     }
 }
