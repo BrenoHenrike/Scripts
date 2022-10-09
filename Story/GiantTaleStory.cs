13413726b0a55a2f0627e5c83d6592cf7ada5d36
vansley123 <105498073+vansley123@users.noreply.github.com>
2022-10-09 17:49:34 -0300

    Update GiantTaleStory.cs

--- Story/GiantTaleStory.cs
+++ Story/GiantTaleStory.cs
@@ -2,7 +2,7 @@
 //cs_include Scripts/CoreStory.cs
 using Skua.Core.Interfaces;
 
-public class GiantTaleStoryWIP {
+public class GiantTaleStory {
   public IScriptInterface Bot =>IScriptInterface.Instance;
   public CoreBots Core =>CoreBots.Instance;
   public CoreStory Story = new();
@@ -23,17 +23,17 @@ public class GiantTaleStoryWIP {
   public void GiantQuests() {
     if (Core.isCompletedBefore(720)) return;
 
-    //Property Appraiser
+    //Property Appraiser 717
     Core.EquipClass(ClassType.Farm);
     Story.MapItemQuest(717, "giant", 119, 1);
 
-    //Pain in the Grass
+    //Pain in the Grass 718
     Story.KillQuest(718, "giant", "Red Ant");
 
-    //Dust Busting
+    //Dust Busting 719
     Story.KillQuest(719, "giant", "Dust Bunny");
 
-    //Cat-astrophe
+    //Cat-astrophe 720
     Core.EquipClass(ClassType.Solo);
     Story.KillQuest(720, "giant", "Giant Cat");
   }
@@ -41,20 +41,20 @@ public class GiantTaleStoryWIP {
   public void SmuurvilQuests() {
     if (Core.isCompletedBefore(725)) return;
 
-    //Skynner's List
+    //Skynner's List 721
     Core.EquipClass(ClassType.Farm);
     Story.KillQuest(721, "smuurvil", "Smuurvil");
 
-    //A Mushy Situation
+    //A Mushy Situation 722
     Story.MapItemQuest(722, "smuurvil", 122, 12);
 
-    //W-Tea F
+    //W-Tea F 723
     Story.KillQuest(723, "smuurvil", "Smuurvil");
 
-    //A Skunkweed By Any Other Name...
+    //A Skunkweed By Any Other Name... 724
     Story.KillQuest(724, "smuurvil", "Smuurvilette");
 
-    //There Is No Spoon
+    //There Is No Spoon 725
     Core.EquipClass(ClassType.Solo);
     Story.KillQuest(725, "smuurvil", "Papa Smuurvil");
   }
@@ -62,26 +62,19 @@ public class GiantTaleStoryWIP {
   public void TableAndAndreQuests() {
     if (Core.isCompletedBefore(749)) return;
 
-    //Spare Parts
-    Story.MapItemQuest(737, "table", new[] {
-      123,
-      124,
-      125,
-      126,
-      127,
-      128
-    });
+    //Spare Parts 737
+    Story.MapItemQuest(737, "table", new[] {123,124,125,126,127,128});
 
-    //Race
+    //Race 743
     Story.ChainQuest(743);
 
-    //Andre's Foot Defeated
+    //Andre's Foot Defeated 747
     Story.KillQuest(747, "andre", "Giant Foot");
 
-    //Flea be Gone
+    //Flea be Gone 748
     Story.KillQuest(748, "andre", "Giant Flea");
 
-    //Free the Key
+    //Free the Key 749
     Story.KillQuest(749, "andre", "Giant Necklace");
   }
 }
