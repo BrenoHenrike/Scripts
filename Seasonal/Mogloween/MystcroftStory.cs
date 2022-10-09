58ec49920022883ee7bddf0f692bf39ca9d96c61
vansley123 <105498073+vansley123@users.noreply.github.com>
2022-10-09 06:45:03 -0300

    Update MystcroftStory.cs

--- Seasonal/Mogloween/MystcroftStory.cs
+++ Seasonal/Mogloween/MystcroftStory.cs
@@ -2,12 +2,14 @@
 //cs_include Scripts/CoreStory.cs
 using Skua.Core.Interfaces;
 
-public class MystcroftStory {
+public class MystcroftStory
+{
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreStory Story = new();
 
-    public void ScriptMain(IScriptInterface bot) {
+    public void ScriptMain(IScriptInterface bot)
+    {
         Core.SetOptions();
 
         Storyline();
@@ -15,7 +17,8 @@ public class MystcroftStory {
         Core.SetOptions(false);
     }
 
-    public void Storyline() {
+    public void Storyline()
+    {
         if (Core.isCompletedBefore(5427))
             return;
 
@@ -45,7 +48,14 @@ public class MystcroftStory {
         Story.KillQuest(5424, "timevoid", "Void Phoenix");
 
         //Ooh, Shiny! 5425
-        Story.KillQuest(4358, "skytower", new[] { "Moonstone", "Sunstone", "Star Sapphire" });
+        if (!Story.QuestProgression(5425))
+                {
+                    Core.EnsureAccept(5425);
+                    Core.HuntMonster("skytower", "Moonstone", "Moonstone Crystal", 10);
+                    Core.HuntMonster("skytower", "Sunstone", "Sapphire Crystal", 10);
+                    Core.HuntMonster("skytower", "Star Sapphire", "Sunstone Crystal", 10);
+                    Core.EnsureComplete(5425);
+                }
 
         //Time to get Dressed Up! 5426
         Story.MapItemQuest(5426, "mystcroftforest", 4800, 1);
