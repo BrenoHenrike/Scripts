cc683ff29742916932ddf50a2e60026baffbb682
LordExelot <22625705+LordExelot@users.noreply.github.com>
2022-10-09 13:49:47 +0200

    CustomAggroMon: Now has SkipOption and Drops for generation

--- Army/CoreArmyLite.cs
+++ Army/CoreArmyLite.cs
@@ -146,13 +146,14 @@ public class CoreArmyLite
     private bool _getCellsForSmartAggroMon = false;
     private List<string> _SmartAggroMonCells = new();
 
-    public void RunGeneratedAggroMon(string map, List<string> monNames, List<int> questIDs, ClassType classtype)
+    public void RunGeneratedAggroMon(string map, List<string> monNames, List<int> questIDs, ClassType classtype, List<string>? drops = null)
     {
         Core.EquipClass(classtype);
 
         if (questIDs.Count > 0)
             Core.RegisterQuests(questIDs.ToArray());
-        else Bot.Drops.Stop();
+        else if (drops == null) 
+            Bot.Drops.Stop();
 
         SmartAggroMonStart(map, monNames.ToArray());
         while (!Bot.ShouldExit)
