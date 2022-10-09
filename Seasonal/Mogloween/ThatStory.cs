9011242002da237b5c17c2bc6b9f7b08049bada9
vansley123 <105498073+vansley123@users.noreply.github.com>
2022-10-09 06:45:53 -0300

    Update ThatStory.cs

--- Seasonal/Mogloween/ThatStory.cs
+++ Seasonal/Mogloween/ThatStory.cs
@@ -2,86 +2,68 @@
 //cs_include Scripts/CoreStory.cs
 using Skua.Core.Interfaces;
 
-public class ThatStory {
-  public IScriptInterface Bot => IScriptInterface.Instance;
-  public CoreBots Core => CoreBots.Instance;
-  public CoreStory Story = new();
+public class ThatStory
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
 
-  public void ScriptMain(IScriptInterface bot) {
-    Core.SetOptions();
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
 
-    Storyline();
+        Storyline();
 
-    Core.SetOptions(false);
-  }
+        Core.SetOptions(false);
+    }
 
-  public void Storyline() {
-    if (Core.isCompletedBefore(7179))
-      return;
+    public void Storyline()
+    {
+        if (Core.isCompletedBefore(7179))
+            return;
 
-    Story.PreLoad(this);
+        Story.PreLoad(this);
 
-    //Yeti Witual 7167
-    if (!Story.QuestProgression(7167)) {
-      Core.EnsureAccept(7167);
-      Core.Logger("Doing Quest: [7167] - \"Yeti Witual\"", "QuestProgression");
-      Core.GetMapItem(6790, 8, "that");
-      Core.HuntMonster("that", "Candy Goblin", "Witch Twuffles", 8);
-      Core.EnsureComplete(7167);
-      Core.Logger("Completed Quest: [7167] - \"Yeti Witual\"", "TryComplete");
-    } else Core.Logger("Already Completed: [7167] - \"Yeti Witual\"", "QuestProgression");
+        //Yeti Witual 7167
+        Story.MapItemQuest(7167, "that", 6790, 8);
+        Story.KillQuest(7167, "that", "Candy Goblin");
 
-    //Get the Fur 7168
-    Story.KillQuest(7168, "that", "Moglinster");
+        //Get the Fur 7168
+        Story.KillQuest(7168, "that", "Moglinster");
 
-    //Vapor Needed 7169
-    Story.KillQuest(7169, "that", "Mystcroft Ghost");
+        //Vapor Needed 7169
+        Story.KillQuest(7169, "that", "Mystcroft Ghost");
 
-    //Trap the Flame 7170
-    Story.KillQuest(7170, "that", "Will o' The Wisp");
+        //Trap the Flame 7170
+        Story.KillQuest(7170, "that", "Will o' The Wisp");
 
-    //Down the Well 7171
-    Story.MapItemQuest(7171, "that", 6791, 1);
+        //Down the Well 7171
+        Story.MapItemQuest(7171, "that", 6791, 1);
 
-    //Cweepie-cwawlie Time 7172
-    Story.KillQuest(7172, "that", new [] {
-      "Well Spider ",
-      "Congealed Fear"
-    });
+        //Cweepie-cwawlie Time 7172
+        Story.KillQuest(7172, "that", new[] { "Well Spider", "Congealed Fear" });
 
-    //Gwoop Destruction 7173
-    Story.KillQuest(7173, "that", "Congealed Fear");
+        //Gwoop Destruction 7173
+        Story.KillQuest(7173, "that", "Congealed Fear");
 
-    //Seawch for Bubble 7174
-    if (!Story.QuestProgression(7174)) {
-      Core.EnsureAccept(7174);
-      Core.Logger("Doing Quest: [7174] - \"Seawch for Bubble\"", "QuestProgression");
-      Core.GetMapItem(6792, 6, "that");
-      Core.GetMapItem(6793, 1, "that");
-      Core.EnsureComplete(7174);
-      Core.Logger("Completed Quest: [7174] - \"Seawch for Bubble\"", "TryComplete");
-    } else Core.Logger("Already Completed: [7174] - \"Seawch for Bubble\"", "QuestProgression");
+        //Seawch for Bubble 7174
+        Story.MapItemQuest(7174, "that", 6792, 6);
+        Story.MapItemQuest(7174, "that", 6793, 1);
 
-    //Free Bubble! 7175
-    if (!Story.QuestProgression(7175)) {
-      Core.EnsureAccept(7175);
-      Core.Logger("Doing Quest: [7175] - \"Free Bubble!\"", "QuestProgression");
-      Core.HuntMonster("that", "Congealed Fear", "Skeleton Key", 8);
-      Core.GetMapItem(6794, 1, "that");
-      Core.EnsureComplete(7175);
-      Core.Logger("Completed Quest: [7175] - \"Free Bubble!\"", "TryComplete");
-    } else Core.Logger("Already Completed: [7175] - \"Free Bubble!\"", "QuestProgression");
+        //Free Bubble! 7175
+        Story.KillQuest(7175, "that", "Congealed Fear");
+        Story.MapItemQuest(7175, "that", 6794, 1);
+        
+        //Echo... echo... echo 7176
+        Story.KillQuest(7176, "that", "Echo of That");
 
-    //Echo... echo... echo 7176
-    Story.KillQuest(7176, "that", "Echo of That");
+        //Gather Hope 7177
+        Story.KillQuest(7177, "that", "Shattered Hope");
 
-    //Gather Hope 7177
-    Story.KillQuest(7177, "that", "Shattered Hope");
+        //Bweak the Blacklights 7178
+        Story.KillQuest(7178, "that", "Blacklights");
 
-    //Bweak the Blacklights 7178
-    Story.KillQuest(7178, "that", "Blacklights");
-
-    //Get Wid of THAT 7179
-    Story.KillQuest(7179, "that", "That");
-  }
+        //Get Wid of THAT 7179
+        Story.KillQuest(7179, "that", "That");
+    }
 }
