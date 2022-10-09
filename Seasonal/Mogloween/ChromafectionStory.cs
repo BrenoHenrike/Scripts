e5ee3d64f8fc8514d2281a97a8ba9457302debc3
vansley123 <105498073+vansley123@users.noreply.github.com>
2022-10-09 06:44:18 -0300

    Update ChromafectionStory.cs

--- Seasonal/Mogloween/ChromafectionStory.cs
+++ Seasonal/Mogloween/ChromafectionStory.cs
@@ -2,12 +2,14 @@
 //cs_include Scripts/CoreStory.cs
 using Skua.Core.Interfaces;
 
-public class ChromafectionStory {
+public class ChromafectionStory
+{
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreStory Story = new();
 
-    public void ScriptMain(IScriptInterface bot) {
+    public void ScriptMain(IScriptInterface bot)
+    {
         Core.SetOptions();
 
         Storyline();
@@ -15,7 +17,8 @@ public class ChromafectionStory {
         Core.SetOptions(false);
     }
 
-    public void Storyline() {
+    public void Storyline()
+    {
         if (Core.isCompletedBefore(6537))
             return;
 
@@ -37,7 +40,7 @@ public class ChromafectionStory {
         Story.MapItemQuest(6535, "chromafection", 6007, 15);
 
         //Whacky Snacky 6536
-        Story.MapItemQuest(6536, "chromafection", 6537, 1);
+        Story.MapItemQuest(6536, "chromafection", 6005, 1);
 
         //Chroma CRASH 6537
         Story.KillQuest(6537, "chromafection", "Chromafection");
