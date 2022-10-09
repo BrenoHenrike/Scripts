b695ca44615343c17fa66c33ac46cfc893f37cf3
vansley123 <105498073+vansley123@users.noreply.github.com>
2022-10-08 21:14:16 -0300

    Updated with better format

--- Seasonal/Mogloween/MystcroftStory.cs
+++ Seasonal/Mogloween/MystcroftStory.cs
@@ -2,14 +2,12 @@
 //cs_include Scripts/CoreStory.cs
 using Skua.Core.Interfaces;
 
-public class MystcroftStory
-{
+public class MystcroftStory {
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreStory Story = new();
 
-    public void ScriptMain(IScriptInterface bot)
-    {
+    public void ScriptMain(IScriptInterface bot) {
         Core.SetOptions();
 
         Storyline();
@@ -17,8 +15,7 @@ public class MystcroftStory
         Core.SetOptions(false);
     }
 
-    public void Storyline()
-    {
+    public void Storyline() {
         if (Core.isCompletedBefore(5427))
             return;
 
@@ -26,45 +23,43 @@ public class MystcroftStory
 
         //Tome of The Otherworld 5418
         Story.KillQuest(5418, "mystcroftforest", "Eerie Apparition");
-		
+
         //Monster Data 5419
         Story.KillQuest(5419, "mystcroftforest", "Eerie Apparition");
-		
+
         //The Best Costumes Need the Best Fabric 5420
         Story.KillQuest(5420, "mystcroftforest", "Eerie Apparition");
-		
+
         //Accessorize! 5421
         Story.MapItemQuest(5421, "mystcroftforest", 4801, 8);
-		Story.KillQuest(5421, "mystcroftforest", "Grim Goblin");
+        Story.KillQuest(5421, "mystcroftforest", "Grim Goblin");
 
         //Make those Munchies 5422
         Story.KillQuest(5422, "mystcroftforest", "Eerie Apparition");
-		Story.MapItemQuest(5422, "mystcroftforest", 4802, 8);
-		
-		//Every Party Needs Drinks 5423
-		Story.KillQuest(5423, "mystcroftforest", "Grim Goblin");
-		
-		//Beautiful Creatures and their Uses 5424
-		Story.KillQuest(5424, "timevoid", "Void Phoenix");
-		
-		//Ooh, Shiny! 5425
-		if (!Story.QuestProgression(5425))
-        {
+        Story.MapItemQuest(5422, "mystcroftforest", 4802, 8);
+
+        //Every Party Needs Drinks 5423
+        Story.KillQuest(5423, "mystcroftforest", "Grim Goblin");
+
+        //Beautiful Creatures and their Uses 5424
+        Story.KillQuest(5424, "timevoid", "Void Phoenix");
+
+        //Ooh, Shiny! 5425
+        if (!Story.QuestProgression(5425)) {
             Core.EnsureAccept(5425);
             Core.Logger("Doing Quest: [5425] - \"Ooh, Shiny!\"", "QuestProgression");
             Core.HuntMonster("skytower", "Moonstone", "Moonstone Crystal", 10);
             Core.HuntMonster("skytower", "Sunstone", "Sunstone Crystal", 10);
-			Core.HuntMonster("skytower", "Star Sapphire", "Sapphire Crystal", 10);
+            Core.HuntMonster("skytower", "Star Sapphire", "Sapphire Crystal", 10);
             Core.EnsureComplete(5425);
             Core.Logger("Completed Quest: [5425] - \"Ooh, Shiny!\"", "TryComplete");
-        }
-        else Core.Logger("Already Completed: [5425] - \"Ooh, Shiny!\"", "QuestProgression");
-		
-		//Time to get Dressed Up! 5426
-		Story.MapItemQuest(5426, "mystcroftforest", 4800, 1);
-		Story.KillQuest(5426, "mystcroftforest", "Grim Goblin");
-		
-		//EEEEK 5427
-		Story.KillQuest(5427, "mystcroftforest", "Barghest");
+        } else Core.Logger("Already Completed: [5425] - \"Ooh, Shiny!\"", "QuestProgression");
+
+        //Time to get Dressed Up! 5426
+        Story.MapItemQuest(5426, "mystcroftforest", 4800, 1);
+        Story.KillQuest(5426, "mystcroftforest", "Grim Goblin");
+
+        //EEEEK 5427
+        Story.KillQuest(5427, "mystcroftforest", "Barghest");
     }
 }
