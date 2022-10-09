ec29dd08701151db6360ffdd6069f0a108a5011a
vansley123 <105498073+vansley123@users.noreply.github.com>
2022-10-09 06:46:14 -0300

    Update TwigsArcadeStory.cs

--- Seasonal/Mogloween/TwigsArcadeStory.cs
+++ Seasonal/Mogloween/TwigsArcadeStory.cs
@@ -2,12 +2,14 @@
 //cs_include Scripts/CoreStory.cs
 using Skua.Core.Interfaces;
 
-public class TwigsArcadeStory {
+public class TwigsArcadeStory
+{
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreStory Story = new();
 
-    public void ScriptMain(IScriptInterface bot) {
+    public void ScriptMain(IScriptInterface bot)
+    {
         Core.SetOptions();
 
         Storyline();
@@ -15,29 +17,19 @@ public class TwigsArcadeStory {
         Core.SetOptions(false);
     }
 
-    public void Storyline() {
+    public void Storyline()
+    {
         if (Core.isCompletedBefore(6579))
             return;
 
         Story.PreLoad(this);
 
         //Stop the Hurting 6568
-        Story.KillQuest(6568, "twigsarcade", new [] {
-            "Scotty Sneevil",
-            "Clucky Moo"
-        });
+        Story.KillQuest(6568, "twigsarcade", new[] { "Scotty Sneevil", "Clucky Moo" });
 
         //Gather the Goods 6569
-        if (!Story.QuestProgression(6569)) {
-            Core.EnsureAccept(6569);
-            Core.Logger("Doing Quest: [6569] - \"Gather the Goods\"", "QuestProgression");
-            Core.HuntMonster("twigsarcade", "Scotty Sneevil", "Wires", 5);
-            Core.HuntMonster("twigsarcade", "Scotty Sneevil", "Small Screw", 16);
-            Core.GetMapItem(6070, 3, "twigsarcade");
-            Core.GetMapItem(6069, 3, "twigsarcade");
-            Core.EnsureComplete(6569);
-            Core.Logger("Completed Quest: [6569] - \"Gather the Goods\"", "TryComplete");
-        } else Core.Logger("Already Completed: [6569] - \"Gather the Goods\"", "QuestProgression");
+        Story.MapItemQuest(6569, "twigsarcade", new[] { 6069, 6070 });
+        Story.KillQuest(6569, "twigsarcade", "Scotty Sneevil");
 
         //Spooky Casings 6570
         Story.KillQuest(6570, "twigsarcade", "Scotty Sneevil");
@@ -49,10 +41,7 @@ public class TwigsArcadeStory {
         Story.MapItemQuest(6572, "twigsarcade", 6071, 1);
 
         //Test the Ghost box 6573
-        Story.KillQuest(6573, "twigsarcade", new [] {
-            "Scotty Sneevil",
-            "Clucky Moo"
-        });
+        Story.KillQuest(6573, "twigsarcade", new[] { "Scotty Sneevil", "Clucky Moo" });
 
         //Get Wid of CWEEPY TWIG 6574
         Story.KillQuest(6574, "twigsarcade", "Cweepy Twig");
