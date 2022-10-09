d513c49fd1b186131051719405469a16994f97c8
Tato <47118168+wtffidy@users.noreply.github.com>
2022-10-09 09:54:58 -0400

    Update BloodMoonToken.cs

--- Other/BloodMoonToken.cs
+++ Other/BloodMoonToken.cs
@@ -21,12 +21,12 @@ public class BloodMoonToken
             return;
         Core.FarmingLogger("Blood Moon Token", 300);
         Core.AddDrop("Blood Moon Token");
-        // Core.RegisterQuests(Core.IsMember ? 6060 : 6059); // uncomment when registerquest is fixed. if more then 1 item is found in inv it only complets once then afks
+        // Core.RegisterQuests(Core.IsMember ? 6060 : 6059); // uncomment when registerquest is fixed. if more then 1 item is found in inv it only complets once then afks/
         while (!Bot.ShouldExit && !Core.CheckInventory("Blood Moon Token", 300))
         {
             Core.EnsureAccept(Core.IsMember ? 6060 : 6059);
-            Core.HuntMonster("bloodmoon", "Black Unicorn", "Black Blood Vial", isTemp: false);
-            Core.HuntMonster("bloodmoon", "Lycan Guard", "Moon Stone", isTemp: false);
+            Core.KillMonster("bloodmoon", "r12a", "Left", "Black Unicorn", "Black Blood Vial", isTemp: false);
+            Core.KillMonster("bloodmoon", "r4a", "Left", "Lycan Guard", "Moon Stone", isTemp: false);
             Core.EnsureComplete(Core.IsMember ? 6060 : 6059);
             Bot.Wait.ForPickup("Blood Moon Token");
         }
