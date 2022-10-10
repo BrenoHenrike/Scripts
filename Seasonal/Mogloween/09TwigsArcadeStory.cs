e38509d47fd397d0738aad197c41de9a17451f62
PapaShokry <islamshokry7@gmail.com>
2022-10-10 15:25:54 +0200

    CoreMogloween: arranging and adding all event story quests

---
+++ Seasonal/Mogloween/09TwigsArcadeStory.cs
@@ -0,0 +1,22 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Seasonal/Mogloween/CoreMogloween.cs
+using Skua.Core.Interfaces;
+
+public class TwigsArcade
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+    public CoreMogloween CoreMogloween = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        CoreMogloween.TwigsArcade();
+
+        Core.SetOptions(false);
+    }
+
+}
