ae1f5d7714dfefe04ffb9adba61917f1c9c8bfb4
PapaShokry <islamshokry7@gmail.com>
2022-10-14 14:10:13 +0200

    Seasonal Stuff From Grimm bots

---
+++ Seasonal/IaraStory.cs
@@ -0,0 +1,41 @@
+using System.Reflection.PortableExecutable;
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using Skua.Core.Interfaces;
+
+public class Iara
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        StoryLine();
+
+        Core.SetOptions(false);
+    }
+
+    public void StoryLine()
+    {
+        if (Core.isCompletedBefore(8288) && Core.isSeasonalMapActive("iara"))
+            return;
+
+        Story.PreLoad(this);
+
+        //Elemental Balance 8258
+        Story.KillQuest(8258, "iara", "Earth Elemental");
+
+        //Bandits and Treasure Hunters 8259
+        Story.KillQuest(8259, "iara", new[] { "Bandit", "Treasure Hunter" });
+
+        //Plants be Spooky 8260
+        Story.KillQuest(8260, "iara", "Seed Spitter");
+
+        //The Test of Will 8261
+        Story.KillQuest(8261, "iara", "Iara");
+
+    }
+}
