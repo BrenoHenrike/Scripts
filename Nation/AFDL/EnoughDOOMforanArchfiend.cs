f8dd7a9b0fcba657c17f9bc66893579d50e4bac0
Tato <47118168+wtffidy@users.noreply.github.com>
2022-10-21 07:48:17 -0400

    fuck  you purple :thumbsup:

--- Nation/AFDL/EnoughDOOMforanArchfiend.cs
+++ Nation/AFDL/EnoughDOOMforanArchfiend.cs
@@ -12,6 +12,7 @@ public class EnoughDOOMforanArchfiend
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreFarms Farm = new CoreFarms();
+    public CoreAdvanced Adv = new();
     public CoreNation Nation = new();
 
     public WillpowerExtraction WillpowerExtraction = new WillpowerExtraction();
@@ -41,37 +42,36 @@ public class EnoughDOOMforanArchfiend
             "Undead Energy", "DoomLord's War Mask", "ShadowFiend Cloak", "Locks of the DoomLord", "Doomblade of Destruction");
 
         // Quest Accept Requirements: "DoomLord's War Mask", "ShadowFiend Cloak", "Locks of the DoomLord", "Doomblade of Destruction" 
+        Nation.FarmUni13();
+        Nation.ApprovalAndFavor(0, 1);
         NulgathDemandsWork.NDWQuest();
 
         //Quest Turnin Items:
-        WillpowerExtraction.Unidentified34(4);
-        NulgathDemandsWork.NDWQuest(new[] {"Unidentified 35"});
-        Nation.ContractExchange(ChooseReward.BloodGemoftheArchfiend);
-        Nation.FarmUni13();
-        Nation.ApprovalAndFavor(0, 5000);
+        Nation.FarmVoucher(false);
         Nation.EssenceofNulgath(100);
-
+        Farm.BattleUnderB("Undead Essence", 1000);
+        Core.EnsureAccept(5260);
+        Core.EquipClass(ClassType.Farm);
+        Core.KillMonster("orecavern", "r3", "Up", "*", "Chaorruption Essence", 75, false);
+        
+        Adv.BuyItem("alchemyacademy", 2115, "Essence Potion", 5, 1, 9770); // see if this works
+        // if (!Core.CheckInventory("Essence Potion", 5))
+        // {
+        //     Farm.Gold(12500000);
+        //     Core.BuyItem("alchemyacademy", 2115, "Gold Voucher 500k", 25);
+        //     Core.BuyItem("alchemyacademy", 2115, "Essence Potion", 5, 1, 9770);
+        //     Bot.Wait.ForItemBuy();
+        // }
+        
+        Nation.ApprovalAndFavor(0, 5000);        
         Core.EquipClass(ClassType.Solo);
         Core.HuntMonster("evilwardage", "Klunk", "Essence of Klunk", isTemp: false);
-        Core.EquipClass(ClassType.Farm);
-        Farm.BattleUnderB("Undead Essence", 1000);
-
-        Nation.FarmVoucher(false);
         Nation.FarmBloodGem(10);
-
-        Core.BuyItem("yulgar", 16, "Aelita's Emerald");
-
-        if (!Core.CheckInventory("Essence Potion", 5))
-        {
-            Farm.Gold(12500000);
-            Core.BuyItem("alchemyacademy", 2115, "Gold Voucher 500k", 25);
-            Core.BuyItem("alchemyacademy", 2115, "Essence Potion", 5, 1, 9770);
-            Bot.Wait.ForItemBuy();
-        }
-        Core.EnsureAccept(5260);
-
-        Core.KillMonster("orecavern", "r3", "Up", "*", "Chaorruption Essence", 75, false);
+        Core.EquipClass(ClassType.Farm);
         Core.HuntMonster("starsinc", "Living Star", "Living Star Essence", 100, false);
+        WillpowerExtraction.Unidentified34(4);
+        NulgathDemandsWork.NDWQuest(new[] {"Unidentified 35"});
+        Core.BuyItem("yulgar", 16, "Aelita's Emerald");
 
         //Quest Turnin
         if (!Bot.Quests.CanComplete(5260))
