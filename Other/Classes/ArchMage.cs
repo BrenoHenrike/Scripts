340716765a7eb2052be2217c0f82e7b6aba5dade
PapaShokry <islamshokry7@gmail.com>
2022-10-30 01:42:26 +0200

    ArchMage: changing Hunt To kill for Unrecognized monster

--- Other/Classes/ArchMage.cs
+++ Other/Classes/ArchMage.cs
@@ -298,16 +298,16 @@ public class Archmage
             Core.FarmingLogger("Mystic Quills", 49);
             Core.FarmingLogger("Mystic Shards", 49);
             Core.RegisterQuests(3050);
-            while (!Bot.ShouldExit && !Core.CheckInventory(new[] {"Mystic Shards", "Mystic Quills"}, 49))
+            while (!Bot.ShouldExit && !Core.CheckInventory(new[] { "Mystic Shards", "Mystic Quills" }, 49))
             {
                 Core.HuntMonster("gilead", "Water Elemental", "Water Core", log: false);
                 Core.HuntMonster("gilead", "Fire Elemental", "Fire Core", log: false);
                 Core.HuntMonster("gilead", "Air Elemental", "Air Core", log: false);
                 Core.HuntMonster("gilead", "Earth Elemental", "Earth Core", log: false);
-                Core.HuntMonster("gilead", "Mana Elemental", "Mana Core", log: false);
+                Core.KillMonster("gilead", "r8", "Left", "Mana Elemental", "Mana Core", log: false);
             }
             Core.CancelRegisteredQuests();
-            
+
             Core.EquipClass(ClassType.Solo);
             if (!Core.CheckInventory("Semiramis Feather"))
             {
