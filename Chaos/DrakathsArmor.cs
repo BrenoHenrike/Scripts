1986cdb1bf8dccd80384aa8e8ce03b6186a4451a
Tato <47118168+wtffidy@users.noreply.github.com>
2022-11-05 07:32:17 -0400

    Update DrakathsArmor.cs

--- Chaos/DrakathsArmor.cs
+++ Chaos/DrakathsArmor.cs
@@ -29,20 +29,18 @@ public class DrakathArmorBot
     }
 
     public void GetBoth()
-    {
-        DrakathArmor();
-        DrakathOriginalArmor();
+    {        
+        LOC.Complete13LOC();
+        DrakathArmorQuest();
+        Core.BuyItem("battleon", 994, "Original Drakath Armor");
+        Core.BuyItem("battleon", 994, "Drakath Armor");
     }
 
     public void DrakathArmor()
     {
         if (Core.CheckInventory("Drakath Armor"))
             return;
-        Core.Logger("Attempting Purchase, will check if shop is unlocked. Next message may be incorrect.");
-        Core.BuyItem("battleon", 994, "Drakath Armor");
-        Bot.Sleep(Core.ActionDelay);
-        if (Core.CheckInventory("Drakath Armor"))
-            return;
+            
         DrakathArmorQuest();
         Core.BuyItem("battleon", 994, "Drakath Armor");
     }
@@ -51,11 +49,7 @@ public class DrakathArmorBot
     {
         if (Core.CheckInventory("Original Drakath Armor"))
             return;
-        Core.Logger("Attempting Purchase, will check if shop is unlocked. Next message may be incorrect.");
-        Core.BuyItem("battleon", 994, "Original Drakath Armor");
-        Bot.Sleep(Core.ActionDelay);
-        if (Core.CheckInventory("Original Drakath Armor"))
-            return;
+            
         LOC.Complete13LOC();
         DrakathArmorQuest();
         Core.BuyItem("battleon", 994, "Original Drakath Armor");
@@ -64,16 +58,7 @@ public class DrakathArmorBot
     public void DrakathArmorQuest()
     {
         Core.AddDrop("Dage's Scroll Fragment", "Treasure Chest", "Face of Chaos", "Get Your Original Drakath's Armor");
-        if (!Core.CheckInventory("Dage's Scroll Fragment", 13))
-        {
-            Bot.Sleep(Core.ActionDelay);
-            Daily.DagesScrollFragment();
-        }
-        if (!Core.CheckInventory("Dage's Scroll Fragment", 13))
-        {
-            Core.Logger("You dont have Dage's Scroll Fragment x13", messageBox: true);
-            return;
-        }
+
         Core.EnsureAccept(3882);
         Farm.BladeofAweREP(6, farmBoA: true);
         BLOD.DoAll();
@@ -83,8 +68,11 @@ public class DrakathArmorBot
         Core.EquipClass(ClassType.Farm);
         Core.KillMonster("swordhavenundead", "Left", "Right", "*", "Treasure Chest", 100, false);
         Core.EquipClass(ClassType.Solo);
-        Core.KillMonster("ultradrakath", "r1", "Left", "Champion of Chaos", "Face of Chaos", 1, false, publicRoom: true);
+        Core.KillMonster("ultradrakath", "r1", "Left", "Champion of Chaos", "Face of Chaos", isTemp: false, publicRoom: true);
         Core.Relogin();
-        Core.EnsureComplete(3882);
+        Daily.DagesScrollFragment();
+        if (!Core.CheckInventory("Dage's Scroll Fragment", 13))
+            Core.Logger($"You Own Dage's Scroll Fragment x{Bot.Inventory.GetQuantity("Dage's Scroll Fragment")}/{13} (Daily Quest), Bot can not continue.", messageBox: true, stopBot: true);
+        else Core.EnsureComplete(3882);
     }
 }
