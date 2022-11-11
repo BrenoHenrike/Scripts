8b7a3e5a6f909197cfd503ab536eeeca32fa45de
vanilla99-git <105498073+vanilla99-git@users.noreply.github.com>
2022-11-11 20:18:14 -0300

    Add files via upload

---
+++ Story/SepulchureSaga/04ShadowfallRise.cs
@@ -0,0 +1,22 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/SepulchureSaga/CoreSepulchure.cs
+using Skua.Core.Interfaces;
+
+public class ShadowfallRise
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+    public CoreSepulchure CoreSS = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        CoreSS.ShadowfallRise();
+
+        Core.SetOptions(false);
+    }
+
+}
