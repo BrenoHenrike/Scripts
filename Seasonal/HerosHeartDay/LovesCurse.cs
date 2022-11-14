4ea74ebeb94d7099ee846b893a3e3e62e2f73994
Vld Mrk <vladamark00@gmail.com>
2022-11-14 17:05:13 +0100

    turns out CoreFarms is not needed

--- Seasonal/HerosHeartDay/LovesCurse.cs
+++ Seasonal/HerosHeartDay/LovesCurse.cs
@@ -1,6 +1,5 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreStory.cs
-//cs_include Scripts/CoreFarms.cs
 using Skua.Core.Interfaces;
 
 public class LovesCurse
@@ -8,7 +7,6 @@ public class LovesCurse
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreStory Story = new();
-    public CoreFarms Farm = new();
 
     public void ScriptMain(IScriptInterface bot)
     {
