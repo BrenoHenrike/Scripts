d389242775dfbf444a0d0ee5d7376bf898f3c038
PapaShokry <islamshokry7@gmail.com>
2022-11-17 00:43:57 +0200

    Mazumi: Fix prolly  Killmon Prob or a farm class

--- Story/Mazumi.cs
+++ Story/Mazumi.cs
@@ -31,6 +31,16 @@ public class Mazumi
 
         // Without a Trace 91
         Story.KillQuest(91, "greenguardwest", new[] { "Kittarian", "River Fishman", "Slime", "River Fishman", "Big Bad Boar" });
+        if (!Story.QuestProgression(91))
+        {
+            Core.EnsureAccept(91);
+            Core.HuntMonster("greenguardwest", "Kittarian", "Kittarian's Walle", 2);
+            Core.HuntMonster("greenguardwest", "River Fishman", "River Fishman's Wallet", 2);
+            Core.HuntMonster("greenguardwest", "Slime", "Slime-Soaked Wallet", 2);
+            Core.HuntMonster("greenguardwest", "Frogzard", "Frogzard's Lint Hoard", 2);
+            Core.HuntMonster("greenguardwest", "Big Bad Boar", "Big Bad Boar's Wallet", 2);
+            Core.EnsureComplete(91);
+        }
 
         // Hit Job 92
         Story.KillQuest(92, "greenguardwest", new[] { "Breken the Vile", "Ogug Stoneaxe" });
