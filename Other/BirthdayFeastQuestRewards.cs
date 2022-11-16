61c831808282b54c9baeaf90497aa215cb6380e2
Tato <47118168+wtffidy@users.noreply.github.com>
2022-11-16 14:11:08 -0500

    all 4 done, deleted the quest bit left there sorry..

--- Other/BirthdayFeastQuestRewards.cs
+++ Other/BirthdayFeastQuestRewards.cs
@@ -14,13 +14,66 @@ public class BirthdayFeastQuestRewards
     {
         Core.SetOptions();
 
-        CoreSS.ShadowfallRise();
-        GetItems();
+        ArlettesQuests();
+        InanitasQuests();
+        MemetsQuests();
+        KotarosQuests();
 
         Core.SetOptions(false);
     }
 
-    public void GetItems()
+    void ArlettesQuests()
+    {
+        List<Skua.Core.Models.Items.ItemBase> RewardOptions = Core.EnsureLoad(8385).Rewards;
+
+        foreach (ItemBase item in RewardOptions)
+            Core.AddDrop(item.Name);
+
+        Core.EquipClass(ClassType.Solo);
+        Core.RegisterQuests(8385);
+        foreach (ItemBase Reward in RewardOptions)
+        {
+            if (Core.CheckInventory(Reward.Name, toInv: false))
+            {
+                Core.Logger($"{Reward.Name} Found.");
+                return;
+            }
+            Core.FarmingLogger(Reward.Name, 1);
+
+            Core.HuntMonster("celestialrealm", "Celestial Bird of Paradise", "Celestial Artifact", 6, log: false);
+            Core.HuntMonster("celestialrealm", " Infernal Imp| Infernal Knight", "Infernal Artifact ", 6, log: false);
+            Core.JumpWait();
+            Core.ToBank(Reward.Name);
+        }
+    }
+
+    void InanitasQuests()
+    {
+        List<Skua.Core.Models.Items.ItemBase> RewardOptions = Core.EnsureLoad(8384).Rewards;
+
+        foreach (ItemBase item in RewardOptions)
+            Core.AddDrop(item.Name);
+
+        Core.EquipClass(ClassType.Solo);
+
+        Core.RegisterQuests(8384);
+        foreach (ItemBase Reward in RewardOptions)
+        {
+            if (Core.CheckInventory(Reward.Name, toInv: false))
+            {
+                Core.Logger($"{Reward.Name} Found.");
+                return;
+            }
+
+            Core.FarmingLogger(Reward.Name, 1);
+            Core.HuntMonster("timeinn", "Ezrajal", "Ezrajal's Feather", log: false);
+            Core.JumpWait();
+            Core.ToBank(Reward.Name);
+        }
+
+    }
+
+    void KotarosQuests()
     {
         List<Skua.Core.Models.Items.ItemBase> RewardOptions = Core.EnsureLoad(8383).Rewards;
 
@@ -29,18 +82,47 @@ public class BirthdayFeastQuestRewards
 
         Core.EquipClass(ClassType.Solo);
 
+        Core.RegisterQuests(8383);
         foreach (ItemBase Reward in RewardOptions)
         {
             if (Core.CheckInventory(Reward.Name, toInv: false))
+            {
+                Core.Logger($"{Reward.Name} Found.");
                 return;
-                
+            }
+
             Core.FarmingLogger(Reward.Name, 1);
 
-            Core.EnsureAccept(8383);
             Core.HuntMonster("shinkansen", "Saint Eta", "Saint Eta's Gauntlet", log: false);
-            Core.EnsureComplete(8383);
-            Core.JumpWait();            
+            Core.JumpWait();
             Core.ToBank(Reward.Name);
         }
+    }
+
+    void MemetsQuests()
+    {
+        List<Skua.Core.Models.Items.ItemBase> RewardOptions = Core.EnsureLoad(8384).Rewards;
+
+        foreach (ItemBase item in RewardOptions)
+            Core.AddDrop(item.Name);
+
+        Core.RegisterQuests(8384);
+        Core.EquipClass(ClassType.Solo);
+
+        foreach (ItemBase Reward in RewardOptions)
+        {
+            if (Core.CheckInventory(Reward.Name, toInv: false))
+            {
+                Core.Logger($"{Reward.Name} Found.");
+                return;
+            }
+
+            Core.FarmingLogger(Reward.Name, 1);
+            Core.HuntMonster("byrodax", "Mutated Critter", "Picture of Mutated Critter", 3, log: false);
+            Core.HuntMonster("byrodax", "Byro-Dax Monstrosity", "Picture of Byro-Dax Monstrosity", log: false);
+            Core.JumpWait();
+            Core.ToBank(Reward.Name);
+        }
+
     }
 }
