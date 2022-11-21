5c789c0bae7a9e4d383ed6e4aaecba512ae38456
JechtAQW <117804788+JechtAQW@users.noreply.github.com>
2022-11-21 11:40:38 -0700

    Added full Apotheosis merge and misc fixes
    Replaced while loops with killmonsters and some other stuff u guys recommended. Added full apotheosis merge too. Stops if missing the required total number of insignias though. Maybe a try/catch would be better for the apotheosis merging part so that if user has some of the weapons already they won't have to get extra insignias for it to continue but idk how to implement that well.

--- ExaltedApotheosisPreReqs.cs
+++ ExaltedApotheosisPreReqs.cs
@@ -19,41 +19,45 @@
 
         public void PreReqs()
         {
-            Core.AddDrop("Exalted Node", "Thaumaturgus Alpha", "Apostate Alpha", "Exalted Relic Piece", "Exalted Forgemetal", "Exalted Artillery Shard");
             if (Core.CheckInventory("Exalted Node", 300) && Core.CheckInventory("Thaumaturgus Alpha") && Core.CheckInventory("Apostate Alpha"))
                 Core.Logger("Got all prerequisites! Kill the ultra bosses manually for insignias next to complete Exalted Apotheosis.");
                 return;
 
+            /// No ultras required
             if (!Core.CheckInventory("Apostate Alpha") && !Core.CheckInventory("Thaumaturgus Alpha"))
-                Core.EquipClass(ClassType.Solo);
-                while (!Bot.ShouldExit && !Core.CheckInventory("Exalted Node", 300))
                 {
-                    Core.KillMonster("timeinn", "r3", "Top", "Energy Elemental", log: true);
-                }
+                    Core.EquipClass(ClassType.Solo);
                 
-                while (!Bot.ShouldExit && !Core.CheckInventory("Exalted Relic Piece", 10))
-                {
-                    Core.KillMonster("timeinn", "r7", "Bottom", "The Warden", log: true);
-                }
+                    Core.KillMonster("timeinn", "r3", "Top", "Energy Elemental", "Exalted Node", 300, isTemp: false);
+                    Core.KillMonster("timeinn", "r7", "Bottom", "The Warden", "Exalted Relic Piece", 10, isTemp: false);
+                    Core.KillMonster("timeinn", "r8", "Left", "The Engineer", "Exalted Artillery Shard", 10, isTemp: false);
+                    Core.KillMonster("timeinn", "r6", "Left", "Ezrajal", "Exalted Forgemetal", 10, isTemp: false);
                 
-                while (!Bot.ShouldExit && !Core.CheckInventory("Exalted Artillery Shard", 10))
-                {
-                    Core.KillMonster("timeinn", "r8", "Left", "The Engineer", log: true);
-                }
+                    Core.BuyItem("timeinn", 2010, "Apostate Alpha");
+                    Core.BuyItem("timeinn", 2010, "Thaumaturgus Alpha");
                 
-                while (!Bot.ShouldExit && !Core.CheckInventory("Exalted Forgemetal", 10))
-                {
-                    Core.KillMonster("timeinn", "r6", "Left", "Ezrajal", log: true);
-                }
-                
-                Core.BuyItem("timeinn", 2010, "Apostate Alpha");
-                Core.BuyItem("timeinn", 2010, "Thaumaturgus Alpha");
-                
-                while(!Bot.ShouldExit && !Core.CheckInventory("Exalted Node", 300))
-                {
-                    Core.KillMonster("timeinn", "r3", "Top", "Energy Elemental", log: true);
-                }
-                Core.Logger("Got all prerequisites! Kill the ultra bosses manually for insignias next to complete Exalted Apotheosis.");
-            return; 
+                    Core.KillMonster("timeinn", "r3", "Top", "Energy Elemental", "Exalted Node", 300, isTemp: false);
+                    
+                    Core.Logger("Got all prerequisites! Kill the ultra bosses manually for insignias next to complete Exalted Apotheosis.");
+                } 
+
+            /// Apotheosis merge once got insignias
+            if (Core.CheckInventory("Ezrajal Insignia", 24) && Core.CheckInventory("Warden Insignia", 24) && Core.CheckInventory("Engineer Insignia", 16))
+            {
+                Core.BuyItem("timeinn", 2010, "Apostate Omega");
+                Core.BuyItem("timeinn", 2010, "Apostate Ultima");
+                Core.BuyItem("timeinn", 2010, "Thaumaturgus Omega");
+                Core.BuyItem("timeinn", 2010, "Thaumaturgus Ultima");
+                Core.BuyItem("timeinn", 2010, "Exalted Unity");
+                Core.BuyItem("timeinn", 2010, "Exalted Penultima");
+                Core.BuyItem("timeinn", 2010, "Exalted Apotheosis");
+                Core.Logger("Congratulations on completing the Exalted Apotheosis weapon!");
+            }
+
+            else            
+            {
+                Core.Logger("Please obtain a total of 24 Ezrajal and Warden Insignias and 16 Engineer Insignias with your army to complete the merge. Currently our boats can't do the Ultra Bosses for you until CoreArmy is finished.");
+                return;
+            }
         }
     }
