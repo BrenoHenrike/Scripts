41ea21ef508fab1ed23d23a48b50e6d953d228f6
JechtAQW <117804788+JechtAQW@users.noreply.github.com>
2022-11-25 10:10:07 -0700

    Added Exe's fixes
    Not sure if the smartaggromon can be in a while loop so lmk.

--- Army/ArmyLightCasterFull.cs
+++ Army/ArmyLightCasterFull.cs
@@ -24,6 +24,8 @@ public class ArmyLightCasterFull
     private static CoreArmyLite sArmy = new();
     public LightMage LM = new LightMage();
     public BurningBladeOfAbezeth BBOA = new BurningBladeOfAbezeth();
+    public string OptionsStorage = "ArmyLightCasterFull";
+    public bool DontPreconfigure = true;
     public List<IOption> Options = new List<IOption>()
     {
         sArmy.player1,
@@ -37,19 +39,28 @@ public class ArmyLightCasterFull
 
     public void ScriptMain(IScriptInterface bot)
     {
-        Core.BankingBlackList.AddRange(Loot);
         Core.SetOptions();
-        bot.Options.RestPackets = false;
-        if (!Core.CheckInventory(31058) || !Core.CheckInventory(30266) || 
-            !Core.CheckInventory(31019) || !Core.CheckInventory(31028))
-        {
-            AggroPart();
-        }
-        GetLC();
+        FullLC();
         Core.SetOptions(false);
     }
 
-    public void AggroPart()
+    public void FullLC()
+    {   
+        Core.BankingBlackList.AddRange(Loot);
+        bot.Options.RestPackets = false;
+        /*Checking if missing burning blade or the other 3 quest swords from lostruinswar, didn't work without using item ids specifically*/
+        if (!Core.CheckInventory(31058) || !Core.CheckInventory(30266) || 
+            !Core.CheckInventory(31019) || !Core.CheckInventory(31028))
+        {
+            ///AggroPart();
+            while (!Bot.ShouldExit && !Core.CheckInventory("Trapped Spirits", 500) && !Core.CheckInventory("Energy of Death", 500) && !Core.CheckInventory("Captured Time", 500))
+                sArmy.SmartAggroMonStart("lostruinswar", monsters);
+        }
+        GetLC();
+    }
+    
+    
+    /*public void AggroPart()
     {
         Core.PrivateRooms = true;
         Core.PrivateRoomNumber = Army.getRoomNr();
@@ -61,11 +72,11 @@ public class ArmyLightCasterFull
         Army.AggroMonStart("lostruinswar");
         Army.DivideOnCells("r2", "r3", "r4", "r5", "r6", "r7");
 
-        while (!Bot.ShouldExit && Core.CheckInventory("Trapped Spirits", 500) && Core.CheckInventory("Energy of Death", 500) && Core.CheckInventory("Captured Time", 500) )
+        while (!Bot.ShouldExit && !Core.CheckInventory("Trapped Spirits", 500) && !Core.CheckInventory("Energy of Death", 500) && !Core.CheckInventory("Captured Time", 500))
             Bot.Combat.Attack("*");
         Army.AggroMonStop(true);
         Core.EnsureComplete(4510, 4511, 4512);
-    }
+    }*/
 
     public void GetLC()
     {
@@ -85,4 +96,5 @@ public class ArmyLightCasterFull
         Adv.rankUpClass("LightCaster");
     }
     private string[] Loot = {"Trapped Spirits", "Energy of Death", "Captured Time"};
+    private string[] monsters = {"Fallen Knight", "Infernal Imp", "Underworld Hound"};
 }
