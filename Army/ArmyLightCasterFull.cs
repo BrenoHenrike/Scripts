84dbea59a8d349cd40a9d9cb6db00cc40598d0d6
JechtAQW <117804788+JechtAQW@users.noreply.github.com>
2022-11-25 00:59:49 -0700

    Revamped entire bot
    Now farms the 4 15% swords via aggromon with 6 alts following the archfiend favor aggromon logic, then does same steps as original Lightcaster.cs to get bboa lightmage and merge the class. Tested on my army and worked for me. Will run more tests tomorrow on the remaining armies.

--- Army/ArmyLightCasterFull.cs
+++ Army/ArmyLightCasterFull.cs
@@ -1,45 +1,88 @@
 //cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
 //cs_include Scripts/Army/CoreArmyLite.cs
+//cs_include Scripts/Other/Classes/LightCaster.cs
+//cs_include Scripts/Other/Classes/LightMage.cs
+//cs_include Scripts/Other/Weapons/AvatarOfDeathsScythe.cs
+//cs_include Scripts/Other/Weapons/GuardianOfSpiritsBlade.cs
+//cs_include Scripts/Other/Weapons/LanceOfTime.cs
+//cs_include Scripts/Other/Weapons/BurningBlade.cs
+//cs_include Scripts/Other/Weapons/BurningBladeOfAbezeth.cs
 using Skua.Core.Interfaces;
 
-public class Generated_ArmyLightCasterFull
+public class ArmyLightCasterFull
 {
-    private IScriptInterface Bot => IScriptInterface.Instance;
-    private CoreBots Core => CoreBots.Instance;
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    public CoreAdvanced Adv => new();
     private CoreArmyLite Army = new();
-    private static CoreArmyLite sArmy = new();
 
-    public string OptionsStorage = "CustomAggroMon";
-    public bool DontPreconfigure = true;
+    private static CoreBots sCore = new();
+    private static CoreArmyLite sArmy = new();
+    public LightMage LM = new LightMage();
+    public BurningBladeOfAbezeth BBOA = new BurningBladeOfAbezeth();
     public List<IOption> Options = new List<IOption>()
     {
-        CoreBots.Instance.SkipOptions,
         sArmy.player1,
         sArmy.player2,
         sArmy.player3,
         sArmy.player4,
         sArmy.player5,
         sArmy.player6,
-        sArmy.player7,
-        sArmy.player8,
-        sArmy.player9,
-        sArmy.player10,
+        sCore.SkipOptions
     };
 
     public void ScriptMain(IScriptInterface bot)
     {
+        Core.BankingBlackList.AddRange(Loot);
         Core.SetOptions();
-
-        ArmyLightCasterFull();
-
+        bot.Options.RestPackets = false;
+        if (!Core.CheckInventory(31058) || !Core.CheckInventory(30266) || 
+            !Core.CheckInventory(31019) || !Core.CheckInventory(31028))
+        {
+            AggroPart();
+        }
+        GetLC();
         Core.SetOptions(false);
     }
 
-    public void ArmyLightCasterFull()
-        => Army.RunGeneratedAggroMon(map, monNames, questIDs, classtype, drops);
-    private List<int> questIDs = new() { 4510, 4511, 4512 };
-    private List<string> monNames = new() { "Underworld Hound", "Infernal Imp", "Fallen Knight", "Fallen Knight", "Fallen Knight", "Diabolical Warlord" };
-    private List<string> drops = new() { "Lance of Time", "Guardian of Spirits' Blade", "Avatar of Death's Scythe", "Burning Blade" };
-    private string map = "lostruinswar-87539";
-    private ClassType classtype = ClassType.Farm;
+    public void AggroPart()
+    {
+        Core.PrivateRooms = true;
+        Core.PrivateRoomNumber = Army.getRoomNr();
+
+        Core.AddDrop(Loot);
+        Core.EquipClass(ClassType.Farm);
+        Core.EnsureAccept(4510, 4511, 4512);
+        Army.AggroMonMIDs(2381,2382,2383,2386,2388,2390);
+        Army.AggroMonStart("lostruinswar");
+        Army.DivideOnCells("r2", "r3", "r4", "r5", "r6", "r7");
+
+        while (!Bot.ShouldExit && Core.CheckInventory("Trapped Spirits", 500) && Core.CheckInventory("Energy of Death", 500) && Core.CheckInventory("Captured Time", 500) )
+            Bot.Combat.Attack("*");
+        Army.AggroMonStop(true);
+        Core.EnsureComplete(4510, 4511, 4512);
+    }
+
+    public void GetLC()
+    {
+        if (Core.CheckInventory(38153))
+            return;
+
+        Core.AddDrop("LightCaster", "Aranx's Pure Light");
+        LM.GetLM(false);
+
+        Core.EquipClass(ClassType.Solo);
+        Bot.Quests.UpdateQuest(6042);
+        Core.EnsureAccept(6495);
+        BBOA.GetBBoA();
+        Adv.BoostHuntMonster("celestialarenad", "Aranx", "Aranx's Pure Light", isTemp: false);
+        Core.EnsureComplete(6495);
+        Bot.Wait.ForPickup("LightCaster");
+        Adv.rankUpClass("LightCaster");
+    }
+    private string[] Loot = {"Trapped Spirits", "Energy of Death", "Captured Time"};
 }
