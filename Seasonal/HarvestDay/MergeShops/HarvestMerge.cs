22e7ede8dd672a2dd538ea5f5a66532a5888d974
LF <lightflare1997@gmail.com>
2022-11-27 10:00:11 -0800

    missing include harvest merge

--- Seasonal/HarvestDay/MergeShops/HarvestMerge.cs
+++ Seasonal/HarvestDay/MergeShops/HarvestMerge.cs
@@ -1,6 +1,7 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
 //cs_include Scripts/Seasonal/HarvestDay/CoreHarvestDay.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
