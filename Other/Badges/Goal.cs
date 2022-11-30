d1a5b83618a8d23890cebed594b3b323ced09fd7
eXtazzy <dederaditya19@gmail.com>
2022-11-30 14:31:55 +0800

    Badges

---
+++ Other/Badges/Goal.cs
@@ -0,0 +1,36 @@
+//cs_include Scripts/CoreBots.cs
+using Skua.Core.Interfaces;
+
+public class GoalBadge
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        Badge();
+
+        Core.SetOptions(false);
+    }
+
+    public void Badge()
+    {
+        if (Core.HasWebBadge(badge))
+        {
+            Core.Logger($"Already have the {badge} badge");
+            return;
+        }
+
+        Core.Logger($"Kicking some balls for the {badge}");
+        Core.Join("chute");
+        while (!Core.HasWebBadge(badge)) //I hope this works
+        {
+            Core.SendPackets("%xt%zm%ia%1%rval%btnPuntting%%");
+            Core.Jump("Enter", "Right");
+        }
+    }
+
+    private string badge = "Goal";
+}
