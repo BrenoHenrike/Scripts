3a9a1922a49cb1417f366687626bfb36dec89669
eXtazzy <dederaditya19@gmail.com>
2022-12-04 13:51:17 +0800

    Weapon: BBoA update

--- Other/Weapons/BurningBladeOfAbezeth.cs
+++ Other/Weapons/BurningBladeOfAbezeth.cs
@@ -1,14 +1,15 @@
 //cs_include Scripts/CoreBots.cs
-//cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Other/MergeShops/CelestialChampMerge.cs
+//cs_include Scripts/Story/QueenofMonsters/Extra/CelestialArena.cs
 using Skua.Core.Interfaces;
 public class BurningBladeOfAbezeth
 {
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
-    public CoreAdvanced Adv = new CoreAdvanced();
-    public CoreFarms Farm = new CoreFarms();
-
+    public CelestialChampion CC = new();
     public void ScriptMain(IScriptInterface bot)
     {
         Core.SetOptions();
@@ -23,10 +24,11 @@ public class BurningBladeOfAbezeth
         if (Core.CheckInventory("Burning Blade Of Abezeth"))
             return;
 
-        Core.EquipClass(ClassType.Solo);
-        Bot.Quests.UpdateQuest(6042);
-        Core.HuntMonster("celestialarenad", "Aranx", "Burning Blade Of Abezeth", isTemp: false);
-        Bot.Wait.ForPickup("Burning Blade Of Abezeth");
-        Adv.EnhanceItem("Burning Blade Of Abezeth", EnhancementType.Lucky);
+        CC.BuyAllMerge("Burning Blade Of Abezeth", mergeOptionsEnum.all);
+        // Core.EquipClass(ClassType.Solo);
+        // Bot.Quests.UpdateQuest(6042);
+        // Core.HuntMonster("celestialarenad", "Aranx", "Champion Sash", 20, isTemp: false);
+        // Adv.BuyItem("celestialarena", , "Burning Blade Of Abezeth");
+        // Adv.EnhanceItem("Burning Blade Of Abezeth", EnhancementType.Lucky);
     }
 }
