179a3fcff6480b69ea2863101d9cdde3f88d8364
Tato <47118168+wtffidy@users.noreply.github.com>
2022-12-09 08:15:19 -0500

    Defeat the 12 Lords of Chaos! patched (the defeat drakath bits should be good now.

--- Story/LordsofChaos/Core13LoC.cs
+++ Story/LordsofChaos/Core13LoC.cs
@@ -2247,26 +2247,26 @@ public class Core13LoC
         if (!Story.QuestProgression(3876))
         {
             Core.EnsureAccept(3876);
-            if (!Core.CheckInventory(26875))
-                Core.KillMonster("finalbattle", "r1", "Left", "Drakath");
+            if (!Bot.Quests.CanComplete(3876))
+                Core.HuntMonsterMapID("finalbattle", 1);
             Core.EnsureComplete(3876);
         }
 
-        // Defeat Drakath! && //Defeat Drakath.. again!
+        //Defeat Drakath.. again!
         if (!Story.QuestProgression(3877))
         {
             Core.EnsureAccept(3877);
-            if (!Core.CheckInventory(26876))
-                Core.KillMonster("finalbattle", "r4", "Left", "Drakath");
+            if (!Bot.Quests.CanComplete(3877))
+                Core.HuntMonsterMapID("finalbattle", 14);
             Core.EnsureComplete(3877);
         }
 
-        //Defeat Drakath!
+        //Defeat Drakath!   
         if (!Story.QuestProgression(3878))
         {
             Core.EnsureAccept(3878);
-            if (!Core.CheckInventory(26877))
-                Core.KillMonster("finalbattle", "r9", "Left", "Drakath");
+            if (!Bot.Quests.CanComplete(3878))
+                Core.HuntMonsterMapID("finalbattle", 23);
             Core.EnsureComplete(3878);
         }
 
