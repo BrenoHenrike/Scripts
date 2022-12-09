4229c9dfa22ec727c53e9dcc575551dc0b504df6
Tato <47118168+wtffidy@users.noreply.github.com>
2022-12-08 21:06:23 -0500

    Update ArmyRetrieveVoidAuras.cs

--- Army/Various/ArmyRetrieveVoidAuras.cs
+++ Army/Various/ArmyRetrieveVoidAuras.cs
@@ -88,9 +88,7 @@ public class RVAArmy
         while (!Bot.ShouldExit && !Core.CheckInventory("Void Aura", Quantity))
         {
             Core.EnsureAccept(4432);
-            Core.EquipClass(ClassType.Farm);
             ArmyKillMonster("timespace", "Frame1", "Left", "Astral Ephemerite", "Astral Ephemerite Essence", EssenceQuantity, false);
-            Core.EquipClass(ClassType.Solo);
             ArmyKillMonster("citadel", "m13", "Left", "Belrot the Fiend", "Belrot the Fiend Essence", EssenceQuantity, false);
             ArmyKillMonster("greenguardwest", "BKWest15", "Down", "Black Knight", "Black Knight Essence", EssenceQuantity, false);
             ArmyKillMonster("mudluk", "Boss", "Down", "Tiger Leech", "Tiger Leech Essence", EssenceQuantity, false);
@@ -130,6 +128,7 @@ public class RVAArmy
             Core.Logger($"[{Bot.Map.PlayerNames.Count}/{Bot.Config.Get<int>("armysize")}] Waiting For The Squad!");
             Bot.Sleep(2500);
         }
+        Core.EquipClass(monster == "Astral Ephemerite" ? ClassType.Farm : ClassType.Solo);
         Bot.Sleep(2500);
         if (item == null)
         {
