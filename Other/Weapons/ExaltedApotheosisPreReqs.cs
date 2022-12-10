e207c899173415af18bf79e2638cf744a9d36f95
Tato <47118168+wtffidy@users.noreply.github.com>
2022-12-10 09:33:24 -0500

    Update ExaltedApotheosisPreReqs.cs

--- Other/Weapons/ExaltedApotheosisPreReqs.cs
+++ Other/Weapons/ExaltedApotheosisPreReqs.cs
@@ -2,6 +2,7 @@
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 using Skua.Core.Interfaces;
+using Skua.Core.Models.Items;
 
 public class ExaltedApotheosisPreReqs
 {
@@ -13,6 +14,7 @@ public class ExaltedApotheosisPreReqs
     {
         Core.SetOptions();
 
+        Core.BankingBlackList.AddRange(new[] { "Ezrajal Insignia", "Warden Insignia", "Engineer Insignia" });
         PreReqs();
 
         Core.SetOptions(false);
@@ -20,11 +22,11 @@ public class ExaltedApotheosisPreReqs
 
     public void PreReqs()
     {
-        if (Core.CheckInventory("Exalted Node", 300, toInv: false) && Core.CheckInventory("Thaumaturgus Alpha", toInv: false) && Core.CheckInventory("Apostate Alpha", toInv: false))
-        {
-            Core.Logger("Got all prerequisites! Kill the ultra bosses manually for insignias next to complete Exalted Apotheosis.");
-            return;
-        }
+        // if (Core.CheckInventory("Exalted Node", 300, toInv: false) && Core.CheckInventory("Thaumaturgus Alpha", toInv: false) && Core.CheckInventory("Apostate Alpha", toInv: false))
+        // {
+        //     Core.Logger("Got all prerequisites! Kill the ultra bosses manually for insignias next to complete Exalted Apotheosis.");
+        //     return;
+        // }
 
         /// No ultras required
         if (!Core.CheckInventory("Apostate Alpha") && !Core.CheckInventory("Thaumaturgus Alpha"))
@@ -42,35 +44,23 @@ public class ExaltedApotheosisPreReqs
             Core.EquipClass(ClassType.Farm);
             Core.KillMonster("timeinn", "r3", "Top", "Energy Elemental", "Exalted Node", 300, isTemp: false);
 
-            Core.Logger("Got all prerequisites! Kill the ultra bosses manually\n"+
+            Core.Logger("Got all prerequisites! Kill the ultra bosses manually\n" +
             "for insignias next to complete Exalted Apotheosis.");
         }
+        
+        // if (Core.CheckInventory("Ezrajal Insignia", 24) && Core.CheckInventory("Warden Insignia", 24) && Core.CheckInventory("Engineer Insignia", 16))
+        // {
+        //     Core.Logger($"{Bot.Inventory.GetQuantity("Ezrajal Insignia") / 24}");
+        //     Core.Logger($"{Bot.Inventory.GetQuantity("Warden Insignia") / 24}");
+        //     Core.Logger($"{Bot.Inventory.GetQuantity("Engineer Insignia") / 16}");
+        //     Core.Logger("Please obtain the rest of the insignias with your army to complete the merge. \n" +
+        //     "Currently our boats can't do the Ultra Bosses for you until CoreArmy is finished.", stopBot: true);
+        // }
 
         /// Apotheosis merge once got insignias
-        if (Core.CheckInventory("Ezrajal Insignia", 24) && Core.CheckInventory("Warden Insignia", 24) && Core.CheckInventory("Engineer Insignia", 16))
-        {
-            /* 
-            ///Keeping these buys in case Adv.BuyItem can't actually merge all the weapons by itself
-            Core.BuyItem("timeinn", 2010, "Apostate Omega");
-            Core.BuyItem("timeinn", 2010, "Apostate Ultima");
-            Core.BuyItem("timeinn", 2010, "Thaumaturgus Omega");
-            Core.BuyItem("timeinn", 2010, "Thaumaturgus Ultima");
-            Core.BuyItem("timeinn", 2010, "Exalted Unity");
-            Core.BuyItem("timeinn", 2010, "Exalted Penultima");
-            Core.BuyItem("timeinn", 2010, "Exalted Apotheosis");
-             */
-            Adv.BuyItem("timeinn", 2010, "Exalted Apotheosis");
+        Adv.BuyItem("timeinn", 2010, "Exalted Apotheosis");
+        if (Core.CheckInventory("Exalted Apotheosis", toInv: false))
             Core.Logger("Congratulations on completing the Exalted Apotheosis weapon!");
-        }
-
-        else
-        {
-            Core.Logger("Please obtain the rest ofthe insinias with your army to complete the merge. \n"+
-            "Currently our boats can't do the Ultra Bosses for you until CoreArmy is finished.");
-            Core.Logger($"{Bot.Inventory.GetQuantity("Ezrajal Insignia")/24}");
-            Core.Logger($"{Bot.Inventory.GetQuantity("Warden Insignia")/24}");
-            Core.Logger($"{Bot.Inventory.GetQuantity("Engineer Insignia")/16}");
-            return;
-        }
+        else Core.Logger("Not Enouguh Insignias keep running this Dailly");
     }
 }
