7754867dc85eff5dffa5cccd8961618fc2ce2cc9
Tato <47118168+wtffidy@users.noreply.github.com>
2023-01-02 20:12:44 -0500

    Update RevontheusSet.cs

--- Other/RevontheusSet.cs
+++ Other/RevontheusSet.cs
@@ -8,6 +8,8 @@ public class RevontheusSet
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
 
+    public bool DontPreconfigure = true;
+    public string OptionsStorage = "RevontheusSet";
     public List<IOption> Options = new List<IOption>()
     {
         CoreBots.Instance.SkipOptions,
