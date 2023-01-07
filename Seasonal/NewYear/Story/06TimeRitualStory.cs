c8e4593df8b4f83e0fd09e251565a6b4b1e4de4f
Levi4Chan <wakuxe002@gmail.com>
2023-01-07 22:00:05 +0800

    New Year event story

---
+++ Seasonal/NewYear/Story/06TimeRitualStory.cs
@@ -0,0 +1,22 @@
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Seasonal/NewYear/CoreNewYear.cs
+using Skua.Core.Interfaces;
+
+public class TimeRitual
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+    public CoreNewYear NY = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        NY.TimeRitual();
+
+        Core.SetOptions(false);
+    }
+
+}
