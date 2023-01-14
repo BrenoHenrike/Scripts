3c78365bf6094daaee14ba6ac5777fbd63bdf116
Tato <47118168+wtffidy@users.noreply.github.com>
2023-01-13 23:06:55 -0500

    might aswell

--- Other/MergeShops/ValariansMerge.cs
+++ Other/MergeShops/ValariansMerge.cs
@@ -76,7 +76,9 @@ public class ValariansMerge
                     //9056 | Magic Dance
                     while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
                     {
+                        Core.EquipClass(ClassType.Farm);
                         Core.HuntMonster("blackmaze", "Globlin", "Globlin Wings", 7);
+                        Core.EquipClass(ClassType.Solo);
                         Core.HuntMonster("blackmaze", "Vi'eel Dreaddacovra", "White Scale");
                         Core.HuntMonster("blackmaze", "Shadow Fernando", "Purple Flame");
                         Bot.Wait.ForPickup(req.Name);
