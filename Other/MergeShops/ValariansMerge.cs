d3368374c613b255f8ce6e484a5f0537f0e54d49
Tato <47118168+wtffidy@users.noreply.github.com>
2023-01-13 22:28:24 -0500

    Update ValariansMerge.cs

--- Other/MergeShops/ValariansMerge.cs
+++ Other/MergeShops/ValariansMerge.cs
@@ -1,6 +1,7 @@
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreStory.cs
 //cs_include Scripts/Story\Friday13th\CoreFriday13th.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
