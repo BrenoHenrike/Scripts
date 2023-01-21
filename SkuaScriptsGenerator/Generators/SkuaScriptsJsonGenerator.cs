d5f1e026cf62ddaa8aa001cb767b487968785213
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-01-22 01:24:52 +0800

    Fix file path for github workflow on run

--- SkuaScriptsGenerator/Generators/SkuaScriptsJsonGenerator.cs
+++ SkuaScriptsGenerator/Generators/SkuaScriptsJsonGenerator.cs
@@ -10,7 +10,8 @@ namespace SkuaScriptsGenerator.Generators
         
             var scripts = new List<ScriptInfo>();
             
-            foreach (var script in Directory.EnumerateFiles("..\\", "*.cs", SearchOption.AllDirectories))
+            // foreach (var script in Directory.EnumerateFiles("..\\", "*.cs", SearchOption.AllDirectories))
+            foreach (var script in Directory.EnumerateFiles("../", "*.cs", SearchOption.AllDirectories))
             {
                 var firstLine = File.ReadLines(script).First();
                 if (firstLine.StartsWith("/*"))
