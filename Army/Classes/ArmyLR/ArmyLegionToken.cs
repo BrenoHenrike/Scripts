04b2ed96d1ff059ee89f8a0aca48e3e61a945776
LordExelot <22625705+LordExelot@users.noreply.github.com>
2023-01-21 12:03:42 +0100

    Army Legion Token: converted to switch system and added classSwap

--- Army/Classes/ArmyLR/ArmyLegionToken.cs
+++ Army/Classes/ArmyLR/ArmyLegionToken.cs
@@ -52,189 +52,193 @@ public class ArmyLegionToken
     {
         Legion.JoinLegion();
 
-        Core.EquipClass(ClassType.Farm);
-
-        if (Method.ToString() == "Dreadrock")
+        switch (Method.ToString())
         {
-            Core.BuyItem("underworld", 216, "Undead Champion");
-            GetItem("dreadrock", new[] { "Fallen Hero", "Hollow Wraith", "Legion Sentinel", "Shadowknight", "Void Mercenary" }, 4849, "Legion Token", quant);
-        }
+            case "Dreadrock":
+                Core.BuyItem("underworld", 216, "Undead Champion");
+                Core.EquipClass(ClassType.Farm);
 
-        if (Method.ToString() == "Shogun_Paragon_Pet")
-        {
-            if (!Core.CheckInventory("Shogun Paragon Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
+                GetItem("dreadrock", new[] { "Fallen Hero", "Hollow Wraith", "Legion Sentinel", "Shadowknight", "Void Mercenary" }, 4849, "Legion Token", quant);
+                break;
 
-            if (Core.CheckInventory("Infernal Caladbolg"))
-                Core.RegisterQuests(3722);
-
-            Adv.BestGear(GearBoost.dmgAll);
-            while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
-            {
-                GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Nothing Heard", 10);
-                GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Nothing to See", 10);
-                GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Area Secured and Quiet", 10);
+            case "Shogun_Paragon_Pet":
+                if (!Core.CheckInventory("Shogun Paragon Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
 
                 if (Core.CheckInventory("Infernal Caladbolg"))
-                {
-                    GetItem("fotia", new[] { "Fotia Elemental" }, 3722, "Betrayer Extinguished", 5);
-                    GetItem("evilwardage", new[] { "Dreadfiend of Nulgath" }, 3722, "Fiend Felled", 2);
-                }
-            }
-        }
+                    Core.RegisterQuests(3722);
 
-        if (Method.ToString() == "Thanatos_Paragon_Pet")
-        {
-            if (!Core.CheckInventory("Thanatos Paragon Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
+                Core.EquipClass(ClassType.Farm);
+                Adv.BestGear(GearBoost.dmgAll);
 
-            Adv.BestGear(GearBoost.Dragonkin);
-            GetItem("dragonheart", new[] { "Zombie Dragon" }, 4100, "Legion Token", quant);
-        }
-
-        if (Method.ToString() == "Bright_Paragon_Pet")
-        {
-            if (!Core.CheckInventory("Bright Paragon Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Core.RegisterQuests(4703);
-            Adv.BestGear(GearBoost.dmgAll);
-            GetItem("brightfortress", "r3", "Right", new[] { "*" }, 4704, "Legion Token", quant);
-            Core.CancelRegisteredQuests();
-
-        }
-
-        if (Method.ToString() == "Arcane_Paragon_Pet")
-        {
-            if (!Core.CheckInventory(" Arcane_Paragon_Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Adv.BestGear(GearBoost.dmgAll);
-            while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
-            {
-                GetItem("dragonheart", new[] { "Granite Dracolich" }, 4896, "Granite Dracolich Soul", 4, isTemp: false);
-                GetItem("dragonheart", new[] { "Tempest Dracolich" }, 4896, "Tempest Dracolich Soul", 4, isTemp: false);
-                GetItem("dragonheart", new[] { "Inferno Dracolich" }, 4896, "Inferno Dracolich Soul", 4, isTemp: false);
-                GetItem("dragonheart", new[] { "Deluge Dracolich" }, 4896, "Deluge Dracolich Soul", 4, isTemp: false);
-            }
-        }
-
-        if (Method.ToString() == "Paragon_Dreadnaught_Pet")
-        {
-            if (!Core.CheckInventory("Paragon_Dreadnaught_Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Adv.BestGear(GearBoost.dmgAll);
-
-            while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
-            {
-                GetItem("laken", new[] { "Augmented Guard" }, 5741, "Stolen Guard", 5);
-                GetItem("laken", new[] { "Cyborg Dog" }, 5741, "Stolen Dog", 6);
-                GetItem("laken", new[] { "Mad Scientist" }, 5741, "Taken Axe", 10);
-            }
-
-        }
-
-        if (Method.ToString() == "Mounted_Paragon_Pet")
-        {
-            if (!Core.CheckInventory("Mounted_Paragon_Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Adv.BestGear(GearBoost.dmgAll);
-            GetItem("frozentower", new[] { "Ice Wolf" }, 5604, "Legion Token", quant);
-
-        }
-
-        if (Method.ToString() == " Ascended_Paragon_Pet")
-        {
-            if (!Core.CheckInventory(" Ascended_Paragon_Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Adv.BestGear(GearBoost.dmgAll);
-            while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
-            {
-                GetItem("tournament", new[] { "Lord Brentan" }, 2747, "Lord Brentan's Regal Blade");
-                GetItem("tournament", new[] { "Roderick" }, 2747, "Roderick's Chaotic Bane");
-                GetItem("tournament", new[] { "Knight of Thorns" }, 2747, "Knight of Thorns' Sword");
-                GetItem("tournament", new[] { "Johann Wryce" }, 2747, "Platinum of Johann Wryce");
-                GetItem("tournament", new[] { "Khai Kaldun" }, 2747, "Khai Kaldun's Scimitar");
-            }
-        }
-
-        if (Method.ToString() == "Festive_Paragon_Dracolich_Rider")
-        {
-            if (!Core.isSeasonalMapActive("frozenruins"))
-                return;
-
-            if (!Core.CheckInventory("Festive_Paragon_Dracolich_Rider"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Adv.BestGear(GearBoost.dmgAll);
-            Core.RegisterQuests(3968);
-            GetItem("frozenruins", new[] { "Frost Fangbeast" }, 3969, "Legion Token", quant);
-            Core.CancelRegisteredQuests();
-
-        }
-
-        if (Method.ToString() == "Holiday_Paragon_Pet")
-        {
-            if (!Core.CheckInventory("Holiday_Paragon_Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Adv.BestGear(GearBoost.dmgAll);
-            while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
-            {
-                GetItem("prison", new[] { "King Alteon's Knight" }, 3256, "Spirit of Loyalty", 6);
-                GetItem("battlewedding", new[] { "Silver Knight" }, 3256, "Spirit of Love", 6);
-                GetItem("lycan", new[] { "Lycan Knight" }, 3256, "Spirit of Love", 6);
-            }
-
-        }
-
-        if (Method.ToString() == "UW3017_Pet")
-        {
-            if (!Core.CheckInventory("UW3017_Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Adv.BestGear(GearBoost.dmgAll);
-            GetItem("underworld", new[] { "Bloodfiend" }, 5738, "Legion Token", quant);
-
-        }
-
-        if (Method.ToString() == "Infernal_Caladbolg")
-        {
-            if (!Core.CheckInventory("Infernal_Caladbolg"))
-                Core.Logger("Sword not owned, stopping", stopBot: true);
-
-            if (Core.CheckInventory("Shogun Paragon Pet"))
-                Core.RegisterQuests(3722);
-
-            Adv.BestGear(GearBoost.dmgAll);
-            while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
-            {
-                if (Core.CheckInventory("Shogun Paragon Pet"))
+                while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
                 {
                     GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Nothing Heard", 10);
                     GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Nothing to See", 10);
                     GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Area Secured and Quiet", 10);
+
+                    if (Core.CheckInventory("Infernal Caladbolg"))
+                    {
+                        GetItem("fotia", new[] { "Fotia Elemental" }, 3722, "Betrayer Extinguished", 5);
+                        GetItem("evilwardage", new[] { "Dreadfiend of Nulgath" }, 3722, "Fiend Felled", 2);
+                    }
                 }
+                break;
 
-                GetItem("fotia", new[] { "Fotia Elemental" }, 3722, "Betrayer Extinguished", 5);
-                GetItem("evilwardage", new[] { "Dreadfiend of Nulgath" }, 3722, "Fiend Felled", 2);
-            }
+            case "Thanatos_Paragon_Pet":
+                if (!Core.CheckInventory("Thanatos Paragon Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
 
-        }
+                Core.EquipClass(ClassType.Solo);
+                Adv.BestGear(GearBoost.Dragonkin);
 
-        if (Method.ToString() == "Hardcore_Paragon_Pet")
-        {
-            if (!Core.CheckInventory("Hardcore_Paragon_Pet"))
-                Core.Logger("Pet not owned, stopping", stopBot: true);
-            Adv.BestGear(GearBoost.dmgAll);
-            while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
-            {
+                GetItem("dragonheart", new[] { "Zombie Dragon" }, 4100, "Legion Token", quant);
+                break;
+
+            case "Bright_Paragon_Pet":
+                if (!Core.CheckInventory("Bright Paragon Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+
+                Core.EquipClass(ClassType.Farm);
+                Adv.BestGear(GearBoost.dmgAll);
+
+                GetItem("brightfortress", "r3", "Right", new[] { "*" }, 4704, "Legion Token", quant);
+                break;
+
+            case "Arcane_Paragon_Pet":
+                if (!Core.CheckInventory("Arcane Paragon Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+
+                Core.EquipClass(ClassType.Farm);
+                Adv.BestGear(GearBoost.dmgAll);
+
+                while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
+                {
+                    GetItem("dragonheart", new[] { "Granite Dracolich" }, 4896, "Granite Dracolich Soul", 4, isTemp: false);
+                    GetItem("dragonheart", new[] { "Tempest Dracolich" }, 4896, "Tempest Dracolich Soul", 4, isTemp: false);
+                    GetItem("dragonheart", new[] { "Inferno Dracolich" }, 4896, "Inferno Dracolich Soul", 4, isTemp: false);
+                    GetItem("dragonheart", new[] { "Deluge Dracolich" }, 4896, "Deluge Dracolich Soul", 4, isTemp: false);
+                }
+                break;
+
+            case "Paragon_Dreadnaught_Pet":
+                if (!Core.CheckInventory("Paragon Dreadnaught Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+
+                Core.EquipClass(ClassType.Farm);
+                Adv.BestGear(GearBoost.dmgAll);
+
+                while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
+                {
+                    GetItem("laken", new[] { "Augmented Guard" }, 5741, "Stolen Guard", 5);
+                    GetItem("laken", new[] { "Cyborg Dog" }, 5741, "Stolen Dog", 6);
+                    GetItem("laken", new[] { "Mad Scientist" }, 5741, "Taken Axe", 10);
+                }
+                break;
+
+            case "Mounted_Paragon_Pet":
+                if (!Core.CheckInventory("Mounted Paragon Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+
+                Core.EquipClass(ClassType.Farm);
+                Adv.BestGear(GearBoost.dmgAll);
+
+                GetItem("frozentower", new[] { "Ice Wolf" }, 5604, "Legion Token", quant);
+                break;
+
+            case "Ascended_Paragon_Pet":
+                if (!Core.CheckInventory("Ascended Paragon Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+
+                Core.EquipClass(ClassType.Solo);
+                Adv.BestGear(GearBoost.dmgAll);
+
+                while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
+                {
+                    GetItem("tournament", new[] { "Lord Brentan" }, 2747, "Lord Brentan's Regal Blade");
+                    GetItem("tournament", new[] { "Roderick" }, 2747, "Roderick's Chaotic Bane");
+                    GetItem("tournament", new[] { "Knight of Thorns" }, 2747, "Knight of Thorns' Sword");
+                    GetItem("tournament", new[] { "Johann Wryce" }, 2747, "Platinum of Johann Wryce");
+                    GetItem("tournament", new[] { "Khai Kaldun" }, 2747, "Khai Kaldun's Scimitar");
+                }
+                break;
+
+            case "Festive_Paragon_Dracolich_Rider":
+                if (!Core.CheckInventory("Festive Paragon Dracolich Rider"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+                if (!Core.isSeasonalMapActive("frozenruins"))
+                    return;
+
+                Core.EquipClass(ClassType.Solo);
+                Adv.BestGear(GearBoost.dmgAll);
+
+                GetItem("frozenruins", new[] { "Frost Fangbeast" }, 3969, "Legion Token", quant);
+                break;
+
+            case "Holiday_Paragon_Pet":
+                if (!Core.CheckInventory("Holiday Paragon Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+
+                Core.EquipClass(ClassType.Farm);
+                Adv.BestGear(GearBoost.dmgAll);
+
+                while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
+                {
+                    GetItem("prison", new[] { "King Alteon's Knight" }, 3256, "Spirit of Loyalty", 6);
+                    GetItem("battlewedding", new[] { "Silver Knight" }, 3256, "Spirit of Love", 6);
+                    GetItem("lycan", new[] { "Lycan Knight" }, 3256, "Spirit of Love", 6);
+                }
+                break;
+
+            case "UW3017_Pet":
+                if (!Core.CheckInventory("UW3017 Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+
+                Core.EquipClass(ClassType.Farm);
+                Adv.BestGear(GearBoost.dmgAll);
+
+                GetItem("underworld", new[] { "Bloodfiend" }, 5738, "Legion Token", quant);
+                break;
+
+            case "Infernal_Caladbolg":
+                if (!Core.CheckInventory("Infernal Caladbolg"))
+                    Core.Logger("Sword not owned, stopping", stopBot: true);
+
+                Adv.BestGear(GearBoost.dmgAll);
+
+                if (Core.CheckInventory("Shogun Paragon Pet"))
+                    Core.RegisterQuests(3722);
+                while (!Bot.ShouldExit && !Core.CheckInventory("Legion Token", quant))
+                {
+                    if (Core.CheckInventory("Shogun Paragon Pet"))
+                    {
+                        GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Nothing Heard", 10);
+                        GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Nothing to See", 10);
+                        GetItem("fotia", new[] { "Fotia Elemental", "Fotia Spirit" }, 5755, "Area Secured and Quiet", 10);
+                    }
+
+                    GetItem("fotia", new[] { "Fotia Elemental" }, 3722, "Betrayer Extinguished", 5);
+                    GetItem("evilwardage", new[] { "Dreadfiend of Nulgath" }, 3722, "Fiend Felled", 2);
+                }
+                break;
+
+            case "Hardcore_Paragon_Pet":
+
+                if (!Core.CheckInventory("Hardcore Paragon Pet"))
+                    Core.Logger("Pet not owned, stopping", stopBot: true);
+
+                Adv.BestGear(GearBoost.dmgAll);
+
                 if (!Bot.Quests.IsUnlocked(793))
                     GetItem("doomvault", new[] { "Binky" }, 3393, "Legion Token", quant);
                 else
                     GetItem("chaosboss", new[] { "Ultra Chaos Warlord" }, 3394, "Legion Token", quant);
-            }
-        }
+                break;
 
-        else
-        {
-            Core.EquipClass(ClassType.Solo);
-            GetItem("legionarena", new[] { "Legion Fiend Rider" }, 6743, "Legion Token", quant);
+            default:
+                Core.EquipClass(ClassType.Solo);
+                GetItem("legionarena", new[] { "Legion Fiend Rider" }, 6743, "Legion Token", quant);
+                break;
         }
     }
 
