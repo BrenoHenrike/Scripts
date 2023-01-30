59a7219cba10730a7e407f6794d65767e59dfb82
Tato <47118168+wtffidy@users.noreply.github.com>
2023-01-30 13:21:14 -0500

    f2p army swag token A

---
+++ Army/Various/ArmySwagTokensF2p.cs
@@ -0,0 +1,74 @@
+/*
+name: Army superfan swag token a
+description: farms super-fan swag token a with f2p method
+tags: "super-fan swag token a", army, f2p
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/Army/CoreArmyLite.cs
+using Skua.Core.Interfaces;
+
+public class ArmySwagTokensF2p
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreArmyLite Army = new();
+    private static CoreArmyLite sArmy = new();
+
+    public string OptionsStorage = "CustomAggroMon";
+    public bool DontPreconfigure = true;
+    public List<IOption> Options = new()
+    {
+        CoreBots.Instance.SkipOptions,
+        sArmy.player1,
+        sArmy.player2,
+        sArmy.player3,
+        sArmy.player4,
+        sArmy.player5,
+        sArmy.player6,
+        sArmy.player7,
+        sArmy.player8,
+        sArmy.player9,
+        sArmy.player10,
+    };
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        Setup();
+        Core.SetOptions(false);
+    }
+
+    public void Setup(int quant = 100)
+    {
+        Core.FarmingLogger($"Super-Fan Swag Token A", quant);
+        while (!Bot.ShouldExit && !Core.CheckInventory("Super-Fan Swag Token A", quant))
+        {
+            while (!Bot.ShouldExit && !Core.CheckInventory("Super-Fan Swag Token D", 500))
+                Swaggro();
+            Army.AggroMonStop();
+
+            Core.Join("Collection", "Begin", "Spawn");
+            if (!Bot.Shops.IsLoaded)
+                Bot.Shops.Load(325);
+
+            //Token Buying
+            while (!Bot.ShouldExit && !Core.CheckInventory("Super-Fan Swag Token C", 10) && Core.CheckInventory("Super-Fan Swag Token D", 10))
+                Bot.Shops.BuyItem("Super-Fan Swag Token C");
+            while (!Bot.ShouldExit && !Core.CheckInventory("Super-Fan Swag Token B", 20) && Core.CheckInventory("Super-Fan Swag Token C", 10))
+                Bot.Shops.BuyItem("Super-Fan Swag Token B");
+            while (!Bot.ShouldExit && !Core.CheckInventory("Super-Fan Swag Token A", quant) && Core.CheckInventory("Super-Fan Swag Token B", 20))
+                Bot.Shops.BuyItem("Super-Fan Swag Token A");
+
+        }
+        Core.CancelRegisteredQuests();
+    }
+
+    public void Swaggro()
+        => Army.RunGeneratedAggroMon(map, monNames, questIDs, classtype, drops);
+    private List<int> questIDs = new() { 1304, 1307 };
+    private List<string> monNames = new() { "Dustbunny of DOOM", "Death on Wings" };
+    private List<string> drops = new() { "Super-Fan Swag Token A", "Super-Fan Swag Token B", "Super-Fan Swag Token C", "Super-Fan Swag Token D" };
+    private string map = "terrarium";
+    private ClassType classtype = ClassType.Farm;
+}
