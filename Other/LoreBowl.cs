6678293b8f14094df085c3ac2d9a1a79d1e947d7
Tato <47118168+wtffidy@users.noreply.github.com>
2023-02-13 13:21:13 -0500

    Lorebowl [half-time release from artix]
    just some bank fader

---
+++ Other/LoreBowl.cs
@@ -0,0 +1,64 @@
+/*
+name: Lorebowl
+description: Kills the mob in /punt for its drops
+tags: LoreBowl, drops, punt, twilly
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using Skua.Core.Interfaces;
+
+public class LoreBowl
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+
+    string[] LoreBowlItems =
+    {
+   "Broncolich Pennant",
+    "Chaos Pennant",
+    "Cheer Lover",
+    "Cheer Lover's Hair",
+    "Cheer Lover's Locks",
+    "Chieftains Pennant",
+    "Corsairs Pennant",
+    "Crows Pennant",
+    "CyberHounds Pennant",
+    "Diabolical Pennant",
+    "Evil Pennant",
+    "Good Pennant",
+    "Harpies Pennant",
+    "Hollowborn Pennant",
+    "Hoofs Pennant",
+    "Horcs Pennant",
+    "Legion Pennant",
+    "Loyalists Pennant",
+    "Mammoths Pennant",
+    "Miner Pennant",
+    "Nation Pennant",
+    "Saints Pennant",
+    "Satyrs Pennant",
+    "Tigers Pennant",
+    "Treasure Chest (Misc)",
+    "Void Nation Cheerleader",
+};
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Drops();
+
+        Core.SetOptions(false);
+    }
+
+    private void Drops()
+    {
+        if (Core.CheckInventory(LoreBowlItems, toInv: false))
+            return;
+
+        while (!Bot.ShouldExit && Core.CheckInventory(LoreBowlItems, toInv: false))
+            Core.HuntMonster("punt", "*", isTemp: false);
+    }
+
+}
