afd7ae8328d4af7cc2c2472c8a28cc02964bce94
d3lfs <slykint.bacalso@cit.edu>
2023-02-15 19:31:36 +0800

    Implement GoogleFormRepsonse Builder

--- SkuaGoogleForm/GoogleFormResponseBuilder.cs
+++ SkuaGoogleForm/GoogleFormResponseBuilder.cs
@@ -2,6 +2,23 @@ namespace SkuaGoogleForm
 {
     public class GoogleFormResponseBuilder
     {
+        protected GoogleFormResponse _root;
+        public GoogleFormResponseBuilder(GoogleFormResponse googleFormResponse)
+        {
+            _root = googleFormResponse;
+        }
+
+        public GoogleFormResponseBuilder AddBodyValue(string entryKey, string? entryValue)
+        {
+            _root.addFormBodyValue(entryKey, entryValue);
+
+            return this;
+        }
         
+        public GoogleFormResponse FormUrlEncodedContent()
+        {
+            _root.FormUrlEncodedContent();
+            return _root;
+        }
     }
 }
