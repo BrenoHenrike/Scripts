87fc299465b998b351a1d023b69995ee2965de3a
d3lfs <slykint.bacalso@cit.edu>
2023-02-15 19:31:12 +0800

    Implement GoogleFormResponse

--- SkuaGoogleForm/GoogleFormResponse.cs
+++ SkuaGoogleForm/GoogleFormResponse.cs
@@ -2,6 +2,44 @@ namespace SkuaGoogleForm
 {
     public class GoogleFormResponse
     {
-        
+        public static string Hash;
+        public string Key, Value;
+        public FormUrlEncodedContent Content; 
+
+        public List<GoogleFormResponse> BodyValue = new List<GoogleFormResponse>();
+
+        protected GoogleFormResponse() {}
+
+        public GoogleFormResponse(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public void addFormBodyValue(string entryKey, string? entryValue)
+        {
+            Key = entryKey;
+            Value = entryValue;
+        }
+
+        public void FormUrlEncodedContent()
+        {
+            Content = new FormUrlEncodedContent(BodyValue.Select(x => new KeyValuePair<string, string>(x.Key, x.Value)));
+        }
+
+        public async Task<HttpResponseMessage> SendPostAsync() 
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://docs.google.com/forms/d/e/");
+                return await client.PostAsync($"{Hash}/formResponse", Content);
+            }
+        }
+
+        public static GoogleFormResponseBuilder Create(string hash) 
+        {
+            Hash = hash;
+            return new GoogleFormResponseBuilder(new GoogleFormResponse());
+        }
     }
 }
