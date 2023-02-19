d295c278066b4ab8b7d1a5d1e19c4683e8f431a7
Tato <47118168+wtffidy@users.noreply.github.com>
2023-02-19 12:29:02 -0500

    tags updated.

--- Story/SambaFlag.cs
+++ Story/SambaFlag.cs
@@ -1,7 +1,7 @@
 /*
-name: null
-description: null
-tags: null
+name: SambasFlag Story
+description: This will finish the SambasFlag Story.
+tags: story, quest, SambasFlag
 */
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreStory.cs
