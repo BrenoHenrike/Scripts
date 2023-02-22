b346fa2ac552a839c1a848f5946590532012772f
Tato <47118168+wtffidy@users.noreply.github.com>
2023-02-22 14:35:35 -0500

    Update MoglinPunter.cs

--- Other/Badges/MoglinPunter.cs
+++ Other/Badges/MoglinPunter.cs
@@ -32,25 +32,51 @@ public class MoglinPunter
         }
         Core.OneTimeMessage("Minigame Explanation", "This minigame works off of a \"value\" system for ponts, so 9999 is 99, for the quest so youll need to get a value of 10000 points which may take a while.", forcedMessageBox: true);
 
+
+        int Punt = 1;
+        double lowestScore = 100;
+
         Core.Logger($"Doing quest for {badge} badge, Purely Rng based, good luck");
         Core.Join("punt");
-        int Punt = 0;
+        Bot.Events.ExtensionPacketReceived += puntingPacketReader;
         while (!Bot.ShouldExit && !Core.HasWebBadge(badge))
         {
             Core.Jump("Enter", "Spawn");
             Bot.Sleep(Core.ActionDelay);
             Core.SendPackets("%xt%zm%ia%1%rval%btnPuntting%%");
             Bot.Wait.ForCellChange("Punt");
-            Bot.Sleep(4000);
+            Bot.Sleep(3500);
             if (Core.CheckInventory("Twilly Be Punted"))
             {
                 Core.ChainComplete(8532);
                 Core.Logger($"Punts to get the badge: {Punt}");
                 return;
             }
-            else Core.Logger($"Punt# {Punt++}");
+        }
+        Bot.Events.ExtensionPacketReceived -= puntingPacketReader;
+
+        void puntingPacketReader(dynamic packet)
+        {
+            string type = packet["params"].type;
+            dynamic data = packet["params"].dataObj;
+            if (type is not null and "json")
+            {
+                string cmd = data.cmd.ToString();
+                switch (cmd)
+                {
+                    case "ia":
+                        if (data.oName.ToString() == "btnPuntting" && data.unm.ToString() == Core.Username())
+                        {
+                            double score = data.val;
+                            score = Math.Round(float.Parse($"{score.ToString()[..^2]}.{score.ToString()[^2..]}"));
+                            lowestScore = lowestScore > score ? score : lowestScore;
+                            Core.Logger($"Punt: #{Punt++} | Score: {score}");
+                        }
+                        break;
+                }
+            }
         }
     }
-
     private string badge = "Moglin Punter";
+
 }
