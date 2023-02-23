8a06fc451496ed84bac2859791bab8d4f86a3d26
LordExelot <22625705+LordExelot@users.noreply.github.com>
2023-02-23 16:33:19 +0100

    OneClient\EldersBlood: released

---
+++ Army/OneClient/EldersBlood.cs
@@ -0,0 +1,40 @@
+/*
+name: The Wheel of Doom (Army)
+description: One-client version of the Wheel of Doom
+tags: daily, dailies, army, wheel, doom, oneclient
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/Army/CoreArmyLite.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Options;
+
+public class ArmyEldersBlood
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreArmyLite Army = new();
+    private CoreDailies Dailies = new();
+
+    public bool DontPreconfigure = true;
+    public string OptionsStorage = "ArmyEldersBlood";
+    public List<IOption> Options = new()
+    {
+        new Option<bool>("randomServers", "Random Servers", "Should the bot use a random server each for each account.", true),
+    };
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        EldersBlood(Bot.Config.Get<bool>("randomServers"));
+
+        Core.SetOptions(false);
+    }
+
+    public void EldersBlood(bool randomServers)
+    {
+        while (Army.doForAll(randomServers))
+            Dailies.EldersBlood();
+    }
+}
