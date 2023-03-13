2e0d8fbd88f71fe99ebd1919f72b5e0bbecdb54e
Tato <47118168+wtffidy@users.noreply.github.com>
2023-03-13 10:02:59 -0400

    Update DarkWarNationMerge.cs

--- Seasonal/StaffBirthdays/DageTheEvil/DarkWarNationMerge.cs
+++ Seasonal/StaffBirthdays/DageTheEvil/DarkWarNationMerge.cs
@@ -71,7 +71,7 @@ public class DarkWarNationMerge
                     Core.RegisterQuests(8578, 8579);
                     while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
                     {
-                        Core.HuntMonster("darkwarlegion", "*", log: false);
+                        Core.HuntMonster("darkwarnation", "*", log: false);
                         Bot.Wait.ForPickup(req.Name);
                     }
                     Core.CancelRegisteredQuests();
