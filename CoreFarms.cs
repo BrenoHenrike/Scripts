78973eb5ea7ab240eec914df49fe09df855bae88
Tato <47118168+wtffidy@users.noreply.github.com>
2023-03-14 05:42:29 -0400

    Update CoreFarms.cs

--- CoreFarms.cs
+++ CoreFarms.cs
@@ -386,7 +386,7 @@ public class CoreFarms
 
         Core.HuntMonster("well", "Gell Oh No", "Black Knight Leg Piece");
         Core.HuntMonster("greendragon", "Greenguard Dragon", "Black Knight Chest Piece");
-        Core.HuntMonster("deathgazer", "DeathGazer", "Black Knight Shoulder Piece");
+        Core.HuntMonster("deathgazer", "Deathgazer", "Black Knight Shoulder Piece");
         Core.HuntMonster("trunk", "Greenguard Basilisk", "Black Knight Arm Piece");
 
         Core.EnsureComplete(318);
