9ec20de7896540c212a8f75fafcad8688b69a71a
Tato <47118168+wtffidy@users.noreply.github.com>
2023-03-14 00:10:47 -0400

    Update LootTheUndresserRewards.cs

--- Seasonal/AqwBirthday/LootTheUndresserRewards.cs
+++ Seasonal/AqwBirthday/LootTheUndresserRewards.cs
@@ -6,6 +6,7 @@ tags: LootTheUndresser, quest reward, Cursed Antique Shop, Cursed Bust
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreStory.cs
 //cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/Story/ThroneofDarkness/CoreToD.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 
