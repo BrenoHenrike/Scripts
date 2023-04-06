d598d0097ab19f5a740330ad2b4f72e55fa6e454
Tato <47118168+wtffidy@users.noreply.github.com>
2023-04-06 07:19:58 -0400

    Update KillerCatacombs[Mem].cs

--- Story/LordsofChaos/ChoasFinaleBonus[Mem]/KillerCatacombs[Mem].cs
+++ Story/LordsofChaos/ChoasFinaleBonus[Mem]/KillerCatacombs[Mem].cs
@@ -37,45 +37,74 @@ public class KillerCatacombs
 
         // The Coward
         Story.KillQuest(3660, "killercatacombs", "Tomb Robber ");
+    
         // Welcome Party
         Story.KillQuest(3661, "killercatacombs", "Tomb Robber ");
+     
         // Scout Ahead
         Story.KillQuest(3662, "killercatacombs", "Ravenous Maw");
+      
         // Dinner Party
         Story.KillQuest(3663, "killercatacombs", "Ravenous Maw");
+      
         // Illumination Preparation
         Story.MapItemQuest(3664, "killercatacombs", 2762, 8);
+      
         // Bones Picked Clean
         Story.KillQuest(3665, "killercatacombs", "Sundered Darkblood ");
+      
         // Biased Window
         Story.MapItemQuest(3666, "killercatacombs", 2761, 3);
         Story.KillQuest(3666, "killercatacombs", "Sundered Darkblood ");
+      
         // Room For Seconds
         Story.KillQuest(3667, "killercatacombs", "Starved Maw ");
+       
         // Magic Doorstop
         Story.KillQuest(3668, "killercatacombs", "Sundered Darkblood ");
+      
         // Silverware Cipher
         Story.KillQuest(3669, "killercatacombs", "Ravenous Maw");
+       
         // Dinner Bell
         Story.KillQuest(3670, "killercatacombs", new[] { "Starved Maw ", "Ravenous Maw" });
+       
         // Faceless Challengers
         Story.KillQuest(3671, "killercatacombs", "Living Armor ");
+       
         // Doorway
         Story.KillQuest(3672, "killercatacombs", "Living Armor ");
+       
         // Eternal Hunger
         Story.KillQuest(3673, "killercatacombs", new[] { "Starved Maw ", "Ravenous Maw", "Living Armor " });
+       
         // You Weren't Invited
         Story.KillQuest(3674, "killercatacombs", "Living Armor ");
+       
         // Teeth and Plates
         Story.KillQuest(3675, "killercatacombs", new[] { "Starved Maw ", "Ravenous Maw", "Living Armor " });
+        
         // The Moment of Silence
         Story.KillQuest(3676, "killercatacombs", "Living Armor");
         Story.MapItemQuest(3676, "killercatacombs", 2763, 10);
+
         // Eternal Servitude
         Story.KillQuest(3677, "killercatacombs", new[] { "Living Armor ", "Sundered Darkblood ", "Unstable Spirit Orb " });
+
         // A Reanimated Dragon
-        Story.KillQuest(3678, "killercatacombs", "Dracolich ");
+        if (!Story.QuestProgression(3678))
+        {
+            Core.EnsureAccept(3678);
+            Core.HuntMonsterMapID("killercatacombs", 73, "Dracolich Slain");
+            Core.EnsureComplete(3678);
+        }
+
         // Lonesome Twin Guardian
-        Story.KillQuest(3679, "killercatacombs", "Dracolich ");
+        if (!Story.QuestProgression(3679))
+        {
+            Core.EnsureAccept(3679);
+            Core.HuntMonsterMapID("killercatacombs", 74, "Dracolich Necromancer Ended");
+            Core.EnsureComplete(3679);
+        }
     }
 }
