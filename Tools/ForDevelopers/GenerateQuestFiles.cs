9ac294f483fcc5509f8f6a65c410ee247a34f7c4
LordExelot <22625705+LordExelot@users.noreply.github.com>
2023-04-16 12:03:17 +0200

    GenerateQuestFiles: chages for an online quest file for ensureLoad

--- Tools/ForDevelopers/GenerateQuestFiles.cs
+++ Tools/ForDevelopers/GenerateQuestFiles.cs
@@ -7,6 +7,8 @@ tags: quests, developer, lists, files, spreadsheet, excel, data
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
+using Skua.Core.Models.Quests;
+using CommunityToolkit.Mvvm.DependencyInjection;
 using Skua.Core.Models;
 using Skua.Core.Interfaces;
 
@@ -20,13 +22,16 @@ public class GetQuests
         GenerateQuestFiles();
     }
 
-    private void GenerateQuestFiles()
+    private async void GenerateQuestFiles()
     {
-        if (Bot.ShowMessageBox("Did you update the Quest.txt file by clicking the update button in [Tools] -> [Loader]", "Quests up to date?", true) == false)
-            return;
+        //if (Bot.ShowMessageBox("Did you update the Quest.txt file by clicking the update button in [Tools] -> [Loader]", "Quests up to date?", true) == false)
+        //    return;
+
+        Core.Logger("Updating Quest.txt");
+        await UpdateQuests();
 
         Core.Logger("Reading Quest.txt");
-        var v = JsonConvert.DeserializeObject<dynamic[]>(File.ReadAllText(ClientFileSources.SkuaQuestsFile));
+        var v = JsonConvert.DeserializeObject<dynamic[]>(File.ReadAllText(ClientFileSources.SkuaQuestsFile))!;
 
         List<string> r = new();
         List<string> d = new();
@@ -47,9 +52,23 @@ public class GetQuests
         Core.Logger("Writing files.");
         Core.WriteFile(Path.Combine(ClientFileSources.SkuaScriptsDIR, "WIP", "QuestIds.txt"), r);
         Core.WriteFile(Path.Combine(ClientFileSources.SkuaScriptsDIR, "WIP", "QuestData.csv"), d);
+        File.Copy(ClientFileSources.SkuaQuestsFile, Path.Combine(ClientFileSources.SkuaScriptsDIR, "QuestData.json"), true);
 
         Core.Logger("Files made:");
         Core.Logger(" - \"Scripts/WIP/QuestIds.txt\"");
         Core.Logger(" - \"Scripts/WIP/QuestData.csv\"");
+        Core.Logger(" - \"Scripts/QuestData.json\"");
     }
+
+    private async Task UpdateQuests()
+    {
+        _loaderCTS = new();
+        List<QuestData> questData =
+            await (service ??= Ioc.Default.GetRequiredService<IQuestDataLoaderService>())
+            .UpdateAsync("Quests.txt", false, null, _loaderCTS.Token);
+        _loaderCTS.Dispose();
+        _loaderCTS = null;
+    }
+    private CancellationTokenSource? _loaderCTS;
+    private IQuestDataLoaderService? service;
 }
