cf4f1c2400b3d5ae3caf59896cd8097264fd598d
Tato <47118168+wtffidy@users.noreply.github.com>
2023-04-19 06:21:43 -0400

    added Blacklist option
    Accessed though the edit options button, wont show otherwise.
    
    options.txt name: BankAllBlackList

--- Farm/BankAllItems.cs
+++ Farm/BankAllItems.cs
@@ -6,11 +6,21 @@ tags: null
 //cs_include Scripts/CoreBots.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
+using Skua.Core.Options;
 
 public class BankAllItems
 {
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
+
+
+    public bool DontPreconfigure = true;
+    public string OptionsStorage = "BankAllBlackList";
+    public List<IOption> Options = new()
+    {
+        new Option<string>("BlackList", "BlackList Items", "Fill in the items teh bot *shouldn't* bank, split with a , (comma)."),
+    };
+
 
     public void ScriptMain(IScriptInterface bot)
     {
@@ -23,11 +33,15 @@ public class BankAllItems
         List<string> blackListedItems = new() { Core.SoloClass, Core.FarmClass, "Treasure Potion" };
         blackListedItems.AddRange(Core.SoloGear);
         blackListedItems.AddRange(Core.FarmGear);
+        if (!String.IsNullOrEmpty(Bot.Config.Get<string>("BlackList")))
+            blackListedItems.AddRange(Bot.Config.Get<string>("BlackList").Split(',', StringSplitOptions.TrimEntries));
+
 
         Bot.Wait.ForMapLoad("battleon");
         Bot.Sleep(Core.ActionDelay);
         Bot.Send.Packet($"%xt%zm%house%1%{Bot.Player.Username}%");
-        
+
+
         foreach (InventoryItem item in Bot.Inventory.Items)
         {
             if (item.Equipped || blackListedItems.Contains(item.Name))
@@ -40,11 +54,11 @@ public class BankAllItems
                     Core.Logger($"{Bot.Player.Username}'s Bank is full");
                     logged = true;
                 }
-                continue;
+               continue;
             }
             Core.ToBank(item.ID);
             Bot.Sleep(Core.ActionDelay);
         }
     }
-
 }
+
