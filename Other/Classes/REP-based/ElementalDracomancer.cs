7311b23aa8d6b0ae8e3a750dac3cdef2df8505a3
Vld Mrk <vladamark00@gmail.com>
2023-04-29 16:27:25 +0200

    Elemental Dracomancer standalone added

---
+++ Other/Classes/REP-based/ElementalDracomancer.cs
@@ -0,0 +1,38 @@
+/*
+name: Elemental Dracomancer Class
+description: This script farms the Elemental Dracomancer class.
+tags: dracomancer,class,etherstorm
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+using Skua.Core.Interfaces;
+
+public class ElementalDracomancer
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreAdvanced Adv = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        GetClass();
+        Core.SetOptions(false);
+    }
+
+    public void GetClass(bool rankUpClass = true)
+    {
+        if (Core.CheckInventory("Elemental Dracomancer"))
+        {
+            Core.Logger("You already own Elemental Dracomancer class.");
+            return;
+        }
+
+        Adv.BuyItem("dragontown", 1285, 11272);
+
+        if (rankUpClass)
+            Adv.rankUpClass("Elemental Dracomancer");
+    }
+}
