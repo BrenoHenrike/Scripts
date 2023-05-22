17f0cebdffabf9d836acba4406e4c05a7114b4c4
Vld Mrk <vladamark00@gmail.com>
2023-05-22 18:15:24 +0200

    tainted claymore and the poisonous deal released

---
+++ Nation/Various/TaintedClaymore.cs
@@ -0,0 +1,68 @@
+/*
+name: Tainted Claymore
+description: This script farms the Tainted Claymore weapon.
+tags: claymore, nulgath,skew,warrior,blade,enchant,poisonous deal
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+using Skua.Core.Interfaces;
+public class TaintedClaymore
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreStory Story = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        DoAll();
+
+        Core.SetOptions(false);
+    }
+
+    public void DoAll()
+    {
+        PreReq();
+        GetWep();
+    }
+
+    public void PreReq()
+    {
+        if (Core.isCompletedBefore(324))
+            return;
+
+        Story.PreLoad(this);
+
+        //Adorable Sisters
+        Story.MapItemQuest(319, "tavern", 56, 7);
+
+        //Warm and Furry
+        Story.KillQuest(320, "pines", "Pine Grizzly");
+
+        //Shell Rock
+        Story.KillQuest(321, "pines", "Red Shell Turtle");
+
+        //Bear Facts
+        Story.KillQuest(322, "pines", "Twistedtooth");
+
+        //The Spittoon Saloon
+        Story.KillQuest(324, "pines", "Red Shell Turtle");
+    }
+
+    public void GetWep()
+    {
+        if (Core.CheckInventory("Tainted Claymore"))
+            return;
+
+        Core.AddDrop("Tainted Claymore");
+
+        Farm.EvilREP(8);
+
+        Core.EnsureAccept(121);
+        Core.HuntMonster("battleundera", "Undead Berserker", "Warrior Claymore Blade", isTemp: false);
+        Core.EnsureComplete(121);
+    }
+}
--- Nation/Various/ThePoisonousDeal.cs
+++ Nation/Various/ThePoisonousDeal.cs
@@ -1,31 +1,31 @@
 /*
-name: The Refreshing Deal
-description: This script farms Gems and Totems of Nulgath using "The Refreshing Deal" Quest.
-tags: refreshing,deal,gem,totem,nulgath,nation,crag,bamboozle,quest
+name: The Poisonous Deal
+description: This script farms Tainted Gems and Dark Crystal Shards using "The Poisonous Deal" Quest.
+tags: poisonous,deal,dark,crystal,shard,tainted,gem,nulgath,nation,crag,bamboozle,quest,claymore
 */
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreStory.cs
 //cs_include Scripts/Nation/CoreNation.cs
-//cs_include Scripts/Nation\Various\PurifiedClaymoreOfDestiny.cs
+//cs_include Scripts/Nation\Various\TaintedClaymore.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Options;
 
-public class TheRefreshingDeal
+public class ThePoisonousDeal
 {
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
     public CoreFarms Farm = new CoreFarms();
     public CoreNation Nation = new();
-    public PurifiedClaymoreOfDestiny PCoD = new();
+    public TaintedClaymore TC = new();
 
-    public string OptionsStorage = "RefreshingDeal";
+    public string OptionsStorage = "PoisonousDeal";
     public bool DontPreconfigure = true;
     public List<IOption> Options = new List<IOption>()
     {
         CoreBots.Instance.SkipOptions,
-        new Option<int>("GemQuantity", "How many Gems of Nulgath?","Max Stack is 1000" ,0),
-        new Option<int>("TotemQuantity", "How many Totems of Nulgath?","Max Stack is 100", 0),
+        new Option<int>("TaintedQuantity", "How many Tainted Gems?","Max Stack is 1000" ,0),
+        new Option<int>("ShardQuantity", "How many Dark Crystal Shards?","Max Stack is 1000", 0),
         new Option<bool>("BankItems", "Bank nation items at the end", "true/false", false),
     };
 
@@ -34,12 +34,12 @@ public class TheRefreshingDeal
         Core.BankingBlackList.AddRange(Nation.bagDrops);
         Core.SetOptions();
 
-        Deal(Bot.Config.Get<int>("GemQuantity"), Bot.Config.Get<int>("TotemQuantity"));
+        Deal(Bot.Config.Get<int>("TaintedQuantity"), Bot.Config.Get<int>("ShardQuantity"));
 
         Core.SetOptions(false);
     }
 
-    public void Deal(int GemQuant, int TotemQuant)
+    public void Deal(int TaintedQuant, int ShardQuant)
     {
         if (!Core.CheckInventory(Nation.CragName))
         {
@@ -47,33 +47,33 @@ public class TheRefreshingDeal
             return;
         }
 
-        Nation.DragonSlayerReward(); //required
-        PCoD.GetPCoD();
+        Nation.DragonSlayerReward();
+        TC.DoAll();
 
-        Core.AddDrop("Gem of Nulgath", "Totem of Nulgath");
+        Core.AddDrop("Tainted Gem", "Dark Crystal Shard");
 
-        Core.FarmingLogger("Gem of Nulgath", GemQuant);
+        Core.FarmingLogger("Tainted Gem", TaintedQuant);
 
-        while (!Bot.ShouldExit && !Core.CheckInventory("Gem of Nulgath", GemQuant))
+        while (!Bot.ShouldExit && !Core.CheckInventory("Tainted Gem", TaintedQuant))
         {
-            Core.EnsureAccept(4777);
+            Core.EnsureAccept(4776);
             Core.HuntMonster("graveyard", "Big Jack Sprat", "Bone Axe", isTemp: false);
             Nation.FarmBloodGem(2);
             Nation.FarmUni10(30);
-            Core.EnsureComplete(4777, 6136);
-            Bot.Wait.ForPickup("Gem of Nulgath");
+            Core.EnsureComplete(4776, 4769);
+            Bot.Wait.ForPickup("Tainted Gem");
         }
 
-        Core.FarmingLogger("Totem of Nulgath", TotemQuant);
+        Core.FarmingLogger("Dark Crystal Shard", ShardQuant);
 
-        while (!Bot.ShouldExit && !Core.CheckInventory("Totem of Nulgath", TotemQuant))
+        while (!Bot.ShouldExit && !Core.CheckInventory("Dark Crystal Shard", ShardQuant))
         {
-            Core.EnsureAccept(4777);
+            Core.EnsureAccept(4776);
             Core.HuntMonster("graveyard", "Big Jack Sprat", "Bone Axe", isTemp: false);
             Nation.FarmBloodGem(2);
             Nation.FarmUni10(30);
-            Core.EnsureComplete(4777, 5357);
-            Bot.Wait.ForPickup("Totem of Nulgath");
+            Core.EnsureComplete(4776, 4770);
+            Bot.Wait.ForPickup("Dark Crystal Shard");
         }
 
         if (Bot.Config.Get<bool>("BankItems"))
