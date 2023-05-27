364b3d1adcc1c4f0e6ef3001d1e2b938e9aa8436
Tato <47118168+wtffidy@users.noreply.github.com>
2023-05-27 07:37:43 -0400

    quant fixes

--- Good/GearOfAwe/ArmorOfAwe.cs
+++ Good/GearOfAwe/ArmorOfAwe.cs
@@ -61,7 +61,7 @@ public class ArmorOfAwe
         Awe.GetAweRelic("Breastplate", 4163, 10, 10, "aqlesson", "Carnax");
         Awe.GetAweRelic("Vambrace", 4166, 15, 15, "bloodtitan", "Ultra Blood Titan");
         Awe.GetAweRelic("Gauntlet", 4169, 25, 5, "alteonbattle", "ULTRA Alteon");
-        Awe.GetAweRelic("Greaves", 4172, 10, 15, "bosschallenge", "Mutated Void Dragon");
+        Awe.GetAweRelic("Greaves", 4172, 10, 10, "bosschallenge", "Mutated Void Dragon");
         Core.BuyItem("museum", 1129, "Armor of Awe");
 
         Core.ToBank("Legendary Awe Pass", "Guardian Awe Pass", "Armor of Awe Pass");
