62670f4edd5608cb987f18320eee1957c704ee70
Hassan0Kamel <103977827+Hassan0Kamel@users.noreply.github.com>
2023-05-27 16:35:33 +0300

    the 2 merge shops of /sunlightzone map
    farms all the items in this map all merges there, the house items and the normal merge shop

---
+++ Other/MergeShops/SunlightZoneHouseMerge.cs
@@ -0,0 +1,102 @@
+/*
+name: Sunlight Zone House Merge
+description: This bot will farm the items belonging to the selected mode for the Sunlight Zone House Merge [2289] in /sunlightzone
+tags: sunlight, zone, house, merge, sunlightzone, stern, song, guest, taras, temporary, rest, disgruntled, mi
+*/
+//cs_include Story/AgeOfRuin/CoreAOR.cs
+//cs_include Scripts/Story\ShadowsOfWar\CoreSoW.cs
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Models.Items;
+using Skua.Core.Options;
+
+public class SunlightZoneHouseMerge
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+    private CoreAdvanced Adv = new();
+    private static CoreAdvanced sAdv = new();
+
+    private CoreSoW SoW = new();
+
+    public CoreAOR AOR = new();
+
+    public List<IOption> Generic = sAdv.MergeOptions;
+    public string[] MultiOptions = { "Generic", "Select" };
+    public string OptionsStorage = sAdv.OptionsStorage;
+    // [Can Change] This should only be changed by the author.
+    //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
+    private bool dontStopMissingIng = false;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.BankingBlackList.AddRange(new[] { "Sun Zone Chit", "Undine Visitor Badge"});
+        Core.SetOptions();
+
+        BuyAllMerge();
+        Core.SetOptions(false);
+    }
+
+    public void BuyAllMerge(string? buyOnlyThis = null, mergeOptionsEnum? buyMode = null)
+    {
+         AOR.SunlightZone();
+        //Only edit the map and shopID here
+        Adv.StartBuyAllMerge("sunlightzone", 2289, findIngredients, buyOnlyThis, buyMode: buyMode);
+
+        #region Dont edit this part
+        void findIngredients()
+        {
+            ItemBase req = Adv.externalItem;
+            int quant = Adv.externalQuant;
+            int currentQuant = req.Temp ? Bot.TempInv.GetQuantity(req.Name) : Bot.Inventory.GetQuantity(req.Name);
+            if (req == null)
+            {
+                Core.Logger("req is NULL");
+                return;
+            }
+
+            switch (req.Name)
+            {
+                default:
+                    bool shouldStop = Adv.matsOnly ? !dontStopMissingIng : true;
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping"), messageBox: shouldStop, stopBot: shouldStop);
+                    break;
+                #endregion
+
+                case "Sun Zone Chit":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.RegisterQuests(9251);
+                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonster("sunlightzone", "Marine Snow", "Marine Sample");
+                        Core.HuntMonster("sunlightzone", "Infernal Illusion", "Infernal Sample", 10);
+                        Core.HuntMonster("sunlightzone", "Seraphic Illusion", "Seraphic Sample", 10);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Undine Visitor Badge":
+                    Core.FarmingLogger(req.Name, quant);
+                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonster("sunlightzone", "Spectral Jellyfish", req.Name, quant);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    break;
+
+            }
+        }
+    }
+
+    public List<IOption> Select = new()
+    {
+        new Option<bool>("77955", "Stern Song House Guest", "Mode: [select] only\nShould the bot buy \"Stern Song House Guest\" ?", false),
+        new Option<bool>("77993", "Tara's Temporary Rest Guest", "Mode: [select] only\nShould the bot buy \"Tara's Temporary Rest Guest\" ?", false),
+        new Option<bool>("77996", "Disgruntled Mi Guest", "Mode: [select] only\nShould the bot buy \"Disgruntled Mi Guest\" ?", false),
+        new Option<bool>("77997", "Disgruntled Song Guest", "Mode: [select] only\nShould the bot buy \"Disgruntled Song Guest\" ?", false),
+    };
+}
---
+++ Other/MergeShops/UndineCommissaryMerge.cs
@@ -0,0 +1,107 @@
+/*
+name: Undine Commissary Merge
+description: This bot will farm the items belonging to the selected mode for the Undine Commissary Merge [2288] in /sunlightzone
+tags: undine, commissary, merge, sunlightzone, fas, casual, ensemble, mis, doctor, defence, director, researcher, clean, bob, cut, horn, songs, high, ponytail, glasses
+*/
+//cs_include Story/AgeOfRuin/CoreAOR.cs
+//cs_include Scripts/Story\ShadowsOfWar\CoreSoW.cs
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Models.Items;
+using Skua.Core.Options;
+
+public class UndineCommissaryMerge
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+    private CoreAdvanced Adv = new();
+    private static CoreAdvanced sAdv = new();
+    
+    private CoreSoW SoW = new();
+
+    public CoreAOR AOR = new();
+
+    public List<IOption> Generic = sAdv.MergeOptions;
+    public string[] MultiOptions = { "Generic", "Select" };
+    public string OptionsStorage = sAdv.OptionsStorage;
+    // [Can Change] This should only be changed by the author.
+    // If true, it will not stop the script if the default case triggers and the user chose to only get mats
+    private bool dontStopMissingIng = false;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.BankingBlackList.AddRange(new[] { "Undine Base Scrip", "Sun Zone Chit"});
+        Core.SetOptions();
+
+        BuyAllMerge();
+        Core.SetOptions(false);
+    }
+
+    public void BuyAllMerge(string? buyOnlyThis = null, mergeOptionsEnum? buyMode = null)
+    {
+        AOR.SunlightZone();
+        //Only edit the map and shopID here
+        Adv.StartBuyAllMerge("sunlightzone", 2288, findIngredients, buyOnlyThis, buyMode: buyMode);
+
+        #region Dont edit this part
+        void findIngredients()
+        {
+            ItemBase req = Adv.externalItem;
+            int quant = Adv.externalQuant;
+            int currentQuant = req.Temp ? Bot.TempInv.GetQuantity(req.Name) : Bot.Inventory.GetQuantity(req.Name);
+            if (req == null)
+            {
+                Core.Logger("req is NULL");
+                return;
+            }
+
+            switch (req.Name)
+            {
+                default:
+                    bool shouldStop = Adv.matsOnly ? !dontStopMissingIng : true;
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping"), messageBox: shouldStop, stopBot: shouldStop);
+                    break;
+                #endregion
+
+                case "Undine Base Scrip":
+                    Core.FarmingLogger(req.Name, quant);
+                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonster("sunlightzone", "Marine Snow", req.Name, quant);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    break;
+
+                case "Sun Zone Chit":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.RegisterQuests(9251);
+                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.HuntMonster("sunlightzone", "Marine Snow", "Marine Sample");
+                        Core.HuntMonster("sunlightzone", "Infernal Illusion", "Infernal Sample", 10);
+                        Core.HuntMonster("sunlightzone", "Seraphic Illusion", "Seraphic Sample", 10);
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+            }
+        }
+    }
+
+    public List<IOption> Select = new()
+    {
+        new Option<bool>("77957", "Fa's Casual Ensemble", "Mode: [select] only\nShould the bot buy \"Fa's Casual Ensemble\" ?", false),
+        new Option<bool>("77960", "Mi's Casual Ensemble", "Mode: [select] only\nShould the bot buy \"Mi's Casual Ensemble\" ?", false),
+        new Option<bool>("77963", "Undine Doctor", "Mode: [select] only\nShould the bot buy \"Undine Doctor\" ?", false),
+        new Option<bool>("77964", "Undine Defence Director", "Mode: [select] only\nShould the bot buy \"Undine Defence Director\" ?", false),
+        new Option<bool>("77965", "Undine Researcher", "Mode: [select] only\nShould the bot buy \"Undine Researcher\" ?", false),
+        new Option<bool>("77958", "Fa's Clean Bob Cut", "Mode: [select] only\nShould the bot buy \"Fa's Clean Bob Cut\" ?", false),
+        new Option<bool>("77959", "Fa's Clean Bob Cut Horn", "Mode: [select] only\nShould the bot buy \"Fa's Clean Bob Cut Horn\" ?", false),
+        new Option<bool>("77961", "Song's High Ponytail Glasses", "Mode: [select] only\nShould the bot buy \"Song's High Ponytail Glasses\" ?", false),
+        new Option<bool>("77962", "Song's High Ponytail", "Mode: [select] only\nShould the bot buy \"Song's High Ponytail\" ?", false),
+    };
+}
