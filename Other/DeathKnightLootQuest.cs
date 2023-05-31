cde3cc844f06140ea9125d27231dba340d6c1ac8
Hassan0Kamel <103977827+Hassan0Kamel@users.noreply.github.com>
2023-06-01 01:02:01 +0300

    Update DeathKnightLootQuest.cs
    edited little things like removing the useless int itemID in our case, deleted all of the other comments

--- Other/DeathKnightLootQuest.cs
+++ Other/DeathKnightLootQuest.cs
@@ -24,19 +24,16 @@ public class DeathKnightLootChestQuest
 
     private CoreFarms Cf = new();
 
-    //Insert Reward ID(load quest then -> tools > grabber > quests > grab > 
-    //click the quest, and on the right scroll down to \"Rewards\", click the ... to the Right, and the ids are beside the names
-    int itemID = 44883;
     int questID = 6509;
-    int quant = 6;
-    //Please fillin the above^
+    int quant = 5;
+
 
     public void ScriptMain(IScriptInterface Bot)
     {
         Core.SetOptions();
 
         DoQuest();
-        RandomReward(6509, 1);
+        RandomReward(6509, 5);
 
         Core.SetOptions(true);
     }
@@ -44,23 +41,11 @@ public class DeathKnightLootChestQuest
     public void DoQuest()
     {
 
-        QuestsIfNeeded(); //fillin below
+        QuestsIfNeeded();
 
-        // RandomReward(0000, 1);//-----|
-        // SelectReward(0000, 1);//-----| Pick one
-        // AutoReward(0000, 1);  //-----|
-        // "//" the one you want to use.
     }
 
-
-    //Use RandomReward for non-guaranteed Rewards
-    //Use AutoReward for Quests with Selectable Rewards that you want all of them
-    //Use SelectReward for Quests with Selectable Rewards that you want a select one of (itemID above is the item).
-    //Use OptionsSelect for a List(must be filled in in the Enum Below.)
-
-    //Basicly when making Set/simple quest reward scripts... pick one of these // the rest in scriptmain 👍
-
-    private void RandomReward(int questID = 6509, int quant = 1)
+    private void RandomReward(int questID = 6509, int quant = 5)
     {
         int i = 0;
 
@@ -98,7 +83,7 @@ public class DeathKnightLootChestQuest
         }
     }
 
-   
+
 
     public void QuestsIfNeeded()
     {
@@ -112,57 +97,3 @@ public class DeathKnightLootChestQuest
         else Core.Logger("Required Items not found, Stopping", stopBot: true);
     }
 }
-//Ignore below its.. storage
-
-
-
-
-// stuff not needed:
-//     public void OptionsSelect(Template reward = new(), int questID = 000)
-//     {
-//         List<ItemBase> RewardOptions = Core.EnsureLoad(questID).Rewards;
-//         List<string> RewardsList = new List<string>();
-//         foreach (Skua.Core.Models.Items.ItemBase Item in RewardOptions)
-//             RewardsList.Add(Item.Name);
-
-//         ItemBase item = Core.EnsureLoad(questID).Rewards.Find(x => x.ID == (int)Bot.Config.Get<Template>("RewardSelect"));
-
-//         if (item == null)
-//             Core.Logger($"{item.Name} not found in Quest Rewards", stopBot: true);
-
-//         if (Core.CheckInventory(item.Name))
-//             return;
-
-//         while (!Core.CheckInventory(item.Name))
-//         {
-//             Core.EnsureAccept(questID);
-//             //Questing stuff here --
-//             if (Bot.Config.Get<Template>("RewardsSelection") != Template.All)
-//                 Core.EnsureComplete(554, item.ID);
-//             else Core.EnsureComplete(questID, item.ID);
-//         }
-//     }
-
-// public enum Template
-// {
-//     All,
-//     Reward1 = 1,
-//     Reward2 = 2,
-//     Reward3 = 3,
-//     Reward4 = 4,
-// };	
-
-// public bool DontPreconfigure = true;
-
-// public string OptionsStorage = "QuestRewardTemplate";
-
-
-// public List<IOption> Options = new List<IOption>()
-// {
-//     CoreBots.Instance.SkipOptions,
-//     new Option<int>("questID", "QuestID", "Insert Quest ID for Bot to use", 0000),
-//     new Option<int>("RewardID", "ItemID:", "Insert Reward ID(load quest then -> tools > grabber > quests > grab > click the quest, and oh the right scroll down to \"Rewards\", click the ... and the ids are beside the names", "ItemName"),
-//     new Option<bool>("SelectReward", "Choose the Reward?", "Select the Reward the Bot will Get, then stop.", false),
-//     new Option<bool>("RandomReward", "Randomized Reward", "For Quests that the reward is \"You may also  receive at random:\"", false),
-//     new Option<bool>("AutoReward", "AllRewards", "Complete the quest, untill all queust rewards are gotten.}", false),
-// };
