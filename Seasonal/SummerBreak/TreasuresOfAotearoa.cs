49c764cf9cb84cfff01cc4379266b1203639f42f
Vld Mrk <vladamark00@gmail.com>
2023-06-26 15:15:46 +0200

    treasure arteroeoa rework

--- Seasonal/SummerBreak/TreasuresOfAotearoa.cs
+++ Seasonal/SummerBreak/TreasuresOfAotearoa.cs
@@ -1,12 +1,11 @@
 /*
 name: Treasures of Aotearoa
-description: This script will farm all the rewards from Treasures of Aotearoa quest.
+description: This script will farm all the rewards of "Treasures of Aotearoa" quest.
 tags: treasures,of,aotearoa,seasonal,summer,break,valencia,dandelion,kiwi,cookie
 */
 //cs_include Scripts/CoreBots.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
-// using Skua.Core.Options;
 
 public class TreasuresOfAotearoa
 {
@@ -18,70 +17,31 @@ public class TreasuresOfAotearoa
     public void ScriptMain(IScriptInterface Bot)
     {
         Core.SetOptions();
-        DoQuest();
+
+        Rewards();
+
         Core.SetOptions(true);
     }
 
-    public void DoQuest()
+    public void Rewards()
     {
-        AutoReward(9279);
-        RandomReward(9279);
-    }
+        Core.AddDrop(Core.QuestRewards(questID));
 
-    public void AutoReward(int questID = 0000)
-    {
-        int[] RewardOptions = new[] { 77816, 77817, 77818, 77819, 77823, 78618 };
-
-        Bot.Drops.Add(Core.QuestRewards(questID));
-
-        foreach (int ID in RewardOptions)
+        foreach (string Item in Core.QuestRewards(questID)[4..9])
         {
-            while (!Bot.ShouldExit && !Core.CheckInventory(ID))
-            {
-                Core.EnsureAccept(questID);
+            var rewards = Core.EnsureLoad(questID).Rewards;
+            ItemBase? item = rewards.Find(x => x.Name == Item);
 
-                Core.HuntMonster("burningbeach", "Water Goblin", "Stolen Egg", 5, log: false);
+            Core.EnsureAccept(questID);
 
-                Core.EnsureComplete(questID, ID);
-            }
+            Core.HuntMonster("burningbeach", "Water Goblin", "Stolen Egg", 5, log: false);
+
+            Core.EnsureComplete(questID, item.ID);
         }
-    }
 
-    private void RandomReward(int questID = 0000, int quant = 1)
-    {
-        int i = 0;
-
-        List<ItemBase> RewardOptions = Core.EnsureLoad(questID).Rewards;
-
-        foreach (ItemBase item in RewardOptions)
-            Bot.Drops.Add(item.Name);
-
-        string[] QuestRewards = RewardOptions.Select(x => x.Name).ToArray();
-
-        Core.EquipClass(ClassType.Farm);
         Core.RegisterQuests(questID);
-        foreach (ItemBase Reward in RewardOptions)
-        {
-            if (Core.CheckInventory(Reward.Name, toInv: false))
-                Core.Logger($"{Reward.Name} Found.");
-            else
-            {
-                Core.FarmingLogger(Reward.Name, 1);
-                while (!Bot.ShouldExit && !Core.CheckInventory(Reward.Name, quant, toInv: false))
-                {
-
-                    Core.HuntMonster("burningbeach", "Water Goblin", "Stolen Egg", 5, log: false);
-
-
-                    i++;
-
-                    if (i % 5 == 0)
-                    {
-                        Core.JumpWait();
-                        Core.ToBank(QuestRewards);
-                    }
-                }
-            }
-        }
+        while (!Bot.ShouldExit && !Core.CheckInventory(Core.QuestRewards(questID)[2..3]))
+            Core.HuntMonster("burningbeach", "Water Goblin", "Stolen Egg", 5, log: false);
+        Core.CancelRegisteredQuests();
     }
 }
