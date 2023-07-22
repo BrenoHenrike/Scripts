c96b5d78d46c179598afab48d7eaaabb31018638
Tato <47118168+wtffidy@users.noreply.github.com>
2023-07-22 07:19:49 -0400

    new metal necros 2023

---
+++ Other/Concerts/BattleConcert2023.cs
@@ -0,0 +1,83 @@
+/*
+name: Battle Concert Class Quests
+description: does the battle concert even quests for the classes.
+tags: concert, metal necro, doom metal necro, neo metal necro
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using Skua.Core.Interfaces;
+
+public class BattleConcertClassQuests
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreStory Story = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        BattleConcertQuests();
+
+        Core.SetOptions(false);
+    }
+
+    public void BattleConcertQuests()
+    {
+        if (Core.isCompletedBefore(9327))
+            return;
+
+        Story.PreLoad(this);
+
+        // Soulless Paps 9317
+        Story.KillQuest(9317, "skulldome", "Photoghoulpher");
+
+        // Vanquished from the Premises 9318
+        Story.KillQuest(9318, "skullhall", "Necroupie");
+
+        // Clear Space, Clear Mind  9319
+        Story.MapItemQuest(9319, "skullhall", new[] { 11948, 11949 });
+        Story.MapItemQuest(9319, "skullhall", 11950, 6);
+
+        // Rusty Metal 9320
+        Story.KillQuest(9320, "skullhall", new[] { "Photoghoulpher", "Necroupie" });
+
+        // Concert Crush 9324
+        Story.KillQuest(9324, "skullhall", "Photoghoulpher");
+
+        // Hashtag Dead Ugly 9322
+        Story.KillQuest(9322, "skullhall", "Necroupie");
+
+        // Ghoulish Fan Culture 9323
+        Story.KillQuest(9323, "skullhall", "Photoghoulpher");
+
+        // No Honor Among Rockers 9321
+        Story.KillQuest(9321, "skullarena", new[] { "Laryn", "Bellum" });
+
+        // Musical Altercations 9325
+        Story.KillQuest(9325, "skullarena", new[] { "Pit", "Medullos" });
+
+        // Lacking Brain Substance 9326
+        if (!Story.QuestProgression(9326))
+        {
+            Core.EnsureAccept(9326);
+
+            Core.HuntMonster("brainmeat", "Larythesia", "Laryn's Shiny Mic");
+            Core.HuntMonster("brainmeat", "Belluthesia", "Bellum's Shiny Toothpick");
+            Core.HuntMonster("brainmeat", "Pitesthesia", "Pit's Shiny Guitar");
+            Core.HuntMonster("brainmeat", "Medullothesia", "Medullos' Shiny Drumstick");
+            Core.EnsureComplete(9326);
+        }
+
+        // Farming Quest
+        // Revive the Encore 9327
+        Core.AddDrop("Bone Pick");
+        Story.KillQuest(9327, "brainmeat", "Brain Matter");
+
+
+        //Class Quest
+        // Doom Metal Necro Class 9328
+
+
+    }
+}
