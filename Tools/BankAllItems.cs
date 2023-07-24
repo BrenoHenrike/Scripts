c75d36f9eac9f6355d73bd4745d6bcd0c2ec2965
Tato <47118168+wtffidy@users.noreply.github.com>
2023-07-24 16:51:01 -0400

    wont bank your classes or set gear now

--- Tools/BankAllItems.cs
+++ Tools/BankAllItems.cs
@@ -13,7 +13,6 @@ public class BankAllItems
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
 
-
     public bool DontPreconfigure = true;
     public string OptionsStorage = "BankAllBlackList";
     public List<IOption> Options = new()
@@ -27,19 +26,22 @@ public class BankAllItems
 
     public void ScriptMain(IScriptInterface bot)
     {
+        Core.SetOptions();
+
         BankAll();
+       
+        Core.SetOptions(false);
     }
 
     public void BankAll()
     {
+        
         bool logged = false;
-        List<string> blackListedItems = new() { Core.SoloClass, Core.FarmClass, "Treasure Potion" };
+        List<string> blackListedItems = new() { "Treasure Potion" };
         blackListedItems.AddRange(Core.SoloGear);
         blackListedItems.AddRange(Core.FarmGear);
         if (!String.IsNullOrEmpty(Bot.Config.Get<string>("BlackList")))
             blackListedItems.AddRange(Bot.Config.Get<string>("BlackList").Split(',', StringSplitOptions.TrimEntries));
-
-
         Bot.Wait.ForMapLoad("battleon");
         Bot.Sleep(Core.ActionDelay);
         Bot.Send.Packet($"%xt%zm%house%1%{Bot.Player.Username}%");
