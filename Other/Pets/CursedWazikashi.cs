20ac90d4eb655048af9903404e90bf447dc67bfd
Tato <47118168+wtffidy@users.noreply.github.com>
2023-08-26 13:04:58 -0400

    release (for 0currentdailygifts + this standalone)

---
+++ Other/Pets/CursedWazikashi.cs
@@ -0,0 +1,51 @@
+/*
+name: Cursed Wazikashi Pet
+description: gets the pet
+tags: Cursed Wazikashi, Pet
+*/
+//cs_include Scripts/CoreBots.cs
+using Skua.Core.Interfaces;
+using Skua.Core.Models.Items;
+
+public class CursedWazikashi
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        CursedWakizashiPet();
+
+        Core.SetOptions(false);
+    }
+
+    public void CursedWakizashiPet()
+    {
+        Core.AddDrop(79316, 79317, 79318, 79319, 79320);
+
+        // Get item to  start quest [required]
+        if (!Core.CheckInventory(79316))
+            Core.GetMapItem(12046, 1, "museum");
+
+        // Crescent's Confession[DoomKitten]
+        Core.KillDoomKitten("Crescent's Confession");
+
+        //Steel Amulet
+        Core.EquipClass(ClassType.Farm);
+        Core.KillMonster("moonyardb", "r28", "Left", "*", "Steel Amulet");
+
+        // Master Pockey Ball
+        if (!Core.CheckInventory(79320))
+            Core.GetMapItem(12046, 1, "superslayin");
+
+        // Broken Bamboo Chunk
+        Core.HuntMonster("shogunwar", "Bamboo Treeant", "Broken Bamboo Chunk");
+
+        // Combin
+        Core.BuyItem("yokairiver", 2326, "Cursed Wakizashi Pet", shopItemID: 12048);
+
+    }
+
+}
