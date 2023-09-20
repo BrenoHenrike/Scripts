bf81fd1120e77a4ee20d895423d8a30ba755d6f1
Tato <47118168+wtffidy@users.noreply.github.com>
2023-09-20 13:09:39 -0400

    + new free boosts methods ( fixed)

---
+++ Other/FreeBoosts/XPBoost.cs
@@ -0,0 +1,25 @@
+/*
+name: null
+description: null
+tags: null
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+using Skua.Core.Interfaces;
+
+public class XpBoost
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreFarms Farm = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.BankingBlackList.AddRange(new[] { "XP Boost! (10 min)", "Fishing Dynamite" });
+        Core.SetOptions();
+
+        Farm.GetBoost(10850, "XP Boost! (10 min)", 9999, 1614, true);
+
+        Core.SetOptions(false);
+    }
+}
