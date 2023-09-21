00318e8d031e9a8c9cc42115eb39254c1c8d6dcd
Tato <47118168+wtffidy@users.noreply.github.com>
2023-09-21 14:09:11 -0400

    rename.

--- Nation/NulgathLarvaeAndKlunk.cs
+++ Nation/NulgathLarvaeAndKlunk.cs
@@ -1,14 +1,14 @@
 /*
-name: NulgathLarvae
-description: null
-tags: null
+name: NulgathLarvaeAndKlunk
+description: will either do "Nulgath (Klunk) [2568]" or "Nulgath (Larvae) [2566]" depending on what is currently available.
+tags: nulgath, larvae, klunk, nation
 */
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/Nation/CoreNation.cs
 using Skua.Core.Interfaces;
 
-public class NulgathLarvae
+public class NulgathLarvaeAndKlunk
 {
     public CoreBots Core => CoreBots.Instance;
     public CoreNation Nation = new();
