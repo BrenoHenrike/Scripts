c97deea642cfedd0d1e7a46d14a8c01027bbf4b0
Obsoleete <h.zarakkhan@gmail.com>
2023-09-26 18:22:44 -0400

    Custom Level Feature for Leveling Script

--- Farm/Leveling.cs
+++ Farm/Leveling.cs
@@ -7,6 +7,7 @@ tags: null
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 using Skua.Core.Interfaces;
+using Skua.Core.Options;
 
 public class Leveling
 {
@@ -14,6 +15,14 @@ public class Leveling
     public CoreBots Core => CoreBots.Instance;
     public CoreFarms Farm = new();
     public CoreAdvanced Adv = new();
+
+    public List<IOption> Options = new List<IOption>()
+    {
+        new Option<string>("level", "Level", "Up to which level would you like to farm?"),
+    };
+
+    private int MinLevel = 2;
+    private int MaxLevel = 100;
 
     public void ScriptMain(IScriptInterface bot)
     {
@@ -27,6 +36,23 @@ public class Leveling
     public void DoLeveling()
     {
         //Adv.BestGear(GenericGearBoost.exp);
-        Farm.Experience();
+
+        string level = Bot.Config!.Get<string>("level");
+
+        if (Int32.TryParse(level.Trim(), out int levelInt)) {
+            if (Bot.Player.Level >= levelInt) {
+                Core.Logger("Level inputted is greater than or equal to current level. Please input a valid level.");
+                return;
+            }
+
+            if (levelInt < MinLevel || levelInt > MaxLevel) {
+                Core.Logger($"Level inputted is invalid. Valid values: {MinLevel} - {MaxLevel}");
+                return;
+            }
+
+            Farm.Experience(levelInt);
+        } else {
+             Core.Logger($"Invalid level input {level}");
+        }
     }
 }
