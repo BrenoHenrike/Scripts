9a3d69cc7dcf4d19ae4be611140d9e320516f787
Vld Mrk <vladamark00@gmail.com>
2023-10-07 17:29:16 +0200

    switched the order

--- Other/Classes/VerusDoomKnight.cs
+++ Other/Classes/VerusDoomKnight.cs
@@ -78,8 +78,8 @@ public class VerusDoomKnightClass
             Core.HuntMonster("lumafortress", "Corrupted Luma", "Doom Worshipper's Blade Of Doom", isTemp: false);
             Core.HuntMonster("innershadows", "Krahen", "Empress' ShadowCloak", isTemp: false);
             Core.HuntMonster("techfortress", "MechaVortrix", "Cybernetic Doom Blade", isTemp: false);
-            Bot.Quests.UpdateQuest(7650);
             Core.GhostItem(55823, "Kyger", category: ItemCategory.Pet, "Time for training!", 1);
+            Bot.Quests.UpdateQuest(7650);
             Core.HuntMonsterMapID("stonewooddeep", 16, "Asherion Armor", isTemp: false);
             Core.EnsureComplete(9412);
         }
