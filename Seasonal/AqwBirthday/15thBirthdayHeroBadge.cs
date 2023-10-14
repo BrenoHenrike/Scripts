b433a45a1a39a0df549adfb8f24d89321c7b64c1
Vld Mrk <vladamark00@gmail.com>
2023-10-14 18:01:58 +0200

    15th Birthday Hero Badge script

---
+++ Seasonal/AqwBirthday/15thBirthdayHeroBadge.cs
@@ -0,0 +1,39 @@
+/*
+name: 15th Birthday Hero Character Page Badge
+description: This script will get the "15th Birthday Hero" Character Page Badge.
+tags: birthday,badge,anniversary,gifts,quest,seasonal
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using Skua.Core.Interfaces;
+
+public class BirthdayHeroBadge
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreStory Story = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        Badge();
+        Core.SetOptions(false);
+    }
+
+    public void Badge()
+    {
+        if (Core.HasWebBadge(badge))
+        {
+            Core.Logger($"Already have the {badge} badge");
+            return;
+        }
+
+        Core.EquipClass(ClassType.Solo);
+
+        //15th Anniversary Gifts
+        Story.KillQuest(6554, "birthday", new[] { "Birthday Cake", "Birthday Cake", "Birthday Cake", "Birthday Cake" });
+    }
+
+    private string badge = "15th Birthday Hero";
+}
