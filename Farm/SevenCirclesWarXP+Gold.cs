ed794f30ce523e1e8f8e2720d1ac7d28f169bca4
Tato <47118168+wtffidy@users.noreply.github.com>
2023-10-20 08:15:29 -0400

    Update SevenCirclesWarXP+Gold.cs

--- Farm/SevenCirclesWarXP+Gold.cs
+++ Farm/SevenCirclesWarXP+Gold.cs
@@ -29,7 +29,7 @@ public class SevenCirclesWarXP
 
     public void DoSevenCirclesWarXP()
     {
-        SC.Circles();
+        SC.CirclesWar();
 
         //Adv.BestGear(GenericGearBoost.exp);
         //Adv.BestGear(GenericGearBoost.gold);
