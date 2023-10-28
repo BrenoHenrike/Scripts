d8b8687b2a1b093e5869c01433f115277181bea0
Tato <47118168+wtffidy@users.noreply.github.com>
2023-10-28 09:54:56 -0400

    shadow of doom storyline -- merge soon :tm:

---
+++ Seasonal/BlackFriday/ShadowofDoom/Camlan.cs
@@ -0,0 +1,28 @@
+/*
+name: Camlan
+description: Does the Shadow of Doom Saga
+tags: shadow of doom, camlan, story, saga, doall, Camlan
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Seasonal/BlackFriday/ShadowofDoom/CoreShadowofDoom.cs
+using Skua.Core.Interfaces;
+
+public class Camlan
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreStory Story = new();
+    private CoreShadowofDoom CoreSoD = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        CoreSoD.Camlan();
+
+        Core.SetOptions(false);
+    }
+
+    
+}
