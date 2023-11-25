4f46191e00e36cf516eb35442ce52d4ade6f6dc2
Tato <47118168+wtffidy@users.noreply.github.com>
2023-11-25 09:31:29 -0500

    non-army Hollow Soul

---
+++ Hollowborn/Materials/HollowSoul.cs
@@ -0,0 +1,51 @@
+/*
+name: TradingandStuff(Dual)
+description: null
+tags: null
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Hollowborn/CoreHollowborn.cs
+//cs_include Scripts/Nation/CoreNation.cs
+
+using Skua.Core.Interfaces;
+
+public class HollowSoul
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreAdvanced Adv = new CoreAdvanced();
+    public CoreHollowborn HB = new CoreHollowborn();
+    public CoreNation Nation = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetYaSoulsHeeeere();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetYaSoulsHeeeere(int HSQuant = 2500)
+    {
+        if (Core.CheckInventory("Hollow Soul", HSQuant))
+            return;
+
+        Core.FarmingLogger("Hollow Soul", HSQuant);
+        Core.EquipClass(ClassType.Farm);
+
+        // Core.RegisterQuests(7553, 7555);
+        while (!Bot.ShouldExit && !Core.CheckInventory("Hollow Soul", HSQuant))
+        {
+            Core.EnsureAccept(7553, 7555);
+            Core.KillMonster("shadowrealm", "r2", "Left", "Gargrowl", "Darkseed", 8, log: false);
+            Core.KillMonster("shadowrealm", "r2", "Left", "Shadow Guardian", "Shadow Medallion", 5, log: false);
+            Core.EnsureComplete(new[] { 7553, 7555 });
+        }
+    }
+}
