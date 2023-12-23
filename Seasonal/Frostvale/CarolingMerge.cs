1c6948d5258b41384f407965a34a3d10521f091b
Tato <47118168+wtffidy@users.noreply.github.com>
2023-12-23 08:23:41 -0500

    Merge updated.

--- Seasonal/Frostvale/CarolingMerge.cs
+++ Seasonal/Frostvale/CarolingMerge.cs
@@ -1,7 +1,7 @@
 /*
 name: Caroling Merge
-description: This will get all or selected items on this merge shop.
-tags: caroling-merge, seasonal, frostvale
+description: This bot will farm the items belonging to the selected mode for the Caroling Merge [2197] in /carolinn
+tags: caroling, merge, carolinn, midwinter, cheermaker, cutie, antlered, beanie, snowflakes, snowy, nimbo, licorice, candy, cane, darkwood, carved, armaments, northlands, paladin, frozen, paladins, glacial, light, destiny, ascended, aurum, wings, noble, leo, scion, scions, requiem, heraldic, lion, companion, frostval, party, favor, gifts, holiday, hoodie, , morph, holly, twilly, twig, zorbak
 */
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
@@ -27,11 +27,10 @@ public class CarolingMerge
 
     public void ScriptMain(IScriptInterface Bot)
     {
-        Core.BankingBlackList.AddRange(new[] { "Red Ribbon", "Wrapping Paper", "Icy Fur", "Silver Tinsel" });
+        Core.BankingBlackList.AddRange(new[] { "Red Ribbon", "Wrapping Paper", "Icy Fur", "Silver Tinsel", "Aurum Wings Blade", "Jingle Bells", "Brunswick Leo Scion", "Brunswick Leo's Requiem", "Brunswick Leo Scion Cane", "Spearmint Candy Cane", "100 Pound Gift", "Chill Hoodie Outfit", "Chill Hat + Hair", "Chill Hat + Locks", "Chill Hat Morph", "Chill Hat Visage" });
         Core.SetOptions();
 
         BuyAllMerge();
-
         Core.SetOptions(false);
     }
 
@@ -58,40 +57,66 @@ public class CarolingMerge
                     bool shouldStop = !Adv.matsOnly || !dontStopMissingIng;
                     Core.Logger($"The bot hasn't been taught how to get {req.Name}." + (shouldStop ? " Please report the issue." : " Skipping"), messageBox: shouldStop, stopBot: shouldStop);
                     break;
-                #endregion
-                case "Silver Tinsel":
-                case "Red Ribbon":
+                    #endregion
+
                     Core.FarmingLogger(req.Name, quant);
-                    Core.EquipClass(ClassType.Solo);
+                    Core.EquipClass(ClassType.Farm);
+                    Core.RegisterQuests(0000);
                     while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
                     {
-                        // Core.HuntMonster("caroling", "Frostval Tree", req.Name, quant, false);
+                        Core.Logger("This item is not setup yet");
                         Bot.Wait.ForPickup(req.Name);
-                        Core.Join("caroling");
-                        for (int i = 0; i <= 5; i++)
-                        {
-                            Bot.Kill.Monster("Frostval Tree");
-                            Core.Sleep();
-                            Bot.Wait.ForPickup(req.Name);
-                            if (i == 5)
-                            {
-                                Core.Join("carolinn-100000");
-                                Core.Sleep(1500);
-                            }
-                        }
                     }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Jingle Bells":
+                    Core.FarmingLogger(req.Name, quant);
+                    Core.EquipClass(ClassType.Farm);
+                    Core.RegisterQuests(0000);
+                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    {
+                        Core.Logger("This item is not setup yet");
+                        Bot.Wait.ForPickup(req.Name);
+                    }
+                    Core.CancelRegisteredQuests();
+                    break;
+
+                case "Aurum Wings Blade":
+                case "Brunswick Leo Scion":
+                case "Brunswick Leo's Requiem":
+                case "Brunswick Leo Scion Cane":
+                case "Spearmint Candy Cane":
+                case "100 Pound Gift":
+                case "Chill Hoodie Outfit":
+                case "Chill Hat + Hair":
+                case "Chill Hat + Locks":
+                case "Chill Hat Visage":
+                case "Chill Hat Morph":
+                    Core.EquipClass(ClassType.Farm);
+                    Core.HuntMonster("carolinn", "Eldritch Gifthulu", req.Name, quant, false);
+                    break;
+
+                case "Silver Tinsel":
+                    Core.EquipClass(ClassType.Farm);
+                    Core.HuntMonster("carolinn", "Frostval Tree", req.Name, quant, false);
+                    break;
+
+                case "Red Ribbon":
+                    Core.EquipClass(ClassType.Farm);
+                    // Jingle Spells - 9520
+                    Core.RegisterQuests(9520);
+                    Core.HuntMonster("carolinn", "Frostval Deer", req.Name, quant, false);
+                    Bot.Wait.ForPickup(req.ID);
+                    Core.CancelRegisteredQuests();
                     break;
 
                 case "Icy Fur":
                 case "Wrapping Paper":
-                    Core.FarmingLogger(req.Name, quant);
                     Core.EquipClass(ClassType.Farm);
-                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
-                    {
-                        Core.HuntMonster("carolinn", "Krumpet", req.Name, quant, false);
-                        Bot.Wait.ForPickup(req.Name);
-                    }
+                    Core.HuntMonster("carolinn", "Krumpet", req.Name, quant, false);
                     break;
+
             }
         }
     }
@@ -113,5 +138,22 @@ public class CarolingMerge
         new Option<bool>("73885", "Northlands Paladin", "Mode: [select] only\nShould the bot buy \"Northlands Paladin\" ?", false),
         new Option<bool>("73887", "Frozen Paladin's Helm", "Mode: [select] only\nShould the bot buy \"Frozen Paladin's Helm\" ?", false),
         new Option<bool>("73890", "Glacial Light of Destiny", "Mode: [select] only\nShould the bot buy \"Glacial Light of Destiny\" ?", false),
+        new Option<bool>("69478", "Ascended Aurum Wings Blade", "Mode: [select] only\nShould the bot buy \"Ascended Aurum Wings Blade\" ?", false),
+        new Option<bool>("79340", "Noble Leo Scion", "Mode: [select] only\nShould the bot buy \"Noble Leo Scion\" ?", false),
+        new Option<bool>("79342", "Noble Leo Scion Hair", "Mode: [select] only\nShould the bot buy \"Noble Leo Scion Hair\" ?", false),
+        new Option<bool>("79343", "Noble Leo Scion Locks", "Mode: [select] only\nShould the bot buy \"Noble Leo Scion Locks\" ?", false),
+        new Option<bool>("79344", "Leo Scion's Requiem", "Mode: [select] only\nShould the bot buy \"Leo Scion's Requiem\" ?", false),
+        new Option<bool>("79346", "Heraldic Lion Companion", "Mode: [select] only\nShould the bot buy \"Heraldic Lion Companion\" ?", false),
+        new Option<bool>("79347", "Noble Leo Scion Cane", "Mode: [select] only\nShould the bot buy \"Noble Leo Scion Cane\" ?", false),
+        new Option<bool>("82548", "Frostval Party Favor", "Mode: [select] only\nShould the bot buy \"Frostval Party Favor\" ?", false),
+        new Option<bool>("82560", "Frostval Party Gifts", "Mode: [select] only\nShould the bot buy \"Frostval Party Gifts\" ?", false),
+        new Option<bool>("82778", "Northlands Holiday Hoodie", "Mode: [select] only\nShould the bot buy \"Northlands Holiday Hoodie\" ?", false),
+        new Option<bool>("82779", "Northlands Holiday Hat", "Mode: [select] only\nShould the bot buy \"Northlands Holiday Hat\" ?", false),
+        new Option<bool>("82780", "Northlands Holiday Hat + Locks", "Mode: [select] only\nShould the bot buy \"Northlands Holiday Hat + Locks\" ?", false),
+        new Option<bool>("82781", "Northlands Holiday Morph", "Mode: [select] only\nShould the bot buy \"Northlands Holiday Morph\" ?", false),
+        new Option<bool>("82782", "Northlands Holiday Visage", "Mode: [select] only\nShould the bot buy \"Northlands Holiday Visage\" ?", false),
+        new Option<bool>("82788", "Holly Holiday Twilly", "Mode: [select] only\nShould the bot buy \"Holly Holiday Twilly\" ?", false),
+        new Option<bool>("82789", "Holly Holiday Twig", "Mode: [select] only\nShould the bot buy \"Holly Holiday Twig\" ?", false),
+        new Option<bool>("82790", "Holly Holiday Zorbak", "Mode: [select] only\nShould the bot buy \"Holly Holiday Zorbak\" ?", false),
     };
 }
