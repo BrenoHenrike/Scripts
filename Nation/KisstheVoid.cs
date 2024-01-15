97edadb8f0dc351bb89645ed04a098f1aee3605d
luanupe <luan.augusto.xavier.oliveira@gmail.com>
2024-01-15 14:19:52 -0300

    feat: fix options

--- Nation/KisstheVoid.cs
+++ Nation/KisstheVoid.cs
@@ -7,19 +7,19 @@ tags: betrayal, blood, gem, archfiend
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/Nation/CoreNation.cs
 using Skua.Core.Interfaces;
-using Skua.Core.ViewModels;
-using Skua.Core.Models;
 using Skua.Core.Options;
-using Skua.Core.Utils;
 
 public class KisstheVoid
 {
     private IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
-    public string[] MultiOptions = { "Generic", "Select" };
+    public CoreNation Nation = new();
+    
     public string OptionsStorage = "Kiss_The_Void";
+    public bool DontPreconfigure = true;
     public List<IOption> Select = new()
     {
+        new Option<int>("22332", "Blood Gem of the Archfiend", "How many \"Blood Gem of the Archfiend\" you need? 0 = none", 100),
         new Option<bool>("25150", "1st Betrayal Blade of Nulgath", "Should the bot farm \"1st Betrayal Blade of Nulgath\"?", false),
         new Option<bool>("25151", "2nd Betrayal Blade of Nulgath", "Should the bot farm \"2nd Betrayal Blade of Nulgath\"?", false),
         new Option<bool>("25152", "3rd Betrayal Blade of Nulgath", "Should the bot farm \"3rd Betrayal Blade of Nulgath\"?", false),
@@ -29,7 +29,6 @@ public class KisstheVoid
         new Option<bool>("25156", "7th Betrayal Blade of Nulgath", "Should the bot farm \"7th Betrayal Blade of Nulgath\"?", false),
         new Option<bool>("25238", "8th Betrayal Blade of Nulgath", "Should the bot farm \"8th Betrayal Blade of Nulgath\"?", false),
     };
-    public CoreNation Nation = new();
 
     public void ScriptMain(IScriptInterface bot)
     {
@@ -44,19 +43,23 @@ public class KisstheVoid
         // Farm selected Betrayals of Nulgath
         Select.ForEach(x => Betrayal(x));
 
-        // Just in case the stack is not maxed out
-        Nation.KisstheVoid(100);
+        // Farm the Blood Gems last
+        int bloodGems = Bot.Config.Get<int>("22332");
+        if (bloodGems > 0) Nation.KisstheVoid(bloodGems);
     }
 
-    private void Betrayal(IOptions details) {
-        Core.Logger(details);
+    private void Betrayal(IOption details) {
         Core.Logger($"ID: {details.Name}, Name: {details.DisplayName}");
+        if (details.Name === "22332") return;
 
         string itemId = details.Name;
         string itemName = details.DisplayName;
         bool farm = Bot.Config.Get<bool>(itemId);
 
-        if (!farm) return;
+        if (!farm) {
+            Core.Logger($"You don't want the \"{itemName}\", ignoring it.");
+            return;
+        }
 
         if (Core.CheckInventory(itemName)) {
             Core.Logger($"You already own the \"{itemName}\".");
