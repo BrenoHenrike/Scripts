312ce36664a22030563e283caa9889d05298b241
luanupe <luan.augusto.xavier.oliveira@gmail.com>
2024-01-15 13:46:45 -0300

    feat: add options to kiss the void

--- Nation/KisstheVoid.cs
+++ Nation/KisstheVoid.cs
@@ -1,22 +1,62 @@
 /*
 name: KisstheVoid
-description: null
-tags: null
+description: This bot will farm Blood Gem of the Archfiend and also the Betrayals (if selected)
+tags: betrayal, blood, gem, archfiend
 */
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/Nation/CoreNation.cs
 using Skua.Core.Interfaces;
+using Skua.Core.Options;
 public class KisstheVoid
 {
     public CoreBots Core => CoreBots.Instance;
+    public string OptionsStorage = "KisstheVoid";
+    public List<IOption> Select = new()
+    {
+        new Option<bool>("25150", "1st Betrayal Blade of Nulgath", "Should the bot farm \"1st Betrayal Blade of Nulgath\" ?", false),
+        new Option<bool>("25151", "2nd Betrayal Blade of Nulgath", "Should the bot farm \"2nd Betrayal Blade of Nulgath\" ?", false),
+        new Option<bool>("25152", "3rd Betrayal Blade of Nulgath", "Should the bot farm \"3rd Betrayal Blade of Nulgath\" ?", false),
+        new Option<bool>("25153", "4th Betrayal Blade of Nulgath", "Should the bot farm \"4th Betrayal Blade of Nulgath\" ?", false),
+        new Option<bool>("25154", "5th Betrayal Blade of Nulgath", "Should the bot farm \"5th Betrayal Blade of Nulgath\" ?", false),
+        new Option<bool>("25155", "6th Betrayal Blade of Nulgath", "Should the bot farm \"6th Betrayal Blade of Nulgath\" ?", false),
+        new Option<bool>("25156", "7th Betrayal Blade of Nulgath", "Should the bot farm \"7th Betrayal Blade of Nulgath\" ?", false),
+        new Option<bool>("25238", "8th Betrayal Blade of Nulgath", "Should the bot farm \"8th Betrayal Blade of Nulgath\" ?", false),
+    };
     public CoreNation Nation = new();
+
     public void ScriptMain(IScriptInterface bot)
     {
         Core.SetOptions();
 
-        Nation.KisstheVoid();
+        Kiss();
 
         Core.SetOptions(false);
+    }
+
+    private void Kiss() {
+        // Farm selected Betrayals of Nulgath
+        Select.ForEach(x => Betrayal(x));
+
+        // Just in case the stack is not maxed out
+        Nation.KisstheVoid(100);
+    }
+
+    private void Betrayal(IOptions details) {
+        Core.Logger(details);
+
+        string itemId = details.Name;
+        string itemName = details.DisplayName;
+        bool? farm = Bot.Config.Get<bool>(itemId);
+
+        if (!farm) return;
+        Core.Logger($"ID: {details.Name}, Name: {details.DisplayName}");
+
+        if (Core.CheckInventory(itemName)) {
+            Core.Logger($"You already own the \"{itemName}\".");
+            return;
+        }
+
+        Nation.KisstheVoid(0, itemName);
     }
 }
