b6466bd5f98b8ea8459f7066803b6773fd074dfd
Tato <47118168+wtffidy@users.noreply.github.com>
2024-01-20 04:54:15 -0500

    + new stories for `Ravenous` Enh

---
+++ Story/Nation/PrimeFiendShard.cs
@@ -0,0 +1,170 @@
+/*
+name: PrimeFiendShardQuests
+description: This script will complete the storyline from the Prime Fiend Shard
+tags: Prime Fiend Shard, nation, ravenous
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Nation/CoreNation.cs
+//cs_include Scripts/Nation/VHL/CoreVHL.cs
+//cs_include Scripts/Story/Nation/VoidRefuge.cs
+//cs_include Scripts/Nation/MergeShops/VoidRefugeMerge.cs
+//cs_include Scripts/Nation/AssistingCragAndBamboozle[Mem].cs
+//cs_include Scripts/Story/Nation/Originul.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleDelve.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleSiege.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleDelveMerge.cs
+//cs_include Scripts/Story/BattleUnder.cs
+//cs_include Scripts/Good/BLOD/CoreBLOD.cs
+//cs_include Scripts/Nation/Various/TheLeeryContract[Member].cs
+//cs_include Scripts/Nation/Various/JuggernautItems.cs
+//cs_include Scripts/Nation/Various/VoidPaladin.cs
+//cs_include Scripts/Evil/NSoD/CoreNSOD.cs
+//cs_include Scripts/Evil/SDKA/CoreSDKA.cs
+//cs_include Scripts/Other/Classes/Necromancer.cs
+//cs_include Scripts/Nation/MergeShops/NulgathDiamondMerge.cs
+//cs_include Scripts/Nation/Various/TarosManslayer.cs
+//cs_include Scripts/Nation/Various/PurifiedClaymoreOfDestiny.cs
+//cs_include Scripts/Nation/Various/VoidSpartan.cs
+//cs_include Scripts/Story/Nation/Fiendshard.cs
+//cs_include Scripts/Nation/Various/SwirlingTheAbyss.cs
+//cs_include Scripts/Hollowborn/TradingandStuff(single).cs
+//cs_include Scripts/Hollowborn/CoreHollowborn.cs
+//cs_include Scripts/Nation/EmpoweringItems.cs
+//cs_include Scripts/Other/Weapons/VoidAvengerScythe.cs
+//cs_include Scripts/Nation/AFDL/NulgathDemandsWork.cs
+//cs_include Scripts/Nation/AFDL/WillpowerExtraction.cs
+//cs_include Scripts/Nation/Various/GoldenHanzoVoid.cs
+//cs_include Scripts/Nation/MergeShops/DilligasMerge.cs
+//cs_include Scripts/Nation/MergeShops/DirtlickersMerge.cs
+//cs_include Scripts/Other/Weapons/WrathofNulgath.cs
+using Skua.Core.Interfaces;
+
+
+public class PrimeFiendShard
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+    private CoreAdvanced Adv = new();
+    public Originul_Story Originul = new();
+    public CoreVHL VHL = new();
+    public static CoreVHL sVHL = new();
+    public CoreNation Nation = new();
+    public VoidRefugeMerge VoidRefugeMerge = new();
+    public TempleDelveMerge TempleDelveMerge = new();
+    public DirtlickersMerge DirtlickersMerge = new();
+    public VoidPaladin VoidPaladin = new();
+    public NulgathDiamondMerge NulgathDiamondMerge = new();
+    public VoidSpartan VoidSpartan = new();
+    public SwirlingTheAbyss SwirlingTheAbyss = new();
+    public TradingandStuffSingle TradingandStuffSingle = new();
+    public VoidAvengerScythe VoidAvengerScythe = new();
+    public NulgathDemandsWork NulgathDemandsWork = new();
+    public ArchfiendDeathLord ArchfienddDeathLord = new();
+    public WrathofNulgath WrathofNulgath = new();
+    public DilligasMerge DilligasMerge = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        Storyline();
+
+        Core.SetOptions(false);
+    }
+
+    public void Storyline()
+    {
+        if (Core.isCompletedBefore(9559))
+            return;
+
+        Core.OneTimeMessage("WARNING!!", "If you're going to use insingias for the roents\n" +
+        "please do so before starting the script, as it will stop otherwise if dont have them.");
+
+        VHL.GetVHL();
+
+        // Prime Fiend Shard [required to accept]
+        Nation.EssenceofNulgath(1);
+        //merge this ^
+
+        // Feed the Fiend Shard 9555
+        if (!Story.QuestProgression(9555))
+        {
+            Core.EnsureAccept(9555);
+            if (!Core.CheckInventory("Dual Dragonbone Axe of Nulgath"))
+            {
+                Core.AddDrop("Dual Dragonbone Axe of Nulgath");
+                // Combat Style: Dragonbone Axe 629
+                Core.EnsureAccept(629);
+                Nation.FarmUni13(1);
+                Nation.FarmTaintedGem(13);
+                Nation.FarmDarkCrystalShard(13);
+                Nation.FarmDiamondofNulgath(13);
+                Nation.Supplies(Nation.Uni(21));
+                Core.HuntMonster("evilmarsh", "Tainted Elemental", "Tainted Rune of Evil", log: false);
+                Core.EnsureComplete(629);
+            }
+            Nation.EssenceofNulgath(20);
+            VoidRefugeMerge.BuyAllMerge("Nation Soulstealer");
+            TempleDelveMerge.BuyAllMerge("Void Nation Caster");
+            DirtlickersMerge.BuyAllMerge("Iron Dreadsaw");
+            Core.EnsureComplete(9555);
+        }
+
+        // Grow the Fiend Shard 9556
+        if (!Story.QuestProgression(9556))
+        {
+            Core.EnsureAccept(9556);
+            Nation.EssenceofNulgath(40);
+            NulgathDiamondMerge.BuyAllMerge("Abyssal Priest of Nulgath");
+            NulgathDiamondMerge.BuyAllMerge("torm Knight");
+            VoidPaladin.DeeperandDeeperintoDarkness();
+            VoidSpartan.GetSpartan();
+            Core.EnsureComplete(9556);
+        }
+
+        // Evolve the Fiend Shard 9557
+        if (!Story.QuestProgression(9557))
+        {
+            Core.EnsureAccept(9557);
+            Nation.EssenceofNulgath(60);
+            SwirlingTheAbyss.STA("Void Soul of Nulgath");
+            TradingandStuffSingle.ArchFiendEnchantedOrbs();
+            NulgathDiamondMerge.BuyAllMerge("Blood Ranger");
+            VoidRefugeMerge.BuyAllMerge("Envenomed Edge of Nulgath");
+            Core.EnsureComplete(9557);
+        }
+
+        // The Fiend Shard's Demand 9558
+        if (!Story.QuestProgression(9558))
+        {
+            Core.EnsureAccept(9558);
+            VoidAvengerScythe.SkewsQuests();
+            NulgathDemandsWork.NDWQuest(new[] { "Doomblade of Destruction" });
+            ArchfienddDeathLord.GetArm(true, ArchfiendDeathLord.RewardChoice.Archfiend_DeathLord);
+            WrathofNulgath.GetSword();
+            DilligasMerge.BuyAllMerge("Ancient Shogun Armor");
+            Core.EnsureComplete(9558);
+        }
+
+        // The Fiend Shard's Finale 9559
+        if (!Story.QuestProgression(9559))
+        {
+            Core.EnsureAccept(9559);
+            Nation.FarmUni13(1);
+            Nation.FarmTaintedGem(750);
+            Nation.FarmDarkCrystalShard(400);
+            Nation.FarmDiamondofNulgath(1000);
+            Nation.FarmTotemofNulgath(60);
+            Nation.FarmGemofNulgath(300);
+            Nation.FarmBloodGem(100);
+            while (!Bot.ShouldExit && (!Core.CheckInventory("Roentgenium of Nulgath", 10) && Core.CheckInventory("Elders' Blood")))
+                VHL.VHLChallenge(10);
+            Core.EnsureAccept(9559);
+        }
+    }
+}
---
+++ Story/Nation/VoidChasm.cs
@@ -0,0 +1,70 @@
+/*
+name: Void Chasm Story
+description: This script will complete the storyline in /voidrefuge
+tags: voidchasm, voidrefuge, refuge, nation, nulgath, story, ana di carcano, gravelyn
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using Skua.Core.Interfaces;
+
+public class VoidChasm
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreStory Story = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        Storyline();
+
+        Core.SetOptions(false);
+    }
+
+    public void Storyline()
+    {
+        if (Core.isCompletedBefore(9553))
+            Story.PreLoad(this);
+
+        // Light Headed 9543
+        Story.KillQuest(9543, "voidchasm", "Paladin Ascendant");
+
+        // Forgiveness of Debt 9544
+        Story.KillQuest(9544, "voidchasm", "Nation Outrider");
+
+        // Hyperspace-Plasia 9545
+        Story.KillQuest(9545, "voidchasm", "Paladin Ascendant");
+        Story.MapItemQuest(9545, "voidchasm", 12619, 6);
+
+        // Tangled Aortas 9546        
+        Story.MapItemQuest(9546, "voidchasm", new[] { 12620, 12621 });
+
+        // Nation Code 9547
+        Story.KillQuest(9547, "voidchasm", "Nation Outrider");
+        Story.MapItemQuest(9547, "voidchasm", 12622);
+
+        // Snake Ears 9548
+        Story.KillQuest(9548, "voidchasm", "Void Fang");
+
+        // Faustian Leftovers 9549
+        Story.KillQuest(9549, "voidchasm", "The Hushed");
+        Story.MapItemQuest(9549, "voidchasm", new[] { 12623, 12627, 12628 });
+
+        // Broken Lives 9550
+        Story.KillQuest(9550, "voidchasm", new[] { "The Hushed", "Void Fang" });
+        Story.MapItemQuest(9550, "voidchasm", 12624, 4);
+
+        // Call of the Fiend 9551
+        Core.EquipClass(ClassType.Solo);
+        Story.MapItemQuest(9551, "voidchasm", new[] { 12625, 12626 });
+        Story.KillQuest(9551, "voidchasm", "Carnage");
+
+        // Famiglia di Carcano 9452
+        Story.KillQuest(9552, "voidchasm", "Carcano");
+
+        // Uprooted Veins 9453
+        Story.KillQuest(9553, "voidrefuge", "Carnage");
+        Story.KillQuest(9553, "voidchasm", new[] { "Carcano", "The Hushed" });
+    }
+}
