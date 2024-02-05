90c4f827bbb3edd7baf3c63df13fcaa95a54e6de
prestushood <45672754+prestushood@users.noreply.github.com>
2024-02-05 05:39:36 -0300

    Fixed VoidBattleMageSet.cs
    Old script was broken so i remade it, tested and working.

--- Nation/Various/VoidBattleMageSet.cs
+++ Nation/Various/VoidBattleMageSet.cs
@@ -4,62 +4,59 @@ description: null
 tags: null
 */
 //cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreStory.cs
 //cs_include Scripts/Story/VasalkarLairWar.cs
 
 using Skua.Core.Interfaces;
-using Skua.Core.Models.Items;
 
 public class VoidBattleMageSet
 {
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
     public LairWar War = new();
+
+    public readonly string[] Rewards =
+    {
+        "Void BattleMage",
+        "Void BattleMage Stare",
+        "Void BattleMage Male Morph",
+        "Void BattleMage Male Hood",
+        "Void BattleMage Locks",
+        "Void BattleMage Female Morph",
+        "Void BattleMage Female Hood",
+        "Void BattleMage Male Crown",
+        "Void BattleMage Female Crown",
+        "Void BattleMage Crown",
+        "Void BattleMage Runes",
+        "Void BattleMage Wrap",
+        "Void BattleMage Wrap Runes",
+        "Void BattleMage Spear",
+        "Void BattleMage Nation Staff"
+    };
 
     public void ScriptMain(IScriptInterface bot)
     {
         Core.SetOptions();
 
-        GetSet();
+        GetBattleMageSet();
 
         Core.SetOptions(false);
     }
 
-    int i = 0;
-
-    public void GetSet()
+    public void GetBattleMageSet()
     {
-        War.Attack();
-        
-        List<ItemBase> RewardOptions = Core.EnsureLoad(6694).Rewards;
-
-        foreach (ItemBase item in RewardOptions)
-            Bot.Drops.Add(item.Name);
-
-        string[] QuestRewards = RewardOptions.Select(x => x.Name).ToArray();
-
-        Core.EquipClass(ClassType.Farm);
-        Core.RegisterQuests(6694);
-        foreach (ItemBase Reward in RewardOptions)
+        int i = 1;
+        while (!Bot.ShouldExit && !Core.CheckInventory(Rewards, toInv: false))
         {
-            if (Core.CheckInventory(Reward.Name, toInv: false))
-                return;
-            else
-            {
-                Core.FarmingLogger(Reward.Name, 1);
-                while (!Bot.ShouldExit && !Core.CheckInventory(Reward.Name, toInv: false))
-                {
-                    Core.KillMonster("lairattack", "Eggs", "Left", "Flame Dragon General", log: false);
-
-                    i++;
-
-                    if (i % 5 == 0)
-                    {
-                        Core.JumpWait();
-                        Core.ToBank(QuestRewards);
-                    }
-                }
-            }
+            War.Attack();
+            Core.EquipClass(ClassType.Solo);
+            Core.EnsureAccept(6694);
+            Core.KillMonster("lairattack", "Eggs", "Left", "Flame Dragon General", log: false);
+            Core.ChainComplete(6694);
+            Bot.Drops.Pickup(Rewards);
+            Core.Logger($"Completed x{i++}");
         }
     }
 }
