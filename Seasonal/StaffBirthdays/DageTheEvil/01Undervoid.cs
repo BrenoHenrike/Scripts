196fb686dd7417f52956d48a8fcddbcb63048fd6
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-03-03 12:56:36 +0100

    Convert Dage Birthday story to Core

---
+++ Seasonal/StaffBirthdays/DageTheEvil/01Undervoid.cs
@@ -0,0 +1,27 @@
+/*
+name: Undervoid Story
+description: This will complete the Undervoid story.
+tags: story, quest, void, nation, legion, underworld, staff, birthday
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Seasonal\StaffBirthdays\DageTheEvil\CoreDageBirthday.cs
+using Skua.Core.Interfaces;
+
+public class UndervoidStory
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new CoreStory();
+    private CoreDageBirthday Dage = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        Dage.Undervoid();
+
+        Core.SetOptions(false);
+    }
+
+}
---
+++ Seasonal/StaffBirthdays/DageTheEvil/02FutureLegion.cs
@@ -0,0 +1,27 @@
+/*
+name: Future Legion Story
+description: This will complete the Future Legion story.
+tags: story, quest, future, legion, legion, staff, birthday
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Seasonal\StaffBirthdays\DageTheEvil\CoreDageBirthday.cs
+using Skua.Core.Interfaces;
+
+public class FutureLegion
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreStory Story = new();
+    private CoreDageBirthday Dage = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        Dage.FutureLegion();
+        Core.SetOptions(false);
+    }
+
+
+}
