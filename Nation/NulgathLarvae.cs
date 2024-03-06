1375df7ff244cf6881581d69c904859778cbb68a
Tato <47118168+wtffidy@users.noreply.github.com>
2024-03-06 04:31:27 -0500

    renamed after klunk removed

--- Nation/NulgathLarvae.cs
+++ Nation/NulgathLarvae.cs
@@ -1,14 +1,14 @@
 /*
-name: NulgathLarvaeAndKlunk
-description: will either do "Nulgath (Klunk) [2568]" or "Nulgath (Larvae) [2566]" depending on what is currently available.
-tags: nulgath, larvae, klunk, nation
+name: NulgathLarvae
+description: will do "Nulgath (Larvae) [2566]"
+tags: nulgath, larvae, nation
 */
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/Nation/CoreNation.cs
 using Skua.Core.Interfaces;
 
-public class NulgathLarvaeAndKlunk
+public class NulgathLarvae
 {
     public CoreBots Core => CoreBots.Instance;
     public CoreNation Nation = new();
