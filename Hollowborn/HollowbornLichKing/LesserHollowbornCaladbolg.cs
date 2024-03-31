181f35afa1a5c77f1547acfa31706ddf2a7a1700
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-03-31 14:26:28 +0200

    add LesserHollowbornCaladbolg.cs

---
+++ Hollowborn/HollowbornLichKing/LesserHollowbornCaladbolg.cs
@@ -0,0 +1,65 @@
+/*
+name: Lesser Hollowborn Caladbolg
+description: This script will get Lesser Hollowborn Caladbolg by completing "How Much For One Rib?" quest.
+tags: lesser caladbolg, Caladbolg, hollowborn,dark unicorn rib,how much for one rib,quest
+*/
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Hollowborn/CoreHollowborn.cs
+//cs_include Scripts/Nation/CoreNation.cs
+//cs_include Scripts/Legion/CoreLegion.cs
+//cs_include Scripts/Legion/Revenant/CoreLR.cs
+//cs_include Scripts/Story/QueenofMonsters/CoreQOM.cs
+//cs_include Scripts/Hollowborn\HollowbornLichKing\CoreHollowbornLichKing.cs
+//cs_include Scripts/Legion\MergeShops\UndeadLegionMerge.cs
+//cs_include Scripts/Legion\Various\SoulSand.cs
+//cs_include Scripts/Hollowborn\Materials\HollowSoul.cs
+//cs_include Scripts/Legion/Various/LetitBurn(SoulEssence).cs
+//cs_include Scripts/Legion/InfiniteLegionDarkCaster.cs
+//cs_include Scripts/Story/Legion/SeraphicWar.cs
+//cs_include Scripts/Legion/LegionExcercise/LegionExercise3.cs
+//cs_include Scripts/Legion/LegionExcercise/LegionExercise4.cs
+//cs_include Scripts/Nation/Various/DragonBlade[mem].cs
+//cs_include Scripts/Legion/Various/LegionBonfire.cs
+using Skua.Core.Interfaces;
+
+public class LesserHollowbornCaladbolg
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreHollowbornLichKing CoreHollowbornLichKing = new();
+    private CoreAdvanced Adv = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        GetLHC();
+
+        Core.SetOptions(false);
+    }
+
+    public void GetLHC()
+    {
+        if (Core.CheckInventory("Lesser Hollowborn Caladbolg"))
+            return;
+
+
+        Core.AddDrop("Lesser Hollowborn Caladbolg");
+
+        CoreHollowbornLichKing.Counterblow(CoreHollowbornLichKing.CounterblowRewards.Altar_Of_the_Hollowborn_Caladbolg);
+
+        Core.EquipClass(ClassType.Solo);
+        while (!Bot.ShouldExit && !Core.CheckInventory("Lesser Hollowborn Caladbolg"))
+        {
+            Core.EnsureAccept(9665);
+            Adv.BoostHuntMonster("doomvault", "Binky", "Dark Unicorn Rib", isTemp: false, log: false);
+            Core.EnsureComplete(9665);
+        }
+        Core.Logger("RNG gods have smiled upon you! Banking the weapon.");
+        Core.ToBank("Lesser Hollowborn Caladbolg");
+    }
+}
