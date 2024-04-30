4c7bb0c966227d3c26e3d15a6ad47117cb251569
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-04-30 18:07:36 +0200

    Add Void Refuge storyline in ArcanaInvoker[Non-Insignia].cs

--- Other/Classes/ArcanaInvoker[Non-Insignia].cs
+++ Other/Classes/ArcanaInvoker[Non-Insignia].cs
@@ -44,6 +44,7 @@ tags: arcana,invoker,class,arcana invoker,darkon,astravia,garden
 //cs_include Scripts/Darkon\CoreDarkon.cs
 //cs_include Scripts/Seasonal\BlackFriday\ShadowofDoom\CoreShadowofDoom.cs
 //cs_include Scripts/Story\FableForest.cs
+//cs_include Scripts/Story\Nation\VoidRefuge.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 
