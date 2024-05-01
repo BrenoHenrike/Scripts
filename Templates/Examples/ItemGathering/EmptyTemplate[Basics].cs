421ae3fb92d0c01851a2994dd505435c26e5c1e5
Tato <47118168+wtffidy@users.noreply.github.com>
2024-05-01 08:04:14 -0400

    Update EmptyTemplate[Basics].cs

--- Templates/Examples/ItemGathering/EmptyTemplate[Basics].cs
+++ Templates/Examples/ItemGathering/EmptyTemplate[Basics].cs
@@ -40,61 +40,9 @@ public class DefaultTemplate
         while (!Bot.ShouldExit && Core.CheckInventory("item", 1))
         {
             Core.HuntMonster("map", "mob", "item", 1, isTemp: false, log: false);
-            Core.HuntMonsterMapID("map", 1, "item", 1, isTemp: false, log :false);
+            Core.HuntMonsterMapID("map", 1, "item", 1, isTemp: false, log: false);
             Core.KillMonster("map", "cell", "pad", "mob", "item", 1, isTemp: false, log: false);
         }
         Core.CancelRegisteredQuests();
     }
-
-    //vv keeping this hre for an example vv
-    // public void IsMonsterAliveConversion()
-    // {
-    //     //vv required for CellKill vv
-    //     Core.Join("map", "cell", "pad");
-    //     //^^ required for CellKill ^^
-
-    //     Quest? quest = Bot.Quests.EnsureLoad(0000);
-    //     ItemBase? Item = quest!.Requirements.FirstOrDefault(x => x.Name == "Desired item");
-
-    //     #region Map Wide (sort of a Suedo-Hunt)
-    //     while (!Bot.ShouldExit && !Core.CheckInventory(Item!.Name, Item.Quantity))
-    //     {
-    //         foreach (Monster mob in Bot.Monsters.MapMonsters)
-    //         {
-    //             while (!Bot.ShouldExit && Bot.Player.Cell != mob.Cell)
-    //             {
-    //                 Core.Jump(mob.Cell, "Left");
-    //                 Core.Sleep();
-    //             }
-
-    //             Bot.Kill.Monster(mob.MapID);
-
-    //             if (Core.CheckInventory(Item!.Name, Item.Quantity))
-    //                 break;
-    //         }
-    //     }
-    //     #endregion Map Wide (sort of a Suedo-Hunt)
-
-
-    //     #region CellKill (strict)
-    //     while (!Bot.ShouldExit && !Core.CheckInventory(Item!.Name, Item.Quantity))
-    //     {
-    //         foreach (Monster mob in Bot.Monsters.CurrentAvailableMonsters)
-    //         {
-    //             while (!Bot.ShouldExit && Bot.Player.Cell != mob.Cell)
-    //             {
-    //                 Core.Jump("cell", "Left");
-    //                 Core.Sleep();
-    //             }
-
-    //             Bot.Kill.Monster(mob.MapID);
-
-    //             if (Core.CheckInventory(Item!.Name, Item.Quantity))
-    //                 break;
-    //         }
-    //     }
-    //     #endregion CellKill (strict)
-    // }
-    // //^^ keeping this hre for an example ^^
-
 }
