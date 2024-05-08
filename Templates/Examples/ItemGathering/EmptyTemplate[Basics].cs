0f0cdb45123669bcbe96d1a90ee387b76903cae4
Tato <47118168+wtffidy@users.noreply.github.com>
2024-05-08 13:12:57 -0400

    Update EmptyTemplate[Basics].cs

--- Templates/Examples/ItemGathering/EmptyTemplate[Basics].cs
+++ Templates/Examples/ItemGathering/EmptyTemplate[Basics].cs
@@ -7,6 +7,7 @@ tags: null
 //cs_include Scripts/CoreStory.cs
 //cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreDailies.cs
 using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 using Skua.Core.Models.Monsters;
@@ -20,9 +21,11 @@ public class DefaultTemplate
     private CoreAdvanced Adv = new();
     private CoreFarms Farm = new();
     private CoreStory Story = new();
+    private CoreDailies Daily = new();
 
     public void ScriptMain(IScriptInterface Bot)
     {
+        Core.BankingBlackList.AddRange(new[] { "item1", "Item2", "Etc" });
         Core.SetOptions();
 
         Example();
@@ -35,7 +38,7 @@ public class DefaultTemplate
         if (Core.CheckInventory("item", 1))
             return;
 
-
+        #region General Item Farming with quest included
         Core.RegisterQuests(000);
         while (!Bot.ShouldExit && Core.CheckInventory("item", 1))
         {
@@ -44,5 +47,15 @@ public class DefaultTemplate
             Core.KillMonster("map", "cell", "pad", "mob", "item", 1, isTemp: false, log: false);
         }
         Core.CancelRegisteredQuests();
+        #endregion
+
+        #region Story Mode:
+        Story.KillQuest(000, "mapname", "MonsterName");
+        Story.KillQuest(000, "mapname", new[] { "Monstername", "Monstername" });
+        Story.MapItemQuest(000, "mapname", 1, 1);
+        Story.MapItemQuest(000, "mapname", new[] { 000, 000, 000, });
+        Story.ChainQuest(000);
+        #endregion
+
     }
 }
