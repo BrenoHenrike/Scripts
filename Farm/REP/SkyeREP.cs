b7d0198c4181194e7dd055c1f2c9b338e041206f
Pedro Hao Tavares <pphenrique9@hotmail.com>
2024-05-10 22:05:02 -0300

    Create SkyeREP.cs

---
+++ Farm/REP/SkyeREP.cs
@@ -0,0 +1,26 @@
+/*
+name: Skye REP
+description: This script will farm Skye REP to rank 10.
+tags: skye, rep, reputation, farm
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+using Skua.Core.Interfaces;
+public class SkyeREP
+{
+    public CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new CoreFarms();
+    public CoreAdvanced Adv = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        //Adv.BestGear(GenericGearBoost.dmgAll);
+        //Adv.BestGear(GenericGearBoost.rep);
+        Farm.SkyeREP();
+
+        Core.SetOptions(false);
+    }
+}
