5b9dd36656fecc4239bb63b174081b08980f2b85
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-05-19 17:40:45 +0200

    chore: Remove unused quest reward scripts for Karkinos Mnimi, Aries Chrysomallus, and Pisces Venaris Mater

---
+++ Story/AgeOfRuin/10Balemorale.cs
@@ -0,0 +1,28 @@
+/*
+name: Balemorale Castle
+description: This script completes the storyline in /balemorale.
+tags: age,of,ruin,saga,story,quest,balemorale,castle,bale morale
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Story\ShadowsOfWar\CoreSoW.cs
+//cs_include Scripts/Story\AgeOfRuin\CoreAOR.cs
+using Skua.Core.Interfaces;
+
+public class BaleMorale
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreAOR AOR = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        AOR.Balemorale();
+
+        Core.SetOptions(false);
+    }
+}
