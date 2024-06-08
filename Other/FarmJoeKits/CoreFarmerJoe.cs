eda299ee56fdf92d9a2f694d45b8073ec0adca3b
Tato <47118168+wtffidy@users.noreply.github.com>
2024-06-08 09:51:40 -0400

    FarmerJoe Enhancements added

--- Other/FarmJoeKits/CoreFarmerJoe.cs
+++ Other/FarmJoeKits/CoreFarmerJoe.cs
@@ -3,90 +3,144 @@ name: null
 description: null
 tags: null
 */
+//cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreDailies.cs
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreStory.cs
-//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Dailies/0AllDailies.cs
+//cs_include Scripts/Dailies/LordOfOrder.cs
+//cs_include Scripts/Enhancement/InventoryEnhancer.cs
+//cs_include Scripts/Good/ArchPaladin.cs
+//cs_include Scripts/Good/BLoD/CoreBLOD.cs
+//cs_include Scripts/Good/GearOfAwe/CapeOfAwe.cs
+//cs_include Scripts/Good/GearOfAwe/CoreAwe.cs
+//cs_include Scripts/Good/Paladin.cs
+//cs_include Scripts/Hollowborn/MergeShops/ShadowrealmMerge.cs
+//cs_include Scripts/Dailies/MineCrafting.cs
+//cs_include Scripts/Enhancement/UnlockForgeEnhancements.cs
+//cs_include Scripts/Nation/AssistingCragAndBamboozle[Mem].cs
 //cs_include Scripts/Nation/CoreNation.cs
 //cs_include Scripts/Nation/VHL/CoreVHL.cs
-//cs_include Scripts/Good/GearOfAwe/CoreAwe.cs
-//cs_include Scripts/Good/BLoD/CoreBLOD.cs
-//cs_include Scripts/Evil/SDKA/CoreSDKA.cs
-//cs_include Scripts/Legion/CoreLegion.cs
-//cs_include Scripts/Legion/YamiNoRonin/CoreYnR.cs
-//cs_include Scripts/Darkon/CoreDarkon.cs
-//cs_include Scripts/Evil/NSoD/CoreNSOD.cs
-
-//cs_include Scripts/Dailies/0AllDailies.cs
-//cs_include Scripts/Good/GearOfAwe/CapeOfAwe.cs
-//cs_include Scripts/Good/Paladin.cs
-//cs_include Scripts/Nation/AssistingCragAndBamboozle[Mem].cs
-//cs_include Scripts/Other/FreeBoosts/FreeBoostsQuest(10mns)[Rng].cs
-//cs_include Scripts/Enhancement/InventoryEnhancer.cs
-//cs_include Scripts/Nation/Various/ArchfiendDeathLord.cs
-//cs_include Scripts/Story/Nation/Originul.cs
-
-//cs_include Scripts/Other/Classes/REP-based/MasterRanger.cs
+//cs_include Scripts/Other/Classes/DragonShinobi.cs
 //cs_include Scripts/Other/Classes/REP-based/EternalInversionist.cs
 //cs_include Scripts/Other/Classes/REP-based/GlacialBerserker.cs
 //cs_include Scripts/Other/Classes/REP-based/Shaman.cs
 //cs_include Scripts/Other/Classes/REP-based/StoneCrusher.cs
-//cs_include Scripts/Other/Classes/REP-based/DarkbloodStormKing.cs
 //cs_include Scripts/Other/Classes/ScarletSorceress.cs
 //cs_include Scripts/Other/Classes/BloodSorceress.cs
-//cs_include Scripts/Other/Classes/DragonShinobi.cs
-//cs_include Scripts/Other/Classes/DragonOfTime.cs
-//cs_include Scripts/Good/ArchPaladin.cs
-//cs_include Scripts/Dailies/LordOfOrder.cs
-//cs_include Scripts/Legion/SwordMaster.cs
-//cs_include Scripts/Other/Classes/Daily-Classes/BlazeBinder.cs
-//cs_include Scripts/Nation/Various/Archfiend.cs
-//cs_include Scripts/Other/Classes/Necromancer.cs
-//cs_include Scripts/Dailies/Cryomancer.cs
-//cs_include Scripts/Dailies/MineCrafting.cs
-
-//cs_include Scripts/Other/Weapons/BurningBlade.cs
+//cs_include Scripts/Other/FreeBoosts/FreeBoostsQuest(10mns)[Rng].cs
 //cs_include Scripts/Other/Weapons/BurningBladeOfAbezeth.cs
+//cs_include Scripts/Other/Weapons/BurningBlade.cs
 //cs_include Scripts/Other/Weapons/DualChainSawKatanas.cs
 //cs_include Scripts/Other/Weapons/EnchantedVictoryBladeWeapons.cs
-//cs_include Scripts/Other/Weapons/GoldenBladeOfFate.cs
-//cs_include Scripts/Other/Weapons/PinkBladeofDestruction.cs
-
-
+//cs_include Scripts/Story/BattleUnder.cs
 //cs_include Scripts/Story/DragonFableOrigins.cs
 //cs_include Scripts/Story/Glacera.cs
 //cs_include Scripts/Story/LordsofChaos/Core13LoC.cs
 //cs_include Scripts/Story/Nation/CitadelRuins.cs
 //cs_include Scripts/Story/QueenofMonsters/Extra/BrightOak.cs
 //cs_include Scripts/Story/ThroneofDarkness/CoreToD.cs
-//cs_include Scripts/Story/BattleUnder.cs
 //cs_include Scripts/Story/Tutorial.cs
 //cs_include Scripts/Story/XansLair.cs
 //cs_include Scripts/Story/Yokai.cs
+//cs_include Scripts/Other/FarmJoeKits/CoreFarmerJoe.cs
+//cs_include Scripts/Tools/BankAllItems.cs
 //cs_include Scripts/Story/Friendship.cs
+//cs_include Scripts/Other/Classes/REP-based/MasterRanger.cs
+//cs_include Scripts/Evil/SDKA/CoreSDKA.cs
+//cs_include Scripts/Legion/SwordMaster.cs
+//cs_include Scripts/Legion/YamiNoRonin/CoreYnR.cs
+//cs_include Scripts/Legion/CoreLegion.cs
+//cs_include Scripts/Story/ShadowsOfWar/CoreSoW.cs
+//cs_include Scripts/Other/MergeShops/SynderesMerge.cs
+//cs_include Scripts/Other/Classes/REP-based/DarkbloodStormKing.cs
+//cs_include Scripts/Nation/Various/ArchfiendDeathLord.cs
+//cs_include Scripts/Story/Nation/Originul.cs
 //cs_include Scripts/Story/Nation/Fiendshard.cs
+//cs_include Scripts/Nation/AFDL/WillpowerExtraction.cs
+//cs_include Scripts/Darkon/CoreDarkon.cs
+//cs_include Scripts/Other/Weapons/GoldenBladeOfFate.cs
+//cs_include Scripts/Other/Weapons/PinkBladeofDestruction.cs
 //cs_include Scripts/Story/Doomwood/CoreDoomwood.cs
 //cs_include Scripts/Story/QueenofMonsters/CoreQoM.cs
 //cs_include Scripts/Story/7DeadlyDragons/Core7DD.cs
 //cs_include Scripts/Other/MysteriousEgg.cs
 //cs_include Scripts/Story/Summer2015AdventureMap/CoreSummer.cs
 //cs_include Scripts/Story/Borgars.cs
-//cs_include Scripts/Story/ShadowsOfWar/CoreSoW.cs
 //cs_include Scripts/Story/ElegyofMadness(Darkon)/CoreAstravia.cs
-//cs_include Scripts/Story/QueenofMonsters/Extra/CelestialArena.cs
-
-//cs_include Scripts/Hollowborn/MergeShops/ShadowrealmMerge.cs
-//cs_include Scripts/Other/MergeShops/SynderesMerge.cs
-//cs_include Scripts/Other/MergeShops/CelestialChampMerge.cs
-//cs_include Scripts/Nation/AFDL/WillpowerExtraction.cs
-
-
+//cs_include Scripts/Other/Classes/DragonOfTime.cs
 //cs_include Scripts/Story/AgeofRuin/CoreAOR.cs
-//cs_include Scripts/Story/ShadowsOfWar/CoreSoW.cs
 //cs_include Scripts/Other/MergeShops/YulgarsUndineMerge.cs
 //cs_include Scripts/Hollowborn/MergeShops/DawnFortressMerge.cs
 //cs_include Scripts/Story/Hollowborn/CoreHollowbornStory.cs
+//cs_include Scripts/Other/MergeShops/CelestialChampMerge.cs
+//cs_include Scripts/Story/QueenofMonsters/Extra/CelestialArena.cs
+//cs_include Scripts/Other/Classes/Daily-Classes/BlazeBinder.cs
+//cs_include Scripts/Nation/Various/Archfiend.cs
+//cs_include Scripts/Evil/NSoD/CoreNSOD.cs
+//cs_include Scripts/Other/Classes/Necromancer.cs
+//cs_include Scripts/Dailies/Cryomancer.cs
+//cs_include Scripts/ShadowsOfWar/CoreSoWMats.cs
+//cs_include Scripts/Story/SepulchureSaga/CoreSepulchure.cs
+//cs_include Scripts/Other/Armor/FireChampionsArmor.cs
+//cs_include Scripts/Chaos/EternalDrakathSet.cs
+//cs_include Scripts/Evil/SepulchuresOriginalHelm.cs
+//cs_include Scripts/Darkon/Various/PrinceDarkonsPoleaxePreReqs.cs
+//cs_include Scripts/Legion/HeadOfTheLegionBeast.cs
+//cs_include Scripts/Good/GearOfAwe/Awescended.cs
+//cs_include Scripts/Nation/AFDL/NulgathDemandsWork.cs
+//cs_include Scripts/Story/ThirdSpell.cs
+//cs_include Scripts/Story/Legion/SevenCircles(War).cs
+//cs_include Scripts/Nation/Various/PrimeFiendShard.cs
+//cs_include Scripts/Other/Armor/MalgorsArmorSet.cs
+//cs_include Scripts/Good/GearOfAwe/ArmorOfAwe.cs
+//cs_include Scripts/Story/StarSinc.cs
+//cs_include Scripts/Nation/Various/VoidPaladin.cs
+//cs_include Scripts/Good/GearOfAwe/HelmOfAwe.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleDelveMerge.cs
+//cs_include Scripts/Story/Nation/VoidRefuge.cs
+//cs_include Scripts/Nation/MergeShops/VoidRefugeMerge.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleDelve.cs
+//cs_include Scripts/Seasonal/StaffBirthdays/Nulgath/TempleSiege.cs
+//cs_include Scripts/Nation/Various/TheLeeryContract[Member].cs
+//cs_include Scripts/Nation/Various/JuggernautItems.cs
+//cs_include Scripts/Nation/MergeShops/NulgathDiamondMerge.cs
+//cs_include Scripts/Nation/Various/TarosManslayer.cs
+//cs_include Scripts/Nation/Various/PurifiedClaymoreOfDestiny.cs
+//cs_include Scripts/Nation/Various/VoidSpartan.cs
+//cs_include Scripts/Nation/Various/SwirlingTheAbyss.cs
+//cs_include Scripts/Hollowborn/TradingandStuff(single).cs
+//cs_include Scripts/Nation/EmpoweringItems.cs
+//cs_include Scripts/Other/Weapons/VoidAvengerScythe.cs
+//cs_include Scripts/Nation/Various/GoldenHanzoVoid.cs
+//cs_include Scripts/Nation/MergeShops/DilligasMerge.cs
+//cs_include Scripts/Nation/MergeShops/DirtlickersMerge.cs
+//cs_include Scripts/Other/Weapons/WrathofNulgath.cs
+//cs_include Scripts/Nation/MergeShops/VoidChasmMerge.cs
+//cs_include Scripts/Story/Nation/VoidChasm.cs
+//cs_include Scripts/Nation/MergeShops/NationMerge.cs
+//cs_include Scripts/Nation/NationLoyaltyRewarded.cs
+//cs_include Scripts/Story/Legion/DarkWarLegionandNation.cs
+//cs_include Scripts/Hollowborn/CoreHollowborn.cs
+//cs_include Scripts/Other/Classes/DragonslayerGeneral.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/DeadLinesMerge.cs
+//cs_include Scripts/Other/WarFuryEmblem.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/ShadowflameFinaleMerge.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/TimekeepMerge.cs
+//cs_include Scripts/Chaos/DrakathsArmor.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/StreamwarMerge.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/WorldsCoreMerge.cs
+//cs_include Scripts/ShadowsOfWar/MergeShops/ManaCradleMerge.cs
+//cs_include Scripts/Other/ShadowDragonDefender.cs
+//cs_include Scripts/Other/Weapons/ShadowReaperOfDoom.cs
+//cs_include Scripts/Story/J6Saga.cs
+//cs_include Scripts/Evil/ADK.cs
+//cs_include Scripts/Good/SilverExaltedPaladin.cs
+//cs_include Scripts/Story/7DeadlyDragons/Extra/HatchTheEgg.cs
+//cs_include Scripts/Story/Nation/Bamboozle.cs
+//cs_include Scripts/Story/DjinnGate.cs
+//cs_include Scripts/Other/Weapons/FortitudeAndHubris.cs
 
 
 
@@ -105,6 +159,7 @@ public class CoreFarmerJoe
     public InventoryEnhancer InvEn = new();
     public SynderesMerge SM = new();
     public ArchfiendDeathLord AFDeath = new();
+    private UnlockForgeEnhancements UnlockForgeEnhancements = new();
 
     //Cores
     public static CoreBots Core => CoreBots.Instance;
@@ -505,7 +560,31 @@ public class CoreFarmerJoe
 
         SetClass(true, false, true);
         DoT.GetDoT();
-        //Add more eventualy >.> please?
+
+        SetClass(true, false, true);
+
+
+        //Forge Enhancements
+        //Cape:
+        UnlockForgeEnhancements.ForgeCapeEnhancement();
+        UnlockForgeEnhancements.Absolution();
+        UnlockForgeEnhancements.Avarice();
+        UnlockForgeEnhancements.Lament();
+        UnlockForgeEnhancements.Vainglory();
+
+        //Weapon:
+        UnlockForgeEnhancements.ForgeWeaponEnhancement();
+        UnlockForgeEnhancements.Lacerate();
+        UnlockForgeEnhancements.Smite();
+        UnlockForgeEnhancements.Praxis();
+        UnlockForgeEnhancements.HerosValiance();
+
+        //Helm:
+        UnlockForgeEnhancements.ForgeHelmEnhancement();
+        UnlockForgeEnhancements.Vim();
+        UnlockForgeEnhancements.Examen();
+        UnlockForgeEnhancements.Anima();
+        UnlockForgeEnhancements.Pneuma();
 
         #endregion Ending & Extras
     }
@@ -537,6 +616,7 @@ public class CoreFarmerJoe
 
     #region other stuff
 
+    #region Extra:
     /// <summary>
     /// Manages the acquisition and equipping of pets based on a specified choice.
     /// </summary>
@@ -673,7 +753,9 @@ public class CoreFarmerJoe
             Farm.Experience(5);
         }
     }
+    #endregion Extra:
 
+    #region BTS
     /// <summary>
     /// Automatically sets the class for both solo and farm activities, if not already set, based on available classes in the player's inventory.
     /// </summary>
@@ -694,7 +776,7 @@ public class CoreFarmerJoe
         // if (!Core.CheckInventory("ArchPaladin"))
         //     soloClassesToCheck = new[] { "ArchPaladin", "Shaman", "Rogue (Rare)", "Rogue", "Healer (Rare)", "Healer" };
         // else
-        string[] soloClassesToCheck = new[] { "ArchPaladin", "Glacial Berserker", "DragonSoul Shinobi", "Shaman", "Rogue (Rare)", "Rogue", "Healer (Rare)", "Healer" };
+        string[] soloClassesToCheck = new[] { "Dragon of Time", "ArchPaladin", "Glacial Berserker", "DragonSoul Shinobi", "Shaman", "Rogue (Rare)", "Rogue", "Healer (Rare)", "Healer" };
 
         string[] farmClassesToCheck = new[] { "Archfiend", "Blaze Binder", "Scarlet Sorceress", "Master Ranger", "Shaman", "Mage (Rare)", "Mage" };
 
@@ -789,7 +871,6 @@ public class CoreFarmerJoe
         return "Generic"; // Return "Generic" as the default value when no valid class is found.
     }
 
-
     /// <summary>
     /// Enhances the first item from the given list of items in the player's inventory, if found.
     /// </summary>
@@ -826,10 +907,10 @@ public class CoreFarmerJoe
             }
         }
     }
+    #endregion BTS
 
 
-
-
+    #region Explanation: 
     /*________________________________________________________________________________________Explanation_____________________________________________________________________________________________
        🖕 The SetClass() method checks whether the swapToSoloClass and swapToFarmClass flags are both set to true. If so, it logs an error message and returns.
        🖕 The method initializes newSoloClass and newFarmClass variables with the current values of SoloClass and FarmClass, respectively.
@@ -858,6 +939,7 @@ public class CoreFarmerJoe
        ℭ𝔬𝔲𝔯𝔱𝔢𝔰𝔶 𝔬𝔣 ℭ𝔥𝔞𝔱𝔊ℙ𝔗 
        ________________________________________________________________________________________Explanation_____________________________________________________________________________________________*/
 
+    #endregion Explanation: 
 
 
 
