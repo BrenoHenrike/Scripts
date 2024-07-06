315b0c418f595e25680942b6e10bb33fee225fd2
Tato <47118168+wtffidy@users.noreply.github.com>
2024-07-06 12:26:51 -0400

    5 new quests in NovaShrine
    ultra will be a grim thing :thumbsup:

---
+++ Story/DragonsOfYokai/07NovaShrine.cs
@@ -0,0 +1,24 @@
+/*
+name: Nova Shrine
+description: This script will complete "Nova Shrine" storyline.
+tags: story, quest,saga,dragons,dragon,nova,shrine,yokairealm, complete, all
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/DragonsOfYokai/CoreDOY.cs
+using Skua.Core.Interfaces;
+
+public class NovaShrine
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreDOY DOY = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        DOY.NovaShrine();
+        Core.SetOptions(false);
+    }
+}
