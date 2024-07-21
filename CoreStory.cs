b869402fb34849534b533db88da876eefad04ac5
Tato <47118168+wtffidy@users.noreply.github.com>
2024-07-21 09:05:40 -0400

    Update CoreStory.cs

--- CoreStory.cs
+++ CoreStory.cs
@@ -260,7 +260,7 @@ public class CoreStory
             Core.EnsureComplete(questData.ID);
         }
 
-        Bot.Wait.ForQuestComplete(questData.ID);
+        Bot.Wait.ForTrue(() => QuestProgression(questData.ID), 20);
         Core.Logger($"Completed Quest: [{questData.ID}] - \"{questData.Name}\"", "QuestProgression");
         Core.Sleep();
     }
