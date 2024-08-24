b5cbc1f48f7c386c89c1aa01b691db4896fb029e
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-08-24 14:56:30 +0200

    feat: Add ColdThunder script to complete the storyline in /ColdThunder

---
+++ Story/AgeOfRuin/16ColdThunder.cs
@@ -0,0 +1,28 @@
+/*
+name: ColdThunder
+description: This script completes the storyline in /ColdThunder.
+tags: age,of,ruin,saga,story,quest,Cold Thunder,sovereign of storms,sos
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Story/ShadowsOfWar/CoreSoW.cs
+//cs_include Scripts/Story/AgeOfRuin/CoreAOR.cs
+using Skua.Core.Interfaces;
+
+public class ColdThunder
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private CoreAOR AOR = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        AOR.ColdThunder();
+
+        Core.SetOptions(false);
+    }
+}
