a1fa6152b0f0c6138b72049f0c3b0b0d1dce0760
Tato <47118168+wtffidy@users.noreply.github.com>
2024-09-11 08:49:28 -0400

    Create OneClientTemplate.cs

---
+++ Army/OneClient/OneClientTemplate.cs
@@ -0,0 +1,40 @@
+/*
+name: null
+description: null
+tags: null
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/Army/CoreArmyLite.cs
+using Skua.Core.Interfaces;
+
+public class OneClientTemplate
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    public CoreFarms Farm = new();
+    private CoreDailies Daily = new();
+    public CoreAdvanced Adv => new();
+    private CoreArmyLite Army = new();
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        Template();
+
+        Core.SetOptions(false);
+    }
+
+    public void Template()
+    {
+        while (!Bot.ShouldExit && Army.doForAll())
+        {
+            // Insert what you want all the accs to do here. this will just once per account (for every account) in the manager.
+            // No actualy army stuff here, just things that can be run solo, but are needed for all the accounts.
+        }
+    }
+}
