199e52527e1bbdef538149f2f6ede37d7c0f3d1b
Tato <47118168+wtffidy@users.noreply.github.com>
2024-09-22 22:32:16 -0400

    Update DrakathsArmor.cs

--- Chaos/DrakathsArmor.cs
+++ Chaos/DrakathsArmor.cs
@@ -20,6 +20,7 @@ public class DrakathArmorBot
     private CoreBots Core => CoreBots.Instance;
     private CoreFarms Farm = new();
     private CoreDailies Daily = new();
+    public CoreAdvanced Adv = new();
     private CoreBLOD BLOD = new();
     private Core13LoC LOC => new();
     private CoreNation Nation = new();
@@ -75,26 +76,25 @@ public class DrakathArmorBot
 
         Core.EnsureAccept(3882);
 
-        Farm.BladeofAweREP(6, farmBoA: true);
+        if (!Core.CheckInventory("Dage's Scroll Fragment", 13))
+        {
+            Daily.DagesScrollFragment();
+        }
         BLOD.BlindingLightOfDestiny();
-        Nation.FarmUni13(3);
-        Farm.Gold(3750000);
-        Core.BuyItem("hyperspace", 194, "Le Chocolat");
+        Adv.BuyItem("hyperspace", 194, "Le Chocolat");
         Core.EquipClass(ClassType.Farm);
         Core.KillMonster("swordhavenundead", "Left", "Right", "*", "Treasure Chest", 100, false);
         Core.EquipClass(ClassType.Solo);
         Core.KillMonster("ultradrakath", "r1", "Left", "Champion of Chaos", "Face of Chaos", isTemp: false, publicRoom: true);
+        Nation.FarmUni13(3);
+        Farm.BladeofAweREP(6, farmBoA: true);
+
         if (!Core.CheckInventory("Dage's Scroll Fragment", 13))
-        {
-            Daily.DagesScrollFragment();
-            if (!Core.CheckInventory("Dage's Scroll Fragment", 13))
-                Core.Logger($"You own \"Dage's Scroll Fragment\" ({Bot.Inventory.GetQuantity("Dage's Scroll Fragment")}/13) [Daily Quest]. Bot can not continue.");
-            else Core.EnsureComplete(3882);
-        }
+            Core.Logger($"You own \"Dage's Scroll Fragment\" ({Bot.Inventory.GetQuantity("Dage's Scroll Fragment")}/13) [Daily Quest]. Bot can not continue.");
         else Core.EnsureComplete(3882);
-        
+
         //wait and pickup drop (if not already picked up)
         Bot.Wait.ForDrop("Get Your Original Drakath's Armor");
-        Bot.Wait.ForPickup("Get Your Original Drakath's Armor"); 
+        Bot.Wait.ForPickup("Get Your Original Drakath's Armor");
     }
 }
