62a57cb55e67c08ff05e53ccff8428d2cfa211d3
Tato <47118168+wtffidy@users.noreply.github.com>
2024-09-29 08:03:14 -0400

    Update CoreArmyLite.cs

--- Army/CoreArmyLite.cs
+++ Army/CoreArmyLite.cs
@@ -1235,7 +1235,7 @@ public class CoreArmyLite
             #region Combat Area
             // Check if there are monsters in the same cell
             // Loop until the bot is instructed to exit
-            while (!Bot.ShouldExit && tryGoto(playerName))
+            while (!Bot.ShouldExit)
             {
                 // Check for priority attacks or available monsters
                 if (!string.IsNullOrEmpty(attackPriority))
