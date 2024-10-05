df5f963e32547c29096835d6dfe1d7a9af1495a6
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-10-05 13:49:34 +0200

    Add Necro Project story script

---
+++ Story/NecroProject.cs
@@ -0,0 +1,83 @@
+/*
+name: Necro Project
+description: This will finish the Necro Project story.
+tags: story, quest, necroproject, sally 
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using Skua.Core.Interfaces;
+
+public class NecroProject
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    public CoreStory Story = new();
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        StoryLine();
+
+        Core.SetOptions(false);
+    }
+
+    public void StoryLine()
+    {
+        if (Core.isCompletedBefore(9901))
+            return;
+
+        Story.PreLoad(this);
+
+        #region Useable Monsters
+        string[] UseableMonsters = new[]
+        {
+    "Noxborg", // UseableMonsters[0],
+	"Noxore", // UseableMonsters[1],
+	"Noxustrian", // UseableMonsters[2],
+	"Imperial Noxus", // UseableMonsters[3]
+};
+        #endregion Useable Monsters
+
+        // 9897 | In-Field Study
+        if (!Story.QuestProgression(9897))
+        {
+            Core.HuntMonsterQuest(9897, new (string? mapName, string? monsterName, ClassType classType)[] {
+        ("necrocavern", "Shadowstone Elemental", ClassType.Farm) });
+        }
+
+
+        // 9898 | Technilliterate
+        if (!Story.QuestProgression(9898))
+        {
+            Core.HuntMonsterQuest(9898, new (string? mapName, string? monsterName, ClassType classType)[] {
+        ("necroproject", UseableMonsters[0], ClassType.Solo) });
+        }
+
+
+        // 9899 | Odd Tastes
+        if (!Story.QuestProgression(9899))
+        {
+            Core.HuntMonsterQuest(9899, new (string? mapName, string? monsterName, ClassType classType)[] {
+        ("necroproject", UseableMonsters[1], ClassType.Solo) });
+        }
+
+
+        // 9900 | My Toxic Pony
+        if (!Story.QuestProgression(9900))
+        {
+            Core.HuntMonsterQuest(9900, new (string? mapName, string? monsterName, ClassType classType)[] {
+        ("necroproject", UseableMonsters[2], ClassType.Solo) });
+        }
+
+
+        // 9901 | Hand-Me-Down
+        if (!Story.QuestProgression(9901))
+        {
+            Core.HuntMonsterQuest(9901, new (string? mapName, string? monsterName, ClassType classType)[] {
+        ("necroproject", UseableMonsters[3], ClassType.Solo) });
+        }
+
+
+    }
+}
