4403bc7fb908713378e882c7d4a3f3df3ebfba00
Tato <47118168+wtffidy@users.noreply.github.com>
2024-10-07 11:41:32 -0400

    regenerated

--- Seasonal/Mogloween/MergeShops/CandiedCostumesMerge.cs
+++ Seasonal/Mogloween/MergeShops/CandiedCostumesMerge.cs
@@ -11,7 +11,7 @@ using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 using Skua.Core.Options;
 
-public class CandiedCostumesMerge
+public class CandiedCostumesMergea
 {
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreBots Core => CoreBots.Instance;
@@ -46,6 +46,7 @@ public class CandiedCostumesMerge
     {
         if (!Core.isSeasonalMapActive("mogloween"))
             return;
+            
         //Only edit the map and shopID here
         Adv.StartBuyAllMerge("candyshop", 1761, findIngredients, buyOnlyThis, buyMode: buyMode);
 
@@ -125,59 +126,78 @@ public class CandiedCostumesMerge
                     Core.HuntMonster("candyshop", "Sugarrush Ghoul", req.Name, isTemp: false);
                     break;
 
+                case "Gold Voucher 25k":
+                    Adv.BuyItem("sunlightzone", 2288, 57304, quant, 7782);
+                    Bot.Wait.ForPickup(req.Name);
+                    break;
+
+                case "Tactical Agent Alpha":
+                case "Tactical Agent Bravo":
+                case "Tactical Agent Bravo Beard":
+                case "Tactical Agent Bravo Locks":
+                case "Tactical Alpha Rifle":
+                case "Tactical Alpha Rifles":
+                case "Backup Ol Reliable Zombie Buster":
+                case "Gummy Brains":
+                    Core.HuntMonster("mogloweengrave", "Zombie Terror", req.Name, req.Quantity, req.Temp);
+                    break;
             }
         }
     }
 
-    public List<IOption> Select =
-        new()
-        {
-            new Option<bool>("42610","Fairy Tale Wanderer","Mode: [select] only\nShould the bot buy \"Fairy Tale Wanderer\" ?",false),
-            new Option<bool>("47883","Flesh Ripper","Mode: [select] only\nShould the bot buy \"Flesh Ripper\" ?",false),
-            new Option<bool>("47884","Horned Flesh Ripper Mask","Mode: [select] only\nShould the bot buy \"Horned Flesh Ripper Mask\" ?",false),
-            new Option<bool>("47885","Flesh Ripper Mask","Mode: [select] only\nShould the bot buy \"Flesh Ripper Mask\" ?",false),
-            new Option<bool>("47886","Flesh Ripper Cape","Mode: [select] only\nShould the bot buy \"Flesh Ripper Cape\" ?",false),
-            new Option<bool>("47887","Flesh Ripper Backblades","Mode: [select] only\nShould the bot buy \"Flesh Ripper Backblades\" ?",false),
-            new Option<bool>("47888","Flesh Ripper Blade","Mode: [select] only\nShould the bot buy \"Flesh Ripper Blade\" ?",false),
-            new Option<bool>("50424","Dual Flesh Ripper Blades","Mode: [select] only\nShould the bot buy \"Dual Flesh Ripper Blades\" ?",false),
-            new Option<bool>("48497","Force Guardian","Mode: [select] only\nShould the bot buy \"Force Guardian\" ?",false),
-            new Option<bool>("48498","Force Guardian Helm","Mode: [select] only\nShould the bot buy \"Force Guardian Helm\" ?",false),
-            new Option<bool>("48499","Force Guardian Hair + Beard","Mode: [select] only\nShould the bot buy \"Force Guardian Hair + Beard\" ?",false),
-            new Option<bool>("48500","Force Guardian Hair","Mode: [select] only\nShould the bot buy \"Force Guardian Hair\" ?",false),
-            new Option<bool>("48501","Force Guardian Locks","Mode: [select] only\nShould the bot buy \"Force Guardian Locks\" ?",false),
-            new Option<bool>("48502","Force Guardian Back Staves","Mode: [select] only\nShould the bot buy \"Force Guardian Back Staves\" ?",false),
-            new Option<bool>("48503","Force Guardian Staff + Cape","Mode: [select] only\nShould the bot buy \"Force Guardian Staff + Cape\" ?",false),
-            new Option<bool>("48504","Force Guardian Daggers","Mode: [select] only\nShould the bot buy \"Force Guardian Daggers\" ?",false),
-            new Option<bool>("48505","Force Guardian Light Staff","Mode: [select] only\nShould the bot buy \"Force Guardian Light Staff\" ?",false),
-            new Option<bool>("48507","Force Guardian Light Blade","Mode: [select] only\nShould the bot buy \"Force Guardian Light Blade\" ?",false),
-            new Option<bool>("48655","Super Scion Ninja","Mode: [select] only\nShould the bot buy \"Super Scion Ninja\" ?",false),
-            new Option<bool>("48656","Super Scion Hair","Mode: [select] only\nShould the bot buy \"Super Scion Hair\" ?",false),
-            new Option<bool>("48657","Super Scion SideMask","Mode: [select] only\nShould the bot buy \"Super Scion SideMask\" ?",false),
-            new Option<bool>("48658","Super Scion SideMask + Scarf","Mode: [select] only\nShould the bot buy \"Super Scion SideMask + Scarf\" ?",false),
-            new Option<bool>("48659","Super Scion Hair + Scarf","Mode: [select] only\nShould the bot buy \"Super Scion Hair + Scarf\" ?",false),
-            new Option<bool>("48660","Super Scion Mask","Mode: [select] only\nShould the bot buy \"Super Scion Mask\" ?",false),
-            new Option<bool>("48661","Super Scion Locks","Mode: [select] only\nShould the bot buy \"Super Scion Locks\" ?",false),
-            new Option<bool>("48662","Super Scion Mask + Locks","Mode: [select] only\nShould the bot buy \"Super Scion Mask + Locks\" ?",false),
-            new Option<bool>("48663","Super Scion Locks + Pigtails","Mode: [select] only\nShould the bot buy \"Super Scion Locks + Pigtails\" ?",false),
-            new Option<bool>("48664","Super Scion Pigtails + SideMask","Mode: [select] only\nShould the bot buy \"Super Scion Pigtails + SideMask\" ?",false),
-            new Option<bool>("48665","Super Scion Masked Pigtails + SideMask","Mode: [select] only\nShould the bot buy \"Super Scion Masked Pigtails + SideMask\" ?",false),
-            new Option<bool>("48666","Super Scion Cape","Mode: [select] only\nShould the bot buy \"Super Scion Cape\" ?",false),
-            new Option<bool>("48667","Super Scion BackFan","Mode: [select] only\nShould the bot buy \"Super Scion BackFan\" ?",false),
-            new Option<bool>("48668","Super Scion Fan + Cape","Mode: [select] only\nShould the bot buy \"Super Scion Fan + Cape\" ?",false),
-            new Option<bool>("48669","Super Scion Back Swords","Mode: [select] only\nShould the bot buy \"Super Scion Back Swords\" ?",false),
-            new Option<bool>("48670","Super Scion Back Scroll","Mode: [select] only\nShould the bot buy \"Super Scion Back Scroll\" ?",false),
-            new Option<bool>("48671","Super Scion BackBlades + Cape","Mode: [select] only\nShould the bot buy \"Super Scion BackBlades + Cape\" ?",false),
-            new Option<bool>("48672","Super Scion Blade","Mode: [select] only\nShould the bot buy \"Super Scion Blade\" ?",false),
-            new Option<bool>("48673","Dual Super Scion Blades","Mode: [select] only\nShould the bot buy \"Dual Super Scion Blades\" ?",false),
-            new Option<bool>("48674","Super Scion Lightning Ball","Mode: [select] only\nShould the bot buy \"Super Scion Lightning Ball\" ?",false),
-            new Option<bool>("48675","Dual Super Scion Balls","Mode: [select] only\nShould the bot buy \"Dual Super Scion Balls\" ?",false),
-            new Option<bool>("36951","Soft Shelled Cape","Mode: [select] only\nShould the bot buy \"Soft Shelled Cape\" ?",false),
-            new Option<bool>("36956","Tacobender","Mode: [select] only\nShould the bot buy \"Tacobender\" ?",false),
-            new Option<bool>("36950","Tacomancer","Mode: [select] only\nShould the bot buy \"Tacomancer\" ?",false),
-            new Option<bool>("36952","Taco on a Stick","Mode: [select] only\nShould the bot buy \"Taco on a Stick\" ?",false),
-            new Option<bool>("36949","Destructo-Tacos","Mode: [select] only\nShould the bot buy \"Destructo-Tacos\" ?",false),
-            new Option<bool>("36948","Taco Admiral Hat","Mode: [select] only\nShould the bot buy \"Taco Admiral Hat\" ?",false),
-            new Option<bool>("36947","Taco Admiral Locks","Mode: [select] only\nShould the bot buy \"Taco Admiral Locks\" ?",false),
-            new Option<bool>("64298","Lady Vayle Costume","Mode: [select] only\nShould the bot buy \"Lady Vayle Costume\" ?",false),
-        };
+    public List<IOption> Select = new()
+    {
+        new Option<bool>("42610", "Fairy Tale Wanderer", "Mode: [select] only\nShould the bot buy \"Fairy Tale Wanderer\" ?", false),
+        new Option<bool>("47883", "Flesh Ripper", "Mode: [select] only\nShould the bot buy \"Flesh Ripper\" ?", false),
+        new Option<bool>("47884", "Horned Flesh Ripper Mask", "Mode: [select] only\nShould the bot buy \"Horned Flesh Ripper Mask\" ?", false),
+        new Option<bool>("47885", "Flesh Ripper Mask", "Mode: [select] only\nShould the bot buy \"Flesh Ripper Mask\" ?", false),
+        new Option<bool>("47886", "Flesh Ripper Cape", "Mode: [select] only\nShould the bot buy \"Flesh Ripper Cape\" ?", false),
+        new Option<bool>("47887", "Flesh Ripper Backblades", "Mode: [select] only\nShould the bot buy \"Flesh Ripper Backblades\" ?", false),
+        new Option<bool>("47888", "Flesh Ripper Blade", "Mode: [select] only\nShould the bot buy \"Flesh Ripper Blade\" ?", false),
+        new Option<bool>("50424", "Dual Flesh Ripper Blades", "Mode: [select] only\nShould the bot buy \"Dual Flesh Ripper Blades\" ?", false),
+        new Option<bool>("48497", "Force Guardian", "Mode: [select] only\nShould the bot buy \"Force Guardian\" ?", false),
+        new Option<bool>("48498", "Force Guardian Helm", "Mode: [select] only\nShould the bot buy \"Force Guardian Helm\" ?", false),
+        new Option<bool>("48499", "Force Guardian Hair + Beard", "Mode: [select] only\nShould the bot buy \"Force Guardian Hair + Beard\" ?", false),
+        new Option<bool>("48500", "Force Guardian Hair", "Mode: [select] only\nShould the bot buy \"Force Guardian Hair\" ?", false),
+        new Option<bool>("48501", "Force Guardian Locks", "Mode: [select] only\nShould the bot buy \"Force Guardian Locks\" ?", false),
+        new Option<bool>("48502", "Force Guardian Back Staves", "Mode: [select] only\nShould the bot buy \"Force Guardian Back Staves\" ?", false),
+        new Option<bool>("48503", "Force Guardian Staff + Cape", "Mode: [select] only\nShould the bot buy \"Force Guardian Staff + Cape\" ?", false),
+        new Option<bool>("48504", "Force Guardian Daggers", "Mode: [select] only\nShould the bot buy \"Force Guardian Daggers\" ?", false),
+        new Option<bool>("48505", "Force Guardian Light Staff", "Mode: [select] only\nShould the bot buy \"Force Guardian Light Staff\" ?", false),
+        new Option<bool>("48507", "Force Guardian Light Blade", "Mode: [select] only\nShould the bot buy \"Force Guardian Light Blade\" ?", false),
+        new Option<bool>("48655", "Super Scion Ninja", "Mode: [select] only\nShould the bot buy \"Super Scion Ninja\" ?", false),
+        new Option<bool>("48656", "Super Scion Hair", "Mode: [select] only\nShould the bot buy \"Super Scion Hair\" ?", false),
+        new Option<bool>("48657", "Super Scion SideMask", "Mode: [select] only\nShould the bot buy \"Super Scion SideMask\" ?", false),
+        new Option<bool>("48658", "Super Scion SideMask + Scarf", "Mode: [select] only\nShould the bot buy \"Super Scion SideMask + Scarf\" ?", false),
+        new Option<bool>("48659", "Super Scion Hair + Scarf", "Mode: [select] only\nShould the bot buy \"Super Scion Hair + Scarf\" ?", false),
+        new Option<bool>("48660", "Super Scion Mask", "Mode: [select] only\nShould the bot buy \"Super Scion Mask\" ?", false),
+        new Option<bool>("48661", "Super Scion Locks", "Mode: [select] only\nShould the bot buy \"Super Scion Locks\" ?", false),
+        new Option<bool>("48662", "Super Scion Mask + Locks", "Mode: [select] only\nShould the bot buy \"Super Scion Mask + Locks\" ?", false),
+        new Option<bool>("48663", "Super Scion Locks + Pigtails", "Mode: [select] only\nShould the bot buy \"Super Scion Locks + Pigtails\" ?", false),
+        new Option<bool>("48664", "Super Scion Pigtails + SideMask", "Mode: [select] only\nShould the bot buy \"Super Scion Pigtails + SideMask\" ?", false),
+        new Option<bool>("48665", "Super Scion Masked Pigtails + SideMask", "Mode: [select] only\nShould the bot buy \"Super Scion Masked Pigtails + SideMask\" ?", false),
+        new Option<bool>("48666", "Super Scion Cape", "Mode: [select] only\nShould the bot buy \"Super Scion Cape\" ?", false),
+        new Option<bool>("48667", "Super Scion BackFan", "Mode: [select] only\nShould the bot buy \"Super Scion BackFan\" ?", false),
+        new Option<bool>("48668", "Super Scion Fan + Cape", "Mode: [select] only\nShould the bot buy \"Super Scion Fan + Cape\" ?", false),
+        new Option<bool>("48669", "Super Scion Back Swords", "Mode: [select] only\nShould the bot buy \"Super Scion Back Swords\" ?", false),
+        new Option<bool>("48670", "Super Scion Back Scroll", "Mode: [select] only\nShould the bot buy \"Super Scion Back Scroll\" ?", false),
+        new Option<bool>("48671", "Super Scion BackBlades + Cape", "Mode: [select] only\nShould the bot buy \"Super Scion BackBlades + Cape\" ?", false),
+        new Option<bool>("48672", "Super Scion Blade", "Mode: [select] only\nShould the bot buy \"Super Scion Blade\" ?", false),
+        new Option<bool>("48673", "Dual Super Scion Blades", "Mode: [select] only\nShould the bot buy \"Dual Super Scion Blades\" ?", false),
+        new Option<bool>("48674", "Super Scion Lightning Ball", "Mode: [select] only\nShould the bot buy \"Super Scion Lightning Ball\" ?", false),
+        new Option<bool>("48675", "Dual Super Scion Balls", "Mode: [select] only\nShould the bot buy \"Dual Super Scion Balls\" ?", false),
+        new Option<bool>("64298", "Lady Vayle Costume", "Mode: [select] only\nShould the bot buy \"Lady Vayle Costume\" ?", false),
+        new Option<bool>("84120", "Enchanted Tactical Agent Alpha", "Mode: [select] only\nShould the bot buy \"Enchanted Tactical Agent Alpha\" ?", false),
+        new Option<bool>("84121", "Enchanted Tactical Agent Bravo", "Mode: [select] only\nShould the bot buy \"Enchanted Tactical Agent Bravo\" ?", false),
+        new Option<bool>("84122", "Tactical Agent Bravo Gasmask", "Mode: [select] only\nShould the bot buy \"Tactical Agent Bravo Gasmask\" ?", false),
+        new Option<bool>("84123", "Tactical Agent Alpha Gasmask", "Mode: [select] only\nShould the bot buy \"Tactical Agent Alpha Gasmask\" ?", false),
+        new Option<bool>("84129", "Tactical Agent Bravo Beard Morph", "Mode: [select] only\nShould the bot buy \"Tactical Agent Bravo Beard Morph\" ?", false),
+        new Option<bool>("84130", "Tactical Agent Bravo Visage", "Mode: [select] only\nShould the bot buy \"Tactical Agent Bravo Visage\" ?", false),
+        new Option<bool>("84146", "Tactical Alpha Gun", "Mode: [select] only\nShould the bot buy \"Tactical Alpha Gun\" ?", false),
+        new Option<bool>("84147", "Tactical Alpha Guns", "Mode: [select] only\nShould the bot buy \"Tactical Alpha Guns\" ?", false),
+        new Option<bool>("84140", "Ol' Reliable Zombie Buster", "Mode: [select] only\nShould the bot buy \"Ol' Reliable Zombie Buster\" ?", false),
+        new Option<bool>("84141", "Ol' Reliable Zombie Busters", "Mode: [select] only\nShould the bot buy \"Ol' Reliable Zombie Busters\" ?", false),
+        new Option<bool>("84142", "Tactical Alpha Blaster", "Mode: [select] only\nShould the bot buy \"Tactical Alpha Blaster\" ?", false),
+        new Option<bool>("84143", "Tactical Alpha Blasters", "Mode: [select] only\nShould the bot buy \"Tactical Alpha Blasters\" ?", false),
+    };
 }
