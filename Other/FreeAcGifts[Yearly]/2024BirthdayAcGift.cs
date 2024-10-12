13fe5e896996a46942d86883dcfcc4b745a38068
Tato <47118168+wtffidy@users.noreply.github.com>
2024-10-12 11:32:11 -0400

    update free acs  quest

---
+++ Other/FreeAcGifts[Yearly]/2024BirthdayAcGift.cs
@@ -0,0 +1,32 @@
+/*
+name: Free Birthday AC Gift 2023
+description: This script will kill Agitated Orb for free 500 ACs.
+tags: ac, free, 500, 2023, birthday
+*/
+//cs_include Scripts/CoreBots.cs
+using Skua.Core.Interfaces;
+
+public class BirthdayAC2024
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        GetFreeAcs();
+        Core.SetOptions(false);
+    }
+
+    public void GetFreeAcs()
+    {
+        if (Bot.Quests.IsAvailable(9937))
+        {
+            Core.EnsureAccept(9937);
+            Core.HuntMonster("yulgar", "Agitated Orb", "Free ACs... and Yogurt");
+            Core.EnsureComplete(9937);
+        }
+
+    }
+}
