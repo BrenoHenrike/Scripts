a988a7d372f62951e9fc415542af2b11e5b733e7
Tato <47118168+wtffidy@users.noreply.github.com>
2024-10-12 11:17:44 -0400

    Update MartialArtist.cs

--- Other/Classes/MartialArtist.cs
+++ Other/Classes/MartialArtist.cs
@@ -37,6 +37,7 @@ public class MartialArtist
         }
 
         Core.AddDrop("Martial Artist");
+        Core.AddDrop(88662, 88661, 88660);
 
         if (!Core.CheckInventory("Martial Artist"))
         {
@@ -47,18 +48,37 @@ public class MartialArtist
             Story.ChainQuest(9922);
 
             // 9923 | 500 Punches and 500 Kicks
+            // 9933 | Deathly Slow Start
             // 9924 | Discount Diploma
             // 9925 | One Million Miles Searching
             // 9926 | Ughhhhhh
             // 9927 | Work Smarter, Not Harder
             foreach (Quest Q in Core.EnsureLoad(Core.FromTo(9923, 9927)))
             {
-                if (!Story.QuestProgression(Q.ID))
+                // 9924 | Discount Diploma
+                if (Q.ID == 9924)
                 {
-                    Core.HuntMonsterQuest(9923, new (string? mapName, string? monsterName, ClassType classType)[] {
-                    ("nexus", "Frogzards", ClassType.Farm),         // Frogzards Defeated (500): Join nexus, kill Frogzards
-                    ("arcangrove", "Gorillaphants", ClassType.Farm), // Gorillaphants Defeated (500): Join arcangrove, kill Gorillaphants
-                    ("etherwardes", "Dragons", ClassType.Farm),      // Dragons Defeated (500): Join etherwardes, kill dragons
+                    // 9933 Deathly Slow Start
+                    if (!Story.QuestProgression(9924))
+                    {
+                        if (!Core.isCompletedBefore(9933))
+                            Core.HuntMonsterQuest(9933, new (string? mapName, string? monsterName, ClassType classType)[] {
+                        ("dreadfight", "Dreadhaven General", ClassType.Solo), // Dreadhaven General's Soul Fragment (10): Join dreadfight, kill Dreadhaven General
+                        ("hakuwar", "Zakhvatchik", ClassType.Solo),           // Zakhvatchik's Soul Fragment (10): Join hakuwar, kill Zakhvatchik (last room)
+                        ("towerofdoom5", "Creel", ClassType.Solo),            // Creel's Soul Fragment (10): Join towerofdoom5, kill Creel (last room)
+                    }, log: true);
+
+                        GoldFarm(1500000);
+                        Core.BuyItem("alchemyacademy", 2115, "Gold Voucher 500k");
+                        Core.ChainComplete(9936);
+                    }
+                }
+                else if (!Story.QuestProgression(Q.ID))
+                {
+                    Core.HuntMonsterQuest(Q.ID, new (string? mapName, string? monsterName, ClassType classType)[] {
+                    ("nexus", "Frogzard", ClassType.Farm),         // Frogzards Defeated (500): Join nexus, kill Frogzards
+                    ("arcangrove", "Gorillaphant", ClassType.Farm), // Gorillaphants Defeated (500): Join arcangrove, kill Gorillaphants
+                    ("etherwardes", "Water Dragon", ClassType.Farm),      // Dragons Defeated (500): Join etherwardes, kill dragons
                 }, log: true);
                 }
             }
@@ -67,7 +87,7 @@ public class MartialArtist
             if (!Story.QuestProgression(9928))
             {
                 Story.KillQuest(9928, "hakuvillage", "The Master");
-                Bot.Wait.ForQuestComplete(9924);
+                Bot.Wait.ForQuestComplete(9928);
             }
 
             Core.BuyItem("hakuvillage", 2490, "Master Artist");
@@ -77,14 +97,22 @@ public class MartialArtist
         if (rankUpClass)
             Adv.RankUpClass("Martial Artist");
     }
+
+    void GoldFarm(int Gold = 100000000)
+    {
+        if (Bot.Player.Gold >= Gold)
+            return;
+
+        while (!Bot.ShouldExit && Bot.Player.Gold <= Gold)
+        {
+            Core.HuntMonsterQuest(9933, new (string? mapName, string? monsterName, ClassType classType)[] {
+                        ("dreadfight", "Dreadhaven General", ClassType.Solo), // Dreadhaven General's Soul Fragment (10): Join dreadfight, kill Dreadhaven General
+                        ("hakuwar", "Zakhvatchik", ClassType.Solo),           // Zakhvatchik's Soul Fragment (10): Join hakuwar, kill Zakhvatchik (last room)
+                        ("towerofdoom5", "Creel", ClassType.Solo),            // Creel's Soul Fragment (10): Join towerofdoom5, kill Creel (last room)
+                    }, log: true);
+            Bot.Wait.ForQuestComplete(9933);
+        }
+    }
 }
 
-// 9911 | Soul Hunter - farm quest?
-//     if (!Story.QuestProgression(9911))
-//     {
-//         Core.HuntMonsterQuest(Bot.Player.IsMember ? 9911 : 9902, new (string? mapName, string? monsterName, ClassType classType)[] {
-//     ("dreadfight", "Dreadhaven General", ClassType.Solo), // Dreadhaven General's Soul Fragment (10): Join dreadfight, kill Dreadhaven General
-//     ("hakuwar", "Zakhvatchik", ClassType.Solo),           // Zakhvatchik's Soul Fragment (10): Join hakuwar, kill Zakhvatchik (last room)
-//     ("towerofdoom5", "Creel", ClassType.Solo),            // Creel's Soul Fragment (10): Join towerofdoom5, kill Creel (last room)
-// }, log: true);
-//     }
+
