c394d60210884bfb76c288d391a6691e4a40a0c5
Tato <47118168+wtffidy@users.noreply.github.com>
2025-12-14 08:46:53 -0500

    Update CoreBots.cs

--- CoreBots.cs
+++ CoreBots.cs
@@ -6205,7 +6205,7 @@ public class CoreBots
     /// Checks, and prompts for the latest Skua Version
     /// <param name="targetVersion">Current Skua Version to Check against</param>
     /// </summary>
-    private void SkuaVersionChecker(string targetVersion = "1.3.0.2")
+    private void SkuaVersionChecker(string targetVersion = "1.3.1.0")
     {
         if (Bot.Version == null || Version.Parse(targetVersion).CompareTo(Bot.Version) <= 0)
             return;
